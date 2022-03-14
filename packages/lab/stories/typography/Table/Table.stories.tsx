8c64f23371a30228d1c57aee249b80602ec0e4f0
Alina Visan <alina.visan@jpmorgan.com>
2022-03-14 14:44:29 +0000

    Add container on table example

--- packages/lab/stories/typography/Table/Table.stories.tsx
+++ packages/lab/stories/typography/Table/Table.stories.tsx
@@ -1,92 +1,110 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { ComponentMeta, Story } from "@storybook/react";
 
 import { Text, LabelCaption, Div } from "@brandname/lab";
 import { transactions } from "./transactions";
 import "./Table.css";
+import { useEffect, useRef, useState } from "react";
 
 export default {
   title: "Lab/Typography",
   component: Text,
+  argTypes: {
+    width: {
+      description: "For this demo only. Set '0' for 100% width",
+      control: { type: "number" },
+    },
+    height: {
+      description: "For this demo only. Set '0' for 100% height",
+      control: { type: "number" },
+    },
+  },
 } as ComponentMeta<typeof Text>;
 
-const TextComponent: ComponentStory<typeof Text> = (props) => {
-  return (
-    <div className="table">
-      <div className="row">
-        <div className="table-cell">
-          <LabelCaption>
-            <strong>Date</strong>
-          </LabelCaption>
-        </div>
-        <div className="table-cell">
-          <LabelCaption>
-            <strong>Amount paid</strong>
-          </LabelCaption>
-        </div>
-        <div className="table-cell">
-          <LabelCaption>
-            <strong>Activity name</strong>
-          </LabelCaption>
-        </div>
-        <div className="table-cell">
-          <LabelCaption>
-            <strong>Status</strong>
-          </LabelCaption>
-        </div>
-        <div className="table-cell">
-          <LabelCaption>
-            <strong>Reference</strong>
-          </LabelCaption>
-        </div>
-        <div className="table-cell">
-          <LabelCaption>
-            <strong>Payment method</strong>
-          </LabelCaption>
-        </div>
-        <div className="table-cell">
-          <LabelCaption>
-            <strong>Payment type</strong>
-          </LabelCaption>
-        </div>
-      </div>
+const TextComponent: Story = (props) => {
+  const { width, height } = props;
 
-      {transactions.slice(0, 500).map((item) => {
-        const {
-          activity_name,
-          amount_paid,
-          date,
-          id,
-          reference,
-          status,
-          payment_type,
-          payment_method: { name },
-        } = item;
-        return (
-          <div className="row" key={id}>
-            <div className="table-cell">
-              <Div>{date}</Div>
-            </div>
-            <div className="table-cell">
-              <Div>{amount_paid}</Div>
-            </div>
-            <div className="table-cell">
-              <Div>{activity_name}</Div>
-            </div>
-            <div className="table-cell">
-              <Div>{status}</Div>
-            </div>
-            <div className="table-cell">
-              <Div>{reference}</Div>
-            </div>
-            <div className="table-cell">
-              <Div>{name}</Div>
-            </div>
-            <div className="table-cell">
-              <Div>{payment_type}</Div>
-            </div>
+  return (
+    <div
+      style={{ width: width || "100vw", height: height || "100vh" }}
+      className="container"
+    >
+      <div className="table">
+        <div className="row">
+          <div className="table-cell">
+            <LabelCaption>
+              <strong>Date</strong>
+            </LabelCaption>
           </div>
-        );
-      })}
+          <div className="table-cell">
+            <LabelCaption>
+              <strong>Amount paid</strong>
+            </LabelCaption>
+          </div>
+          <div className="table-cell">
+            <LabelCaption>
+              <strong>Activity name</strong>
+            </LabelCaption>
+          </div>
+          <div className="table-cell">
+            <LabelCaption>
+              <strong>Status</strong>
+            </LabelCaption>
+          </div>
+          <div className="table-cell">
+            <LabelCaption>
+              <strong>Reference</strong>
+            </LabelCaption>
+          </div>
+          <div className="table-cell">
+            <LabelCaption>
+              <strong>Payment method</strong>
+            </LabelCaption>
+          </div>
+          <div className="table-cell">
+            <LabelCaption>
+              <strong>Payment type</strong>
+            </LabelCaption>
+          </div>
+        </div>
+
+        {transactions.slice(0, 500).map((item) => {
+          const {
+            activity_name,
+            amount_paid,
+            date,
+            id,
+            reference,
+            status,
+            payment_type,
+            payment_method: { name },
+          } = item;
+          return (
+            <div className="row" key={id}>
+              <div className="table-cell">
+                <Div>{date}</Div>
+              </div>
+              <div className="table-cell">
+                <Div>{amount_paid}</Div>
+              </div>
+              <div className="table-cell">
+                <Div>{activity_name}</Div>
+              </div>
+              <div className="table-cell">
+                <Div>{status}</Div>
+              </div>
+              <div className="table-cell">
+                <Div>{reference}</Div>
+              </div>
+              <div className="table-cell">
+                <Div>{name}</Div>
+              </div>
+              <div className="table-cell">
+                <Div>{payment_type}</Div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
@@ -106,4 +124,8 @@ TableExample.parameters = {
       "onOverflow",
     ],
   },
+};
+TableExample.args = {
+  width: undefined,
+  height: undefined,
 };
