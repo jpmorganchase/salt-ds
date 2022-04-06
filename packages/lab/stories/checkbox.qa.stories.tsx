aad4c9879fbb1650963aff961129501528e9688e
o716774 <o716774@jpmcfid.jpmorgan.com>
2022-04-06 15:13:24 +0100

    checkbox css api; start on qa

---
+++ packages/lab/stories/checkbox.qa.stories.tsx
@@ -0,0 +1,50 @@
+import { Checkbox } from "@brandname/lab";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { AllRenderer, QAContainer } from "docs/components";
+
+export default {
+  title: "Lab/Checkbox/QA",
+  component: Checkbox,
+} as ComponentMeta<typeof Checkbox>;
+
+const Checkboxes = () => (
+  <div data-jpmui-test="checkboxes">
+    <Checkbox label="I understand ADA requires Labels on unchecked checkboxes" />
+    <Checkbox
+      defaultChecked
+      label="I understand ADA requires Labels on checked checkboxes"
+    />
+    <Checkbox
+      defaultChecked
+      indeterminate
+      label="I understand ADA requires Labels on indeterminate checkboxes"
+    />
+    <Checkbox
+      disabled
+      label="I understand ADA requires Labels on indeterminate checkboxes"
+    />
+  </div>
+);
+
+export const AllExamplesGrid: ComponentStory<typeof Checkbox> = () => {
+  return (
+    <AllRenderer>
+      <Checkboxes />
+    </AllRenderer>
+  );
+};
+
+export const CompareWithOriginalToolkit: ComponentStory<
+  typeof Checkbox
+> = () => {
+  return (
+    <QAContainer
+      width={1145}
+      height={512}
+      className="uitkCheckboxQA"
+      imgSrc="/visual-regression-screenshots/Checkbox-vr-snapshot.png"
+    >
+      <AllExamplesGrid />
+    </QAContainer>
+  );
+};
