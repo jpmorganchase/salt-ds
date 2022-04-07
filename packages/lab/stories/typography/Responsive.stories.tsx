9916b90c3418d97e92226b3374fb3666d0506c27
Alina Visan <alina.visan@jpmorgan.com>
2022-04-07 16:28:56 +0300

    music for the ears - less text in stories

--- packages/lab/stories/typography/Responsive.stories.tsx
+++ packages/lab/stories/typography/Responsive.stories.tsx
@@ -24,7 +24,8 @@ const ResponsiveTextComponent: ComponentStory<typeof Text> = () => {
     <div
       style={{
         display: "grid",
-        gridTemplateColumns: "auto 1fr",
+        gridTemplateColumns: "1fr 1fr",
+        width: "30vw",
       }}
     >
       <div style={box}>
@@ -38,17 +39,9 @@ const ResponsiveTextComponent: ComponentStory<typeof Text> = () => {
       </div>
       <div style={{ ...box }}>
         <Div>
-          The European <i>languages</i> are members of the same family. Their
-          separate existence is a myth. For science, music, sport, etc,{" "}
-          <a href="">Europe uses the same vocabulary</a>. The languages only
-          differ in their grammar, their pronunciation and their most common
-          words. Everyone realizes why a new common language would be desirable:
-          one could refuse to pay expensive translators. The European languages
-          are members of the same family. Their separate existence is a myth.
-          For science, music, sport, etc, Europe uses the same vocabulary. The
-          languages only differ in their grammar, their pronunciation and their
-          most common words. Everyone realizes why a new common language would
-          be desirable: one could refuse to pay expensive translators.
+          The king's son said he was to send her up to him, but the mother
+          answered, oh, no, she is much too dirty, she cannot show herself. But
+          he absolutely insisted on it, and Cinderella had to be called.
         </Div>
       </div>
       <div style={box}>
@@ -62,17 +55,10 @@ const ResponsiveTextComponent: ComponentStory<typeof Text> = () => {
       </div>
       <div style={{ ...box, height: 70 }}>
         <Div truncate={false}>
-          The European <i>languages</i> are members of the same family. Their
-          separate existence is a myth. For science, music, sport, etc,{" "}
-          <a href="">Europe uses the same vocabulary</a>. The languages only
-          differ in their grammar, their pronunciation and their most common
-          words. Everyone realizes why a new common language would be desirable:
-          one could refuse to pay expensive translators. The European languages
-          are members of the same family. Their separate existence is a myth.
-          For science, music, sport, etc, Europe uses the same vocabulary. The
-          languages only differ in their grammar, their pronunciation and their
-          most common words. Everyone realizes why a new common language would
-          be desirable: one could refuse to pay expensive translators.
+          She first washed her hands and face clean, and then went and bowed
+          down before the king's son, who gave her the golden shoe. Then she
+          seated herself on a stool, drew her foot out of the heavy wooden shoe,
+          and put it into the slipper, which fitted like a glove.
         </Div>
       </div>
       <div style={box}>
@@ -84,12 +70,9 @@ const ResponsiveTextComponent: ComponentStory<typeof Text> = () => {
       </div>
       <div style={{ ...box, height: 40 }}>
         <Div>
-          The European languages are members of the same family. Their separate
-          existence is a myth. For science, music, sport, etc, Europe uses the
-          same vocabulary. The languages only differ in their grammar, their
-          pronunciation and their most common words. Everyone realizes why a new
-          common language would be desirable: one could refuse to pay expensive
-          translators.
+          And when she rose up and the king's son looked at her face he
+          recognized the beautiful maiden who had danced with him and cried,
+          that is the true bride.
         </Div>
       </div>
       <div style={box}>
@@ -101,25 +84,15 @@ const ResponsiveTextComponent: ComponentStory<typeof Text> = () => {
       </div>
       <div style={{ ...box, display: "flex" }}>
         <P maxRows={2} expanded={expanded} style={{ marginRight: 20 }}>
-          Far far away, behind the word mountains, far from the countries
-          Vokalia and Consonantia, there live the blind texts. Separated they
-          live in Bookmarksgrove right at the coast of the Semantics, a large
-          language ocean. A small river named Duden flows by their place and
-          supplies it with the necessary regelialia. It is a paradisematic
-          country, in which roasted parts of sentences fly into your mouth. Even
-          the all-powerful Pointing has no control about the blind texts it is
-          an almost unorthographic life One day however a small line of blind
-          text by the name of Lorem Ipsum decided to leave for the far World of
-          Grammar. The Big Oxmox advised her not to do so, because there were
-          thousands of bad Commas, wild Question Marks and devious Semikoli, but
-          the Little Blind Text didn't listen. She packed her seven versalia,
-          put her initial into the belt and made herself on the way. When she
-          reached the first hills of the Italic Mountains, she had a last view
-          back on the skyline of her hometown Bookmarksgrove, the headline of
-          Alphabet Village and the subline of her own road, the Line Lane.
+          The step-mother and the two sisters were horrified and became pale
+          with rage, he, however, took Cinderella on his horse and rode away
+          with her.
         </P>
         <Button
           variant="secondary"
+          aria-expanded={expanded}
+          aria-controls="content1"
+          aria-label={expanded ? "Collapse text" : "Expand text"}
           onClick={() => {
             setExpand(!expanded);
           }}
@@ -136,16 +109,9 @@ const ResponsiveTextComponent: ComponentStory<typeof Text> = () => {
       </div>
       <div style={box}>
         <Span maxRows={2}>
-          A wonderful serenity has taken possession of my entire soul, like
-          these sweet mornings of spring which I enjoy with my whole heart. I am
-          alone, and feel the charm of existence in this spot, which was created
-          for the bliss of souls like mine. I am so happy, my dear friend, so
-          absorbed in the exquisite sense of mere tranquil existence, that I
-          neglect my talents. I should be incapable of drawing a single stroke
-          at the present moment; and yet I feel that I never was a greater
-          artist than now. When, while the lovely valley teems with vapour
-          around me, and the meridian sun strikes the upper surface of the
-          impenetrable foliage of my trees, and but a few stray gleams steal
+          When the wedding with the king's son was to be celebrated, the two
+          false sisters came and wanted to get into favor with cinderella and
+          share her good fortune.
         </Span>
       </div>
     </div>
