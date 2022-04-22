a3b0825f209ade0ad79e5cc3278ad506a2c55dcc
Alina Visan <alycry.alina@gmail.com>
2022-04-22 18:53:54 +0300

    Add cypress tests

--- packages/lab/src/__tests__/__e2e__/typography/Text.cy.tsx
+++ packages/lab/src/__tests__/__e2e__/typography/Text.cy.tsx
@@ -1,156 +1,206 @@
-import { Text, H1 } from "@brandname/lab";
+import {
+  Text,
+  Div,
+  P,
+  Code,
+  Figure1,
+  Figure2,
+  Figure3,
+  H1,
+  H2,
+  H3,
+  H4,
+  HelpText,
+  LabelCaption,
+  Span,
+} from "@brandname/lab";
 
-describe("GIVEN a Text", () => {
-  it('should be a "div" html tag', () => {
-    cy.mount(
-      <Text>
-        Far far away, behind the word mountains, far from the countries Vokalia
-        and Consonantia, there live the blind texts. Separated they live in
-        Bookmarksgrove right at the coast of the Semantics, a large language
-        ocean. A small river named Duden flows by their place and supplies it
-      </Text>
-    );
-    cy.get("div").should("have.class", "uitkText");
-  });
-  it("should be a 'p' html tag", () => {
-    cy.mount(
-      <Text elementType="p">
-        Far far away, behind the word mountains, far from the countries Vokalia
-        and Consonantia, there live the blind texts. Separated they live in
-        Bookmarksgrove right at the coast of the Semantics, a large language
-        ocean. A small river named Duden flows by their place and supplies it
-      </Text>
-    );
-    cy.get("p").should("have.class", "uitkText");
-  });
-  it("should display only 2 rows", () => {
-    cy.mount(
-      <Text maxRows={2}>
-        Far far away, behind the word mountains, far from the countries Vokalia
-        and Consonantia, there live the blind texts. Separated they live in
-        Bookmarksgrove right at the coast of the Semantics, a large language
-        ocean. A small river named Duden flows by their place and supplies it
-        with the necessary regelialia. It is a paradisematic country, in which
-        roasted parts of sentences fly into your mouth. Even the all-powerful
-        Pointing has no control about the blind texts it is an almost
-        unorthographic life One day however a small line of blind text by the
-        name of Lorem Ipsum decided to leave for the far World of Grammar. The
-        Big Oxmox advised her not to do so, because there were thousands of bad
-        Commas, wild Question Marks and devious Semikoli, but the Little Blind
-        Text didn't listen. She packed her seven versalia, put her initial into
-        the belt and made herself on the way. When she reached the first hills
-        of the Italic Mountains, she had a last view back on the skyline of her
-        hometown Bookmarksgrove, the headline of Alphabet Village and the
-        subline of her own road, the Line Lane.
-      </Text>
-    );
-    cy.get(".uitkText").should("have.class", "uitkText-lineClamp");
-  });
-  it("should truncate when text doesn't fit inside container", () => {
-    cy.mount(
-      <div style={{ height: 60 }}>
-        <Text>
-          Far far away, behind the word mountains, far from the countries
-          Vokalia and Consonantia, there live the blind texts. Separated they
-          live in Bookmarksgrove right at the coast of the Semantics, a large
-          language ocean. A small river named Duden flows by their place and
-          supplies it with the necessary regelialia. It is a paradisematic
-          country, in which roasted parts of sentences fly into your mouth. Even
-          the all-powerful Pointing has no control about the blind texts it is
-          an almost unorthographic life One day however a small line of blind
-          text by the name of Lorem Ipsum decided to leave for the far World of
-          Grammar. The Big Oxmox advised her not to do so, because there were
-          thousands of bad Commas, wild Question Marks and devious Semikoli, but
-          the Little Blind Text didn't listen. She packed her seven versalia,
-          put her initial into the belt and made herself on the way. When she
-          reached the first hills of the Italic Mountains, she had a last view
-          back on the skyline of her hometown Bookmarksgrove, the headline of
-          Alphabet Village and the subline of her own road, the Line Lane.
-        </Text>
-      </div>
-    );
-    cy.get(".uitkText").should("have.class", "uitkText-lineClamp");
+const textExample = `Far far away, behind the word mountains, far from the countries Vokalia
+and Consonantia, there live the blind texts. Separated they live in
+Bookmarksgrove right at the coast of the Semantics, a large language
+ocean. A small river named Duden flows by their place and supplies it
+with the necessary regelialia. It is a paradisematic country, in which
+roasted parts of sentences fly into your mouth. Even the all-powerful
+Pointing has no control about the blind texts it is an almost
+unorthographic life One day however a small line of blind text by the
+name of Lorem Ipsum decided to leave for the far World of Grammar. The
+Big Oxmox advised her not to do so, because there were thousands of bad
+Commas, wild Question Marks and devious Semikoli, but the Little Blind
+Text didn't listen. She packed her seven versalia, put her initial into
+the belt and made herself on the way. When she reached the first hills
+of the Italic Mountains, she had a last view back on the skyline of her
+hometown Bookmarksgrove, the headline of Alphabet Village and the
+subline of her own road, the Line Lane.`;
+
+const componentsArray = [
+  { component: Text, name: "Text", tag: "div" },
+  { component: Div, name: "Div", tag: "div" },
+  { component: P, name: "P", tag: "p" },
+  { component: Code, name: "Code", tag: "code" },
+  { component: Figure1, name: "Figure1", tag: "div" },
+  { component: Figure2, name: "Figure2", tag: "div" },
+  { component: Figure3, name: "Figure3", tag: "div" },
+  { component: H1, name: "H1", tag: "h1" },
+  { component: H2, name: "H2", tag: "h2" },
+  { component: H3, name: "H3", tag: "h3" },
+  { component: H4, name: "H4", tag: "h4" },
+  { component: HelpText, name: "HelpText", tag: "div" },
+  { component: LabelCaption, name: "LabelCaption", tag: "label" },
+  { component: Span, name: "Span", tag: "span" },
+];
+
+// Render correctly
+describe("GIVEN a Text Component with elementType", () => {
+  componentsArray.forEach(({ component, name, tag }) => {
+    it(`${name} component should return correct ${tag} element`, () => {
+      const Component = component;
+
+      cy.mount(<Component>{textExample}</Component>);
+      cy.get(tag).should("have.class", "uitkText");
+    });
   });
 });
 
-describe("GIVEN an expandable Text", () => {
-  it("should collapse", () => {
-    cy.mount(
-      <Text maxRows={2} expanded={false}>
-        Far far away, behind the word mountains, far from the countries Vokalia
-        and Consonantia, there live the blind texts. Separated they live in
-        Bookmarksgrove right at the coast of the Semantics, a large language
-        ocean. A small river named Duden flows by their place and supplies it
-        with the necessary regelialia. It is a paradisematic country, in which
-        roasted parts of sentences fly into your mouth. Even the all-powerful
-        Pointing has no control about the blind texts it is an almost
-        unorthographic life One day however a small line of blind text by the
-        name of Lorem Ipsum decided to leave for the far World of Grammar. The
-        Big Oxmox advised her not to do so, because there were thousands of bad
-        Commas, wild Question Marks and devious Semikoli, but the Little Blind
-        Text didn't listen. She packed her seven versalia, put her initial into
-        the belt and made herself on the way. When she reached the first hills
-        of the Italic Mountains, she had a last view back on the skyline of her
-        hometown Bookmarksgrove, the headline of Alphabet Village and the
-        subline of her own road, the Line Lane.
-      </Text>
-    );
-    cy.get(".uitkText").should("have.class", "uitkText-lineClamp");
-  });
-  it("should expand", () => {
-    cy.mount(
-      <Text maxRows={2} expanded={true}>
-        Far far away, behind the word mountains, far from the countries Vokalia
-        and Consonantia, there live the blind texts. Separated they live in
-        Bookmarksgrove right at the coast of the Semantics, a large language
-        ocean. A small river named Duden flows by their place and supplies it
-        with the necessary regelialia. It is a paradisematic country, in which
-        roasted parts of sentences fly into your mouth. Even the all-powerful
-        Pointing has no control about the blind texts it is an almost
-        unorthographic life One day however a small line of blind text by the
-        name of Lorem Ipsum decided to leave for the far World of Grammar. The
-        Big Oxmox advised her not to do so, because there were thousands of bad
-        Commas, wild Question Marks and devious Semikoli, but the Little Blind
-        Text didn't listen. She packed her seven versalia, put her initial into
-        the belt and made herself on the way. When she reached the first hills
-        of the Italic Mountains, she had a last view back on the skyline of her
-        hometown Bookmarksgrove, the headline of Alphabet Village and the
-        subline of her own road, the Line Lane.
-      </Text>
-    );
-    cy.get(".uitkText").should("not.have.class", "uitkText-lineClamp");
+// No Truncation
+describe("GIVEN a Text component with maxRows=2 and truncate=false", () => {
+  componentsArray.forEach(({ component, name }) => {
+    it(`${name} should not be truncated`, () => {
+      const Component = component;
+
+      cy.mount(
+        <Component maxRows={2} truncate={false}>
+          {textExample}
+        </Component>
+      );
+      const textComponent = cy.get(".uitkText");
+      textComponent
+        .should("not.have.class", "uitkText-lineClamp")
+        .should("not.have.css", "-webkit-line-clamp", "2");
+    });
   });
 });
 
-describe("GIVEN an H1", () => {
-  it("should be an h1 html tag width strong emphasis", () => {
-    cy.mount(
-      <H1>
-        Testing Heading 1 <strong>component</strong>
-      </H1>
-    );
-    cy.contains(/Testing Heading 1 component/);
-    cy.get("h1.uitkText").then(($heading) => {
-      expect($heading).to.have.css("font-size", "24px");
-      expect($heading).to.have.css("font-weight", "700");
-    });
-    cy.get("strong").then(($emphasis) => {
-      expect($emphasis).to.have.css("font-weight", "800");
+// Truncation + Tooltip
+describe("GIVEN a Text component with maxRows=2", () => {
+  componentsArray.forEach(({ component, name }) => {
+    it(`${name} should display only 2 rows and show Tooltip on focus and hover`, () => {
+      const Component = component;
+
+      cy.mount(<Component maxRows={2}>{textExample}</Component>);
+      const textComponent = cy.get(".uitkText");
+      textComponent
+        .should("have.class", "uitkText-lineClamp")
+        .should("have.css", "-webkit-line-clamp", "2");
+
+      textComponent.focus();
+      cy.get('[role="tooltip"]').should("be.visible");
+
+      textComponent.trigger("mouseenter");
+      cy.get('[role="tooltip"]').should("be.visible");
     });
   });
-  it("should be an h1 html tag syled as h2 with small emphasis", () => {
-    cy.mount(
-      <H1 styleAs="h2">
-        Testing Heading 1 <small>component</small>
-      </H1>
-    );
-    cy.contains(/Testing Heading 1 component/);
-    cy.get("h1.uitkText").then(($heading) => {
-      expect($heading).to.have.css("font-size", "18px");
-      expect($heading).to.have.css("font-weight", "600");
+});
+
+// Truncation + No Tooltip
+describe("GIVEN a Text component with maxRows=2 and showTooltip=false ", () => {
+  componentsArray.forEach(({ component, name }) => {
+    it(`${name} should display only 2 rows but should not show Tooltip on focus`, () => {
+      const Component = component;
+
+      cy.mount(
+        <Component maxRows={2} showTooltip={false}>
+          {textExample}
+        </Component>
+      );
+      const textComponent = cy.get(".uitkText");
+      textComponent
+        .should("have.class", "uitkText-lineClamp")
+        .should("have.css", "-webkit-line-clamp", "2");
+
+      textComponent.focus();
+      cy.get('[role="tooltip"]').should("not.exist");
     });
-    cy.get("small").then(($emphasis) => {
-      expect($emphasis).to.have.css("font-weight", "400");
+  });
+});
+
+// Expanded
+describe("GIVEN Text component with expanded=true and maxRows=2", () => {
+  componentsArray.forEach(({ component, name }) => {
+    it(`${name} should not be truncated`, () => {
+      const Component = component;
+
+      cy.mount(
+        <Component expanded={true} maxRows={2}>
+          {textExample}
+        </Component>
+      );
+      cy.get(".uitkText")
+        .should("not.have.class", "uitkText-lineClamp")
+        .should("not.have.css", "-webkit-line-clamp", "2");
+    });
+  });
+});
+
+// Collapsed with maxRows
+describe("GIVEN Text component with expanded=false and maxRows=2", () => {
+  componentsArray.forEach(({ component, name }) => {
+    it(`${name} should display only 2 rows`, () => {
+      const Component = component;
+
+      cy.mount(
+        <Component expanded={false} maxRows={2}>
+          {textExample}
+        </Component>
+      );
+      cy.get(".uitkText")
+        .should("have.class", "uitkText-lineClamp")
+        .should("have.css", "-webkit-line-clamp", "2");
+    });
+  });
+});
+
+// Collapsed without maxRows
+describe("GIVEN Text component with expanded=true", () => {
+  componentsArray.forEach(({ component, name }) => {
+    it(`${name} should not be truncated and display 1 row`, () => {
+      const Component = component;
+
+      cy.mount(<Component expanded={false}>{textExample}</Component>);
+      cy.get(".uitkText")
+        .should("have.class", "uitkText-lineClamp")
+        .should("have.css", "-webkit-line-clamp", "1");
+    });
+  });
+});
+
+// Size restricted by parent container
+describe("GIVEN Text component with parent height 80px", () => {
+  componentsArray.forEach(({ component, name }) => {
+    it(`${name} should be truncated`, () => {
+      const Component = component;
+
+      cy.mount(
+        <div style={{ width: 200, height: 100 }}>
+          <Component>{textExample}</Component>
+        </div>
+      );
+      cy.get(".uitkText").should("have.class", "uitkText-lineClamp");
+    });
+  });
+});
+
+// Scrollable
+describe("GIVEN Text component with parent height 80px and truncate=false", () => {
+  componentsArray.forEach(({ component, name }) => {
+    it(`${name} should be scrollable`, () => {
+      const Component = component;
+
+      cy.mount(
+        <div style={{ width: 200, height: 100 }}>
+          <Component truncate={false}>{textExample}</Component>
+        </div>
+      );
+      cy.get(".uitkText").should("have.class", "uitkText-overflow");
     });
   });
 });
