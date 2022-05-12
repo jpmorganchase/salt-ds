da29796dbe031fce79c99d4adc1174c8b8d04a9d
libertymayc <liberty.curtis@live.co.uk>
2022-05-12 17:38:06 +0100

    updates to stories

--- packages/lab/stories/card.qa.stories.tsx
+++ packages/lab/stories/card.qa.stories.tsx
@@ -8,24 +8,28 @@ export default {
   component: Card,
 } as ComponentMeta<typeof Card>;
 
+export const AllExamplesGrid: ComponentStory<typeof Card> = () => {
+  return (
+    <AllRenderer>
+      <Card className="uitkCardQA">
+        <div>
+          <h1 style={{ margin: 0 }}>Card with density</h1>
+          <span>Content</span>
+        </div>
+      </Card>
+    </AllRenderer>
+  );
+};
+
 export const CompareWithOriginalToolkit: ComponentStory<typeof Card> = () => {
   return (
     <QAContainer
       width={600}
-      height={632}
+      height={636}
       className="uitkCardQA"
       imgSrc="/visual-regression-screenshots/Card-vr-snapshot.png"
     >
-      <AllRenderer>
-        <Card>
-          <div>
-            <h1 style={{ margin: 0, lineHeight: "1.3em" }}>
-              Card with density
-            </h1>
-            <span>Content</span>
-          </div>
-        </Card>
-      </AllRenderer>
+      <AllExamplesGrid />
     </QAContainer>
   );
 };
