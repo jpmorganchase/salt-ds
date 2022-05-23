5538e1d9f4d6e00a54438d2a44b081c24a95e96f
Fernanda Castillo <fersitacastillo123@gmail.com>
2022-05-23 10:35:39 +0100

    Split Layout docs (#110)
    rearrange layout docs to their individual sections

--- packages/lab/stories/layout/grid-item.stories.tsx
+++ packages/lab/stories/layout/grid-item.stories.tsx
@@ -2,8 +2,14 @@ import { GridItem, GridLayout } from "@jpmorganchase/uitk-lab";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 export default {
-  title: "Layout/GridItem",
+  title: "Layout/GridLayout/GridItem",
   component: GridItem,
+  argTypes: {
+    colSpan: { control: { type: "number" } },
+    rowSpan: { control: { type: "number" } },
+    horizontalAlignment: { control: { type: "select" } },
+    verticalAlignment: { control: { type: "select" } },
+  },
 } as ComponentMeta<typeof GridItem>;
 
 const gridItemStyles = {
@@ -14,25 +20,13 @@ const gridItemStyles = {
   justifyContent: "center",
 };
 
-const Template: ComponentStory<typeof GridItem> = () => {
-  return (
-    <GridItem>
-      <div style={{ ...gridItemStyles, background: "lightcyan" }}>
-        <p>Item</p>
-      </div>
-    </GridItem>
-  );
-};
-export const ToolkitGridItem = Template.bind({});
-ToolkitGridItem.args = {};
-
 const gridLayoutStyle = { background: "lightblue" };
 
-const VerticalAlignmentInsideGrid: ComponentStory<typeof GridItem> = (args) => {
+const GridItemStory: ComponentStory<typeof GridItem> = (args) => {
   return (
     <div style={gridLayoutStyle}>
       <GridLayout rows={2} columns={5}>
-        <GridItem rowSpan={2} colSpan={1} {...args}>
+        <GridItem {...args}>
           <div
             style={{
               ...gridItemStyles,
@@ -61,55 +55,8 @@ const VerticalAlignmentInsideGrid: ComponentStory<typeof GridItem> = (args) => {
     </div>
   );
 };
-export const ToolkitGridItemVerticalAlignment =
-  VerticalAlignmentInsideGrid.bind({});
-ToolkitGridItemVerticalAlignment.args = {};
-
-ToolkitGridItemVerticalAlignment.argTypes = {
-  colSpan: { control: { type: "number" } },
-  rowSpan: { control: { type: "number" } },
-};
-
-const HorizontalAlignmentInsideGrid: ComponentStory<typeof GridItem> = (
-  args
-) => {
-  return (
-    <div style={gridLayoutStyle}>
-      <GridLayout rows={2} columns={5}>
-        <GridItem rowSpan={2} colSpan={1}>
-          <div
-            style={{
-              ...gridItemStyles,
-              background: "lightcoral",
-            }}
-          >
-            <p>Item</p>
-          </div>
-        </GridItem>
-        <GridItem colSpan={2} {...args}>
-          <div style={{ ...gridItemStyles, background: "lightcyan" }}>
-            <p>Item</p>
-          </div>
-        </GridItem>
-        <GridItem colSpan={2}>
-          <div style={{ ...gridItemStyles, background: "lightcoral" }}>
-            <p>Item</p>
-          </div>
-        </GridItem>
-        <GridItem colSpan={4}>
-          <div style={{ ...gridItemStyles, background: "lightcoral" }}>
-            <p>Item</p>
-          </div>
-        </GridItem>
-      </GridLayout>
-    </div>
-  );
-};
-export const ToolkitGridItemHorizontalAlignment =
-  HorizontalAlignmentInsideGrid.bind({});
-ToolkitGridItemHorizontalAlignment.args = {};
-
-ToolkitGridItemHorizontalAlignment.argTypes = {
-  colSpan: { control: { type: "number" } },
-  rowSpan: { control: { type: "number" } },
+export const GridItemWrapper = GridItemStory.bind({});
+GridItemWrapper.args = {
+  colSpan: 1,
+  rowSpan: 2,
 };
