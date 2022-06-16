b5e441b0e199bd95727aac62ff6cc84459f761b3
origami-z <davidcui2@me.com>
2022-06-16 18:51:59 +0100

    Move Icon and createIcon from core to icons package

--- packages/icons/src/components/ThumbsUpSolid.tsx
+++ packages/icons/src/components/ThumbsUpSolid.tsx
@@ -1,4 +1,4 @@
-import { createIcon } from "@jpmorganchase/uitk-core";
+import { createIcon } from "../icon/createIcon";
 
 export const ThumbsUpSolidIcon = createIcon(
   <svg viewBox="0 0 12 12" data-testid="ThumbsUpSolidIcon">
