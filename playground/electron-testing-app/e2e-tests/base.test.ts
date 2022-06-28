d79d6ed1f4edddb3cd0763fca2883f2587370cc1
Immanuel Baskaran <immanuel.baskaran.16@ucl.ac.uk>
2022-06-28 10:13:31 +0100

    Hotfix for flaky desktop cascadingMenu test

--- playground/electron-testing-app/e2e-tests/base.test.ts
+++ playground/electron-testing-app/e2e-tests/base.test.ts
@@ -79,7 +79,7 @@ test("Opens the cascading menu in a new child window", async () => {
 
   await cascadingMenuPage
     .locator("#UITK-cascading-menu-item-1 >> text=Level 1 Menu Item")
-    .click();
+    .click({ force: true });
   expect(electronApp.windows().length).toBe(1);
 });
 
