17b0aac4db7094456d01cd2930e4afb1a2a45e0d
Alina Visan <alycry.alina@gmail.com>
2022-07-01 11:32:05 +0100

    amends on ContactStatus component and QA stories

--- packages/lab/stories/content-status.qa.stories.tsx
+++ packages/lab/stories/content-status.qa.stories.tsx
@@ -1,6 +1,8 @@
+import { ComponentPropsWithoutRef, Fragment } from "react";
+import { DensityValues, ToolkitProvider } from "@jpmorganchase/uitk-core";
 import { ContentStatus } from "@jpmorganchase/uitk-lab";
-import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { AllRenderer, QAContainer } from "docs/components";
+import { ComponentMeta, ComponentStory, Story } from "@storybook/react";
+import { QAContainer, BackgroundBlock } from "docs/components";
 import "./content-status.qa.stories.css";
 
 export default {
@@ -8,53 +10,99 @@ export default {
   component: ContentStatus,
 } as ComponentMeta<typeof ContentStatus>;
 
-export const AllExamplesGrid: ComponentStory<typeof ContentStatus> = () => {
+const BgBlock = (props: ComponentPropsWithoutRef<"div">) => (
+  <BackgroundBlock style={{ width: 200 }} {...props} />
+);
+
+const ContentStatusExamples: ComponentStory<typeof ContentStatus> = (props) => {
   return (
-    <AllRenderer>
-      <div
-        style={{
-          background: "inherit",
-          display: "inline-grid",
-          gridTemplateColumns: "1fr 1fr",
-          gap: "4px",
-        }}
-      >
-        <ContentStatus
-          actionLabel="[CUSTOM ACTION]"
-          message="Supplementary content can go here if required."
-          status="warning"
-          title="No permission to access [content]"
-        />
-        <ContentStatus
-          message="Supplementary content can go here if required."
-          status="success"
-        />
-        <ContentStatus
-          message="It should be temporary, so please try again."
-          status="error"
-          title="There's been a system error"
-          actionLabel="RELOAD"
-        />
-        <ContentStatus
-          actionLabel="[CUSTOM ACTION]"
-          message="Supplementary content can go here if required."
-          title="No [content] available"
-        />
-        <ContentStatus
-          message="Supplementary content can go here if required."
-          status="loading"
-        />
-        <ContentStatus
-          message="Supplementary content can go here if required."
-          status="loading"
-          value={38}
-        />
-      </div>
-    </AllRenderer>
+    <>
+      <ContentStatus
+        className={props.className}
+        actionLabel="[CUSTOM ACTION]"
+        message="Supplementary content can go here if required."
+        status="warning"
+        title="No permission to access [content]"
+        onActionClick={() => {}}
+      />
+      <ContentStatus
+        className={props.className}
+        message="Supplementary content can go here if required."
+        status="success"
+      />
+      <ContentStatus
+        className={props.className}
+        message="It should be temporary, so please try again."
+        status="error"
+        title="There's been a system error"
+        actionLabel="RELOAD"
+        onActionClick={() => {}}
+      />
+      <ContentStatus
+        className={props.className}
+        actionLabel="[CUSTOM ACTION]"
+        message="Supplementary content can go here if required."
+        title="No [content] available"
+        onActionClick={() => {}}
+      />
+      <ContentStatus
+        className={props.className}
+        message="Supplementary content can go here if required."
+        status="loading"
+      />
+      <ContentStatus
+        className={props.className}
+        message="Supplementary content can go here if required."
+        status="loading"
+        value={38}
+      />
+    </>
+  );
+};
+
+export const AllExamplesGrid: Story = (props: { className?: string }) => {
+  return (
+    <div className="uitkContentStatusQA">
+      {DensityValues.map((d, i) => {
+        return (
+          <Fragment key={`${d}-${i}`}>
+            <ToolkitProvider
+              density={d}
+              theme="light"
+              key={`theme-light-${d}`}
+              applyClassesToChild
+            >
+              <BgBlock>
+                <ContentStatusExamples className={props.className} />
+              </BgBlock>
+            </ToolkitProvider>
+            <ToolkitProvider
+              applyClassesToChild
+              density={d}
+              theme="dark"
+              key={`theme-dark-${d}`}
+            >
+              <BgBlock>
+                <ContentStatusExamples className={props.className} />
+              </BgBlock>
+            </ToolkitProvider>
+          </Fragment>
+        );
+      })}
+    </div>
   );
 };
 
 AllExamplesGrid.parameters = {
+  chromatic: { disableSnapshot: false },
+};
+
+export const BackwardsCompatGrid = AllExamplesGrid.bind({});
+BackwardsCompatGrid.args = {
+  className: "backwardsCompat",
+};
+
+BackwardsCompatGrid.parameters = {
   chromatic: { disableSnapshot: false },
 };
 
@@ -63,12 +111,12 @@ export const CompareWithOriginalToolkit: ComponentStory<
 > = () => {
   return (
     <QAContainer
-      width={1460}
-      height={1123}
+      width={1600}
+      height={900}
       className="uitkContentStatusQA"
       imgSrc="/visual-regression-screenshots/ContentStatus-vr-snapshot.png"
     >
-      <AllExamplesGrid />
+      <BackwardsCompatGrid className="backwardsCompat" />
     </QAContainer>
   );
 };
