6480391e255a2ea3c43f5ca459a58d53c909919c
Alina Visan <alycry.alina@gmail.com>
2022-07-11 15:54:09 +0300

    style fixes on QAContainer

--- packages/lab/src/typography/getComputedStyles.ts
+++ packages/lab/src/typography/getComputedStyles.ts
@@ -8,10 +8,7 @@ export function getComputedStyles(el: HTMLElement) {
 
   const fontSize = parseFloat(rawFontSize.replace("px", ""));
   return {
-    lineHeight:
-      lineHeight === "normal"
-        ? fontSize
-        : parseFloat(lineHeight.replace("px", "")),
+    lineHeight: parseFloat(lineHeight.replace("px", "")),
     height: parseFloat(height.replace("px", "")),
     width: parseFloat(width.replace("px", "")),
   };
