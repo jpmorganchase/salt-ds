ef8380059853ae5cb64a772d8a693a8967943af4
Alina Visan <alycry.alina@gmail.com>
2022-07-12 12:19:04 +0300

    more style changes on QA stories

--- packages/lab/stories/content-status.qa.stories.tsx
+++ packages/lab/stories/content-status.qa.stories.tsx
@@ -1,6 +1,6 @@
 import { ContentStatus } from "@jpmorganchase/uitk-lab";
 import { ComponentMeta, ComponentStory, Story } from "@storybook/react";
-import { QAContainer, QAContainerProps } from "docs/components";
+import { AllRenderer, QAContainer, QAContainerProps } from "docs/components";
 
 import "./content-status.qa.stories.css";
 
@@ -11,71 +11,58 @@ export default {
 
 export const AllExamplesGrid: Story<QAContainerProps> = (props) => {
   return (
-    <QAContainer
-      width={1600}
-      height={900}
-      cols={6}
-      vertical
-      className="uitkContentStatusQA"
-      {...props}
-    >
-      <ContentStatus
-        className={props.className}
-        actionLabel="[CUSTOM ACTION]"
-        message="Supplementary content can go here if required."
-        status="warning"
-        title="No permission to access [content]"
-        onActionClick={() => {}}
-      />
-      <ContentStatus
-        className={props.className}
-        message="Supplementary content can go here if required."
-        status="success"
-      />
-      <ContentStatus
-        className={props.className}
-        message="It should be temporary, so please try again."
-        status="error"
-        title="There's been a system error"
-        actionLabel="RELOAD"
-        onActionClick={() => {}}
-      />
-      <ContentStatus
-        className={props.className}
-        actionLabel="[CUSTOM ACTION]"
-        message="Supplementary content can go here if required."
-        title="No [content] available"
-        onActionClick={() => {}}
-      />
-      <ContentStatus
-        className={props.className}
-        message="Supplementary content can go here if required."
-        status="loading"
-      />
-      <ContentStatus
-        className={props.className}
-        message="Supplementary content can go here if required."
-        status="loading"
-        value={38}
-      />
-    </QAContainer>
+    <AllRenderer className="uitkContentStatusQA">
+      <div
+        style={{
+          background: "inherit",
+          display: "flex",
+          flexDirection: "column",
+          gap: "2px",
+        }}
+      >
+        <ContentStatus
+          className={props.className}
+          actionLabel="[CUSTOM ACTION]"
+          message="Supplementary content can go here if required."
+          status="warning"
+          title="No permission to access [content]"
+          onActionClick={() => {}}
+        />
+        <ContentStatus
+          className={props.className}
+          message="Supplementary content can go here if required."
+          status="success"
+        />
+        <ContentStatus
+          className={props.className}
+          message="It should be temporary, so please try again."
+          status="error"
+          title="There's been a system error"
+          actionLabel="RELOAD"
+          onActionClick={() => {}}
+        />
+        <ContentStatus
+          className={props.className}
+          actionLabel="[CUSTOM ACTION]"
+          message="Supplementary content can go here if required."
+          title="No [content] available"
+          onActionClick={() => {}}
+        />
+        <ContentStatus
+          className={props.className}
+          message="Supplementary content can go here if required."
+          status="loading"
+        />
+        <ContentStatus
+          className={props.className}
+          message="Supplementary content can go here if required."
+          status="loading"
+          value={38}
+        />
+      </div>
+    </AllRenderer>
   );
 };
-
-// export const AllExamplesGrid: Story = (props: { className?: string }) => {
-//   const { className } = props;
-//   return (
-//     <QAContainer
-//       width={1600}
-//       height={900}
-//       cols={4}
-//       // itemWidthAuto
-//       className="uitkContentStatusQA"
-//     >
-//       <ContentStatusExamples className={className} />
-//     </QAContainer>
-//   );
-// };
 
 AllExamplesGrid.parameters = {
   chromatic: { disableSnapshot: false },
@@ -94,9 +81,13 @@ export const CompareWithOriginalToolkit: ComponentStory<
   typeof ContentStatus
 > = () => {
   return (
-    <AllExamplesGrid
+    <QAContainer
+      width={1600}
+      height={900}
+      className="uitkContentStatusQA"
       imgSrc="/visual-regression-screenshots/ContentStatus-vr-snapshot.png"
-      className="backwardsCompat"
-    />
+    >
+      <AllExamplesGrid />
+    </QAContainer>
   );
 };
