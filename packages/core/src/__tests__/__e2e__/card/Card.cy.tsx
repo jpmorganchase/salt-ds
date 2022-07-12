be3590ff0f15e5c7d6e501dbda26e81de0169cca
origami-z <davidcui2@me.com>
2022-07-12 18:19:48 +0100

    Migrate card jest test to cypress

---
+++ packages/core/src/__tests__/__e2e__/card/Card.cy.tsx
@@ -0,0 +1,16 @@
+import { composeStories } from "@storybook/testing-react";
+import * as cardStories from "@stories/card.stories";
+import { checkAccessibility } from "../../../../../../cypress/tests/checkAccessibility";
+
+const composedStories = composeStories(cardStories);
+const { DefaultCard } = composedStories;
+
+describe("Given a Card", () => {
+  checkAccessibility(composedStories);
+
+  it("should render children", () => {
+    cy.mount(<DefaultCard />);
+    cy.findByText("This is Card").should("be.visible");
+    cy.findByText("Using Nested DOM Elements").should("be.visible");
+  });
+});
