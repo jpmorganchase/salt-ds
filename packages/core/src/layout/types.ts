fb67b559bbf1b07c222c84797d2ad7b4632f9b44
Fernanda Castillo <fersitacastillo123@gmail.com>
2022-07-22 15:18:26 +0100

    Add Deck layout component (#140)

--- packages/core/src/layout/types.ts
+++ packages/core/src/layout/types.ts
@@ -1,8 +1,7 @@
-import { ComponentPropsWithoutRef } from "react";
-import { FlexLayout } from "./FlexLayout";
-
 export type LayoutDirection = "row" | "column";
 
 export type LayoutSeparator = "start" | "center" | "end";
 
-export type FlexLayoutProps = ComponentPropsWithoutRef<typeof FlexLayout>;
+export type LayoutAnimation = "slide" | "fade";
+export type LayoutAnimationDirection = "horizontal" | "vertical";
+export type LayoutAnimationTransition = "increase" | "decrease";
