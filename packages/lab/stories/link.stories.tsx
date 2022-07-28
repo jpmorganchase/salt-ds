d2705cec5778d68962ade37913385417819f8b36
Alina Visan <alycry.alina@gmail.com>
2022-07-28 17:04:06 +0300

    add link doc story

--- packages/lab/stories/link.stories.tsx
+++ packages/lab/stories/link.stories.tsx
@@ -1,7 +1,9 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { ToolkitProvider } from "@jpmorganchase/uitk-core";
 import { Link } from "@jpmorganchase/uitk-lab";
 
+import "./Link.stories.newapp-link.css";
 import "./link.stories.css";
-import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 export default {
   title: "Lab/Link",
@@ -57,3 +59,38 @@ export const WithTooltip: ComponentStory<typeof Link> = () => {
     </div>
   );
 };
+
+export const CustomStyling: ComponentStory<typeof Link> = () => (
+  <>
+    <ToolkitProvider theme={["light", "newapp"]}>
+      <Link href="https://www.google.com">Link to URL</Link>
+    </ToolkitProvider>
+    <ToolkitProvider theme={["light", "newapp"]}>
+      <Link href="https://www.google.com" disabled>
+        Link to URL
+      </Link>
+    </ToolkitProvider>
+    <ToolkitProvider theme={["light", "newapp"]}>
+      <div style={{ width: 50 }}>
+        <Link truncate={true} maxRows={1} href="https://www.google.com">
+          Link to URL with tooltip
+        </Link>
+      </div>
+    </ToolkitProvider>
+    <ToolkitProvider theme={["dark", "newapp"]}>
+      <Link href="https://www.google.com">Link to URL</Link>
+    </ToolkitProvider>
+    <ToolkitProvider theme={["dark", "newapp"]}>
+      <Link href="https://www.google.com" disabled>
+        Link to URL
+      </Link>
+    </ToolkitProvider>
+    <ToolkitProvider theme={["dark", "newapp"]}>
+      <div style={{ width: 50 }}>
+        <Link truncate={true} maxRows={1} href="https://www.google.com">
+          Link to URL with tooltip
+        </Link>
+      </div>
+    </ToolkitProvider>
+  </>
+);
