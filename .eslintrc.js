4e8a08af391c5b9a4b50f034a142e451d6515070
origami-z <davidcui2@me.com>
2022-08-08 14:51:25 +0100

    Disable explicit-module-boundary-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
               "(useIsomorphicLayoutEffect|useLayoutEffectOnce|useLayoutEffectSkipFirst)",
           },
         ],
+        "@typescript-eslint/explicit-module-boundary-types": 0,
       },
     },
     {
