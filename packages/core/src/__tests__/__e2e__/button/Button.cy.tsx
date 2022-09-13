8a21a6461241f21deb186129ca0ffa340e01e3f0
Immanuel Baskaran <immanuel.baskaran.16@ucl.ac.uk>
2022-09-13 10:59:03 +0100

    Added tests to improve coverage of Button

--- packages/core/src/__tests__/__e2e__/button/Button.cy.tsx
+++ packages/core/src/__tests__/__e2e__/button/Button.cy.tsx
@@ -13,6 +13,30 @@ describe("Given a Button", () => {
     cy.findByText(FeatureButton.args?.children as string).should("be.visible");
   });
 
+  it("should call onClick when interacted with", () => {
+    const clickSpy = cy.stub().as("clickSpy");
+    cy.mount(<FeatureButton onClick={clickSpy} />);
+    cy.realPress("Tab");
+    cy.findByRole("button").should("be.focused");
+    cy.realPress("Enter");
+    cy.get("@clickSpy").should("be.called");
+    cy.realPress("Space");
+    cy.get("@clickSpy").should("be.called");
+    cy.findByRole("button").realClick();
+    cy.get("@clickSpy").should("be.called");
+  });
+
+  it("should call onBlur when blurred", () => {
+    const blurSpy = cy.stub().as("blurSpy");
+    cy.mount(<FeatureButton onBlur={blurSpy} />);
+    cy.findByText(FeatureButton.args?.children as string).should("be.visible");
+    cy.realPress("Tab");
+    cy.findByRole("button").should("be.focused");
+    cy.realPress("Tab");
+    cy.findByRole("button").should("not.be.focused");
+    cy.get("@blurSpy").should("be.called");
+  });
+
   it("should be focusable when disabled and focusableWhenDisabled", () => {
     const clickSpy = cy.stub().as("clickSpy");
     cy.mount(<FocusableWhenDisabled onClick={clickSpy} />);
