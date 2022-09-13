c5d9e3b25a6bb658c1231d9b0df3f61aa9089541
joshwooding <12938082+joshwooding@users.noreply.github.com>
2022-09-14 00:12:53 +0100

    Refactor content status to use StatusIcon

--- packages/lab/src/content-status/ContentStatus.tsx
+++ packages/lab/src/content-status/ContentStatus.tsx
@@ -5,138 +5,117 @@ import {
   useId,
 } from "@jpmorganchase/uitk-core";
 import cx from "classnames";
-import {
-  ForwardedRef,
-  forwardRef,
-  HTMLAttributes,
-  MouseEvent,
-  Ref,
-  useEffect,
-} from "react";
-import { CircularProgressProps } from "../progress";
-import { SpinnerProps } from "../spinner";
+import { forwardRef, HTMLAttributes, MouseEvent, Ref, useEffect } from "react";
+
 import { Div } from "../text";
-import { renderStatusIndicator } from "./internal/renderStatusIndicator";
+import {
+  StatusIndicator,
+  StatusIndicatorProps,
+} from "./internal/StatusIndicator";
 
 import "./ContentStatus.css";
 
 const withBaseName = makePrefixer("uitkContentStatus");
 
-export type ContentStatusStatus =
-  | "loading"
-  | "error"
-  | "success"
-  | "warning"
-  | "info";
-
-export type Status = ContentStatusStatus;
-
-export interface ContentStatusProps extends HTMLAttributes<HTMLDivElement> {
-  CircularProgressProps?: Partial<CircularProgressProps>;
-  SpinnerProps?: Partial<SpinnerProps>;
+export interface ContentStatusProps
+  extends HTMLAttributes<HTMLDivElement>,
+    StatusIndicatorProps {
   actionLabel?: string;
   buttonRef?: Ref<any>;
-  disableAnnouncer?: boolean;
-  message?: string;
   onActionClick?: (evt: MouseEvent<HTMLButtonElement>) => void;
-  status?: ContentStatusStatus;
-  title?: string;
-  unit?: string;
-  value?: number;
 }
 
-export const ContentStatus = forwardRef(function ContentStatus(
-  {
-    CircularProgressProps,
-    SpinnerProps,
-    actionLabel,
-    buttonRef,
-    className,
-    children,
-    disableAnnouncer,
-    message,
-    onActionClick,
-    status = "info",
-    title,
-    unit = "%",
-    value,
-    ...rest
-  }: ContentStatusProps,
-  ref?: ForwardedRef<HTMLDivElement>
-) {
-  const id = useId();
+export const ContentStatus = forwardRef<HTMLDivElement, ContentStatusProps>(
+  function ContentStatus(
+    {
+      CircularProgressProps,
+      SpinnerProps,
+      actionLabel,
+      buttonRef,
+      className,
+      children,
+      disableAnnouncer,
+      message,
+      onActionClick,
+      status = "info",
+      title,
+      unit = "%",
+      value,
+      ...rest
+    },
+    ref
+  ) {
+    const id = useId();
 
-  const hasDefaultActionContent = actionLabel && onActionClick;
-  const hasActions = children || hasDefaultActionContent;
-  const hasContent = title || message || hasActions;
+    const hasDefaultActionContent = actionLabel && onActionClick;
+    const hasActions = children || hasDefaultActionContent;
+    const hasContent = title || message || hasActions;
 
-  const { announce } = useAriaAnnouncer();
+    const { announce } = useAriaAnnouncer();
 
-  useEffect(() => {
-    if (disableAnnouncer) return;
+    useEffect(() => {
+      if (disableAnnouncer) return;
 
-    const toBeAnnounced = [];
-    if (title) {
-      toBeAnnounced.push(title);
-    }
-    if (message) {
-      toBeAnnounced.push(message);
-    }
-    // Loading is announced by the spinner
-    if (status !== "loading") {
-      toBeAnnounced.push(status);
-    }
-    if (toBeAnnounced.length > 0) {
-      announce(toBeAnnounced.join(" "));
-    }
-  }, [announce, children, disableAnnouncer, message, status, title]);
+      const toBeAnnounced = [];
+      if (title) {
+        toBeAnnounced.push(title);
+      }
+      if (message) {
+        toBeAnnounced.push(message);
+      }
+      // Loading is announced by the spinner
+      if (status !== "loading") {
+        toBeAnnounced.push(status);
+      }
+      if (toBeAnnounced.length > 0) {
+        announce(toBeAnnounced.join(" "));
+      }
+    }, [announce, disableAnnouncer, message, status, title]);
 
-  return (
-    <div className={cx(withBaseName(), className)} ref={ref} {...rest}>
-      {renderStatusIndicator({
-        CircularProgressProps,
-        SpinnerProps,
-        actionLabel,
-        className,
-        children,
-        disableAnnouncer,
-        id,
-        message,
-        onActionClick,
-        status,
-        title,
-        unit,
-        value,
-        ...rest,
-      })}
-      {hasContent && (
-        <div
-          aria-labelledby={id}
-          className={cx(withBaseName("content"))}
+    return (
+      <div className={cx(withBaseName(), className)} ref={ref} {...rest}>
+        <StatusIndicator
+          CircularProgressProps={CircularProgressProps}
+          SpinnerProps={SpinnerProps}
+          disableAnnouncer={disableAnnouncer}
           id={id}
-          role="region"
-        >
-          {title && (
-            <Div truncate className={cx(withBaseName("title"))}>
-              {title}
-            </Div>
-          )}
-          {message && (
-            <Div truncate className={cx(withBaseName("message"))}>
-              {message}
-            </Div>
-          )}
-          {hasActions && (
-            <div className={cx(withBaseName("actions"))}>
-              {children || (
-                <Button onClick={onActionClick} ref={buttonRef}>
-                  {actionLabel}
-                </Button>
-              )}
-            </div>
-          )}
-        </div>
-      )}
-    </div>
-  );
-});
+          message={message}
+          status={status}
+          title={title}
+          unit={unit}
+          value={value}
+          {...rest}
+        />
+        {hasContent && (
+          <div
+            // `aria-labelledby` to itself so that children of this div will show up in screen reader, and we don't need to join to aria-label
+            aria-labelledby={id}
+            className={cx(withBaseName("content"))}
+            id={id}
+            role="region"
+          >
+            {title && (
+              <Div truncate className={cx(withBaseName("title"))}>
+                {title}
+              </Div>
+            )}
+            {message && (
+              <Div truncate className={cx(withBaseName("message"))}>
+                {message}
+              </Div>
+            )}
+            {hasActions && (
+              <div className={cx(withBaseName("actions"))}>
+                {children || (
+                  <Button onClick={onActionClick} ref={buttonRef}>
+                    {actionLabel}
+                  </Button>
+                )}
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    );
+  }
+);
