73cb7fc9cb4ca243d159823a07ce1e0c31cc87da
origami-z <davidcui2@me.com>
2022-09-25 10:52:23 +0100

    Fix QueryInput story

--- packages/lab/src/utils/useSlideSelection.ts
+++ packages/lab/src/utils/useSlideSelection.ts
@@ -1,4 +1,4 @@
-import { LayoutAnimationTransition } from "@jpmorganchase/uitk-core";
+import { LayoutAnimationTransition } from "@jpmorganchase/uitk-core/src/layout/types";
 import { useState } from "react";
 
 export const useSlideSelection = (
