e7bf0343f9205fd203e42f68947bfe19e8b1abfa
Immanuel Baskaran <immanuel.baskaran.16@ucl.ac.uk>
2022-10-04 11:42:32 +0100

    fixed merge conflict in index.ts

--- packages/lab/src/index.ts
+++ packages/lab/src/index.ts
@@ -10,10 +10,6 @@ export type {
   ListItemBase as ListItemBaseDeprecated,
   ListSelectHandler as ListSelectHandlerDeprecated,
 } from "./list-deprecated";
-export {
-  ListItemBase as ListItemBaseDeprecated,
-  useListItem as useListItemDeprecated,
-} from "./list-deprecated";
 
 export * from "./accordion";
 export * from "./app-header";
