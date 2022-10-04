40bc7d71c81182fe6b9f4bb16d2ec3cc40b9f483
Joana Moreira <joanamoreira989@gmail.com>
2022-10-04 16:08:01 +0100

    add comments to expected errors

--- packages/core/src/__tests__/@typings/GridLayout.tsx
+++ packages/core/src/__tests__/@typings/GridLayout.tsx
@@ -30,26 +30,31 @@ import { GridLayout } from "@jpmorganchase/uitk-core";
 /*
  * Should not allow unrelated html attributes
  */
+// Property 'href' does not exist on element type 'div'
 // @ts-expect-error
 <GridLayout as="div" href="www.example.com">
   Grid layout content
 </GridLayout>;
 
+// Property 'href' does not exist on element type 'main'
 // @ts-expect-error
 <GridLayout as="main" href="www.example.com">
   Grid layout content
 </GridLayout>;
 
+// Property 'checked' does not exist on element type 'img'
 // @ts-expect-error
 <GridLayout as="img" checked>
   Grid layout content
 </GridLayout>;
 
+// Property 'label' does not exist on element type 'div'
 // @ts-expect-error
 <GridLayout as="div" label="example label">
   Grid layout content
 </GridLayout>;
 
+// Property 'name' does not exist on element type 'nav'
 // @ts-expect-error
 <GridLayout as="nav" name="example name">
   Grid layout content
