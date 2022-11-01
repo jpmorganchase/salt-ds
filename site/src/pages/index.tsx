de275cd1178f7268a8a5ae967f4924efcf82a718
Joana Moreira <joanamoreira989@gmail.com>
2022-11-01 14:36:40 +0000

    replace homepage link with odyssey button

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
+import { Button } from "@jpmorganchase/uitk-core";
+
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 
@@ -8,18 +10,21 @@ import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
+
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
+          <Button>
+            {/* <Link
             className="button button--secondary button--lg"
             to="/docs/getting-started"
-          >
+          > */}
             Getting Started
-          </Link>
+            {/* </Link> */}
+          </Button>
           <Link
             className="button button--secondary button--lg"
             to="/docs/components"
