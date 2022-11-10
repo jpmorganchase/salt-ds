3dee5246a4413ecab4d6307b5cb56d1d299a0428
Rustam <rustam.arazov@gmail.com>
2022-11-10 11:26:14 +0000

    Odyssey ag-grid theme

--- packages/ag-grid-theme/stories/examples/AddRemoveRows.tsx
+++ packages/ag-grid-theme/stories/examples/AddRemoveRows.tsx
@@ -5,13 +5,22 @@ import dataGridExampleData from "../dependencies/dataGridExampleData";
 // ideally these css files would be loaded from a link tag
 // pointing to static asset directory for caching
 import { ColDef } from "ag-grid-community";
-import { Button } from "@jpmorganchase/uitk-core";
+import { Button, Switch } from "@jpmorganchase/uitk-core";
 import { AgGridReact, AgGridReactProps } from "ag-grid-react";
 import { useAgGridHelpers } from "../dependencies/useAgGridHelpers";
 import "../../uitk-ag-theme.css";
+import "../../odyssey-ag-theme.css";
 
 export const AddRemoveRows = (props: AgGridReactProps) => {
-  const { containerProps, agGridProps, api, columnApi } = useAgGridHelpers();
+  const [isNewTheme, setNewTheme] = useState(false);
+
+  const onThemeChange = () => {
+    setNewTheme(!isNewTheme);
+  };
+
+  const { containerProps, agGridProps, api, columnApi } = useAgGridHelpers(
+    isNewTheme ? "ag-theme-odyssey" : undefined
+  );
 
   const [columnDefs] = useState<ColDef[]>(dataGridExampleColumns);
 
@@ -68,6 +77,13 @@ export const AddRemoveRows = (props: AgGridReactProps) => {
 
   return (
     <div style={{ marginTop: 25 }}>
+      <div>
+        <Switch
+          checked={isNewTheme}
+          onChange={onThemeChange}
+          label="New theme"
+        />
+      </div>
       <div
         style={{ height: 400, width: 820, marginBottom: 25 }}
         {...containerProps}
--- packages/ag-grid-theme/stories/examples/Default.tsx
+++ packages/ag-grid-theme/stories/examples/Default.tsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../uitk-ag-theme.css";
 import dataGridExampleData from "../dependencies/dataGridExampleData";
 import dataGridExampleColumns from "../dependencies/dataGridExampleColumns";
 import { AgGridReact, AgGridReactProps } from "ag-grid-react";
 import { useAgGridHelpers } from "../dependencies/useAgGridHelpers";
+import { Switch } from "@jpmorganchase/uitk-core";
 
 const Default = (props: AgGridReactProps) => {
-  const { containerProps, agGridProps } = useAgGridHelpers();
+  const [isNewTheme, setNewTheme] = useState(false);
+
+  const onThemeChange = () => {
+    setNewTheme(!isNewTheme);
+  };
+
+  const { containerProps, agGridProps } = useAgGridHelpers(
+    isNewTheme ? "ag-theme-odyssey" : undefined
+  );
 
   return (
-    <div style={{ height: 500, width: 900 }} {...containerProps}>
-      <AgGridReact
-        columnDefs={dataGridExampleColumns}
-        rowData={dataGridExampleData}
-        rowSelection="single"
-        {...agGridProps}
-        {...props}
-      />
+    <div>
+      <div>
+        <Switch
+          checked={isNewTheme}
+          onChange={onThemeChange}
+          label="New theme"
+        />
+      </div>
+      <div style={{ height: 500, width: 900 }} {...containerProps}>
+        <AgGridReact
+          columnDefs={dataGridExampleColumns}
+          rowData={dataGridExampleData}
+          rowSelection="single"
+          {...agGridProps}
+          {...props}
+        />
+      </div>
     </div>
   );
 };
