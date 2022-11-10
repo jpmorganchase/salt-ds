74ea32086f50702c6a05515c76cf6137a0ab49bc
Fernanda Castillo <fernanda.g.castillogomez@jpmchase.com>
2022-11-10 11:16:36 +0000

    wrap paths in svg

--- packages/icons/stories/icon.stories.tsx
+++ packages/icons/stories/icon.stories.tsx
@@ -55,9 +55,11 @@ export const CustomSVGIcon: ComponentStory<typeof Icon> = () => {
   const CustomIcon = useMemo(
     () => (props: IconProps) => {
       return (
-        <Icon viewBox="0 0 18 18" {...props}>
-          <path d="M16,2V16H2V2Zm.5-1H1.5a.5.5,0,0,0-.5.5v15a.5.5,0,0,0,.5.5h15a.5.5,0,0,0,.5-.5V1.5A.5.5,0,0,0,16.5,1Z" />
-          <rect height="4" rx="0.25" width="12" x="3" y="11" />
+        <Icon {...props}>
+          <svg viewBox="0 0 18 18">
+            <path d="M16,2V16H2V2Zm.5-1H1.5a.5.5,0,0,0-.5.5v15a.5.5,0,0,0,.5.5h15a.5.5,0,0,0,.5-.5V1.5A.5.5,0,0,0,16.5,1Z" />
+            <rect height="4" rx="0.25" width="12" x="3" y="11" />
+          </svg>
         </Icon>
       );
     },
