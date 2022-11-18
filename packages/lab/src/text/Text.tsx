0db656d5ae326c380c18684b803acebfd5223010
Alina Visan <alycry.alina@gmail.com>
2022-11-18 22:34:22 +0200

    remove truncation from Text

--- packages/lab/src/text/Text.tsx
+++ packages/lab/src/text/Text.tsx
@@ -1,11 +1,4 @@
-import {
-  makePrefixer,
-  Tooltip,
-  TooltipProps,
-  useForkRef,
-  useTooltip,
-  polymorphicRef,
-} from "@jpmorganchase/uitk-core";
+import { makePrefixer, polymorphicRef } from "@jpmorganchase/uitk-core";
 import cx from "classnames";
 import {
   ComponentPropsWithoutRef,
@@ -14,7 +7,6 @@ import {
   forwardRef,
   ReactElement,
 } from "react";
-import { useTruncation } from "./useTruncation";
 
 import "./Text.css";
 
@@ -27,36 +19,9 @@ interface TextPropsBase<E extends ElementType> {
    */
   elementType?: ElementType;
   /**
-   * When set, this will enforce the text to be truncated.
-   */
-  maxRows?: number;
-  /**
-   * If 'true', component will apply the logic for truncation. If 'false' then text will be displayed at 100% height and will show scrollbar if the parent restricts it's height.
-   * Defaults to 'false'
-   */
-  truncate?: boolean;
-  /**
-   * If 'true' it will show the Tooltip only if the text is truncated.
-   * Defaults to 'true'
-   */
-  showTooltip?: boolean;
-  /**
-   * Customise Tooltip
-   */
-  tooltipProps?: Partial<TooltipProps>;
-  /**
-   * Customize the Tooltip text
-   */
-  tooltipText?: string;
-  /**
    * Customise styling.
    */
   style?: CSSProperties;
-  /**
-   * Callback function triggered when overflow state changes.
-   * @params [boolean] isOverflowed
-   */
-  onOverflowChange?: (isOverflowed: boolean) => unknown;
   /**
    * Match styling to a specified heading
    */
@@ -70,65 +35,6 @@ type PolymorphicText = <T extends ElementType>(
   p: TextProps<T> & { ref?: polymorphicRef<T> }
 ) => ReactElement<TextProps<T>>;
 
-const TruncatingText = forwardRef(function TruncatingText<
-  T extends ElementType
->(props: TextProps<T>, ref?: polymorphicRef<T>): ReactElement<TextProps<T>> {
-  const {
-    children,
-    className,
-    elementType = "div",
-    maxRows,
-    onOverflowChange,
-    style,
-    styleAs,
-    tooltipProps,
-    tooltipText,
-    truncate: _truncate,
-    showTooltip: _showTooltip,
-    tabIndex,
-    ...restProps
-  }: TextProps = props;
-  const { setContainerRef, hasTooltip, tooltipTextDefault, rows } =
-    useTruncation(props, ref);
-
-  const { getTooltipProps, getTriggerProps } = useTooltip({
-    enterDelay: 150,
-    placement: "top",
-    disabled: !hasTooltip,
-  });
-
-  const { ref: triggerRef, ...triggerProps } = getTriggerProps({
-    className: cx(withBaseName(), className, withBaseName("lineClamp"), {
-      [withBaseName(styleAs || "")]: styleAs,
-    }),
-    tabIndex: hasTooltip || elementType === "a" ? 0 : -1,
-    style: {
-      ...style,
-      // @ts-ignore
-      "--text-max-rows": rows,
-    },
-    ...restProps,
-  });
-
-  const handleRef = useForkRef(triggerRef, setContainerRef);
-
-  const Component: ElementType = elementType;
-
-  return (
-    <>
-      <Component {...triggerProps} ref={handleRef}>
-        {children}
-      </Component>
-      <Tooltip
-        {...getTooltipProps({
-          title: tooltipText || tooltipTextDefault,
-          ...tooltipProps,
-        })}
-      />
-    </>
-  );
-});
-
 export const Text = forwardRef(function Text<T extends ElementType>(
   props: TextProps<T>,
   ref?: polymorphicRef<T>
@@ -137,24 +43,13 @@ export const Text = forwardRef(function Text<T extends ElementType>(
     children,
     className,
     elementType = "div",
-    maxRows,
-    onOverflowChange,
-    showTooltip,
     style,
     styleAs,
-    tooltipProps,
-    tooltipText,
-    truncate = false,
-    tabIndex,
     ...restProps
   } = props;
 
   // Rendering
   const Component: ElementType = elementType;
-
-  if (truncate) {
-    return <TruncatingText {...props} />;
-  }
 
   return (
     <Component
--- packages/lab/stories/text/BaseText.stories.tsx
+++ packages/lab/stories/text/BaseText.stories.tsx
@@ -18,90 +18,38 @@ export default {
         type: "select",
       },
     },
-    maxRows: {
-      description: "Set 0 if you want to ignore",
-      control: { type: "number" },
-    },
-    showTooltip: { control: { type: "boolean" } },
     styleAs: {
       options: ["h1", "h2", "h3", "h4"],
       control: { type: "select" },
-    },
-    truncate: { control: { type: "boolean" } },
-    parentWidth: {
-      description: "For this demo only. Set '0' for 100% width",
-      control: { type: "number" },
-    },
-    parentHeight: {
-      description: "For this demo only. Set '0' for 100% height",
-      control: { type: "number" },
     },
   },
 } as ComponentMeta<typeof Text>;
 
 interface BaseComponentStoryProps extends TextProps<"div"> {
   children: string;
-  parentWidth?: string;
-  parentHeight?: string;
 }
 
 const BaseComponent: Story<BaseComponentStoryProps> = (args) => {
-  const {
-    children,
-    elementType,
-    truncate,
-    showTooltip,
-    maxRows,
-    parentWidth,
-    parentHeight,
-    styleAs,
-  } = args;
-
-  const [width, setWidth] = useState<string>();
-  const [height, setHeight] = useState<string>();
+  const { children, elementType, styleAs } = args;
 
   const mappedProps = {
     elementType,
-    maxRows,
-    truncate,
-    showTooltip,
     styleAs,
   };
 
-  useEffect(() => {
-    setWidth(parentWidth);
-  }, [parentWidth]);
-
-  useEffect(() => {
-    setHeight(parentHeight);
-  }, [parentHeight]);
-
   return (
-    <div
-      style={{
-        border: "1px solid #ccc",
-        padding: 20,
-        width: width || "100%",
-        height: height || "auto",
-      }}
-    >
+    <div>
       {/* We're using dangerouslySetInnerHTML here so we can edit the text within the storybook. Don't do this at home! :) */}
-      <Text {...mappedProps} dangerouslySetInnerHTML={{ __html: children }} />
+      <Text
+        elementType={elementType}
+        styleAs={styleAs}
+        dangerouslySetInnerHTML={{ __html: children }}
+      />
     </div>
   );
 };
 
 export const BaseTextComponent = BaseComponent.bind({});
-BaseTextComponent.parameters = {
-  controls: {
-    exclude: ["tooltipProps", "style", "onOverflowChange"],
-  },
-};
 BaseTextComponent.args = {
   children: `Heaven yielding moved appear, gathering place. <strong>Cattle fifth Sea without thing</strong> unto fifth third Forth isn't be moveth to him greater place fifth creeping had. Good dominion behold in earth also signs had brought after, fowl dominion have there. Us stars first morning whales fruit yielding whose winged thing. Were in. Upon. Cattle she'd whales they're. Was you'll very years behold fowl us meat fruit have earth great. Were green yielding it under. Fly first likeness night one make kind us spirit said let created, upon fruitful.`,
-  truncate: false,
-  showTooltip: true,
-  maxRows: undefined,
-  parentWidth: undefined,
-  parentHeight: undefined,
 };
