c1d5d12b9be40fa8a180501f0918553a71a701ca
Joana Moreira <joanamoreira989@gmail.com>
2022-11-22 16:15:46 +0000

    allow card component to receive icon and keyline colour as props

--- site/src/components/card/Card.tsx
+++ site/src/components/card/Card.tsx
@@ -1,45 +1,23 @@
-import { ComponentPropsWithoutRef, useMemo, useRef } from "react";
+import {
+  ComponentPropsWithoutRef,
+  useRef,
+  cloneElement,
+  CSSProperties,
+} from "react";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
-import PencilIcon from "@site/static/img/pencil.svg";
-import CodeIcon from "@site/static/img/code.svg";
-import ArrowsIcon from "@site/static/img/arrows.svg";
+
 import useOnScreen from "../../utils/useOnScreen";
 
 import styles from "./Card.module.css";
 
-type CardIconName = "pencil" | "code" | "arrows";
-
-type CardIconsType = {
-  name: CardIconName;
-  icon: JSX.Element;
-}[];
-
-const cardIcons: CardIconsType = [
-  { name: "pencil", icon: <PencilIcon className={styles.icon} /> },
-  { name: "code", icon: <CodeIcon className={styles.icon} /> },
-  { name: "arrows", icon: <ArrowsIcon className={styles.icon} /> },
-];
-
-const getKeylineColor = (icon: CardIconName) => {
-  switch (icon) {
-    case "pencil":
-      return "purple-50";
-    case "code":
-      return "teal-50";
-    case "arrows":
-      return "orange-30";
-    default:
-      return "orange-30";
-  }
-};
-
 export interface CardProps extends ComponentPropsWithoutRef<"div"> {
-  icon: CardIconName;
+  icon: JSX.Element;
   title: string;
   description: string;
   url: string;
   linkText: string;
+  keylineColor: CSSProperties["color"];
 }
 
 const Card = ({
@@ -48,12 +26,8 @@ const Card = ({
   description,
   url,
   linkText,
+  keylineColor,
 }: CardProps): JSX.Element => {
-  const cardIcon = useMemo(
-    () => cardIcons.find((cardIcon) => cardIcon.name === icon),
-    [icon]
-  );
-
   const ref = useRef<HTMLDivElement>();
 
   const onScreen: boolean = useOnScreen<HTMLDivElement>(ref, "-100px");
@@ -61,7 +35,9 @@ const Card = ({
   return (
     <>
       <div className={styles.card}>
-        <div className={styles.iconContainer}>{cardIcon.icon}</div>
+        <div className={styles.iconContainer}>
+          {cloneElement(icon, { ...icon.props, className: styles.icon })}
+        </div>
         <div className={styles.cardContent}>
           <h2>{title}</h2>
           <p>{description}</p>
@@ -70,7 +46,7 @@ const Card = ({
         <div
           className={clsx(styles.keyline, { [styles.animate]: onScreen })}
           style={{
-            backgroundColor: `var(--uitk-color-${getKeylineColor(icon)})`,
+            backgroundColor: keylineColor,
           }}
           ref={ref}
         />
