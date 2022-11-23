568740b065ce637c18e94d35ea0bc6469e243ff9
origami-z <davidcui2@me.com>
2022-11-23 11:20:14 +0000

    Exclude figma plugin from typecheck and lint

--- tooling/figma-style-generator/code.js
+++ tooling/figma-style-generator/code.js
@@ -15,88 +15,90 @@ figma.showUI(__html__, { width: 436, height: 495 });
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = (msg) => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === "create-style") {
-    let newStyleCounter = 0;
-    const jsonInput = msg.jsonInput;
-    const inputJsonObj = JSON.parse(jsonInput);
-    transformToLocalStyle(inputJsonObj);
-    figma.notify(`${newStyleCounter} local styles created 😎`);
-    // parsedJson: JSONObj
-    function transformToLocalStyle(parsedJson) {
-      function recurse(node, key) {
-        Object.keys(node).map((path) => {
-          if (path !== "value") {
-            if (path !== "type") {
-              recurse(node[path], (key ? key + "/" : "") + path);
-            }
-          } else {
-            // console.log(key, node[path]);
-            const name = key;
-            let value = node[path];
-            let description = "";
-            let referencePointer = "";
-            let iterationCount = 0;
-            while (value.startsWith("{")) {
-              // get the real value from inputJson
-              referencePointer = value.replace(/[\{\}]/g, "");
-              const keys = referencePointer.split(".");
-              if (iterationCount === 0) {
-                // description should be the reference name
-                // We want keep the description to the immediate resolved value
-                description = referencePointer;
-              }
-              value = getValueFromObj(inputJsonObj, keys);
-              iterationCount++;
-              // console.log(referencePointer, value);
-              if (iterationCount > ITERATION_THRESHOLD) {
-                figma.notify(`Can not find value with {${referencePointer}}`, {
-                  error: true,
+    // One way of distinguishing between different types of messages sent from
+    // your HTML page is to use an object with a "type" property like this.
+    if (msg.type === "create-style") {
+        let newStyleCounter = 0;
+        const jsonInput = msg.jsonInput;
+        const inputJsonObj = JSON.parse(jsonInput);
+        transformToLocalStyle(inputJsonObj);
+        figma.notify(`${newStyleCounter} local styles created 😎`);
+        // parsedJson: JSONObj
+        function transformToLocalStyle(parsedJson) {
+            function recurse(node, key) {
+                Object.keys(node).map((path) => {
+                    if (path !== "value") {
+                        if (path !== "type") {
+                            recurse(node[path], (key ? key + "/" : "") + path);
+                        }
+                    }
+                    else {
+                        // console.log(key, node[path]);
+                        const name = key;
+                        let value = node[path];
+                        let description = "";
+                        let referencePointer = "";
+                        let iterationCount = 0;
+                        while (value.startsWith("{")) {
+                            // get the real value from inputJson
+                            referencePointer = value.replace(/[\{\}]/g, "");
+                            const keys = referencePointer.split(".");
+                            if (iterationCount === 0) {
+                                // description should be the reference name
+                                // We want keep the description to the immediate resolved value
+                                description = referencePointer;
+                            }
+                            value = getValueFromObj(inputJsonObj, keys);
+                            iterationCount++;
+                            // console.log(referencePointer, value);
+                            if (iterationCount > ITERATION_THRESHOLD) {
+                                figma.notify(`Can not find value with {${referencePointer}}`, {
+                                    error: true,
+                                });
+                                break;
+                            }
+                        }
+                        if (iterationCount < ITERATION_THRESHOLD) {
+                            const newPaintStyle = figma.createPaintStyle();
+                            newPaintStyle.name = name;
+                            newPaintStyle.description = description;
+                            const transparent = value === "transparent";
+                            newPaintStyle.paints = [
+                                {
+                                    type: "SOLID",
+                                    color: transparent ? { r: 0, g: 0, b: 0 } : extractRgb(value),
+                                    opacity: transparent ? 0 : 1,
+                                },
+                            ];
+                            newStyleCounter++;
+                        }
+                    }
                 });
-                break;
-              }
             }
-            if (iterationCount < ITERATION_THRESHOLD) {
-              const newPaintStyle = figma.createPaintStyle();
-              newPaintStyle.name = name;
-              newPaintStyle.description = description;
-              const transparent = value === "transparent";
-              newPaintStyle.paints = [
-                {
-                  type: "SOLID",
-                  color: transparent ? { r: 0, g: 0, b: 0 } : extractRgb(value),
-                  opacity: transparent ? 0 : 1,
-                },
-              ];
-              newStyleCounter++;
-            }
-          }
-        });
-      }
-      recurse(parsedJson, "");
+            recurse(parsedJson, "");
+        }
     }
-  }
-  function getValueFromObj(obj, keys) {
-    if (keys.length) {
-      const firstKey = keys.shift();
-      return getValueFromObj(obj[firstKey], keys);
-    } else {
-      return obj.value;
+    function getValueFromObj(obj, keys) {
+        if (keys.length) {
+            const firstKey = keys.shift();
+            return getValueFromObj(obj[firstKey], keys);
+        }
+        else {
+            return obj.value;
+        }
     }
-  }
-  function extractRgb(colorString) {
-    const array = colorString
-      .replace(/[^\d,]/g, "")
-      .split(",")
-      .map((x) => Number.parseInt(x) / 255);
-    return {
-      r: array[0],
-      g: array[1],
-      b: array[2],
-    };
-  }
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+    function extractRgb(colorString) {
+        const array = colorString
+            .replace(/[^\d,]/g, "")
+            .split(",")
+            .map((x) => Number.parseInt(x) / 255);
+        return {
+            r: array[0],
+            g: array[1],
+            b: array[2],
+        };
+    }
+    // Make sure to close the plugin when you're done. Otherwise the plugin will
+    // keep running, which shows the cancel button at the bottom of the screen.
+    figma.closePlugin();
 };
