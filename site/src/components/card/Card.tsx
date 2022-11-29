3b7e8607a360cd327266c159be34f67a82d94f87
Joana Moreira <31695437+JoanaMMoreira@users.noreply.github.com>
2022-11-29 11:53:44 +0000

    Getting started overview page (#733)
    Co-authored-by: libertymayc <91206958+libertymayc@users.noreply.github.com>

--- site/src/components/card/Card.tsx
+++ site/src/components/card/Card.tsx
@@ -18,6 +18,7 @@ export interface CardProps extends ComponentPropsWithoutRef<"div"> {
   url: string;
   linkText: string;
   keylineColor: CSSProperties["color"];
+  theme?: "light" | "dark";
 }
 
 const Card = ({
@@ -27,31 +28,32 @@ const Card = ({
   url,
   linkText,
   keylineColor,
+  theme = "dark",
 }: CardProps): JSX.Element => {
   const ref = useRef<HTMLDivElement>();
 
   const onScreen: boolean = useOnScreen<HTMLDivElement>(ref, "-100px");
 
+  const useLightTheme = theme !== "dark";
+
   return (
-    <>
-      <div className={styles.card}>
-        <div className={styles.iconContainer}>
-          {cloneElement(icon, { ...icon.props, className: styles.icon })}
-        </div>
-        <div className={styles.cardContent}>
-          <h2>{title}</h2>
-          <p>{description}</p>
-          <Link to={url}>{linkText}</Link>
-        </div>
-        <div
-          className={clsx(styles.keyline, { [styles.animate]: onScreen })}
-          style={{
-            backgroundColor: keylineColor,
-          }}
-          ref={ref}
-        />
+    <div className={clsx(styles.card, { [styles.lightTheme]: useLightTheme })}>
+      <div className={styles.iconContainer}>
+        {cloneElement(icon, { ...icon.props, className: styles.icon })}
       </div>
-    </>
+      <div className={styles.cardContent}>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <Link to={url}>{linkText}</Link>
+      </div>
+      <div
+        className={clsx(styles.keyline, { [styles.animate]: onScreen })}
+        style={{
+          backgroundColor: keylineColor,
+        }}
+        ref={ref}
+      />
+    </div>
   );
 };
 
