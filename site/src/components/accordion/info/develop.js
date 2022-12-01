9d2c8d8b156ffa6e25dc7466c2a9c941b4464d33
libertymayc <liberty.curtis@live.co.uk>
2022-12-01 11:40:57 +0000

    Refactor

---
+++ site/src/components/accordion/info/develop.js
@@ -0,0 +1,86 @@
+export const fontFileTable = [
+  {
+    id: "font-file-table",
+    summary: "Web Font Files",
+    details: (
+      <>
+        <table>
+          <thead>
+            <tr>
+              <th>Font-family</th>
+              <th>Font-weight</th>
+              <th>Font-style</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Open Sans</td>
+              <td>300</td>
+              <td>Normal</td>
+            </tr>
+            <tr>
+              <td>Open Sans</td>
+              <td>300</td>
+              <td>Italic</td>
+            </tr>
+            <tr>
+              <td>Open Sans</td>
+              <td>400/normal</td>
+              <td>Normal</td>
+            </tr>
+            <tr>
+              <td>Open Sans</td>
+              <td>400/normal</td>
+              <td>Italic</td>
+            </tr>
+            <tr>
+              <td>Open Sans</td>
+              <td>500</td>
+              <td>Normal</td>
+            </tr>
+            <tr>
+              <td>Open Sans</td>
+              <td>500</td>
+              <td>Italic</td>
+            </tr>
+            <tr>
+              <td>Open Sans</td>
+              <td>600</td>
+              <td>Normal</td>
+            </tr>
+            <tr>
+              <td>Open Sans</td>
+              <td>600</td>
+              <td>Italic</td>
+            </tr>
+            <tr>
+              <td>Open Sans</td>
+              <td>700/bold</td>
+              <td>Normal</td>
+            </tr>
+            <tr>
+              <td>Open Sans</td>
+              <td>700/bold</td>
+              <td>Italic</td>
+            </tr>
+            <tr>
+              <td>Open Sans</td>
+              <td>800</td>
+              <td>Normal</td>
+            </tr>
+            <tr>
+              <td>Open Sans</td>
+              <td>800</td>
+              <td>Italic</td>
+            </tr>
+            <tr>
+              <td>PT Sans</td>
+              <td>400/normal</td>
+              <td>Normal</td>
+            </tr>
+          </tbody>
+        </table>
+      </>
+    ),
+  },
+];
