64f43e15f37fb763c5b77ad1a99fbc0ec3cb3a2a
Eleanor Haproff <eleanor@haproff.com>
2022-12-01 10:00:23 +0000

    Add content and assets

---
+++ site/docs/getting-started/DesignCards.tsx
@@ -0,0 +1,74 @@
+import Assets1 from "@site/static/img/design-getting-started/assets1.png";
+import Assets2 from "@site/static/img/design-getting-started/assets2.png";
+import Assets3 from "@site/static/img/design-getting-started/assets3.png";
+import Assets4 from "@site/static/img/design-getting-started/assets4.png";
+
+import styles from "./DesignCards.module.css";
+
+const cardInfo = [
+  {
+    img: Assets1,
+    content: (
+      <p>
+        You can access the components in the left-hand panel using the Assets
+        tab.
+      </p>
+    ),
+  },
+  {
+    img: Assets2,
+    content: (
+      <p>
+        The components are organized in alphabetical order. Select a mode, i.e.
+        Light mode, then the component and simply drag it to the Canvas or a
+        Frame in your project.
+      </p>
+    ),
+  },
+  {
+    img: Assets3,
+    content: (
+      <p>
+        Or, if you prefer you can use the search function to find the component
+        you need.
+      </p>
+    ),
+  },
+  {
+    img: Assets4,
+    content: (
+      <p>
+        You can change density and further manipulate the setup in the
+        contextual panel on the right-hand side.
+      </p>
+    ),
+  },
+];
+
+const DesignCards = () => {
+  return (
+    <>
+      <div className={styles.row}>
+        {cardInfo.slice(0, 2).map((cardInfo) => (
+          <Row {...cardInfo} />
+        ))}
+      </div>
+      <div className={styles.row}>
+        {cardInfo.slice(2, 4).map((cardInfo) => (
+          <Row {...cardInfo} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+const Row = ({ img, content }) => (
+  <div className={styles.card}>
+    <div className={styles.imgContainer}>
+      <img src={img} />
+    </div>
+    <div className={styles.textContainer}>{content}</div>
+  </div>
+);
+
+export default DesignCards;
