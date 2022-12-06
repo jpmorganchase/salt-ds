945933e79428135f25fdceee8436887a20a3163a
Fernanda Castillo <fersitacastillo123@gmail.com>
2022-12-06 11:22:33 +0000

    remove unnecessary div wrapping grid item story

--- packages/core/stories/layout/grid-item.stories.tsx
+++ packages/core/stories/layout/grid-item.stories.tsx
@@ -19,24 +19,22 @@ const renderGridContent = (
 );
 const GridItemStory: ComponentStory<typeof GridItem> = (args) => {
   return (
-    <div>
-      <GridLayout
-        rows={2}
-        columns={{ xs: 2, md: 5 }}
-        className="layout-container"
-      >
-        <GridItem className="layout-active-content" {...args}>
-          <p>Item</p>
-        </GridItem>
-        {renderGridContent}
-        {renderGridContent}
-        {renderGridContent}
-        {renderGridContent}
-        <GridItem colSpan={{ xs: 2, md: 4 }}>
-          <p>Item spanning 4 columns</p>
-        </GridItem>
-      </GridLayout>
-    </div>
+    <GridLayout
+      rows={2}
+      columns={{ xs: 2, md: 5 }}
+      className="layout-container"
+    >
+      <GridItem className="layout-active-content" {...args}>
+        <p>Item</p>
+      </GridItem>
+      {renderGridContent}
+      {renderGridContent}
+      {renderGridContent}
+      {renderGridContent}
+      <GridItem colSpan={{ xs: 2, md: 4 }}>
+        <p>Item spanning 4 columns</p>
+      </GridItem>
+    </GridLayout>
   );
 };
 export const GridItemWrapper = GridItemStory.bind({});
