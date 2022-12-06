b8686a1efadbef811936d8a976379a699ec2a72d
James Nash <16718916+james-nash@users.noreply.github.com>
2022-12-06 11:50:34 +0000

    styling tweaks

--- site/src/components/accordion/info/develop.tsx
+++ site/src/components/accordion/info/develop.tsx
@@ -17,68 +17,120 @@ export const fontFileTable: AccordionBaseProps["accordionInfo"] = [
           <tbody>
             <tr>
               <td>Open Sans</td>
-              <td>300</td>
-              <td>Normal</td>
+              <td>
+                <code>300</code>
+              </td>
+              <td>
+                <code>normal</code>
+              </td>
             </tr>
             <tr>
               <td>Open Sans</td>
-              <td>300</td>
-              <td>Italic</td>
+              <td>
+                <code>300</code>
+              </td>
+              <td>
+                <code>italic</code>
+              </td>
             </tr>
             <tr>
               <td>Open Sans</td>
-              <td>400/normal</td>
-              <td>Normal</td>
+              <td>
+                <code>400</code> / <code>normal</code>
+              </td>
+              <td>
+                <code>normal</code>
+              </td>
             </tr>
             <tr>
               <td>Open Sans</td>
-              <td>400/normal</td>
-              <td>Italic</td>
+              <td>
+                <code>400</code> / <code>normal</code>
+              </td>
+              <td>
+                <code>italic</code>
+              </td>
             </tr>
             <tr>
               <td>Open Sans</td>
-              <td>500</td>
-              <td>Normal</td>
+              <td>
+                <code>500</code>
+              </td>
+              <td>
+                <code>normal</code>
+              </td>
             </tr>
             <tr>
               <td>Open Sans</td>
-              <td>500</td>
-              <td>Italic</td>
+              <td>
+                <code>500</code>
+              </td>
+              <td>
+                <code>italic</code>
+              </td>
             </tr>
             <tr>
               <td>Open Sans</td>
-              <td>600</td>
-              <td>Normal</td>
+              <td>
+                <code>600</code>
+              </td>
+              <td>
+                <code>normal</code>
+              </td>
             </tr>
             <tr>
               <td>Open Sans</td>
-              <td>600</td>
-              <td>Italic</td>
+              <td>
+                <code>600</code>
+              </td>
+              <td>
+                <code>italic</code>
+              </td>
             </tr>
             <tr>
               <td>Open Sans</td>
-              <td>700/bold</td>
-              <td>Normal</td>
+              <td>
+                <code>700</code> / <code>bold</code>
+              </td>
+              <td>
+                <code>normal</code>
+              </td>
             </tr>
             <tr>
               <td>Open Sans</td>
-              <td>700/bold</td>
-              <td>Italic</td>
+              <td>
+                <code>700</code> / <code>bold</code>
+              </td>
+              <td>
+                <code>italic</code>
+              </td>
             </tr>
             <tr>
               <td>Open Sans</td>
-              <td>800</td>
-              <td>Normal</td>
+              <td>
+                <code>800</code>
+              </td>
+              <td>
+                <code>normal</code>
+              </td>
             </tr>
             <tr>
               <td>Open Sans</td>
-              <td>800</td>
-              <td>Italic</td>
+              <td>
+                <code>800</code>
+              </td>
+              <td>
+                <code>italic</code>
+              </td>
             </tr>
             <tr>
               <td>PT Sans</td>
-              <td>400/normal</td>
-              <td>Normal</td>
+              <td>
+                <code>400</code> / <code>normal</code>
+              </td>
+              <td>
+                <code>normal</code>
+              </td>
             </tr>
           </tbody>
         </table>
