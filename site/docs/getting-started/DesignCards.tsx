db3b801f286cfaf0634698422f046dbfdcda72d2
Eleanor Haproff <eleanor@haproff.com>
2022-12-13 16:31:12 +0000

    Revert image changes

--- site/docs/getting-started/DesignCards.tsx
+++ site/docs/getting-started/DesignCards.tsx
@@ -1,19 +1,18 @@
-import Assets1 from "./DesignCardImages/Asset1";
-import Assets2 from "./DesignCardImages/Asset2";
-import Assets3 from "./DesignCardImages/Asset3";
-import Assets4 from "./DesignCardImages/Asset4";
+import Assets1 from "@site/static/img/design-getting-started/assets1.png";
+import Assets2 from "@site/static/img/design-getting-started/assets2.png";
+import Assets3 from "@site/static/img/design-getting-started/assets3.png";
+import Assets4 from "@site/static/img/design-getting-started/assets4.png";
 import SelectIconImg from "@site/static/img/design-getting-started/SelectIconImg.png";
 
 import styles from "./DesignCards.module.css";
 
-type CardInfoType = {
-  img: JSX.Element;
-  content?: JSX.Element;
-};
+type CardInfoType = { img: string; altText: string; content?: JSX.Element };
 
 const cardInfo: CardInfoType[] = [
   {
-    img: <Assets1 />,
+    img: Assets1,
+    altText:
+      'Screenshot of Figma\'s assets panel displaying the "Salt: Components Light" library',
     content: (
       <p>
         Use the “Assets” tab In the left-hand panel to access the components,
@@ -22,7 +21,9 @@ const cardInfo: CardInfoType[] = [
     ),
   },
   {
-    img: <Assets2 />,
+    img: Assets2,
+    altText:
+      "Screenshot of a light mode Button compponent being dragged out of Figma's assets panel",
     content: (
       <p>
         Select a mode, choose the component you need and simply drag it to the
@@ -31,7 +32,9 @@ const cardInfo: CardInfoType[] = [
     ),
   },
   {
-    img: <Assets3 />,
+    img: Assets3,
+    altText:
+      "Screenshot of the word 'button' being searched for in the Figma assets panel and Salt components with matching names being displayed",
     content: (
       <p>
         Or, if you prefer, you can use the search function to find the component
@@ -40,7 +43,9 @@ const cardInfo: CardInfoType[] = [
     ),
   },
   {
-    img: <Assets4 />,
+    img: Assets4,
+    altText:
+      "Screenshot of the Figma's contextual panel for Salt's Button component",
     content: (
       <p>
         To change density and further manipulate the setup, use the contextual
@@ -54,13 +59,13 @@ const DesignCards = () => {
   return (
     <div className={styles.designCardsContainer}>
       <div className={styles.row}>
-        {cardInfo.slice(0, 2).map((cardInfo, index) => (
-          <Card {...cardInfo} key={index} />
+        {cardInfo.slice(0, 2).map((cardInfo) => (
+          <Card {...cardInfo} key={cardInfo.img} />
         ))}
       </div>
       <div className={styles.row}>
-        {cardInfo.slice(2, 4).map((cardInfo, index) => (
-          <Card {...cardInfo} key={index} />
+        {cardInfo.slice(2, 4).map((cardInfo) => (
+          <Card {...cardInfo} key={cardInfo.img} />
         ))}
       </div>
     </div>
@@ -68,20 +73,16 @@ const DesignCards = () => {
 };
 
 export const SoloImgCard = () => (
-  <div className={styles.card}>
-    <img
-      src={SelectIconImg}
-      alt="Screenshot of Figma's swap instance menu displaying a list of Salt icons"
-    />
-    <div className={styles.textContainer}>
-      <p>Swapping an icon in Figma</p>
-    </div>
-  </div>
+  <Card
+    img={SelectIconImg}
+    content={<p>Swapping an icon in Figma</p>}
+    altText="Screenshot of Figma's swap instance menu displaying a list of Salt icons"
+  />
 );
 
-const Card = ({ img, content }: CardInfoType) => (
+const Card = ({ img, altText, content }: CardInfoType) => (
   <div className={styles.card}>
-    {img}
+    <img src={img} alt={altText} />
     <div className={styles.textContainer}>{content}</div>
   </div>
 );
