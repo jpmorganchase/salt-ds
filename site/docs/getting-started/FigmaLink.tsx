33b1bb5e973f36574f86f6107d8bc0e9a9130718
Eleanor Haproff <eleanor@haproff.com>
2022-12-14 13:09:35 +0000

    :fire: FigmaLink file

--- site/docs/getting-started/FigmaLink.tsx
+++
@@ -1,6 +0,0 @@
-import Link from "@docusaurus/Link";
-import styles from "./FigmaLink.module.css";
-
-const FigmaLink = () => <Link className={styles.link}>View in Figma</Link>;
-
-export default FigmaLink;
