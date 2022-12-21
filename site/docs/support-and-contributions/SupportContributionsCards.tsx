2e10eb8751d7a4ce6a97992881a68f1b466967a7
Joana Moreira <joanamoreira989@gmail.com>
2022-12-21 12:37:46 +0000

    refactor supp and contributions cards

--- site/docs/support-and-contributions/SupportContributionsCards.tsx
+++ site/docs/support-and-contributions/SupportContributionsCards.tsx
@@ -1,70 +1,24 @@
-import {
-  DiamondIcon,
-  DocumentIcon,
-  FlagIcon,
-  MessageIcon,
-} from "@salt-ds/icons";
-
 import { GridLayout, GridItem } from "@salt-ds/core";
 import Card, { CardProps } from "@site/src/components/card/Card";
 import styles from "./SupportContributionsCards.module.css";
 
-const cards: CardProps[] = [
-  {
-    inlineIcon: <DocumentIcon size={1.7} />,
-    title: "Provide feedback",
-    description: (
-      <p>
-        Join the Salt community on GitHub to give us feedback and help improve
-        the design system.
-      </p>
-    ),
-    url: "https://github.com/jpmorganchase/salt-ds/discussions/categories/feedback",
-    footerText: "Share your thoughts on GitHub",
-    keylineColor: "var(--site-tertiary-accent-orange)",
-  },
-  {
-    inlineIcon: <FlagIcon size={1.7} />,
-    title: "Raise a bug",
-    description: (
-      <p>
-        Let us know if you spot any issues with our components and we'll add
-        them to our backlog.
-      </p>
-    ),
-    url: "https://github.com/jpmorganchase/salt-ds/issues/new?assignees=&labels=type%3A+bug+%F0%9F%AA%B2%2Cstatus%3A+awaiting+triage&template=bug_report.yml",
-    footerText: "Report a bug on GitHub",
-    keylineColor: "var(--site-tertiary-accent-purple)",
-  },
-  {
-    inlineIcon: <DiamondIcon size={1.7} />,
-    title: "Request a feature",
-    description: (
-      <p>
-        If your team needs a feature or functionality that’s not currently
-        provided, we may be able to assist.
-      </p>
-    ),
-    url: "https://github.com/jpmorganchase/salt-ds/issues/new?assignees=&labels=type%3A+enhancement+%F0%9F%92%A1%2Cstatus%3A+awaiting+triage&template=feature_request.yml",
-    footerText: "Ask on GitHub",
-    keylineColor: "var(--site-tertiary-accent-teal)",
-  },
-  {
-    inlineIcon: <MessageIcon size={1.7} />,
-    title: "Contact us directly",
-    description: (
-      <p>
-        Email us if you have a complex product or design-related query and we’ll
-        do our best to help.
-      </p>
-    ),
-    url: "mailto:salt.design.system@jpmorgan.com",
-    footerText: "Send us an email",
-    keylineColor: "var(--site-tertiary-accent-green)",
-  },
-];
+export const SupportContributionsCard = ({
+  keylineColor,
+  icon,
+  children,
+  ...rest
+}: CardProps) => (
+  <GridItem>
+    <Card
+      description={children}
+      keyLineAnimation={false}
+      keylineColor={`var(--site-tertiary-accent-${keylineColor})`}
+      {...rest}
+    />
+  </GridItem>
+);
 
-const SupportContributionsCards = () => (
+const SupportContributionsCards = ({ children }) => (
   <div className={styles.supportContributionsCards}>
     <GridLayout
       columns={{
@@ -74,13 +28,7 @@ const SupportContributionsCards = () => (
       gap={3}
       rows={2}
     >
-      {cards.map((card) => {
-        return (
-          <GridItem>
-            <Card {...card} key={card.title} keyLineAnimation={false} />
-          </GridItem>
-        );
-      })}
+      {children}
     </GridLayout>
   </div>
 );
