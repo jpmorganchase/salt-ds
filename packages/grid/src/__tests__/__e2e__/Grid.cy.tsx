4a44681cc7e946d2c0e590c8d2f18cce0086c0f8
Zarko Pernar <zarko.pernar@gmail.com>
2023-01-06 13:32:49 +0000

    increase scroll in grid virt test

--- packages/grid/src/__tests__/__e2e__/Grid.cy.tsx
+++ packages/grid/src/__tests__/__e2e__/Grid.cy.tsx
@@ -157,7 +157,7 @@ describe("Grid", () => {
     cy.mount(<LotsOfColumns />);
     assertGridReady();
     cy.findByTestId("grid-scrollable")
-      .scrollTo(0, 40, { easing: "linear", duration: 100 })
+      .scrollTo(0, 45, { easing: "linear", duration: 100 })
       .then(() => {
         return;
       });
