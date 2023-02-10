90bd53a7c3babf5f9ab1068ed1f68527cd25ff15
Zarko Pernar <zarko.pernar@gmail.com>
2023-02-10 14:11:22 +0000

    fix check icon

--- packages/grid/src/BaseCell.tsx
+++ packages/grid/src/BaseCell.tsx
@@ -42,7 +42,7 @@ const icons = {
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
       // height="1em"
-      viewBox="0 0 8 8"
+      viewBox="0 0 14 14"
       fill="none"
     >
       <path
