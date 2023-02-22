7185cb320891610f20c4322f68fdd884b775212c
libertymayc <liberty.curtis@live.co.uk>
2023-02-22 16:27:06 +0000

    Characteristics doc table styling

--- docs/components/ColorBlock.tsx
+++ docs/components/ColorBlock.tsx
@@ -1,23 +1,64 @@
 import { clsx } from "clsx";
+import { useEffect, useState } from "react";
+import {
+  characteristic,
+  getCharacteristicValue,
+  makePrefixer,
+} from "@salt-ds/core";
 
 import "./ColorBlock.css";
+
+const withBaseName = makePrefixer("ColorBlock");
 
 export const ColorBlock = ({
   colorVar,
   className,
+  hideToken,
 }: {
   colorVar: string;
   className?: string;
+  hideToken?: boolean;
 }) => {
+  const characteristicName = colorVar
+    .split("--salt-")[1]
+    .split("-")[0] as characteristic;
+  const [color, setColor] = useState<string | null>();
+  const [withBorder, setWithBorder] = useState<boolean>(false);
+  const [transparent, setTransparent] = useState<boolean>(false);
+
+  useEffect(() => {
+    setColor(
+      getCharacteristicValue(
+        "salt-theme",
+        characteristicName,
+        colorVar.split(`${characteristicName}-`)[1]
+      )
+    );
+  }, []);
+
+  useEffect(() => {
+    if (color?.replaceAll(" ", "").includes("255,255,255")) {
+      setWithBorder(true);
+    }
+    if (color?.includes("transparent")) {
+      setTransparent(true);
+    }
+  }, [color]);
+
   return (
     <>
-      <div className={clsx("ColorBlock-cell")}>
-        <div
-          className={clsx("ColorBlock-cellInner", className)}
-          style={{ background: `var(${colorVar})` }}
-        />
-      </div>
-      <code className="DocGrid-code">{colorVar}</code>
+      <div
+        style={!transparent ? { background: `var(${colorVar})` } : undefined}
+        className={clsx(
+          withBaseName(),
+          {
+            [withBaseName("withBorder")]: withBorder,
+            [withBaseName("transparent")]: transparent,
+          },
+          className
+        )}
+      />
+      {!hideToken && <code className="DocGrid-code">{colorVar}</code>}
     </>
   );
 };
--- tooling/css-variable-docgen-components/src/CharacteristicUsageRow.tsx
+++ tooling/css-variable-docgen-components/src/CharacteristicUsageRow.tsx
@@ -1,10 +1,57 @@
 import Markdown from "markdown-to-jsx";
+import { characteristic, getCharacteristicValue } from "@salt-ds/core";
+import { ColorBlock } from "docs/components/ColorBlock";
 import { Name, Description, StyledTd } from "./common";
+import { useEffect, useState } from "react";
+import { ShadowBlockCell } from "docs/components/ShadowBlock";
 
 interface CharacteristicUsageRowProps {
   name: string;
   tokens: string[];
 }
+
+const isColor = (value: string) => {
+  const s = new Option().style;
+  s.color = value;
+  return s.color.length;
+};
+
+const isShadow = (value: string) => {
+  const s = new Option().style;
+  s.boxShadow = value;
+  return s.boxShadow.length;
+};
+
+const TokenInfo = (props: { token: string }) => {
+  const { token } = props;
+  const characteristicName = token
+    .split("--salt-")[1]
+    .split("-")[0] as characteristic;
+  const [value, setValue] = useState<string | null>();
+
+  useEffect(() => {
+    setValue(
+      getCharacteristicValue(
+        "salt-theme",
+        characteristicName,
+        token.split(`${characteristicName}-`)[1]
+      )
+    );
+  }, []);
+
+  return (
+    <Description className="characteristicTokenDoc-inTable" key={token}>
+      {value &&
+        ((isColor(value) || value === "transparent") && !isShadow(value) ? (
+          <ColorBlock hideToken colorVar={token} />
+        ) : isShadow(value) ? (
+          <ShadowBlockCell shadowVar={token} />
+        ) : (
+          <Markdown className="token">{value.trim()}</Markdown>
+        ))}
+    </Description>
+  );
+};
 
 export const CharacteristicUsageRow = (props: CharacteristicUsageRowProps) => {
   const { name, tokens } = props;
@@ -14,15 +61,15 @@ export const CharacteristicUsageRow = (props: CharacteristicUsageRowProps) => {
       <StyledTd>
         <Name>{name}</Name>
       </StyledTd>
-
       <td>
         {tokens &&
           tokens.map((token) => (
             <Description key={token}>
-              <Markdown>{token}</Markdown>
+              <Markdown className="characteristicTokenDoc">{token}</Markdown>
             </Description>
           ))}
       </td>
+      <td>{tokens && tokens.map((token) => <TokenInfo token={token} />)}</td>
     </tr>
   );
 };
