3cd247e34dc572b017f117a0e6646e72799aedcf
libertymayc <91206958+libertymayc@users.noreply.github.com>
2023-02-23 16:58:55 +0000

    Characteristics table styling for component docs (#1188)
    Co-authored-by: Alina Visan <alycry.alina@gmail.com>

--- tooling/css-variable-docgen-components/src/CharacteristicUsageRow.tsx
+++ tooling/css-variable-docgen-components/src/CharacteristicUsageRow.tsx
@@ -1,10 +1,61 @@
 import Markdown from "markdown-to-jsx";
+import { characteristic, getCharacteristicValue } from "@salt-ds/core";
+import { ColorBlock } from "docs/components/ColorBlock";
 import { Name, Description, StyledTd } from "./common";
+import { useEffect, useState } from "react";
+import { ShadowBlockCell } from "docs/components/ShadowBlock";
 
 interface CharacteristicUsageRowProps {
   name: string;
   tokens: string[];
 }
+
+const isColor = (value: string) => {
+  const s = new Option().style;
+  s.color = value;
+  return s.color.length;
+};
+
+const isShadow = (value: string) => {
+  const s = new Option().style;
+  s.boxShadow = value;
+  return s.boxShadow.length;
+};
+
+const TokenBlock = (props: {
+  value: string;
+  token: string;
+}): React.ReactElement => {
+  const { value, token } = props;
+
+  if (isColor(value) || value === "transparent") {
+    return <ColorBlock hideToken colorVar={token} />;
+  }
+
+  if (isShadow(value)) {
+    return <ShadowBlockCell shadowVar={token} />;
+  }
+
+  return <Markdown className="token">{value.trim()}</Markdown>;
+};
+
+const TokenInfo = (props: { token: string }) => {
+  const { token } = props;
+  const characteristicName = token
+    .split("--salt-")[1]
+    .split("-")[0] as characteristic;
+  const value = getCharacteristicValue(
+    "salt-theme",
+    characteristicName,
+    token.split(`${characteristicName}-`)[1]
+  );
+
+  return (
+    <Description className="characteristicTokenDoc-inTable" key={token}>
+      {value && <TokenBlock value={value} token={token} />}
+    </Description>
+  );
+};
 
 export const CharacteristicUsageRow = (props: CharacteristicUsageRowProps) => {
   const { name, tokens } = props;
@@ -14,13 +65,18 @@ export const CharacteristicUsageRow = (props: CharacteristicUsageRowProps) => {
       <StyledTd>
         <Name>{name}</Name>
       </StyledTd>
-
       <td>
         {tokens &&
           tokens.map((token) => (
             <Description key={token}>
-              <Markdown>{token}</Markdown>
+              <Markdown className="characteristicTokenDoc">{token}</Markdown>
             </Description>
+          ))}
+      </td>
+      <td>
+        {tokens &&
+          tokens.map((token, i) => (
+            <TokenInfo token={token} key={`${token}-${i}`} />
           ))}
       </td>
     </tr>
