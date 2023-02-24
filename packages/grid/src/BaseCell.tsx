6273d9a1a989798a9ee217b39d4d2bc1e6496597
Zarko Pernar <zarko.pernar@gmail.com>
2023-02-24 10:56:48 -0500

    extract icons to separate file

--- packages/grid/src/BaseCell.tsx
+++ packages/grid/src/BaseCell.tsx
@@ -5,6 +5,11 @@ import { GridCellProps } from "./GridColumn";
 import { GridColumnModel } from "./Grid";
 import { Cell, Cursor, useFocusableContent } from "./internal";
 import { CornerTag } from "./CornerTag";
+import {
+  CellErrorIcon,
+  CellSuccessIcon,
+  CellWarningIcon,
+} from "./internal/CellStatusIcons";
 
 const withBaseName = makePrefixer("saltGridBaseCell");
 
@@ -13,44 +18,9 @@ export function getCellId<T>(rowKey: string, column: GridColumnModel<T>) {
 }
 
 const icons = {
-  warning: (
-    <svg
-      aria-hidden
-      height="1em"
-      viewBox="0 0 10 8"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M4.6188 0L9.2376 8H0L4.6188 0Z" />
-    </svg>
-  ),
-  error: (
-    <svg
-      aria-hidden
-      width="1em"
-      height="1em"
-      viewBox="0 0 8 8"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <circle cx="4" cy="4" r="4" />
-    </svg>
-  ),
-  success: (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      viewBox="0 0 14 14"
-      fill="none"
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M13.7668 2.63645L5.06034 12.0309L0.277532 7.56074L1.47248 6.28223L4.97136 9.55242L12.4832 1.4469L13.7668 2.63645Z"
-        fill="#24874B"
-      />
-    </svg>
-  ),
+  warning: CellWarningIcon,
+  error: CellErrorIcon,
+  success: CellSuccessIcon,
 };
 
 // Default component for grid cells. Provides selection, on-hover highlighting,
