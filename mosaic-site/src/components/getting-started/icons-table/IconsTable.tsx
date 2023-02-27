c40d7b622da1ac58de8bd2140463b63ee1713ef1
Joana Moreira <joanamoreira989@gmail.com>
2023-02-27 11:14:11 +0000

    move docs components into seperate files

--- mosaic-site/src/components/getting-started/icons-table/IconsTable.tsx
+++ mosaic-site/src/components/getting-started/icons-table/IconsTable.tsx
@@ -1,7 +1,4 @@
 import { ReactElement, cloneElement, useRef, useEffect } from "react";
-import { SaltProvider, Density } from "@salt-ds/core";
-import { PrintIcon } from "@salt-ds/icons";
-
 import styles from "./IconsTable.module.css";
 
 type IconsTableProps = { children: ReactElement; caption: string };
@@ -22,18 +19,3 @@ export const IconsTable = ({ children, caption }: IconsTableProps) => {
     </div>
   );
 };
-
-type IconProps = {
-  density: Density;
-  iconSize: number;
-  children: ReactElement;
-};
-
-export const DensityIcon = ({ density, iconSize, children }: IconProps) => (
-  <>
-    <SaltProvider density={density}>
-      <PrintIcon size={iconSize} />
-    </SaltProvider>
-    <p className={styles.colored}>{children}</p>
-  </>
-);
