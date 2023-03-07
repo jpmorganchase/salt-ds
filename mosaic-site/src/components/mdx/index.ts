fcde760eba0604e660986a4b209a1a5b0d3e709c
Joana Moreira <joanamoreira989@gmail.com>
2023-03-07 14:11:25 +0000

    add custom code block

--- mosaic-site/src/components/mdx/index.ts
+++ mosaic-site/src/components/mdx/index.ts
@@ -5,6 +5,7 @@ import { Heading2 } from "./h2";
 import { Heading3 } from "./h3";
 import { Heading4 } from "./h4";
 import { Paragraph as p } from "./p";
+import { Pre as pre } from "./pre";
 import { Table as table } from "./table";
 import { Thead as thead } from "./thead";
 import { Tr as tr } from "./tr";
@@ -14,4 +15,4 @@ const h2 = withAnchorHeading(Heading2);
 const h3 = withAnchorHeading(Heading3);
 const h4 = withAnchorHeading(Heading4);
 
-export { a, code, h2, h3, h4, p, table, thead, tr, ul }; // replace default MDX components
+export { a, code, h2, h3, h4, p, pre, table, thead, tr, ul }; // replace default MDX components
