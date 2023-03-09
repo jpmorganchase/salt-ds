641177ec7c773e1f5192dd85e734fe19c845cdf1
mark-tate <143323+mark-tate@users.noreply.github.com>
2023-03-09 15:00:33 +0000

    mosaic chores (#1236) (#1253)
    Co-authored-by: Joana Moreira <joanamoreira989@gmail.com>

---
+++ mosaic-site/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+};
+export default NextAuth(authOptions);
