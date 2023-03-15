5fd71cdfeb1a444fb0155f6078ac7c08e7601f40
Joana Moreira <31695437+JoanaMMoreira@users.noreply.github.com>
2023-03-15 15:50:14 +0000

    Migrate site components (#1254)

---
+++ mosaic-site/src/components/mdx/anchorHeading/AnchorHeading.tsx
@@ -0,0 +1,79 @@
+import React, {
+  useState,
+  ReactNode,
+  FC,
+  ElementType,
+  PropsWithChildren,
+  HTMLProps,
+} from "react";
+import clsx from "clsx";
+import { Link, LinkProps } from "@jpmorganchase/mosaic-components";
+import styles from "./AnchorHeading.module.css";
+
+interface TypographyProps {
+  children: ReactNode;
+  /** Additional class name for root class override */
+  className?: string;
+  /** Root element type */
+  component?: ElementType;
+  /** Component id */
+  id?: string;
+  /** role */
+  role?: string;
+}
+
+export interface AnchorHeadingProps extends HTMLProps<HTMLHeadingElement> {
+  children: ReactNode[];
+  Component: FC<PropsWithChildren<TypographyProps>>;
+  LinkProps?: LinkProps;
+}
+
+export const AnchorHeading: FC<PropsWithChildren<AnchorHeadingProps>> = ({
+  Component,
+  children,
+  className,
+  id,
+  LinkProps: LinkPropsProp = {},
+  ...rest
+}) => {
+  const [hovered, setHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
+  const handleMouseLeave = () => {
+    setHovered(false);
+  };
+
+  const { link } = LinkPropsProp;
+  const anchorLink = link || `#${id}`;
+  return (
+    <div className={clsx(styles.root, className)}>
+      <Link
+        className={styles.link}
+        {...LinkPropsProp}
+        endIcon="none"
+        link={anchorLink}
+        hovered={hovered}
+        variant="heading"
+        LinkBaseProps={{
+          onMouseEnter: handleMouseEnter,
+          onMouseLeave: handleMouseLeave,
+        }}
+      >
+        <Component id={id} {...rest}>
+          {children}
+          <span className={styles.badgeContainer}>{hovered ? "#" : null}</span>
+        </Component>
+      </Link>
+    </div>
+  );
+};
+
+type WithAnchorHeadingProps = JSX.IntrinsicAttributes &
+  AnchorHeadingProps & { children?: React.ReactNode };
+
+export const withAnchorHeading =
+  (Component: FC<PropsWithChildren<TypographyProps>>) =>
+  (props: WithAnchorHeadingProps) =>
+    <AnchorHeading {...props} Component={Component} />;
--- mosaic-site/src/components/mdx/h3/Heading3.tsx
+++ mosaic-site/src/components/mdx/h3/Heading3.tsx
@@ -1,6 +1,13 @@
-import { ReactNode } from "react";
+import { FC, ReactNode } from "react";
 import styles from "./Heading3.module.css";
 
-export const Heading3 = ({ children }: { children: ReactNode }) => (
-  <h3 className={styles.heading3}>{children}</h3>
+type Heading3Props = {
+  children: ReactNode;
+  id?: string;
+};
+
+export const Heading3: FC<Heading3Props> = ({ children, ...rest }) => (
+  <h3 className={styles.heading3} {...rest}>
+    {children}
+  </h3>
 );
