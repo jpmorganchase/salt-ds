b0e8d111f837574033e7ed38e3d198da3a84802b
David Miller <d.miller01@gmail.com>
2023-03-16 14:11:06 +0000

    added cypress tests for CountrySymbol

---
+++ packages/countries/src/__tests__/__e2e__/LazyCountrySymbol.cy.tsx
@@ -0,0 +1,9 @@
+import { composeStories } from "@storybook/testing-react";
+import * as countrySymbolStory from "../../../stories/LazyCountrySymbol.stories";
+import { checkAccessibility } from "../../../../../cypress/tests/checkAccessibility";
+
+const composedStories = composeStories(countrySymbolStory);
+
+describe("Given an LazyCountrySYmbol", () => {
+  checkAccessibility(composedStories);
+});
