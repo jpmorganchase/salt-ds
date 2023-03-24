2d37a77a9dd45520eb68f35f521ad017610bd51f
Joana Moreira <31695437+JoanaMMoreira@users.noreply.github.com>
2023-03-24 12:24:55 +0000

    Fix docs content height issue (#1334)
    Co-authored-by: Fernanda Castillo <fersitacastillo123@gmail.com>

--- mosaic-site/src/layouts/LayoutFullWidth/LayoutFullWidth.tsx
+++ mosaic-site/src/layouts/LayoutFullWidth/LayoutFullWidth.tsx
@@ -2,8 +2,12 @@ import React, { FC } from "react";
 import { LayoutProps } from "../types/index";
 import styles from "./LayoutFullWidth.module.css";
 
-export const LayoutFullWidth: FC<LayoutProps> = ({ Footer, children }) => (
-  <div className={styles.root}>
+export const LayoutFullWidth: FC<LayoutProps> = ({
+  Footer,
+  children,
+  ...rest
+}) => (
+  <div className={styles.root} {...rest}>
     <main>{children}</main>
     {Footer && Footer}
   </div>
