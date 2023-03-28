87be0b6fab901982bd5c88e96244a148c4b9aad4
David Miller <d.miller01@gmail.com>
2023-03-28 13:36:04 +0100

    add story for all country symbols with/without suspense

---
+++ packages/countries/stories/LazyCountrySymbol.qa.stories.tsx
@@ -0,0 +1,39 @@
+import { Meta, Story } from "@storybook/react";
+import { countryMetaMap, LazyCountrySymbol } from "@salt-ds/countries";
+import { Suspense } from "react";
+
+export default {
+  title: "Country Symbols/Lazy Country Symbol/QA",
+} as Meta;
+
+const sizes = [1, 2, 3] as const;
+
+export const AllLazyCountrySymbols: Story = () => {
+  return (
+    <Suspense>
+      {sizes.map((size) => (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(15, auto)",
+            gap: 8,
+            padding: "12px 0",
+          }}
+        >
+          {Object.values(countryMetaMap).map(({ countryCode }) => (
+            <LazyCountrySymbol
+              key={countryCode}
+              code={countryCode}
+              id={`${size}-${countryCode}`}
+              size={size}
+            />
+          ))}
+        </div>
+      ))}
+    </Suspense>
+  );
+};
+
+AllLazyCountrySymbols.parameters = {
+  chromatic: { disableSnapshot: false },
+};
