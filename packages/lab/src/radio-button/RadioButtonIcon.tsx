4f6fa6f5c75e7b3c13089305d8356170dd168943
Alina Visan <alycry.alina@gmail.com>
2023-03-28 11:22:36 +0300

    update tokens

--- packages/lab/src/radio-button/RadioButtonIcon.tsx
+++ packages/lab/src/radio-button/RadioButtonIcon.tsx
@@ -19,25 +19,28 @@ export const RadioButtonIcon = ({
   disabled,
 }: RadioButtonIconProps) => {
   return (
-    <svg
-      className={clsx(withBaseName(), {
-        [withBaseName("checked")]: checked,
-        [withBaseName("error")]: error,
-        [withBaseName("disabled")]: disabled,
-      })}
-      height="14"
-      viewBox="0 0 14 14"
-      width="14"
-    >
-      <circle className={withBaseName("border")} cx="7" cy="7" r="6.5" />
-      {checked && (
-        <circle
-          className={withBaseName("inner-checked")}
-          cx="7"
-          cy="7"
-          r="3.2"
-        />
-      )}
-    </svg>
+    <>
+      <svg
+        className={clsx(withBaseName(), {
+          [withBaseName("checked")]: checked,
+          [withBaseName("error")]: error,
+          [withBaseName("disabled")]: disabled,
+        })}
+        height="14"
+        viewBox="0 0 14 14"
+        width="14"
+        fill="none"
+      >
+        <circle className={withBaseName("border")} cx="7" cy="7" r="6.5" />
+        {checked && (
+          <circle
+            className={withBaseName("inner-checked")}
+            cx="7"
+            cy="7"
+            r="3.2"
+          />
+        )}
+      </svg>
+    </>
   );
 };
