b02cb1d44e2b87e7b4330436e2c014b306be103a
ImmanuelBaskaran <immanuel.baskaran.16@ucl.ac.uk>
2023-03-30 11:45:18 +0100

    ran prettier

--- packages/lab/src/checkbox/internal/useCheckboxGroup.ts
+++ packages/lab/src/checkbox/internal/useCheckboxGroup.ts
@@ -1,5 +1,8 @@
 import { useContext } from "react";
-import {CheckboxGroupContext, CheckboxGroupState} from "./CheckboxGroupContext";
+import {
+  CheckboxGroupContext,
+  CheckboxGroupState,
+} from "./CheckboxGroupContext";
 
 export function useCheckboxGroup(): CheckboxGroupState {
   return useContext(CheckboxGroupContext);
