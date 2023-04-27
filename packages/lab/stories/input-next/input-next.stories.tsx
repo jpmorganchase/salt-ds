97089a800cbbe502cca0aff8c1cfb8dc4c16ff7f
libertymayc <liberty.curtis@live.co.uk>
2023-04-27 10:25:54 +0100

    Use FlowLayout in stories

--- packages/lab/stories/input-next/input-next.stories.tsx
+++ packages/lab/stories/input-next/input-next.stories.tsx
@@ -25,62 +25,58 @@ export const Secondary: ComponentStory<typeof InputNext> = (args) => {
 
 export const Disabled: ComponentStory<typeof InputNext> = (args) => {
   return (
-    <>
+    <FlowLayout>
       <InputNext
         disabled={true}
         defaultValue={args.defaultValue ?? "Primary disabled"}
         variant="primary"
         {...args}
       />
-      <div style={{ height: 16 }} />
       <InputNext
         disabled={true}
         defaultValue={args.defaultValue ?? "Secondary disabled"}
         variant="secondary"
         {...args}
       />
-    </>
+    </FlowLayout>
   );
 };
 
 export const Readonly: ComponentStory<typeof InputNext> = (args) => {
   return (
-    <>
+    <FlowLayout>
       <InputNext
         readOnly={true}
         defaultValue={args.defaultValue ?? "Primary readonly"}
         variant="primary"
         {...args}
       />
-      <div style={{ height: 16 }} />
       <InputNext
         readOnly={true}
         defaultValue={args.defaultValue ?? "Secondary readonly"}
         variant="secondary"
         {...args}
       />
-    </>
+    </FlowLayout>
   );
 };
 
 export const TextAlignments: ComponentStory<typeof InputNext> = (args) => {
   return (
-    <>
-      <div className="demo alignCenter">
-        <InputNext defaultValue={args.defaultValue ?? "Value 1"} {...args} />
-        <InputNext defaultValue={args.defaultValue ?? "Value 2"} {...args} />
+    <FlowLayout>
+      <div className="alignCenter">
+        <InputNext defaultValue={args.defaultValue ?? "Value"} {...args} />
       </div>
-      <div className="demo alignRight">
-        <InputNext defaultValue={args.defaultValue ?? "Value 1"} {...args} />
-        <InputNext defaultValue={args.defaultValue ?? "Value 2"} {...args} />
+      <div className="alignRight">
+        <InputNext defaultValue={args.defaultValue ?? "Value"} {...args} />
       </div>
-    </>
+    </FlowLayout>
   );
 };
 
 export const ValidationStatus: ComponentStory<typeof InputNext> = (args) => {
   return (
-    <FlowLayout>
+    <FlowLayout style={{maxWidth: "266px"}}>
       <InputNext
         defaultValue={args.defaultValue ?? "Error value"}
         validationStatus="error"
