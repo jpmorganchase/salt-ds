ad72be0204d7712d41df1ee2300e04b54fa60bbf
Moreira, Joana M <joana.m.moreira@jpmchase.com>
2023-05-09 14:23:51 +0100

    add examples list

--- site/src/components/components/LivePreview.tsx
+++ site/src/components/components/LivePreview.tsx
@@ -5,20 +5,25 @@ import { Switch } from "@salt-ds/lab";
 import { SaltProvider } from "@salt-ds/core";
 import { Pre } from "../mdx/pre";
 import { useLivePreviewControls } from "./useLivePreviewControls";
+import useIsMobileView from "../../utils/useIsMobileView";
 import styles from "./LivePreview.module.css";
 
 type LivePreviewProps = {
   componentName: string;
   exampleName: string;
+  list?: ReactElement;
   children?: ReactNode;
 };
 
 export const LivePreview: FC<LivePreviewProps> = ({
   componentName,
   exampleName,
+  list,
   children,
 }) => {
   const [checked, setChecked] = useState(false);
+
+  const isMobileView = useIsMobileView();
 
   const ComponentExample: () => ReactElement =
     require(`../../examples/${componentName}`)[exampleName];
@@ -38,21 +43,31 @@ export const LivePreview: FC<LivePreviewProps> = ({
     <>
       {children}
       <div className={styles.container}>
-        <SaltProvider mode={mode}>
-          <div className={styles.componentPreview}>
-            <div className={styles.example}>
-              <SaltProvider density={density}>
-                {ComponentExample && <ComponentExample />}
+        <div
+          className={clsx(styles.componentPreview, {
+            [styles.smallViewport]: isMobileView,
+          })}
+        >
+          {list && list}
+          <SaltProvider mode={mode}>
+            <div className={styles.exampleWithSwitch}>
+              <div className={styles.example}>
+                <SaltProvider density={density}>
+                  {ComponentExample && <ComponentExample />}
+                </SaltProvider>
+              </div>
+              <SaltProvider density="medium">
+                <Switch
+                  checked={checked}
+                  onChange={handleChange}
+                  className={styles.switch}
+                  label="Show code"
+                />
               </SaltProvider>
             </div>
-            <Switch
-              checked={checked}
-              onChange={handleChange}
-              className={styles.switch}
-              label="Show code"
-            />
-          </div>
-        </SaltProvider>
+          </SaltProvider>
+        </div>
+
         {checked && (
           <Pre className={styles.codePreview}>
             <div className="language-tsx">
