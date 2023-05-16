6b7f9f17c23bf2306b75ba532fa1c989d290f50e
Alina Visan <alycry.alina@gmail.com>
2023-05-16 18:05:27 +0300

    Update BannerCloseButton.tsx

--- packages/lab/src/banner/BannerCloseButton.tsx
+++ packages/lab/src/banner/BannerCloseButton.tsx
@@ -1,6 +1,5 @@
 import { Button, ButtonProps, makePrefixer } from "@salt-ds/core";
 import { CloseIcon } from "@salt-ds/icons";
-import { clsx } from "clsx";
 
 export const BannerCloseButton = (props: ButtonProps) => (
   <Button {...props} variant="secondary">
