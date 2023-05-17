36c907e39d1ef29ce1881ced77e462a8133fc093
Alina Visan <alycry.alina@gmail.com>
2023-05-17 15:01:09 +0300

    revert some changes

--- packages/lab/stories/banner/banner.stories.tsx
+++ packages/lab/stories/banner/banner.stories.tsx
@@ -76,9 +76,12 @@ export const MultipleLines = (props: BannerProps) => {
         <Banner {...props}>
           <BannerContent>
             <div>
-              Our guidance for hyphen and dash usage differs from that of the
-              “AP Stylebook” and is aligned with the “J.P. Morgan Brand
-              Guidelines” (also known as the Masterbrand guide).
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur.
             </div>
             <Link href={"#"}>Read more...</Link>
           </BannerContent>
