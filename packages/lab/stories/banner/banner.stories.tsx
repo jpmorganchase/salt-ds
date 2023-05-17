b13384661899ab9f74ec70b6b0bba90d7a010980
Alina Visan <alycry.alina@gmail.com>
2023-05-17 17:50:52 +0300

    Update banner.stories.tsx

--- packages/lab/stories/banner/banner.stories.tsx
+++ packages/lab/stories/banner/banner.stories.tsx
@@ -76,14 +76,9 @@ export const MultipleLines = (props: BannerProps) => {
         <Banner {...props}>
           <BannerContent>
             <div>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur.
+              Banner example containing multiple lines. This is supposed to showcase the alignment of the status icon, content and close button within the Banner.
             </div>
-            <Link href={"#"}>Read more...</Link>
+            <Link href={"#"}>Link example...</Link>
           </BannerContent>
           <BannerCloseButton onClick={onClose} />
         </Banner>
