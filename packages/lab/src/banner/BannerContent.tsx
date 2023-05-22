4d0a8fec4fb74ff1939a9ae6f86fd7e02d430d34
Immanuel Baskaran <immanuel.baskaran.16@ucl.ac.uk>
2023-05-22 16:26:48 +0100

    refactor BannerContent.tsx to use useComponentCssInjection

--- packages/lab/src/banner/BannerContent.tsx
+++ packages/lab/src/banner/BannerContent.tsx
@@ -1,10 +1,19 @@
 import { ReactNode } from "react";
 import { makePrefixer } from "@salt-ds/core";
 
-import "./BannerContent.css";
+import bannerCss from "./BannerContent.css";
+import { useComponentCssInjection } from "@salt-ds/styles";
+import { useWindow } from "@salt-ds/window";
 
 const withBaseName = makePrefixer("saltBannerContent");
 
 export const BannerContent = ({ children }: { children: ReactNode }) => {
+  const { window: targetWindow } = useWindow();
+  useComponentCssInjection({
+    testId: "salt-banner",
+    css: bannerCss,
+    window: targetWindow,
+  });
+
   return <span className={withBaseName()}>{children}</span>;
 };
