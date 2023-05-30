125c4b64ce0ea7c1335315f06d842dcb3de5d536
Alina Visan <alycry.alina@gmail.com>
2023-05-30 16:28:00 +0300

    remove announcer

--- packages/lab/src/banner/Banner.tsx
+++ packages/lab/src/banner/Banner.tsx
@@ -1,14 +1,6 @@
-import { forwardRef, HTMLAttributes, useEffect, useState } from "react";
-import {
-  makePrefixer,
-  StatusIndicator,
-  useAriaAnnouncer,
-  useForkRef,
-  ValidationStatus,
-} from "@salt-ds/core";
+import { forwardRef, HTMLAttributes } from "react";
+import { makePrefixer, StatusIndicator, ValidationStatus } from "@salt-ds/core";
 import { clsx } from "clsx";
-
-import getInnerText from "./internal/getInnerText";
 
 import { useWindow } from "@salt-ds/window";
 import { useComponentCssInjection } from "@salt-ds/styles";
@@ -16,14 +8,6 @@ import { useComponentCssInjection } from "@salt-ds/styles";
 import bannerCss from "./Banner.css";
 
 export interface BannerProps extends HTMLAttributes<HTMLDivElement> {
-  /**
-   * Announcement message for screen reader 250ms after the banner is mounted.
-   */
-  announcement?: string;
-  /**
-   * If false, the built-in ARIA announcer will be applied. If true, the default browser behaviour will be applied instead.
-   */
-  disableAnnouncer?: boolean;
   /**
    * Emphasize the styling by applying a background color: defaults to false
    */
@@ -37,15 +21,7 @@ export interface BannerProps extends HTMLAttributes<HTMLDivElement> {
 const withBaseName = makePrefixer("saltBanner");
 
 export const Banner = forwardRef<HTMLDivElement, BannerProps>(function Banner(
-  {
-    announcement: announcementProp,
-    disableAnnouncer = true,
-    children,
-    className,
-    variant = "primary",
-    status = "info",
-    ...rest
-  },
+  { children, className, variant = "primary", status = "info", ...rest },
   ref
 ) {
   const targetWindow = useWindow();
@@ -55,21 +31,6 @@ export const Banner = forwardRef<HTMLDivElement, BannerProps>(function Banner(
     window: targetWindow,
   });
 
-  const { announce } = useAriaAnnouncer();
-
-  const [containerNode, setContainerNode] = useState<HTMLDivElement | null>(
-    null
-  );
-
-  const handleRef = useForkRef<HTMLDivElement>(setContainerNode, ref);
-
-  useEffect(() => {
-    if (!disableAnnouncer && containerNode) {
-      const announcement = announcementProp || getInnerText(containerNode);
-      announce(announcement, 250);
-    }
-  }, [announce, disableAnnouncer, containerNode, announcementProp]);
-
   return (
     <div
       className={clsx(
@@ -78,7 +39,7 @@ export const Banner = forwardRef<HTMLDivElement, BannerProps>(function Banner(
         withBaseName(variant),
         className
       )}
-      ref={handleRef}
+      ref={ref}
       {...rest}
       aria-live="polite"
     >
