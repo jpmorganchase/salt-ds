0f1e7469eb62e8457f92691b1577380a11f0543d
Alina Visan <alycry.alina@gmail.com>
2023-05-30 16:28:47 +0300

    Update Banner.cy.tsx

--- packages/lab/src/__tests__/__e2e__/banner/Banner.cy.tsx
+++ packages/lab/src/__tests__/__e2e__/banner/Banner.cy.tsx
@@ -20,7 +20,7 @@ describe("GIVEN a Banner", () => {
     cy.findByTestId("ErrorSolidIcon").should("exist");
   });
 
-  it("THEN should announce the contents of the Banner", () => {
+  xit("THEN should announce the contents of the Banner", () => {
     const message = "example announcement";
     cy.mount(
       <Banner>
