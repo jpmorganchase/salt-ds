cae305329716c52205aea55d8fa1096910d65d06
James Nash <16718916+james-nash@users.noreply.github.com>
2023-05-31 16:26:38 +0100

    Moved GridLayout to Salt site component exports

--- site/src/components/index.ts
+++ site/src/components/index.ts
@@ -1,3 +1,6 @@
+// importing this directly in MDX file didn't work, so adding here instead
+export { GridLayout } from "@salt-ds/core";
+
 export * from "./accordion";
 export * from "./app-header";
 export * from "./callout";
