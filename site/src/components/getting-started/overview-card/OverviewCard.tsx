65fcb94a6fa0c3c405a507052e94f5a62b15d0b5
James Nash <16718916+james-nash@users.noreply.github.com>
2023-05-31 13:05:29 +0100

    Replaced OverviewCards with GridLayout

---
+++ site/src/components/getting-started/overview-card/OverviewCard.tsx
@@ -0,0 +1,14 @@
+import { Card, CardProps } from "../../card/Card";
+
+export const OverviewCard = ({
+  keylineColor,
+  children,
+  ...rest
+}: CardProps) => (
+  <Card
+    keyLineAnimation={false}
+    keylineColor={`var(--site-tertiary-accent-${keylineColor})`}
+    {...rest}
+    description={children}
+  />
+);
