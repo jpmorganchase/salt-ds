04cee864876b5cc54f22f2cf98216d63afbb0b92
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2023-06-23 10:56:16 +0100

    Forms pattern

---
+++ site/src/layouts/DetailPattern/Resources.tsx
@@ -0,0 +1,51 @@
+import { Heading4 } from "../../components/mdx/h4";
+import { SiteState, useMeta, useStore } from "@jpmorganchase/mosaic-store";
+import styles from "./Resources.module.css";
+import { Image } from "@jpmorganchase/mosaic-site-components";
+import { Link, Text } from "@salt-ds/core";
+
+const LinkWithLogo = ({ href, label }: { href: string; label: string }) => (
+  <div className={styles.link}>
+    <Image src={`/img/figma_logo.svg`} alt={`figma logo`} />
+    <Link href={href} target="_blank">
+      {label}
+    </Link>
+  </div>
+);
+
+type Data = {
+  resources: Array<{ href: string; label: string; internal?: boolean }>;
+};
+
+type CustomSiteState = SiteState & { data?: Data };
+
+export function Resources() {
+  const {
+    meta: { title },
+  } = useMeta();
+
+  const resources =
+    useStore((state: CustomSiteState) => state.data?.resources) ?? [];
+
+  const internalResources = resources.filter(({ internal }) => internal);
+  const externalResources = resources.filter(({ internal }) => !internal);
+
+  return (
+    <div className={styles.root}>
+      <Heading4>{title} resources</Heading4>
+      <div className={styles.list}>
+        {externalResources.map(({ href, label }) => (
+          <LinkWithLogo key={href} href={href} label={label} />
+        ))}
+      </div>
+      <Text className={styles.subtitle} styleAs="label">
+        JPM employees only:
+      </Text>
+      <div className={styles.list}>
+        {internalResources.map(({ href, label }) => (
+          <LinkWithLogo key={href} href={href} label={label} />
+        ))}
+      </div>
+    </div>
+  );
+}
