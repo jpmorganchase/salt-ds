e46781a6e56f3fae49b911ab6ffc72f03c68cf90
Alina Visan <alycry.alina@gmail.com>
2023-06-23 12:03:22 +0300

    add stories

--- packages/lab/stories/toast/toast.stories.tsx
+++ packages/lab/stories/toast/toast.stories.tsx
@@ -2,17 +2,55 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { Toast } from "@salt-ds/lab";
 
 export default {
-  title: "Lab/Toast",
-  component: Toast,
+    title: "Lab/Toast",
+    component: Toast,
 } as ComponentMeta<typeof Toast>;
 
-const Template: ComponentStory<typeof Toast> = (args) => {
-  return (
-    <Toast {...args}>
-      <div>Info title</div>
-      <div>LEI Updated</div>
-    </Toast>
-  );
+const Template: ComponentStory<typeof Toast> = ({ children, ...args }) => {
+    return <Toast {...args}>{children}</Toast>;
 };
 
-export const Default = Template.bind({});
+export const DefaultInfo = Template.bind({});
+DefaultInfo.args = {
+    status: "info",
+    children: (
+        <>
+            <div>Info title</div>
+            <div>LEI Updated</div>
+        </>
+    ),
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+    status: "warning",
+    children: (
+        <>
+            <div>Warning title</div>
+            <div>Pre-Trade Transparency</div>
+
+        </>
+    ),
+};
+
+export const Error = Template.bind({});
+Error.args = {
+    status: "error",
+    children: (
+        <>
+            <div>Error title</div>
+            <div>CIT Check Failed System Error - Bypass Check</div>
+        </>
+    ),
+};
+
+export const Success = Template.bind({});
+Success.args = {
+    status: "success",
+    children: (
+        <>
+            <div>Success title</div>
+            <div>Clear Transact</div>
+        </>
+    ),
+};
