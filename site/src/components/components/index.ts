76722eaab84e726ca6d6769242e9a6ce2998871a
Moreira, Joana M <joana.m.moreira@jpmchase.com>
2023-06-27 14:38:27 +0100

    remove duplicate import

--- site/src/components/components/index.ts
+++ site/src/components/components/index.ts
@@ -2,5 +2,4 @@ export * from "./ComponentsList";
 export * from "./PropsTable";
 export * from "./LivePreview";
 export * from "./LivePreviewControls";
-export * from "./PropsTable";
 export * from "./useLivePreviewControls";
