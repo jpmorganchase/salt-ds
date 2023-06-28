d1556bca7c2dff9b24b26dd6cb2f20d463f8669d
Moreira, Joana M <joana.m.moreira@jpmchase.com>
2023-06-28 09:45:56 +0100

    remove props table styles

--- site/src/components/components/PropsTable.tsx
+++ site/src/components/components/PropsTable.tsx
@@ -2,8 +2,6 @@ import { FC, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import { Table } from "../mdx/table";
 
-import styles from "./PropsTable.module.css";
-
 type PropsTableType = {
   /**
    * Package name e.g. core
@@ -48,36 +46,34 @@ export const PropsTable: FC<PropsTableType> = ({
   }, []);
 
   return (
-    <div className={styles.tableWrapper}>
-      <Table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-            <th>Description</th>
-            <th>Default</th>
-          </tr>
-        </thead>
-        <tbody>
-          {propsTableData &&
-            Object.values(propsTableData).map(
-              ({ name, type, description, defaultValue }) => (
-                <tr key={name}>
-                  <td>{name}</td>
-                  <td>
-                    <code>{type.name}</code>
-                  </td>
-                  <td>
-                    <ReactMarkdown>{description}</ReactMarkdown>
-                  </td>
-                  <td>
-                    <code>{defaultValue ? defaultValue.value : "-"}</code>
-                  </td>
-                </tr>
-              )
-            )}
-        </tbody>
-      </Table>
-    </div>
+    <Table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type</th>
+          <th>Description</th>
+          <th>Default</th>
+        </tr>
+      </thead>
+      <tbody>
+        {propsTableData &&
+          Object.values(propsTableData).map(
+            ({ name, type, description, defaultValue }) => (
+              <tr key={name}>
+                <td>{name}</td>
+                <td>
+                  <code>{type.name}</code>
+                </td>
+                <td>
+                  <ReactMarkdown>{description}</ReactMarkdown>
+                </td>
+                <td>
+                  <code>{defaultValue ? defaultValue.value : "-"}</code>
+                </td>
+              </tr>
+            )
+          )}
+      </tbody>
+    </Table>
   );
 };
