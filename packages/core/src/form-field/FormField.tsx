f50c3ae9d4417a6ee2c7dda1a2d001ca7f1314a4
libertymayc <liberty.curtis@live.co.uk>
2023-06-28 11:23:41 +0100

    minor fix

--- packages/core/src/form-field/FormField.tsx
+++ packages/core/src/form-field/FormField.tsx
@@ -56,7 +56,6 @@ export const FormField = forwardRef(
       onFocus,
       readOnly = false,
       validationStatus,
-      style,
       ...restProps
     }: FormFieldProps,
     ref: ForwardedRef<HTMLDivElement>
