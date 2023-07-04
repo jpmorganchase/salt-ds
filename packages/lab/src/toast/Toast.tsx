71a179bda32ac8172585d2177a58d841172f7a5e
Alina Visan <alycry.alina@gmail.com>
2023-07-04 13:56:04 +0300

    prettier

--- packages/lab/src/toast/Toast.tsx
+++ packages/lab/src/toast/Toast.tsx
@@ -1,10 +1,10 @@
 import { clsx } from "clsx";
 import { ForwardedRef, forwardRef, HTMLAttributes, ReactNode } from "react";
 import {
-    Button,
-    makePrefixer,
-    StatusIndicator,
-    ValidationStatus,
+  Button,
+  makePrefixer,
+  StatusIndicator,
+  ValidationStatus,
 } from "@salt-ds/core";
 import { CloseIcon } from "@salt-ds/icons";
 import { useWindow } from "@salt-ds/window";
@@ -15,48 +15,54 @@ import toastCss from "./Toast.css";
 const withBaseName = makePrefixer("saltToast");
 
 export interface ToastProps extends HTMLAttributes<HTMLDivElement> {
-    children?: ReactNode;
-    /**
-     * If `true` the close button will not be displayed.
-     */
-    hideClose?: boolean
-    /**
-     *  A string to determine the current state of the Toast.
-     */
-    status?: ValidationStatus;
-    onClose?: () => void;
+  children?: ReactNode;
+  /**
+   * If `true` the close button will not be displayed.
+   */
+  hideClose?: boolean;
+  /**
+   *  A string to determine the current state of the Toast.
+   */
+  status?: ValidationStatus;
+  onClose?: () => void;
 }
 
 export const Toast = forwardRef(function Toast(
-    { children, hideClose = false, onClose, status = "info", ...restProps }: ToastProps,
-    ref: ForwardedRef<HTMLDivElement>
+  {
+    children,
+    hideClose = false,
+    onClose,
+    status = "info",
+    ...restProps
+  }: ToastProps,
+  ref: ForwardedRef<HTMLDivElement>
 ) {
-    const targetWindow = useWindow();
-    useComponentCssInjection({
-        testId: "salt-toast",
-        css: toastCss,
-        window: targetWindow,
-    });
+  const targetWindow = useWindow();
+  useComponentCssInjection({
+    testId: "salt-toast",
+    css: toastCss,
+    window: targetWindow,
+  });
 
-    const handleOnClick = () => {
-        onClose?.();
-    };
+  const handleOnClick = () => {
+    onClose?.();
+  };
 
-    return (
-        <div
-            className={clsx(withBaseName(), withBaseName(status))}
-            {...restProps}
-            ref={ref}
-        >
-            <StatusIndicator status={status} className={withBaseName("icon")} />
-            <ToastContent>{children}</ToastContent>
-            {!hideClose && (
-                <div className={clsx(withBaseName("close"))}>
-                    <Button variant="secondary" onClick={handleOnClick}>
-                        <CloseIcon />
-                    </Button>
-                </div>
-            )}
+  return (
+    <div
+      className={clsx(withBaseName(), withBaseName(status))}
+      {...restProps}
+      ref={ref}
+    >
+      <StatusIndicator status={status} className={withBaseName("icon")} />
+      <ToastContent>{children}</ToastContent>
+      {!hideClose && (
+        <div className={clsx(withBaseName("close"))}>
+          <Button variant="secondary" onClick={handleOnClick}>
+            <CloseIcon />
+          </Button>
         </div>
-    );
+      )}
+    </div>
+  );
 });
--- packages/lab/stories/toast/toast.stories.tsx
+++ packages/lab/stories/toast/toast.stories.tsx
@@ -2,40 +2,40 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { Toast } from "@salt-ds/lab";
 
 export default {
-    title: "Lab/Toast",
-    component: Toast,
+  title: "Lab/Toast",
+  component: Toast,
 } as ComponentMeta<typeof Toast>;
 
 const Template: ComponentStory<typeof Toast> = ({ children, ...args }) => {
-    return <Toast {...args}>{children}</Toast>;
+  return <Toast {...args}>{children}</Toast>;
 };
 
 export const DefaultInfo = Template.bind({});
 DefaultInfo.args = {
-    status: "info",
-    children: <div>LEI Updated</div>,
+  status: "info",
+  children: <div>LEI Updated</div>,
 };
 
 export const Warning = Template.bind({});
 Warning.args = {
-    status: "warning",
-    children: <div>Pre-Trade Transparency</div>,
+  status: "warning",
+  children: <div>Pre-Trade Transparency</div>,
 };
 
 export const Error = Template.bind({});
 Error.args = {
-    status: "error",
-    children: <div>CIT Check Failed System Error - Bypass Check</div>,
+  status: "error",
+  children: <div>CIT Check Failed System Error - Bypass Check</div>,
 };
 
 export const Success = Template.bind({});
 Success.args = {
-    status: "success",
-    children: <div>Clear Transact</div>,
+  status: "success",
+  children: <div>Clear Transact</div>,
 };
 
 export const HideClose = Template.bind({});
 HideClose.args = {
-    children: <div>LEI Updated</div>,
-    hideClose: true
+  children: <div>LEI Updated</div>,
+  hideClose: true,
 };
