517ca6f2cefa3fb328734308e3600faf298ce094
David Miller <d.miller01@gmail.com>
2023-07-04 11:50:57 +0100

    refactor SteppedTracker to clone children instead of using context

--- packages/lab/src/stepped-tracker/SteppedTracker.tsx
+++ packages/lab/src/stepped-tracker/SteppedTracker.tsx
@@ -5,104 +5,19 @@ import {
   ReactNode,
   Children,
   useState,
-  useRef,
-  useEffect,
-  useCallback,
+  cloneElement,
+  isValidElement,
 } from "react";
 import { clsx } from "clsx";
 import { useComponentCssInjection } from "@salt-ds/styles";
 import { useWindow } from "@salt-ds/window";
-import { makePrefixer } from "@salt-ds/core";
-
-import { TrackerStepProvider } from "./TrackerStep";
+import { makePrefixer, mergeProps } from "@salt-ds/core";
 
 import steppedTrackerCss from "./SteppedTracker.css";
 
 const withBaseName = makePrefixer("saltSteppedTracker");
 
-type OnTruncatedCallback = (isTruncated: boolean) => void;
-
-const useDetectTruncatedText = (callback: OnTruncatedCallback) => {
-  const [observedMap] = useState(() => new Map<number, HTMLElement>());
-  const isTruncatedRef = useRef<boolean>(false);
-
-  // Store callback as a ref to prevent re-creating ResizeObserver
-  const callbackRef = useRef<(isOverflowing: boolean) => void>(callback);
-  useEffect(() => {
-    callbackRef.current = callback;
-  }, [callback]);
-
-  const getRo = useCallback(() => {
-    const observer = new ResizeObserver((entries) => {
-      let isTruncated = false;
-
-      for (const entry of entries) {
-        const entryElem = entry.target as HTMLElement;
-
-        if (entryElem.offsetWidth < entryElem.scrollWidth) {
-          isTruncated = true;
-          break;
-        }
-      }
-
-      if (isTruncated !== isTruncatedRef.current) {
-        isTruncatedRef.current = isTruncated;
-        callbackRef.current(isTruncated);
-      }
-    });
-
-    return observer;
-  }, []);
-
-  // Manage ResizeObserver lifecycle;
-  const [ro, setRo] = useState(() => getRo());
-
-  useEffect(() => {
-    return () => {
-      ro.disconnect();
-    };
-  }, [ro]);
-
-  useEffect(() => {
-    setRo(getRo());
-  }, [getRo]);
-
-  const registerChildRef = useCallback(
-    (i: number, el: HTMLElement | null) => {
-      if (!el) {
-        const existingRef = observedMap.get(i);
-        if (existingRef) {
-          ro.unobserve(existingRef);
-          observedMap.delete(i);
-        }
-      } else {
-        const existingRef = observedMap.get(i);
-        if (el === existingRef) {
-          return;
-        }
-
-        if (existingRef) {
-          ro.unobserve(existingRef);
-        }
-        observedMap.set(i, el);
-
-        ro.observe(el);
-      }
-    },
-    [observedMap, ro]
-  );
-
-  const getOverflowRef = useCallback(
-    (i: number) => {
-      return (el: HTMLElement | null) => {
-        registerChildRef(i, el);
-      };
-    },
-    [registerChildRef]
-  );
-
-  return getOverflowRef;
-};
+import useDetectTruncatedText from "./useDetectTruncatedText";
 
 export interface SteppedTrackerProps extends ComponentPropsWithoutRef<"ul"> {
   /**
@@ -133,25 +48,9 @@ export const SteppedTracker = forwardRef<HTMLUListElement, SteppedTrackerProps>(
 
     const [hasTooltips, setHasTooltips] = useState(false);
 
-    const getOverflowRef = useDetectTruncatedText((isTruncated) => {
-      setHasTooltips(isTruncated);
-    });
+    const getOverflowRef = useDetectTruncatedText(setHasTooltips);
 
     const totalSteps = Children.count(children);
-
-    const Steps = Children.map(children, (child, i) => (
-      <TrackerStepProvider
-        value={{
-          activeStep,
-          stepNumber: i,
-          totalSteps,
-          getOverflowRef,
-          hasTooltip: hasTooltips,
-        }}
-      >
-        {child}
-      </TrackerStepProvider>
-    ));
 
     return (
       <ul
@@ -166,7 +65,18 @@ export const SteppedTracker = forwardRef<HTMLUListElement, SteppedTrackerProps>(
         {...restProps}
         ref={ref}
       >
-        {Steps}
+        {Children.map(children, (child, i) => {
+          if (!isValidElement(child)) {
+            return child;
+          }
+          return cloneElement(child, {
+            _isActive: activeStep === i,
+            _hasConnector: i < totalSteps - 1,
+            _overflowRef: getOverflowRef(i),
+            _hasTooltip: hasTooltips,
+            _totalSteps: totalSteps,
+          });
+        })}
       </ul>
     );
   }
