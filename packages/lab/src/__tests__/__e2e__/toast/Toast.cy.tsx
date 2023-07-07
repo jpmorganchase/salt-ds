228f4f6fc21100ead2de9c551cdecacaf82f8026
Alina Visan <alycry.alina@gmail.com>
2023-07-07 14:21:03 +0300

    Update Toast.cy.tsx

--- packages/lab/src/__tests__/__e2e__/toast/Toast.cy.tsx
+++ packages/lab/src/__tests__/__e2e__/toast/Toast.cy.tsx
@@ -1,5 +1,5 @@
 import { composeStories } from "@storybook/testing-react";
-import { Toast } from "@salt-ds/lab";
+import { Toast, ToastClose, ToastContent } from "@salt-ds/lab";
 import * as toastStories from "@stories/toast/toast.stories";
 import { checkAccessibility } from "../../../../../../cypress/tests/checkAccessibility";
 
@@ -8,39 +8,54 @@ const composedStories = composeStories(toastStories);
 describe("Given a Toast", () => {
   checkAccessibility(composedStories);
 
-  describe("THEN it renders correctly", () => {
-    it("renders info state", () => {
-      cy.mount(<Toast>Toast content</Toast>);
+  describe("WHEN ToastContent", () => {
+    it("AND no status, THEN renders info state", () => {
+      cy.mount(
+        <Toast>
+          <ToastContent>Toast content</ToastContent>
+        </Toast>
+      );
       cy.findAllByTestId("InfoSolidIcon").should("exist");
     });
 
-    it("renders error state", () => {
-      cy.mount(<Toast status="error">Toast content</Toast>);
+    it("AND status=error, THEN renders error state", () => {
+      cy.mount(
+        <Toast status="error">
+          <ToastContent>Toast content</ToastContent>
+        </Toast>
+      );
       cy.findAllByTestId("ErrorSolidIcon").should("exist");
     });
 
-    it("renders warning state", () => {
-      cy.mount(<Toast status="warning">Toast content</Toast>);
+    it("AND status=warning, THEN renders warning state", () => {
+      cy.mount(
+        <Toast status="warning">
+          <ToastContent>Toast content</ToastContent>
+        </Toast>
+      );
       cy.findAllByTestId("WarningSolidIcon").should("exist");
     });
 
-    it("renders success state", () => {
-      cy.mount(<Toast status="success">Toast content</Toast>);
+    it("AND status=success, THEN renders success state", () => {
+      cy.mount(
+        <Toast status="success">
+          <ToastContent>Toast content</ToastContent>
+        </Toast>
+      );
       cy.findAllByTestId("SuccessTickIcon").should("exist");
-    });
-
-    it("renders close button", () => {
-      cy.mount(<Toast>Toast content</Toast>);
-      cy.findAllByRole("button").should("exist");
-      cy.findAllByTestId("CloseIcon").should("exist");
     });
   });
 
-  describe("WHEN hideClose", () => {
-    it("THEN the close button is not displayed", () => {
-      cy.mount(<Toast hideClose>Toast content</Toast>);
-      cy.findAllByRole("button").should("not.exist");
-      cy.findAllByTestId("CloseIcon").should("not.exist");
+  describe("WHEN ToastClose", () => {
+    it("renders close button", () => {
+      cy.mount(
+        <Toast>
+          <ToastContent>Toast content</ToastContent>
+          <ToastClose />
+        </Toast>
+      );
+      cy.findAllByRole("button").should("exist");
+      cy.findAllByTestId("CloseIcon").should("exist");
     });
   });
 });
