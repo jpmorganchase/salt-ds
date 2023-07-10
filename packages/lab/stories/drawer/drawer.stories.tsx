6283c92f563e2b4681e8f3c6315977aed47c5bfd
Moreira, Joana M <joana.m.moreira@jpmchase.com>
2023-07-10 11:36:15 +0100

    update close button

--- packages/lab/stories/drawer/drawer.stories.tsx
+++ packages/lab/stories/drawer/drawer.stories.tsx
@@ -6,6 +6,7 @@ import {
   FlexItem,
   FlexLayout,
 } from "@salt-ds/core";
+import { CloseIcon } from "@salt-ds/icons";
 import { FormField, Input, Drawer, DRAWER_POSITIONS } from "@salt-ds/lab";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import "./drawer.stories.css";
@@ -30,29 +31,30 @@ type DrawerContentExampleProps = {
 };
 
 const DrawerContentExample = ({ onClick }: DrawerContentExampleProps) => (
-  <StackLayout className="drawer-example">
-    <FlexItem grow={1}>
-      <h2 id="drawer_label">Lorem ipsum</h2>
-      <p id="drawer_description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nunc lacus,
-        scelerisque ut elit nec, commodo blandit est. Duis mollis dui at nisl
-        faucibus, id maximus urna pellentesque. Praesent consequat vulputate
-        dolor, a mattis metus suscipit vitae. Donec ullamcorper, neque sit amet
-        laoreet ornare, diam eros posuere metus, id consectetur tellus nisl id
-        ipsum. Fusce sit amet cursus mauris, vel scelerisque enim. Quisque eu
-        dolor tortor. Nulla facilisi. Vestibulum at neque sit amet neque
-        facilisis porttitor a ac risus.Mauris consequat sollicitudin commodo.
-        Vestibulum ac diam vulputate, condimentum purus non, eleifend erat. Nunc
-        auctor iaculis mi eu hendrerit. Suspendisse potenti. Cras tristique
-        vehicula iaculis. Morbi faucibus volutpat tellus, sit amet fringilla dui
-        rhoncus a. Suspendisse nunc nulla, mattis sed commodo ac, cursus ut
-        augue.
-      </p>
-    </FlexItem>
-    <FlowLayout justify="end">
-      <Button onClick={onClick}>Close Drawer</Button>
-    </FlowLayout>
-  </StackLayout>
+  <>
+    <Button
+      onClick={onClick}
+      variant="secondary"
+      className="drawer-close-button"
+    >
+      <CloseIcon />
+    </Button>
+    <h2 id="drawer_label">Lorem ipsum</h2>
+    <p id="drawer_description">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nunc lacus,
+      scelerisque ut elit nec, commodo blandit est. Duis mollis dui at nisl
+      faucibus, id maximus urna pellentesque. Praesent consequat vulputate
+      dolor, a mattis metus suscipit vitae. Donec ullamcorper, neque sit amet
+      laoreet ornare, diam eros posuere metus, id consectetur tellus nisl id
+      ipsum. Fusce sit amet cursus mauris, vel scelerisque enim. Quisque eu
+      dolor tortor. Nulla facilisi. Vestibulum at neque sit amet neque facilisis
+      porttitor a ac risus.Mauris consequat sollicitudin commodo. Vestibulum ac
+      diam vulputate, condimentum purus non, eleifend erat. Nunc auctor iaculis
+      mi eu hendrerit. Suspendisse potenti. Cras tristique vehicula iaculis.
+      Morbi faucibus volutpat tellus, sit amet fringilla dui rhoncus a.
+      Suspendisse nunc nulla, mattis sed commodo ac, cursus ut augue.
+    </p>
+  </>
 );
 
 const DefaultDrawerStory: ComponentStory<typeof Drawer> = (args) => {
