a011560154dcb4bb2e8bb405d9f5950148bc75c0
Alina Visan <alycry.alina@gmail.com>
2023-07-11 18:00:12 +0300

    Update Toast.tsx

--- packages/lab/src/toast/Toast.tsx
+++ packages/lab/src/toast/Toast.tsx
@@ -1,5 +1,5 @@
 import { clsx } from "clsx";
-import { ComponentPropsWithoutRef, forwardRef, ReactNode } from "react";
+import { ComponentPropsWithoutRef, forwardRef } from "react";
 import { makePrefixer, StatusIndicator, ValidationStatus } from "@salt-ds/core";
 import { useWindow } from "@salt-ds/window";
 import { useComponentCssInjection } from "@salt-ds/styles";
@@ -9,7 +9,6 @@ import toastCss from "./Toast.css";
 const withBaseName = makePrefixer("saltToast");
 
 export interface ToastProps extends ComponentPropsWithoutRef<"div"> {
-  children?: ReactNode;
   /**
    *  A string to determine the current state of the Toast.
    */
@@ -31,9 +30,9 @@ export const Toast = forwardRef<HTMLDivElement, ToastProps>(function Toast(
   return (
     <div
       className={clsx(withBaseName(), withBaseName(status), className)}
+      role="alert"
       {...rest}
       ref={ref}
-      role="alert"
     >
       <StatusIndicator status={status} className={withBaseName("icon")} />
       {children}
