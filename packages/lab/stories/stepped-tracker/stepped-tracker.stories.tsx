66e4a880fc71fa42c72cf375153b0a8049518829
David Miller <d.miller01@gmail.com>
2023-07-11 11:38:42 +0100

    added initial documentation for SteppedTracker

--- packages/lab/stories/stepped-tracker/stepped-tracker.stories.tsx
+++ packages/lab/stories/stepped-tracker/stepped-tracker.stories.tsx
@@ -1,363 +1,210 @@
 import { useState } from "react";
 
 import { SteppedTracker, TrackerStep } from "@salt-ds/lab";
-import { Button, StackLayout } from "@salt-ds/core";
-import { ComponentStory } from "@storybook/react";
+import { Button, StackLayout, FlexLayout, Tooltip } from "@salt-ds/core";
+import { RefreshIcon } from "@salt-ds/icons";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 export default {
   title: "Lab/Stepped Tracker",
-  component: "SteppedTracker",
+  component: SteppedTracker,
+  subcomponents: { TrackerStep },
+} as ComponentMeta<typeof SteppedTracker>;
+
+type Step = {
+  label: string;
+  state: "default" | "completed";
 };
 
-const defaultSteps = [
+type Steps = Step[];
+
+const defaultSteps: Steps = [
   {
     label: "Step One",
-    completed: false,
+    state: "default",
   },
   {
     label: "Step Two",
-    completed: false,
+    state: "default",
   },
   {
     label: "Step Three",
-    completed: false,
+    state: "default",
   },
   {
     label: "Step Four",
-    completed: false,
+    state: "default",
   },
 ];
 
 export const Default: ComponentStory<typeof SteppedTracker> = () => {
+  return (
+    <StackLayout
+      direction="column"
+      align="stretch"
+      gap={10}
+      style={{ width: "100%", minWidth: 600, maxWidth: 800, margin: "auto" }}
+    >
+      <SteppedTracker activeStep={0}>
+        <TrackerStep state="default">Step One</TrackerStep>
+        <TrackerStep state="default">Step Two</TrackerStep>
+        <TrackerStep state="default">Step Three</TrackerStep>
+        <TrackerStep state="default">Step Four</TrackerStep>
+      </SteppedTracker>
+      <SteppedTracker activeStep={2}>
+        <TrackerStep state="completed">Step One</TrackerStep>
+        <TrackerStep state="completed">Step Two</TrackerStep>
+        <TrackerStep state="default">Step Three</TrackerStep>
+        <TrackerStep state="default">Step Four</TrackerStep>
+      </SteppedTracker>
+      <SteppedTracker activeStep={3}>
+        <TrackerStep state="completed">Step One</TrackerStep>
+        <TrackerStep state="completed">Step Two</TrackerStep>
+        <TrackerStep state="completed">Step Three</TrackerStep>
+        <TrackerStep state="completed">Step Four</TrackerStep>
+      </SteppedTracker>
+    </StackLayout>
+  );
+};
+
+export const AutoProgress: ComponentStory<typeof SteppedTracker> = () => {
   const [activeStep, setActiveStep] = useState(0);
   const [steps, setSteps] = useState(defaultSteps);
+  const totalSteps = steps.length;
 
-  const onNext = () => {
+  const onComplete = () => {
+    if (activeStep < totalSteps - 1) {
+      setActiveStep((old) => old + 1);
+    }
+
     setSteps((old) =>
-      old.map((step, i) => ({
-        ...step,
-        completed: i === activeStep ? true : step.completed,
-      }))
+      old.map((step, i) =>
+        i === activeStep
+          ? {
+              ...step,
+              state: "completed",
+            }
+          : step
+      )
     );
-    setActiveStep((old) => (old < steps.length - 1 ? old + 1 : old));
+  };
+
+  const onRefresh = () => {
+    setActiveStep(0);
+    setSteps(defaultSteps);
   };
 
   return (
-    <StackLayout direction="column" align="center">
-      <SteppedTracker activeStep={activeStep} style={{ width: "100vw" }}>
-        {steps.map(({ label, completed }, key) => (
-          <TrackerStep completed={completed} key={key}>
+    <StackLayout
+      direction="column"
+      align="stretch"
+      style={{ width: "100%", minWidth: 600, maxWidth: 800, margin: "auto" }}
+    >
+      <SteppedTracker activeStep={activeStep}>
+        {steps.map(({ label, state }, key) => (
+          <TrackerStep state={state} key={key}>
             {label}
           </TrackerStep>
         ))}
       </SteppedTracker>
-      <Button onClick={onNext} variant="cta">
-        Next Step
-      </Button>
+      <FlexLayout justify="center" gap={1}>
+        <Button onClick={onComplete}>Complete Step</Button>
+        <Tooltip content="Reset">
+          <Button onClick={onRefresh}>
+            <RefreshIcon />
+          </Button>
+        </Tooltip>
+      </FlexLayout>
     </StackLayout>
   );
 };
 
 const longText = "very ".repeat(30);
 
-const truncatedSteps = [
-  {
-    label: `Step One: I am some ${longText} long text which will be truncated`,
-    completed: false,
-  },
-  {
-    label: `Step Two: I am some ${longText} long text which will be truncated`,
-    completed: false,
-  },
-  {
-    label: `Step Three: I am some ${longText} long text which will be truncated`,
-    completed: false,
-  },
-  {
-    label: `Step Four: I am some ${longText} long text which will be truncated`,
-    completed: false,
-  },
-];
-
 export const Truncated: ComponentStory<typeof SteppedTracker> = () => {
-  const [activeStep, setActiveStep] = useState(0);
-  const [steps, setSteps] = useState(truncatedSteps);
-
-  const onNext = () => {
-    setSteps((old) =>
-      old.map((step, i) => ({
-        ...step,
-        completed: i === activeStep ? true : step.completed,
-      }))
-    );
-    setActiveStep((old) => (old < steps.length - 1 ? old + 1 : old));
-  };
-
   return (
-    <StackLayout direction="column" align="center">
-      <SteppedTracker activeStep={activeStep} style={{ width: "100vw" }}>
-        {steps.map(({ label, completed }, key) => (
-          <TrackerStep completed={completed} key={key}>
-            {label}
-          </TrackerStep>
-        ))}
+    <StackLayout
+      direction="column"
+      align="stretch"
+      style={{ width: "100%", minWidth: 600, maxWidth: 800, margin: "auto" }}
+    >
+      <SteppedTracker activeStep={0}>
+        <TrackerStep state="default">{`Step One: I am some ${longText} long text which will be truncated`}</TrackerStep>
+        <TrackerStep state="default">{`Step Two: I am some ${longText} long text which will be truncated`}</TrackerStep>
+        <TrackerStep state="default">{`Step Three: I am some ${longText} long text which will be truncated`}</TrackerStep>
+        <TrackerStep state="default">{`Step Four: I am some ${longText} long text which will be truncated`}</TrackerStep>
       </SteppedTracker>
-      <Button onClick={onNext} variant="cta">
-        Next Step
-      </Button>
     </StackLayout>
   );
 };
-
-const singleTruncatedSteps = [
-  {
-    label: `Step One`,
-    completed: false,
-  },
-  {
-    label: `Step Two: truncated on some screen sizes `,
-    completed: false,
-  },
-  {
-    label: `Step Three`,
-    completed: false,
-  },
-  {
-    label: `Step Four`,
-    completed: false,
-  },
-];
 
 export const SingleTruncated: ComponentStory<typeof SteppedTracker> = () => {
+  return (
+    <StackLayout
+      direction="column"
+      align="stretch"
+      style={{ width: "100%", minWidth: 600, maxWidth: 800, margin: "auto" }}
+    >
+      <SteppedTracker activeStep={0}>
+        <TrackerStep state="default">{`Step One`}</TrackerStep>
+        <TrackerStep state="default">{`Step Two: I am truncated on smaller screen sizes`}</TrackerStep>
+        <TrackerStep state="default">{`Step Three`}</TrackerStep>
+        <TrackerStep state="default">{`Step Four`}</TrackerStep>
+      </SteppedTracker>
+    </StackLayout>
+  );
+};
+
+export const NonSequentialProgress: ComponentStory<
+  typeof SteppedTracker
+> = () => {
   const [activeStep, setActiveStep] = useState(0);
-  const [steps, setSteps] = useState(singleTruncatedSteps);
+  const [steps, setSteps] = useState(defaultSteps);
+  const totalSteps = steps.length;
 
   const onNext = () => {
-    setSteps((old) =>
-      old.map((step, i) => ({
-        ...step,
-        completed: i === activeStep ? true : step.completed,
-      }))
-    );
     setActiveStep((old) => (old < steps.length - 1 ? old + 1 : old));
   };
 
+  const onPrevious = () => {
+    setActiveStep((old) => (old > 0 ? old - 1 : old));
+  };
+
+  const onToggleStep = () => {
+    setSteps((old) =>
+      old.map((step, i) =>
+        i === activeStep
+          ? {
+              ...step,
+              state: step.state === "default" ? "completed" : "default",
+            }
+          : step
+      )
+    );
+  };
+
   return (
-    <StackLayout direction="column" align="center">
-      <SteppedTracker activeStep={activeStep} style={{ width: "100vw" }}>
-        {steps.map(({ label, completed }, key) => (
-          <TrackerStep completed={completed} key={key}>
+    <StackLayout
+      direction="column"
+      align="stretch"
+      style={{ width: "100%", minWidth: 600, maxWidth: 800, margin: "auto" }}
+    >
+      <SteppedTracker activeStep={activeStep}>
+        {steps.map(({ label, state }, key) => (
+          <TrackerStep state={state} key={key}>
             {label}
           </TrackerStep>
         ))}
       </SteppedTracker>
-      <Button onClick={onNext} variant="cta">
-        Next Step
-      </Button>
-    </StackLayout>
-  );
-};
-
-const completedSteps = [
-  {
-    label: "Step One",
-    completed: true,
-  },
-  {
-    label: "Step Two",
-    completed: true,
-  },
-  {
-    label: "Step Three",
-    completed: true,
-  },
-  {
-    label: "Step Four",
-    completed: true,
-  },
-];
-
-export const Completed: ComponentStory<typeof SteppedTracker> = () => {
-  const [activeStep, setActiveStep] = useState(3);
-  const [steps, setSteps] = useState(completedSteps);
-
-  const onNext = () => {
-    setSteps((old) =>
-      old.map((step, i) => ({
-        ...step,
-        completed: i === activeStep ? true : step.completed,
-      }))
-    );
-    setActiveStep((old) => (old < steps.length - 1 ? old + 1 : old));
-  };
-
-  return (
-    <StackLayout direction="column" align="center">
-      <SteppedTracker activeStep={activeStep} style={{ width: "100vw" }}>
-        {steps.map(({ label, completed }, key) => (
-          <TrackerStep completed={completed} key={key}>
-            {label}
-          </TrackerStep>
-        ))}
-      </SteppedTracker>
-      <Button onClick={onNext} variant="cta">
-        Next Step
-      </Button>
-    </StackLayout>
-  );
-};
-
-const halfwaySteps = [
-  {
-    label: "Step One",
-    completed: true,
-  },
-  {
-    label: "Step Two",
-    completed: true,
-  },
-  {
-    label: "Step Three",
-    completed: false,
-  },
-  {
-    label: "Step Four",
-    completed: false,
-  },
-];
-
-export const HalfWay: ComponentStory<typeof SteppedTracker> = () => {
-  const [activeStep, setActiveStep] = useState(2);
-  const [steps, setSteps] = useState(halfwaySteps);
-
-  const onNext = () => {
-    setSteps((old) =>
-      old.map((step, i) => ({
-        ...step,
-        completed: i === activeStep ? true : step.completed,
-      }))
-    );
-    setActiveStep((old) => (old < steps.length - 1 ? old + 1 : old));
-  };
-
-  return (
-    <StackLayout direction="column" align="center">
-      <SteppedTracker activeStep={activeStep} style={{ width: "100vw" }}>
-        {steps.map(({ label, completed }, key) => (
-          <TrackerStep completed={completed} key={key}>
-            {label}
-          </TrackerStep>
-        ))}
-      </SteppedTracker>
-      <Button onClick={onNext} variant="cta">
-        Next Step
-      </Button>
-    </StackLayout>
-  );
-};
-
-const disabledSteps = [
-  {
-    label: "Step One",
-    completed: false,
-  },
-  {
-    label: "Step Two",
-    completed: false,
-  },
-  {
-    label: "Step Three",
-    completed: false,
-  },
-  {
-    label: "Step Four",
-    completed: false,
-  },
-];
-
-export const FullyDisabled: ComponentStory<typeof SteppedTracker> = () => {
-  const [activeStep, setActiveStep] = useState(0);
-  const [steps, setSteps] = useState(disabledSteps);
-
-  const onNext = () => {
-    setSteps((old) =>
-      old.map((step, i) => ({
-        ...step,
-        completed: i === activeStep ? true : step.completed,
-      }))
-    );
-    setActiveStep((old) => (old < steps.length - 1 ? old + 1 : old));
-  };
-
-  return (
-    <StackLayout direction="column" align="center">
-      <SteppedTracker
-        activeStep={activeStep}
-        style={{ width: "100vw" }}
-        disabled
-      >
-        {steps.map(({ label, completed }, key) => (
-          <TrackerStep completed={completed} key={key}>
-            {label}
-          </TrackerStep>
-        ))}
-      </SteppedTracker>
-      <Button onClick={onNext} variant="cta">
-        Next Step
-      </Button>
-    </StackLayout>
-  );
-};
-
-const partlyDisabledSteps = [
-  {
-    label: "Step One",
-    completed: false,
-    disabled: false,
-  },
-  {
-    label: "Step Two",
-    completed: false,
-    disabled: false,
-  },
-  {
-    label: "Step Three",
-    completed: false,
-    disabled: true,
-  },
-  {
-    label: "Step Four",
-    completed: false,
-    disabled: true,
-  },
-];
-
-export const PartlyDisabled: ComponentStory<typeof SteppedTracker> = () => {
-  const [activeStep, setActiveStep] = useState(0);
-  const [steps, setSteps] = useState(partlyDisabledSteps);
-
-  const onNext = () => {
-    setSteps((old) =>
-      old.map((step, i) => ({
-        ...step,
-        completed: i === activeStep ? true : step.completed,
-      }))
-    );
-    setActiveStep((old) => (old < steps.length - 1 ? old + 1 : old));
-  };
-
-  return (
-    <StackLayout direction="column" align="center">
-      <SteppedTracker activeStep={activeStep} style={{ width: "100vw" }}>
-        {steps.map(({ label, completed, disabled }, key) => (
-          <TrackerStep completed={completed} disabled={disabled} key={key}>
-            {label}
-          </TrackerStep>
-        ))}
-      </SteppedTracker>
-      <Button onClick={onNext} variant="cta">
-        Next Step
-      </Button>
+      <FlexLayout justify="center" gap={1}>
+        <Button onClick={onPrevious} disabled={activeStep <= 0}>
+          Previous
+        </Button>
+        <Button onClick={onNext} disabled={activeStep >= totalSteps - 1}>
+          Next
+        </Button>
+        <Button onClick={onToggleStep}>Toggle Step</Button>
+      </FlexLayout>
     </StackLayout>
   );
 };
