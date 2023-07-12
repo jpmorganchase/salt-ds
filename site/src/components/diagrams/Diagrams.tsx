133d77d98c91c967fa4d2a4133b130c6045377f4
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2023-07-12 19:23:21 +0100

    Forms pattern page (#1895)

---
+++ site/src/components/diagrams/Diagrams.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+import styles from "./Diagrams.module.css";
+
+export const Diagrams = ({ children }: { children: ReactNode }) => {
+  return <div className={styles.diagramsContainer}>{children}</div>;
+};
