27aac14c0153730785294e435ada914e9cedacd5
Zarko Pernar <zarko.pernar@gmail.com>
2023-07-12 14:55:40 +0100

    clean up stories

--- packages/lab/stories/pill/pill.stories.tsx
+++ packages/lab/stories/pill/pill.stories.tsx
@@ -6,13 +6,8 @@
  * Refresh of a page is needed.
  */
 
-import { SyntheticEvent, useState } from "react";
-import {
-  Banner,
-  BannerContent,
-  SaltProvider,
-  StackLayout,
-} from "@salt-ds/core";
+import { useState } from "react";
+import { SaltProvider } from "@salt-ds/core";
 import { Pill } from "@salt-ds/lab";
 import { FavoriteIcon } from "@salt-ds/icons";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
@@ -27,7 +22,7 @@ export const Default: ComponentStory<typeof Pill> = (args) => {
 };
 
 export const Clickable: ComponentStory<typeof Pill> = () => {
-  const handleClick = (e: SyntheticEvent<HTMLDivElement>) => {
+  const handleClick = () => {
     console.log("clicked");
   };
   return <Pill onClick={handleClick}>Clickable Pill</Pill>;
@@ -63,68 +58,6 @@ export const Icon: ComponentStory<typeof Pill> = () => {
     <Pill icon={<FavoriteIcon />} onClick={() => console.log("Clicked.")}>
       Pill with Icon
     </Pill>
-  );
-};
-
-export const Colors: ComponentStory<typeof Pill> = () => {
-  const pills = [
-    {
-      color: "var(--salt-palette-success-foreground)",
-      bg: "var(--salt-palette-success-background)",
-      label: "Palette success",
-    },
-    {
-      color: "var(--salt-palette-warning-foreground)",
-      bg: "var(--salt-palette-warning-background)",
-      label: "Palette warning",
-    },
-    {
-      color: "var(--salt-palette-info-foreground)",
-      bg: "var(--salt-palette-info-background)",
-      label: "Palette info",
-    },
-    {
-      color: "var(--salt-palette-error-foreground)",
-      bg: "var(--salt-palette-error-background)",
-      label: "Palette error",
-    },
-  ];
-  return (
-    <div
-      style={{
-        width: 400,
-      }}
-    >
-      <StackLayout gap={4}>
-        <Banner>
-          <BannerContent>
-            These colors are not part of the component. They are here just to
-            illustrate how to customize the component with in different colors.
-            Make sure to always use the colors that have sufficient contrast
-            between foreground and background in line with WCAG 2.1.
-          </BannerContent>
-        </Banner>
-        <div
-          style={{
-            display: "flex",
-            gap: "var(--salt-spacing-50)",
-          }}
-        >
-          {pills.map((pill) => (
-            <div
-              key={pill.label}
-              style={{
-                // @ts-ignore
-                "--saltPill-text-color": pill.color,
-                "--saltPill-background": pill.bg,
-              }}
-            >
-              <Pill>{pill.label}</Pill>
-            </div>
-          ))}
-        </div>
-      </StackLayout>
-    </div>
   );
 };
 
