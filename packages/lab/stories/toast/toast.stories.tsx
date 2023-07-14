2e2572ab3f2ffccf3dc27b89f06df4fdcb9567d1
Alina Visan <alycry.alina@gmail.com>
2023-07-14 14:43:33 +0300

    updates

--- packages/lab/stories/toast/toast.stories.tsx
+++ packages/lab/stories/toast/toast.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { Button, FlowLayout } from "@salt-ds/core";
+import { Button, FlowLayout, Text } from "@salt-ds/core";
 import { Toast, ToastClose, ToastContent, ToastProps } from "@salt-ds/lab";
 
 export default {
@@ -17,25 +17,53 @@ const Template: ComponentStory<typeof Toast> = ({ children, ...args }) => (
 export const DefaultInfo = Template.bind({});
 DefaultInfo.args = {
   status: "info",
-  children: <div>LEI Updated</div>,
+  children: (
+    <div>
+      <Text>
+        <strong>File update</strong>
+      </Text>
+      <div>A new version of this file is available with 37 updates. </div>
+    </div>
+  ),
 };
 
 export const Warning = Template.bind({});
 Warning.args = {
   status: "warning",
-  children: <div>Pre-Trade Transparency</div>,
+  children: (
+    <div>
+      <Text>
+        <strong>File access</strong>
+      </Text>
+      <div>Viewers of this file can see comments and suggestions. </div>
+    </div>
+  ),
 };
 
 export const Error = Template.bind({});
 Error.args = {
   status: "error",
-  children: <div>CIT Check Failed System Error - Bypass Check</div>,
+  children: (
+    <div>
+      <Text>
+        <strong>System error</strong>
+      </Text>
+      <div>Connection timed out. Failed to retrieve data. </div>
+    </div>
+  ),
 };
 
 export const Success = Template.bind({});
 Success.args = {
   status: "success",
-  children: <div>Clear Transact</div>,
+  children: (
+    <div>
+      <Text>
+        <strong>Project file upload</strong>
+      </Text>
+      <div>Project file has successfully uploaded to the shared drive. </div>
+    </div>
+  ),
 };
 
 export const NoCloseButton = (args: ToastProps) => (
@@ -47,11 +75,15 @@ export const NoCloseButton = (args: ToastProps) => (
 export const SingleLine = (args: ToastProps) => (
   <FlowLayout style={{ width: 352 }}>
     <Toast {...args}>
-      <div>This is a toast message. This is a toast message.</div>
+      <ToastContent>
+        <div>This is a toast message. This is a toast message.</div>
+      </ToastContent>
       <ToastClose />
     </Toast>
     <Toast {...args}>
-      <div>This is a toast message. This is a toast message.</div>
+      <ToastContent>
+        <div>This is a toast message. This is a toast message.</div>
+      </ToastContent>
     </Toast>
   </FlowLayout>
 );
