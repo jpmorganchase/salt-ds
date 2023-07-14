14747f6748486d777551dc90c692ca3c7f18b183
Alina Visan <alycry.alina@gmail.com>
2023-07-14 15:04:24 +0300

    remove close button styling

--- packages/lab/src/toast/ToastClose.tsx
+++ packages/lab/src/toast/ToastClose.tsx
@@ -2,10 +2,7 @@ import { clsx } from "clsx";
 import { ForwardedRef, forwardRef } from "react";
 import { Button, ButtonProps, makePrefixer } from "@salt-ds/core";
 import { CloseIcon } from "@salt-ds/icons";
-import { useWindow } from "@salt-ds/window";
-import { useComponentCssInjection } from "@salt-ds/styles";
 
-import toastCloseCss from "./ToastClose.css";
 
 const withBaseName = makePrefixer("saltToastClose");
 
@@ -13,13 +10,6 @@ export const ToastClose = forwardRef(function ToastClose(
   { className, ...restProps }: ButtonProps,
   ref: ForwardedRef<HTMLButtonElement>
 ) {
-  const targetWindow = useWindow();
-  useComponentCssInjection({
-    testId: "salt-toast-close",
-    css: toastCloseCss,
-    window: targetWindow,
-  });
-
   return (
     <Button
       variant="secondary"
