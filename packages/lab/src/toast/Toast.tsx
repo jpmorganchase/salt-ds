ff3eea5dabc68271013b4c5802b5f4e027de4ba9
Alina Visan <alycry.alina@gmail.com>
2023-07-17 16:23:09 +0300

    Toast (#1892)
    Co-authored-by: Josh Wooding <12938082+joshwooding@users.noreply.github.com>

---
+++ packages/lab/src/toast/Toast.tsx
@@ -0,0 +1,41 @@
+import { clsx } from "clsx";
+import { ComponentPropsWithoutRef, forwardRef } from "react";
+import { makePrefixer, StatusIndicator, ValidationStatus } from "@salt-ds/core";
+import { useWindow } from "@salt-ds/window";
+import { useComponentCssInjection } from "@salt-ds/styles";
+
+import toastCss from "./Toast.css";
+
+const withBaseName = makePrefixer("saltToast");
+
+export interface ToastProps extends ComponentPropsWithoutRef<"div"> {
+  /**
+   *  A string to determine the current state of the Toast.
+   */
+  status?: ValidationStatus;
+}
+
+export const Toast = forwardRef<HTMLDivElement, ToastProps>(function Toast(
+  props,
+  ref
+) {
+  const { children, className, status = "info", ...rest } = props;
+  const targetWindow = useWindow();
+  useComponentCssInjection({
+    testId: "salt-toast",
+    css: toastCss,
+    window: targetWindow,
+  });
+
+  return (
+    <div
+      className={clsx(withBaseName(), withBaseName(status), className)}
+      role="alert"
+      {...rest}
+      ref={ref}
+    >
+      <StatusIndicator status={status} className={withBaseName("icon")} />
+      {children}
+    </div>
+  );
+});
