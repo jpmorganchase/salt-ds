de59e55dbbb4c36fbc27da2e427fb2aceea9b845
Alina Visan <alycry.alina@gmail.com>
2023-07-21 17:34:29 +0300

    add ToastGroup

---
+++ packages/lab/src/toast-group/ToastGroup.tsx
@@ -0,0 +1,31 @@
+import { clsx } from "clsx";
+import { ComponentPropsWithoutRef, forwardRef } from "react";
+import { makePrefixer } from "@salt-ds/core";
+import { useWindow } from "@salt-ds/window";
+import { useComponentCssInjection } from "@salt-ds/styles";
+
+import toastGroupCss from "./ToastGroup.css";
+
+const withBaseName = makePrefixer("saltToastGroup");
+
+export interface ToastGroupProps extends ComponentPropsWithoutRef<"div"> {
+  placement?: "top-right" | "bottom-right"
+}
+
+export const ToastGroup = forwardRef<HTMLDivElement, ToastGroupProps>(function ToastGroup(props, ref) {
+
+  const { children, className, placement = "bottom-right" } = props
+
+  const targetWindow = useWindow();
+  useComponentCssInjection({
+    testId: "salt-toast",
+    css: toastGroupCss,
+    window: targetWindow,
+  });
+
+  return (
+    <div className={clsx(withBaseName(), withBaseName(placement), className)}>
+      {children}
+    </div>
+  )
+})
