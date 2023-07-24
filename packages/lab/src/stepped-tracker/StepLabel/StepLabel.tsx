b66e4482476615d1cf2c657858327794362eb3fd
David Miller <d.miller01@gmail.com>
2023-07-24 15:24:00 +0100

    remove truncation functionality from SteppedTracker

--- packages/lab/src/stepped-tracker/StepLabel/StepLabel.tsx
+++ packages/lab/src/stepped-tracker/StepLabel/StepLabel.tsx
@@ -1,17 +1,15 @@
 import {
   forwardRef,
   ReactNode,
-  useMemo,
   ComponentPropsWithoutRef,
 } from "react";
 import { clsx } from "clsx";
-import { useForkRef, makePrefixer, useIdMemo, Label } from "@salt-ds/core";
+import { makePrefixer, Label } from "@salt-ds/core";
 import { useComponentCssInjection } from "@salt-ds/styles";
 import { useWindow } from "@salt-ds/window";
 
 import stepLabelCss from "./StepLabel.css";
 
-import { useSteppedTrackerContext } from "../SteppedTrackerContext";
 import { useTrackerStepTooltipContext } from "../TrackerStep";
 
 const withBaseName = makePrefixer("saltStepLabel");
@@ -22,8 +20,6 @@ export interface StepLabelProps extends ComponentPropsWithoutRef<"label"> {
 
 export const StepLabel = forwardRef<HTMLLabelElement, StepLabelProps>(
   function StepLabel({ children, className, ...rest }, ref?) {
-    const id = useIdMemo();
-
     const targetWindow = useWindow();
     useComponentCssInjection({
       testId: "salt-step-label",
@@ -31,19 +27,14 @@ export const StepLabel = forwardRef<HTMLLabelElement, StepLabelProps>(
       window: targetWindow,
     });
 
-    const { getOverflowRef } = useSteppedTrackerContext();
-    const overflowRef = useMemo(() => getOverflowRef(id), [id, getOverflowRef]);
-
     const isInTooltip = useTrackerStepTooltipContext();
-
-    const combinedRef = useForkRef<HTMLLabelElement>(overflowRef, ref);
 
     return isInTooltip ? (
       <span>{children}</span>
     ) : (
       <Label
         className={clsx(withBaseName(), className)}
-        ref={combinedRef}
+        ref={ref}
         {...rest}
       >
         {children}
