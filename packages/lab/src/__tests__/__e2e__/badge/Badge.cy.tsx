9789aba1498b863328ff6f0c3cc0f8f43f31c8c1
lukeac123 <luke.atkinson-coyle@outlook.com>
2023-07-24 13:31:30 +0100

    badgeV1

--- packages/lab/src/__tests__/__e2e__/badge/Badge.cy.tsx
+++ packages/lab/src/__tests__/__e2e__/badge/Badge.cy.tsx
@@ -2,53 +2,37 @@ import { Badge } from "@salt-ds/lab";
 
 describe("GIVEN a Badge", () => {
   it("THEN can render a Badge with badge number larger than max", () => {
-    cy.mount(<Badge badgeContent={100} max={99} />);
+    cy.mount(<Badge value={100} max={99} />);
     cy.findByText("99+").should("exist");
   });
 
   it("THEN can render a Badge with badge number equal to max", () => {
-    cy.mount(<Badge badgeContent={99} max={99} />);
+    cy.mount(<Badge value={99} max={99} />);
     cy.findByText("99").should("exist");
   });
 
   it("THEN can render a Badge with badge number smaller than max", () => {
-    cy.mount(<Badge badgeContent={98} max={99} />);
+    cy.mount(<Badge value={98} max={99} />);
     cy.findByText("98").should("exist");
   });
 
-  it("THEN can render a Badge with default icon", () => {
-    cy.mount(<Badge badgeContent={98} />);
-    cy.findByRole("img", { name: "message" }).should("exist");
+  it("THEN can render a string", () => {
+    cy.mount(<Badge value={"lots"} />);
+    cy.findByText("lots").should("exist");
+  });
+
+  it("THEN can render a badge with a truncated string", () => {
+    cy.mount(<Badge value={"lots and lots"} />);
+    cy.findByText("lots...").should("exist");
   });
 
   it("THEN can render with a custom text child", () => {
-    cy.mount(<Badge badgeContent={1}>Lorem Ipsum</Badge>);
+    cy.mount(<Badge value={1}>Lorem Ipsum</Badge>);
     cy.findByText("Lorem Ipsum").should("exist");
   });
 
-  it("SHOULD reference the child and badge content with aria-labelledby", () => {
-    cy.mount(
-      <Badge badgeContent="BADGE_CONTENT">
-        <div data-testid="badgeChild" />
-      </Badge>
-    );
-
-    cy.findByTestId("badgeChild").invoke("attr", "id").as("childId");
-    cy.findByText("BADGE_CONTENT").invoke("attr", "id").as("contentId");
-
-    cy.get("@childId").then((childId) => {
-      cy.get("@contentId").then((contentId) => {
-        cy.findByRole("img").should(
-          "have.attr",
-          "aria-labelledby",
-          `${childId} ${contentId}`
-        );
-      });
-    });
-  });
-
-  it("SHOULD have no a11y violations on load", () => {
-    cy.mount(<Badge />);
-    cy.checkAxeComponent();
+  it("THEN can render with a custom text child", () => {
+    cy.mount(<Badge value={1}>Lorem Ipsum</Badge>);
+    cy.findByText("Lorem Ipsum").should("exist");
   });
 });
