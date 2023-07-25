1dacc94f8cdf6a7bdf87c598f67cd5507f714528
libertymayc <liberty.curtis@live.co.uk>
2023-07-25 16:01:42 +0100

    prettier

--- packages/ag-grid-theme/stories/examples/Icons.tsx
+++ packages/ag-grid-theme/stories/examples/Icons.tsx
@@ -1,45 +1,40 @@
-import { useComponentCssInjection } from '@salt-ds/styles';
-import { useWindow } from '@salt-ds/window';
+import { useComponentCssInjection } from "@salt-ds/styles";
+import { useWindow } from "@salt-ds/window";
 import { useAgGridThemeSwitcher } from "../dependencies/ThemeSwitcher";
 import { useAgGridHelpers } from "../dependencies/useAgGridHelpers";
-import saltStyles from '../../css/_export-salt-icons.module.scss';
-import uitkStyles from '../../css/_export-uitk-icons.module.scss';
+import saltStyles from "../../css/_export-salt-icons.module.scss";
+import uitkStyles from "../../css/_export-uitk-icons.module.scss";
 
-import iconCss from  "./Icons.css";
+import iconCss from "./Icons.css";
 
 const Icons = () => {
-    const targetWindow = useWindow();
-    useComponentCssInjection({
-      testId: "salt-ag-grid-icons",
-      css: iconCss,
-      window: targetWindow,
-    });
-    const { switcher, themeName } = useAgGridThemeSwitcher();
-    
-  const { containerProps, agGridProps } = useAgGridHelpers(
-    `ag-theme-${themeName}`
+  const targetWindow = useWindow();
+  useComponentCssInjection({
+    testId: "salt-ag-grid-icons",
+    css: iconCss,
+    window: targetWindow,
+  });
+  const { switcher, themeName } = useAgGridThemeSwitcher();
+
+  const { containerProps } = useAgGridHelpers(`ag-theme-${themeName}`);
+  const styles = themeName === "salt" ? saltStyles : uitkStyles;
+
+  return (
+    <>
+      {switcher}
+      {Object.keys(styles).map(
+        (key) => {
+          if (styles[key].startsWith('"')) {
+            return (
+              <div {...containerProps} style={{ "--icon-content": styles[key] }}>
+                {key}
+              </div>
+            );
+          }
+        }
+      )}
+    </>
   );
-  
-    return (
-        <>
-            {switcher}
-            {Object.keys(themeName === "salt" ? saltStyles : uitkStyles).map((key) => {
-                const value = themeName === "salt" ? saltStyles[key] : uitkStyles[key];
-                console.log(themeName, value)
-                if(value.startsWith("\"")) {
-                    return (
-                        <div 
-                            {...containerProps}
-                            style={{"--icon-content": value}}
-                        >
-                            {key}
-                        </div>
-                    );
-                }
-            })}
-      </>
-    );
-  };
-  
-  export default Icons;
-  
+};
+
+export default Icons;
