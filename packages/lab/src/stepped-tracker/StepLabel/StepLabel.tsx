f9291944205476241863627d246e127cdc73f777
David Miller <d.miller01@gmail.com>
2023-07-25 15:20:52 +0100

    Remove truncation functionality from Stepped Tracker (#2045)
    Co-authored-by: Josh Wooding <12938082+joshwooding@users.noreply.github.com>

--- packages/lab/src/stepped-tracker/StepLabel/StepLabel.tsx
+++ packages/lab/src/stepped-tracker/StepLabel/StepLabel.tsx
@@ -1,29 +1,17 @@
-import {
-  forwardRef,
-  ReactNode,
-  useMemo,
-  ComponentPropsWithoutRef,
-} from "react";
+import { forwardRef, ComponentPropsWithoutRef } from "react";
 import { clsx } from "clsx";
-import { useForkRef, makePrefixer, useIdMemo, Label } from "@salt-ds/core";
+import { makePrefixer, Label } from "@salt-ds/core";
 import { useComponentCssInjection } from "@salt-ds/styles";
 import { useWindow } from "@salt-ds/window";
 
 import stepLabelCss from "./StepLabel.css";
 
-import { useSteppedTrackerContext } from "../SteppedTrackerContext";
-import { useTrackerStepTooltipContext } from "../TrackerStep";
-
 const withBaseName = makePrefixer("saltStepLabel");
 
-export interface StepLabelProps extends ComponentPropsWithoutRef<"label"> {
-  children: ReactNode;
-}
+export type StepLabelProps = ComponentPropsWithoutRef<"label">;
 
 export const StepLabel = forwardRef<HTMLLabelElement, StepLabelProps>(
-  function StepLabel({ children, className, ...rest }, ref?) {
-    const id = useIdMemo();
-
+  function StepLabel({ children, className, ...rest }, ref) {
     const targetWindow = useWindow();
     useComponentCssInjection({
       testId: "salt-step-label",
@@ -31,21 +19,8 @@ export const StepLabel = forwardRef<HTMLLabelElement, StepLabelProps>(
       window: targetWindow,
     });
 
-    const { getOverflowRef } = useSteppedTrackerContext();
-    const overflowRef = useMemo(() => getOverflowRef(id), [id, getOverflowRef]);
-
-    const isInTooltip = useTrackerStepTooltipContext();
-
-    const combinedRef = useForkRef<HTMLLabelElement>(overflowRef, ref);
-
-    return isInTooltip ? (
-      <span>{children}</span>
-    ) : (
-      <Label
-        className={clsx(withBaseName(), className)}
-        ref={combinedRef}
-        {...rest}
-      >
+    return (
+      <Label className={clsx(withBaseName(), className)} ref={ref} {...rest}>
         {children}
       </Label>
     );
