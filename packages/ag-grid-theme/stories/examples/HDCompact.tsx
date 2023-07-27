f5bf2a68c6822ab0ad21460be18f89a749efaf27
libertymayc <liberty.curtis@live.co.uk>
2023-07-27 13:14:30 +0100

    Fixes for styling

---
+++ packages/ag-grid-theme/stories/examples/HDCompact.tsx
@@ -0,0 +1,67 @@
+import { ChangeEvent, useState } from "react";
+import { AgGridReact, AgGridReactProps } from "ag-grid-react";
+import {
+  StackLayout,
+  FlexItem,
+  FlexLayout,
+  Checkbox,
+  useDensity,
+} from "@salt-ds/core";
+import dataGridExampleColumns from "../dependencies/dataGridExampleColumns";
+import dataGridExampleData from "../dependencies/dataGridExampleData";
+import { useAgGridThemeSwitcher } from "../dependencies/ThemeSwitcher";
+import { useAgGridHelpers } from "../dependencies/useAgGridHelpers";
+
+const statusBar = {
+  statusPanels: [
+    {
+      statusPanel: "agTotalRowCountComponent",
+      align: "left",
+    },
+    { statusPanel: "agFilteredRowCountComponent" },
+    { statusPanel: "agSelectedRowCountComponent" },
+    { statusPanel: "agAggregationComponent" },
+  ],
+};
+
+const HDCompact = (props: AgGridReactProps) => {
+  const [compact, setCompact] = useState(false);
+  const { switcher, themeName } = useAgGridThemeSwitcher();
+  const { agGridProps, containerProps } = useAgGridHelpers(
+    `ag-theme-${themeName}`,
+    compact
+  );
+  const density = useDensity();
+
+  const handleCompactChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCompact(event.target.checked);
+  };
+
+  return (
+    <StackLayout gap={4}>
+      <FlexLayout direction="row">
+        {switcher}
+        <FlexItem>
+          <Checkbox
+            checked={compact}
+            label="Compact (for high density only)"
+            onChange={handleCompactChange}
+            disabled={density !== "high"}
+          />
+        </FlexItem>
+      </FlexLayout>
+      <div {...containerProps}>
+        <AgGridReact
+          columnDefs={dataGridExampleColumns}
+          rowData={dataGridExampleData}
+          statusBar={statusBar}
+          rowSelection="multiple"
+          {...agGridProps}
+          {...props}
+        />
+      </div>
+    </StackLayout>
+  );
+};
+
+export default HDCompact;
