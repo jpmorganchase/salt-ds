666bfdf98cf1b7a70d2dba207845d9b4f607a18a
Alina Visan <alycry.alina@gmail.com>
2023-07-28 13:21:50 +0300

    Update ToastGroup.cy.tsx

--- packages/lab/src/__tests__/__e2e__/toast-group/ToastGroup.cy.tsx
+++ packages/lab/src/__tests__/__e2e__/toast-group/ToastGroup.cy.tsx
@@ -12,32 +12,38 @@ describe("Given a ToastGroup", () => {
 
   describe("WHEN placement is top-right", () => {
     it("THEN should render correctly", () => {
-
       cy.mount(
         <ToastGroup placement="top-right">
           <Toast>
             <ToastContent>This is a toast</ToastContent>
-            <Button variant="secondary"><CloseIcon /></Button>
+            <Button variant="secondary">
+              <CloseIcon />
+            </Button>
           </Toast>
         </ToastGroup>
       );
-      cy.get('.saltToastGroup-top-right').should('exist').and("have.css", 'top', '0px')
-      cy.findByRole("alert").should('exist')
-    })
+      cy.get(".saltToastGroup-top-right")
+        .should("exist")
+        .and("have.css", "top", "0px");
+      cy.findByRole("alert").should("exist");
+    });
   });
   describe("WHEN placement is bottom-right", () => {
     it("THEN should render correctly", () => {
-
       cy.mount(
         <ToastGroup placement="bottom-right">
           <Toast>
             <ToastContent>This is a toast</ToastContent>
-            <Button variant="secondary"><CloseIcon /></Button>
+            <Button variant="secondary">
+              <CloseIcon />
+            </Button>
           </Toast>
         </ToastGroup>
       );
-      cy.get('.saltToastGroup-bottom-right').should('exist').and("have.css", 'bottom', '0px')
-      cy.findByRole("alert").should('exist')
-    })
+      cy.get(".saltToastGroup-bottom-right")
+        .should("exist")
+        .and("have.css", "bottom", "0px");
+      cy.findByRole("alert").should("exist");
+    });
   });
 });
