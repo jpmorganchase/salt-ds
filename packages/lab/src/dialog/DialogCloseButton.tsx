793cb861e93a0ebdcf64f3879d8dcd0d260d89e5
Zarko Pernar <zarko.pernar@gmail.com>
2023-08-07 20:17:50 +0100

    remove unnecessary aria-attr

--- packages/lab/src/dialog/DialogCloseButton.tsx
+++ packages/lab/src/dialog/DialogCloseButton.tsx
@@ -5,14 +5,12 @@ import { useWindow } from "@salt-ds/window";
 import { useComponentCssInjection } from "@salt-ds/styles";
 import { CloseIcon } from "@salt-ds/icons";
 
-import { useDialogContext } from "./DialogContext";
 import dialogCloseButtonCss from "./DialogCloseButton.css";
 
 const withBaseName = makePrefixer("saltDialogCloseButton");
 
 export const DialogCloseButton = forwardRef<HTMLButtonElement, ButtonProps>(
   function DialogCloseButton({ className, ...rest }, ref) {
-    const { dialogId } = useDialogContext();
     const targetWindow = useWindow();
     useComponentCssInjection({
       testId: "salt-dialog-close-button",
@@ -23,7 +21,6 @@ export const DialogCloseButton = forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <Button
         ref={ref}
-        aria-controls={dialogId}
         aria-label="Close dialog"
         variant="secondary"
         className={clsx(withBaseName(), className)}
