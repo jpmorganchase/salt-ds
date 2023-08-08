a58e83aeb1c551e626dd7c585a036a8cdf711931
Joana Moreira <31695437+JoanaMMoreira@users.noreply.github.com>
2023-08-08 15:02:43 +0100

    Navigation item rename and component rendering (#2135)

--- packages/lab/src/index.ts
+++ packages/lab/src/index.ts
@@ -53,7 +53,7 @@ export * from "./list-next";
 export * from "./logo";
 export * from "./menu-button";
 export * from "./metric";
-export * from "./nav-item";
+export * from "./navigation-item";
 export * from "./overlay";
 export * from "./pagination";
 export * from "./parent-child-item";
---
+++ packages/lab/stories/navigation-item/navigation-item.stories.tsx
@@ -0,0 +1,340 @@
+import { NavigationItem, NavigationItemProps, Badge } from "@salt-ds/lab";
+import { Story } from "@storybook/react";
+import { useState } from "react";
+import { NotificationIcon } from "@salt-ds/icons";
+
+import "./navigation-item.stories.css";
+
+export default {
+  title: "Lab/Navigation Item",
+  component: NavigationItem,
+};
+
+const Template: Story<NavigationItemProps> = (args) => {
+  const [active, setActive] = useState(false);
+
+  const handleActiveToggle = () => {
+    setActive((current) => !current);
+  };
+
+  return (
+    <NavigationItem active={active} onClick={handleActiveToggle} {...args} />
+  );
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  children: "Navigation Item",
+  href: "#",
+};
+
+export const WithIcon: Story<NavigationItemProps> = (args) => {
+  const [active, setActive] = useState(false);
+
+  const handleActiveToggle = () => {
+    setActive((current) => !current);
+  };
+
+  return (
+    <NavigationItem
+      active={active}
+      onClick={handleActiveToggle}
+      IconComponent={NotificationIcon}
+      {...args}
+    />
+  );
+};
+WithIcon.args = {
+  children: "Navigation Item",
+  href: "#",
+};
+
+export const WithBadge: Story<NavigationItemProps> = (args) => {
+  const [active, setActive] = useState(false);
+
+  const handleActiveToggle = () => {
+    setActive((current) => !current);
+  };
+
+  return (
+    <NavigationItem
+      active={active}
+      onClick={handleActiveToggle}
+      BadgeComponent={<Badge value="NEW" />}
+      {...args}
+    />
+  );
+};
+WithBadge.args = {
+  children: "Navigation Item",
+  href: "#",
+};
+
+const items = [
+  "Navigation Item 1",
+  "Navigation Item 2",
+  "Navigation Item 3",
+  "Navigation Item 4",
+  "Navigation Item 5",
+];
+
+const multipleLevelNesting = [
+  {
+    name: "Navigation Item 1 - Level 0",
+  },
+  {
+    name: "Navigation Item 2 - Level 0",
+    subNav: [
+      {
+        name: "Navigation Item 1 - Level 1",
+        subNav: [
+          "Navigation Item 1 - Level 2",
+          "Navigation Item 2 - Level 2",
+          "Navigation Item 3 - Level 2",
+        ],
+      },
+    ],
+  },
+];
+
+export const HorizontalGroup = () => {
+  const [active, setActive] = useState(items[0]);
+
+  return (
+    <nav>
+      <ul className="horizontal">
+        {items.map((item) => (
+          <li key={item}>
+            <NavigationItem
+              active={active === item}
+              href="#"
+              onClick={(event) => {
+                // Prevent default to avoid navigation
+                event.preventDefault();
+                setActive(item);
+              }}
+            >
+              {item}
+            </NavigationItem>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export const HorizontalGroupWithIconAndBadge = () => {
+  const [active, setActive] = useState(items[0]);
+
+  return (
+    <nav>
+      <ul className="horizontal">
+        {items.map((item, index) => (
+          <li key={item}>
+            <NavigationItem
+              active={active === item}
+              href="#"
+              onClick={(event) => {
+                // Prevent default to avoid navigation
+                event.preventDefault();
+                setActive(item);
+              }}
+              IconComponent={NotificationIcon}
+              BadgeComponent={index === 2 && <Badge value="NEW" />}
+            >
+              {item}
+            </NavigationItem>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export const VerticalGroup = () => {
+  const [active, setActive] = useState(items[0]);
+  return (
+    <nav>
+      <ul className="vertical">
+        {items.map((item) => (
+          <li key={item}>
+            <NavigationItem
+              active={active === item}
+              href="#"
+              orientation="vertical"
+              onClick={(event) => {
+                // Prevent default to avoid navigation
+                event.preventDefault();
+                setActive(item);
+              }}
+            >
+              {item}
+            </NavigationItem>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export const VerticalGroupWithIconAndBadge = () => {
+  const [active, setActive] = useState(items[0]);
+  const badgeValues = ["NEW", 1, 22, "Vanilla", 3, "Chocolate"];
+
+  return (
+    <nav>
+      <ul className="vertical">
+        {items.map((item, index) => (
+          <li key={item}>
+            <NavigationItem
+              active={active === item}
+              href="#"
+              orientation="vertical"
+              onClick={(event) => {
+                // Prevent default to avoid navigation
+                event.preventDefault();
+                setActive(item);
+              }}
+              IconComponent={NotificationIcon}
+              BadgeComponent={
+                badgeValues[index] && <Badge value={badgeValues[index]} />
+              }
+            >
+              {item}
+            </NavigationItem>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export const VerticalNestedGroup = () => {
+  const [active, setActive] = useState(multipleLevelNesting[0].name);
+
+  const [expanded, setExpanded] = useState<string[]>([]);
+
+  return (
+    <nav>
+      <ul className="vertical">
+        {multipleLevelNesting.map(({ name, subNav }) => (
+          <li key={name}>
+            <NavigationItem
+              active={
+                active === name ||
+                (!expanded.includes(name) &&
+                  subNav?.some((item) => active === `${name} - ${item.name}`))
+              }
+              blurSelected={
+                !expanded.includes(name) &&
+                subNav?.some(
+                  (item) =>
+                    active === `${name} - ${item.name}` ||
+                    item.subNav.some(
+                      (nestedItem) =>
+                        active === `${name} - ${item.name} - ${nestedItem}`
+                    )
+                )
+              }
+              href="#"
+              orientation="vertical"
+              onClick={(event) => {
+                // Prevent default to avoid navigation
+                event.preventDefault();
+              }}
+              onExpand={() => {
+                if (expanded.includes(name)) {
+                  setExpanded(expanded.filter((item) => item !== name));
+                } else {
+                  setExpanded([...expanded, name]);
+                }
+              }}
+              parent={subNav && subNav.length > 0}
+              expanded={expanded.includes(name)}
+              IconComponent={NotificationIcon}
+            >
+              {name}
+            </NavigationItem>
+            {expanded.includes(name) && (
+              <ul className="vertical">
+                {subNav?.map((item) => {
+                  const itemValue = `${name} - ${item.name}`;
+
+                  return (
+                    <li key={itemValue}>
+                      <NavigationItem
+                        active={
+                          active === itemValue ||
+                          (!expanded.includes(item.name) &&
+                            item.subNav?.some(
+                              (item) => active === `${name} - ${item}`
+                            ))
+                        }
+                        blurSelected={
+                          !expanded.includes(item.name) &&
+                          item.subNav?.some(
+                            (nestedItem) =>
+                              active ===
+                              `${name} - ${item.name} - ${nestedItem}`
+                          )
+                        }
+                        href="#"
+                        orientation="vertical"
+                        onClick={(event) => {
+                          // Prevent default to avoid navigation
+                          event.preventDefault();
+                        }}
+                        level={1}
+                        onExpand={() => {
+                          if (expanded.includes(item.name)) {
+                            setExpanded(
+                              expanded.filter(
+                                (element) => element !== item.name
+                              )
+                            );
+                          } else {
+                            setExpanded([...expanded, item.name]);
+                          }
+                        }}
+                        parent={item.subNav && item.subNav.length > 0}
+                        expanded={expanded.includes(item.name)}
+                      >
+                        {item.name}
+                      </NavigationItem>
+
+                      {expanded.includes(item.name) && (
+                        <ul className="vertical">
+                          {item.subNav.map((nestedItem) => {
+                            const itemValue = `${name} - ${item.name} - ${nestedItem}`;
+
+                            return (
+                              <li key={itemValue}>
+                                <NavigationItem
+                                  active={active === itemValue}
+                                  href="#"
+                                  orientation="vertical"
+                                  onClick={(event) => {
+                                    // Prevent default to avoid navigation
+                                    event.preventDefault();
+                                    setActive(itemValue);
+                                  }}
+                                  level={2}
+                                >
+                                  {nestedItem}
+                                </NavigationItem>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      )}
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
