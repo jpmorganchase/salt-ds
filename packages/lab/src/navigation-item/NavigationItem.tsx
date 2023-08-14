92a5e3ef1ebe4ee88daf6c31a8d9894411a06470
Joana Moreira <31695437+JoanaMMoreira@users.noreply.github.com>
2023-08-14 14:19:30 +0100

    Remove `IconComponent` and `BadgeComponent` from Navigation Item and update docs (#2170)

--- packages/lab/src/navigation-item/NavigationItem.tsx
+++ packages/lab/src/navigation-item/NavigationItem.tsx
@@ -3,11 +3,9 @@ import {
   forwardRef,
   MouseEventHandler,
   MouseEvent,
-  ComponentType,
   ReactNode,
 } from "react";
 import { makePrefixer, Link, Button } from "@salt-ds/core";
-import { IconProps } from "@salt-ds/icons";
 import { clsx } from "clsx";
 import { ExpansionIcon } from "./ExpansionIcon";
 
@@ -21,7 +19,7 @@ export interface NavigationItemProps extends ComponentPropsWithoutRef<"div"> {
    */
   active?: boolean;
   /**
-   * Whether the navigation item has active children.
+   * Whether the parent navigation item has active nested items.
    */
   blurSelected?: boolean;
   /**
@@ -37,7 +35,7 @@ export interface NavigationItemProps extends ComponentPropsWithoutRef<"div"> {
    */
   orientation?: "horizontal" | "vertical";
   /**
-   * Whether the navigation item is a parent with nested children.
+   * Whether the navigation item is a parent with nested items.
    */
   parent?: boolean;
   /**
@@ -48,14 +46,6 @@ export interface NavigationItemProps extends ComponentPropsWithoutRef<"div"> {
    * Href to be passed to the Link element.
    */
   href?: string;
-  /**
-   * Icon component to be displayed next to the navigation item label.
-   */
-  IconComponent?: ComponentType<IconProps> | null;
-  /**
-   * Badge component to be displayed next to the navigation item label.
-   */
-  BadgeComponent?: ReactNode;
 }
 
 const withBaseName = makePrefixer("saltNavigationItem");
@@ -78,8 +68,6 @@ export const NavigationItem = forwardRef<HTMLDivElement, NavigationItemProps>(
       level = 0,
       onExpand,
       href,
-      IconComponent,
-      BadgeComponent,
       style: styleProp,
       ...rest
     } = props;
@@ -146,11 +134,7 @@ export const NavigationItem = forwardRef<HTMLDivElement, NavigationItemProps>(
             withBaseName(orientation)
           )}
         >
-          {IconComponent && (
-            <IconComponent aria-hidden className={withBaseName("icon")} />
-          )}
           <span className={withBaseName("label")}>{children}</span>
-          {BadgeComponent}
           {parent && (
             <ExpansionIcon
               expanded={expanded}
--- packages/lab/stories/navigation-item/navigation-item.stories.tsx
+++ packages/lab/stories/navigation-item/navigation-item.stories.tsx
@@ -18,17 +18,25 @@ const Template: Story<NavigationItemProps> = (args) => {
   };
 
   return (
-    <NavigationItem active={active} onClick={handleActiveToggle} {...args} />
+    <NavigationItem
+      active={active}
+      onClick={(event) => {
+        // Prevent default to avoid navigation
+        event.preventDefault();
+        handleActiveToggle();
+      }}
+      {...args}
+    />
   );
 };
 
 export const Default = Template.bind({});
 Default.args = {
-  children: "Navigation Item",
+  children: "Label",
   href: "#",
 };
 
-export const WithIcon: Story<NavigationItemProps> = (args) => {
+export const Vertical: Story<NavigationItemProps> = (args) => {
   const [active, setActive] = useState(false);
 
   const handleActiveToggle = () => {
@@ -38,60 +46,160 @@ export const WithIcon: Story<NavigationItemProps> = (args) => {
   return (
     <NavigationItem
       active={active}
-      onClick={handleActiveToggle}
-      IconComponent={NotificationIcon}
+      onClick={(event) => {
+        // Prevent default to avoid navigation
+        event.preventDefault();
+        handleActiveToggle();
+      }}
       {...args}
     />
   );
 };
-WithIcon.args = {
-  children: "Navigation Item",
+Vertical.args = {
+  children: "Label",
+  orientation: "vertical",
   href: "#",
 };
 
-export const WithBadge: Story<NavigationItemProps> = (args) => {
-  const [active, setActive] = useState(false);
+export const WithIcon: Story<NavigationItemProps> = () => {
+  const [horizontalActive, setHorizontalActive] = useState(false);
 
-  const handleActiveToggle = () => {
-    setActive((current) => !current);
+  const [verticalActive, setVerticalActive] = useState(false);
+
+  const handleHorizontalActiveToggle = () => {
+    setHorizontalActive((current) => !current);
+  };
+
+  const handleVerticalActiveToggle = () => {
+    setVerticalActive((current) => !current);
   };
 
   return (
-    <NavigationItem
-      active={active}
-      onClick={handleActiveToggle}
-      BadgeComponent={<Badge value="NEW" />}
-      {...args}
-    />
+    <div className="navigationItemContainer">
+      <NavigationItem
+        active={horizontalActive}
+        onClick={(event) => {
+          // Prevent default to avoid navigation
+          event.preventDefault();
+          handleHorizontalActiveToggle();
+        }}
+        href="#"
+      >
+        <NotificationIcon />
+        Label
+      </NavigationItem>
+
+      <NavigationItem
+        active={verticalActive}
+        onClick={(event) => {
+          // Prevent default to avoid navigation
+          event.preventDefault();
+          handleVerticalActiveToggle();
+        }}
+        href="#"
+        orientation="vertical"
+      >
+        <NotificationIcon />
+        Label
+      </NavigationItem>
+    </div>
   );
 };
-WithBadge.args = {
-  children: "Navigation Item",
-  href: "#",
+
+export const WithBadge: Story<NavigationItemProps> = () => {
+  const [horizontalActive, setHorizontalActive] = useState(false);
+
+  const [verticalActive, setVerticalActive] = useState(false);
+
+  const handleHorizontalActiveToggle = () => {
+    setHorizontalActive((current) => !current);
+  };
+
+  const handleVerticalActiveToggle = () => {
+    setVerticalActive((current) => !current);
+  };
+
+  return (
+    <div className="navigationItemContainer">
+      <NavigationItem
+        active={horizontalActive}
+        onClick={(event) => {
+          // Prevent default to avoid navigation
+          event.preventDefault();
+          handleHorizontalActiveToggle();
+        }}
+        href="#"
+      >
+        Label
+        <Badge value="New" />
+      </NavigationItem>
+
+      <NavigationItem
+        active={verticalActive}
+        onClick={(event) => {
+          // Prevent default to avoid navigation
+          event.preventDefault();
+          handleVerticalActiveToggle();
+        }}
+        href="#"
+        orientation="vertical"
+      >
+        Label
+        <Badge value="New" />
+      </NavigationItem>
+    </div>
+  );
 };
 
-const items = [
-  "Navigation Item 1",
-  "Navigation Item 2",
-  "Navigation Item 3",
-  "Navigation Item 4",
-  "Navigation Item 5",
-];
+export const WithNestedItems: Story<NavigationItemProps> = () => {
+  const [horizontalActive, setHorizontalActive] = useState(false);
+
+  const [verticalActive, setVerticalActive] = useState(false);
+
+  const handleHorizontalActiveToggle = () => {
+    setHorizontalActive((current) => !current);
+  };
+
+  const handleVerticalActiveToggle = () => {
+    setVerticalActive((current) => !current);
+  };
+
+  return (
+    <div className="navigationItemContainer">
+      <NavigationItem
+        active={horizontalActive}
+        onExpand={handleHorizontalActiveToggle}
+        href="#"
+        parent
+      >
+        Label
+      </NavigationItem>
+
+      <NavigationItem
+        active={verticalActive}
+        onExpand={handleVerticalActiveToggle}
+        href="#"
+        orientation="vertical"
+        parent
+      >
+        Label
+      </NavigationItem>
+    </div>
+  );
+};
+
+const items = ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"];
 
 const multipleLevelNesting = [
   {
-    name: "Navigation Item 1 - Level 0",
+    name: "Label 1 - level 0",
   },
   {
-    name: "Navigation Item 2 - Level 0",
+    name: "Label 2 - level 0",
     subNav: [
       {
-        name: "Navigation Item 1 - Level 1",
-        subNav: [
-          "Navigation Item 1 - Level 2",
-          "Navigation Item 2 - Level 2",
-          "Navigation Item 3 - Level 2",
-        ],
+        name: "Label 1 - level 1",
+        subNav: ["Label 1 - level 2", "Label 2 - level 2", "Label 3 - level 2"],
       },
     ],
   },
@@ -139,10 +247,10 @@ export const HorizontalGroupWithIconAndBadge = () => {
                 event.preventDefault();
                 setActive(item);
               }}
-              IconComponent={NotificationIcon}
-              BadgeComponent={index === 2 && <Badge value="NEW" />}
             >
+              <NotificationIcon />
               {item}
+              {index === 2 && <Badge value="New" />}
             </NavigationItem>
           </li>
         ))}
@@ -179,7 +287,7 @@ export const VerticalGroup = () => {
 
 export const VerticalGroupWithIconAndBadge = () => {
   const [active, setActive] = useState(items[0]);
-  const badgeValues = ["NEW", 1, 22, "Vanilla", 3, "Chocolate"];
+  const badgeValues = ["New", 1, 22, "Vanilla", 3, "Chocolate"];
 
   return (
     <nav>
@@ -195,12 +303,10 @@ export const VerticalGroupWithIconAndBadge = () => {
                 event.preventDefault();
                 setActive(item);
               }}
-              IconComponent={NotificationIcon}
-              BadgeComponent={
-                badgeValues[index] && <Badge value={badgeValues[index]} />
-              }
             >
+              <NotificationIcon />
               {item}
+              {badgeValues[index] && <Badge value={badgeValues[index]} />}
             </NavigationItem>
           </li>
         ))}
@@ -241,6 +347,7 @@ export const VerticalNestedGroup = () => {
               onClick={(event) => {
                 // Prevent default to avoid navigation
                 event.preventDefault();
+                setActive(name);
               }}
               onExpand={() => {
                 if (expanded.includes(name)) {
@@ -251,8 +358,8 @@ export const VerticalNestedGroup = () => {
               }}
               parent={subNav && subNav.length > 0}
               expanded={expanded.includes(name)}
-              IconComponent={NotificationIcon}
             >
+              <NotificationIcon />
               {name}
             </NavigationItem>
             {expanded.includes(name) && (
