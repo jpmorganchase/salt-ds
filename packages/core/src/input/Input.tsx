fa77a8946b4d55c41e67f751e7d66b0da689e40e
Liberty Curtis <91206958+libertymayc@users.noreply.github.com>
2023-08-14 14:05:47 +0200

    Make readonly focusable in Input and MultilineInput (#2184)

--- packages/core/src/input/Input.tsx
+++ packages/core/src/input/Input.tsx
@@ -196,11 +196,10 @@ export const Input = forwardRef<HTMLDivElement, InputProps>(function Input(
         readOnly={isReadOnly}
         ref={inputRef}
         role={role}
-        tabIndex={isReadOnly || isDisabled ? -1 : 0}
+        tabIndex={isDisabled ? -1 : 0}
         onBlur={handleBlur}
         onChange={handleChange}
-        // From ADA: read-only field doesn't need to be focusable since it's not a field but text
-        onFocus={!isDisabled && !isReadOnly ? handleFocus : undefined}
+        onFocus={!isDisabled ? handleFocus : undefined}
         placeholder={placeholder}
         value={value}
         {...restA11yProps}
