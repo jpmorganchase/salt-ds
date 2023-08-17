c1a97c872324374eb85c28e638bdf9cf46ceb35d
R662301 <lily.cooksley@jpmchase.com>
2023-08-17 18:11:49 +0100

    delete console log

--- site/src/components/example-container/ExampleContainer.tsx
+++ site/src/components/example-container/ExampleContainer.tsx
@@ -41,7 +41,6 @@ export const ExampleContainer: FC<ExampleContainerProps> = ({
   customPillText,
   className,
 }) => {
-  console.log({ customPillText });
   return (
     <div
       className={clsx(
