61679098e50bb43f18bb20834a3e2cdc07a47b3a
R662301 <lily.cooksley@jpmchase.com>
2023-08-22 17:28:13 +0100

    add sizes example to avatar doc

---
+++ site/src/examples/avatar/Sizes.tsx
@@ -0,0 +1,17 @@
+import { Avatar, FlowLayout, Label, StackLayout } from "@salt-ds/core";
+import { ReactElement } from "react";
+
+const sizes = [1, 2, 3, 4] as const;
+
+export const Sizes = (): ReactElement => {
+    return (
+      <FlowLayout gap={7} align="end">
+        {sizes.map((size) => (
+          <StackLayout key={size} align="center">
+            <Avatar key={size} size={size} />
+            <Label>size: {size}</Label>
+          </StackLayout>
+        ))}
+      </FlowLayout>
+    );
+  };
