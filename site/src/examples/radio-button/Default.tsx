0aa2672e5f25dfb647aefd6576430632f9930175
Felicia Chong <felckh@gmail.com>
2023-08-22 17:14:14 +0100

    update default direction to vertical

--- site/src/examples/radio-button/Default.tsx
+++ site/src/examples/radio-button/Default.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from "react";
 import { RadioButton, RadioButtonGroup } from "@salt-ds/core";
 
 export const Default = (): ReactElement => (
-  <RadioButtonGroup direction={"horizontal"}>
+  <RadioButtonGroup>
     <RadioButton key="option1" label="NAMR" value="option1" />
     <RadioButton key="option2" label="APAC" value="option2" />
     <RadioButton key="option3" label="EMEA" value="option3" />
