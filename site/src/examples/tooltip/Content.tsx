28970c6578a3611e700249008c5f3bd26372caa2
Alina Visan <alycry.alina@gmail.com>
2023-08-24 14:47:31 +0300

    tooltip docs to site

---
+++ site/src/examples/tooltip/Content.tsx
@@ -0,0 +1,21 @@
+import { ReactElement } from "react";
+import { Button, Text, Tooltip } from "@salt-ds/core";
+
+export const Content = (): ReactElement => (
+  <Tooltip
+    content={
+      <>
+        <Text styleAs="h3">Persona B</Text>
+        <Text styleAs="h4">personab@example.com</Text>
+        <ul style={{ paddingLeft: 20, margin: 0 }}>
+          <li>Role</li>
+          <li>Position</li>
+          <li>Location</li>
+          <li>City, Country</li>
+        </ul>
+      </>
+    }
+  >
+    <Button>Custom Content</Button>
+  </Tooltip>
+);
