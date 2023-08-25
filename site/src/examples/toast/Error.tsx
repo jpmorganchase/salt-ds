f75021e05f62e946a5d436495556923c351d72dc
Alina Visan <alycry.alina@gmail.com>
2023-08-25 17:26:29 +0300

    cleanup

--- site/src/examples/toast/Error.tsx
+++ site/src/examples/toast/Error.tsx
@@ -1,39 +1,19 @@
 import { ReactElement } from "react";
-import { Button, FlowLayout, Text, Toast, ToastContent } from "@salt-ds/core";
+import { Button, Text, Toast, ToastContent } from "@salt-ds/core";
 import { CloseIcon } from "@salt-ds/icons";
 
 export const Error = (): ReactElement => (
-  <div style={{ width: 260 }}>
-    <Toast status="error">
-      <ToastContent>
-        <div>
-          <Text>
-            <strong>System error</strong>
-          </Text>
-          <div>Connection timed out. Failed to retrieve data. </div>
-        </div>
-      </ToastContent>
-      <Button variant="secondary">
-        <CloseIcon />
-      </Button>
-    </Toast>
-    {/* <Toast status="error">
-      <ToastContent>
-        <div>
-          <Text>
-            <strong>System error</strong>
-          </Text>
-          <div>Connection timed out. Failed to retrieve data. </div>
-        </div>
-        <FlowLayout
-          gap={1}
-          justify="end"
-          style={{ marginTop: "var(--salt-spacing-100)" }}
-        >
-          <Button>Dismiss</Button>
-          <Button variant="cta">Try again</Button>
-        </FlowLayout>
-      </ToastContent>
-    </Toast> */}
-  </div>
+  <Toast status="error" style={{ width: 260 }}>
+    <ToastContent>
+      <div>
+        <Text>
+          <strong>System error</strong>
+        </Text>
+        <div>Connection timed out. Failed to retrieve data. </div>
+      </div>
+    </ToastContent>
+    <Button variant="secondary">
+      <CloseIcon />
+    </Button>
+  </Toast>
 );
--- site/src/examples/toast/Warning.tsx
+++ site/src/examples/toast/Warning.tsx
@@ -1,37 +1,19 @@
 import { ReactElement } from "react";
-import { Button, FlowLayout, Text, Toast, ToastContent } from "@salt-ds/core";
+import { Button, Text, Toast, ToastContent } from "@salt-ds/core";
 import { CloseIcon } from "@salt-ds/icons";
 
 export const Warning = (): ReactElement => (
-  <div style={{ width: 260 }}>
-    <Toast status="warning">
-      <ToastContent>
-        <div>
-          <Text>
-            <strong>File access</strong>
-          </Text>
-          <div>Viewers of this file can see comments and suggestions. </div>
-        </div>
-      </ToastContent>
-      <Button variant="secondary">
-        <CloseIcon />
-      </Button>
-    </Toast>
-    {/* <Toast status="warning">
-      <ToastContent>
-        <div>
-          <Text>
-            <strong>File access</strong>
-          </Text>
-          <div>Viewers of this file can see comments and suggestions. </div>
-        </div>
-        <FlowLayout gap={1} style={{ marginTop: "var(--salt-spacing-100)" }}>
-          <Button variant="cta" style={{ width: "100%" }}>
-            Edit permissions
-          </Button>
-          <Button style={{ width: "100%" }}>Dismiss</Button>
-        </FlowLayout>
-      </ToastContent>
-    </Toast> */}
-  </div>
+  <Toast status="warning" style={{ width: 260 }}>
+    <ToastContent>
+      <div>
+        <Text>
+          <strong>File access</strong>
+        </Text>
+        <div>Viewers of this file can see comments and suggestions. </div>
+      </div>
+    </ToastContent>
+    <Button variant="secondary">
+      <CloseIcon />
+    </Button>
+  </Toast>
 );
