db0ea0d846ebebe898fa4aa0ea34daa230369d8a
Felicia Chong <felckh@gmail.com>
2023-08-25 15:46:35 +0100

    add Switch docs to site

---
+++ site/src/examples/switch/Default.tsx
@@ -0,0 +1,4 @@
+import { ReactElement } from "react";
+import { Switch } from "@salt-ds/lab";
+
+export const Default = (): ReactElement => <Switch label="Default" />;
---
+++ site/src/examples/switch/Disabled.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from "react";
+import { Switch } from "@salt-ds/lab";
+
+export const Disabled = (): ReactElement => (
+  <Switch label="Disabled" disabled />
+);
