2bef54ad803262f70ea844accfd57b09691eb9b7
David Miller <d.miller01@gmail.com>
2023-08-25 16:01:08 +0100

    remove click handler from disabled pill

--- site/src/examples/pill/Disabled.tsx
+++ site/src/examples/pill/Disabled.tsx
@@ -1,12 +1,6 @@
 import { ReactElement } from "react";
 import { PillNext } from "@salt-ds/lab";
 
-const handleClick = () => {
-  console.log("clicked");
-};
-
 export const Disabled = (): ReactElement => (
-  <PillNext disabled onClick={handleClick}>
-    Disabled Pill
-  </PillNext>
+  <PillNext disabled>Disabled Pill</PillNext>
 );
