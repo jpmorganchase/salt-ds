31280de6fb276cf27de384e11e87f1e19c02be6d
lilyvc <45168453+lilyvc@users.noreply.github.com>
2023-08-25 15:37:38 +0000

    add examples

---
+++ site/src/examples/dialog/Scroll.tsx
@@ -0,0 +1,100 @@
+import { ReactElement, useState } from "react";
+import { Button, StackLayout } from "@salt-ds/core";
+import {
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogCloseButton,
+} from "@salt-ds/lab";
+
+export const Scroll = (): ReactElement => {
+  const [open, setOpen] = useState(true);
+
+  const handleRequestOpen = () => {
+    setOpen(true);
+  };
+
+  const onOpenChange = (value: boolean) => {
+    setOpen(value);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button data-testid="dialog-button" onClick={handleRequestOpen}>
+        Open default dialog
+      </Button>
+      <Dialog style={{ width: 500 }} open={open} onOpenChange={onOpenChange}>
+        <DialogTitle>Congratulations! You have created a Dialog.</DialogTitle>
+        <DialogContent>
+          {" "}
+          <StackLayout>
+            <div>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </div>
+            <div>
+              It has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </div>
+            <div>
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsum is that it has a more-or-less normal
+              distribution of letters, as opposed to using 'Content here,
+              content here', making it look like readable English.
+            </div>
+            <div>
+              Many desktop publishing packages and web page editors now use
+              Lorem Ipsum as their default model text, and a search for 'lorem
+              ipsum' will uncover many web sites still in their infancy. Various
+              versions have evolved over the years, sometimes by accident,
+              sometimes on purpose (injected humour and the like).
+            </div>
+            <div>
+              Contrary to popular belief, Lorem Ipsum is not simply random text.
+              It has roots in a piece of classical Latin literature from 45 BC,
+              making it over 2000 years old. Richard McClintock, a Latin
+              professor at Hampden-Sydney College in Virginia, looked up one of
+              the more obscure Latin words, consectetur, from a Lorem Ipsum
+              passage, and going through the cites of the word in classical
+              literature, discovered the undoubtable source.
+            </div>
+            <div>
+              Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus
+              Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero,
+              written in 45 BC. This book is a treatise on the theory of ethics,
+              very popular during the Renaissance. The first line of Lorem
+              Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
+              section 1.10.32.
+            </div>
+          </StackLayout>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            style={{ marginRight: "auto" }}
+            variant="secondary"
+            onClick={handleClose}
+          >
+            Cancel
+          </Button>
+          <Button onClick={handleClose}>Previous</Button>
+          <Button variant="cta" onClick={handleClose}>
+            Next
+          </Button>
+        </DialogActions>
+        <DialogCloseButton onClick={handleClose} />
+      </Dialog>
+    </>
+  );
+};
