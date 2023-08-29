3dcbe849cd1dbededefb93bc98a7a0eb0489816a
David Miller <d.miller01@gmail.com>
2023-08-29 11:17:46 +0100

    update icon docs 1

--- site/src/examples/icon/AllIcons.tsx
+++ site/src/examples/icon/AllIcons.tsx
@@ -13,12 +13,9 @@ export const AllIcons = (): ReactElement => {
   const [inputText, setInputText] = useState("");
 
   return (
-    <StackLayout
-      style={{ padding: 10, height: "100%", width: "100%" }}
-      align="stretch"
-    >
+    <StackLayout style={{ padding: 10, height: "100%", width: "100%" }}>
       <FormField>
-        <FormFieldLabel>Search Icons</FormFieldLabel>
+        <FormFieldLabel>Search icons</FormFieldLabel>
         <Input
           value={inputText}
           inputProps={{
@@ -28,18 +25,25 @@ export const AllIcons = (): ReactElement => {
           }}
         />
       </FormField>
-      <FlowLayout gap={2}>
-        {Object.entries(allIcons)
-          .filter(([name, Icon]) => new RegExp(inputText, "i").test(name))
-          .map(([name, Icon]) => {
-            return (
-              <StackLayout style={{ width: "150px" }} gap={1} align="center">
-                <Icon size={2} />
-                <Text>{name}</Text>
-              </StackLayout>
-            );
-          })}
-      </FlowLayout>
+      <div style={{ overflow: "auto", maxHeight: 300 }}>
+        <FlowLayout gap={3}>
+          {Object.entries(allIcons)
+            .filter(([name, Icon]) => new RegExp(inputText, "i").test(name))
+            .map(([name, Icon]) => {
+              return (
+                <StackLayout
+                  style={{ width: 140 }}
+                  gap={1}
+                  align="center"
+                  key={name}
+                >
+                  <Icon size={2} />
+                  <Text>{name}</Text>
+                </StackLayout>
+              );
+            })}
+        </FlowLayout>
+      </div>
     </StackLayout>
   );
 };
