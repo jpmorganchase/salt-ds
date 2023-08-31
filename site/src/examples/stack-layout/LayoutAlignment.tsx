0b375c337d6e73712587442952fac3869848fec9
Joana Moreira <31695437+JoanaMMoreira@users.noreply.github.com>
2023-08-31 13:36:40 +0100

    Stack layout site docs (#2320)
    Co-authored-by: feliciackh <54270624+feliciackh@users.noreply.github.com>

---
+++ site/src/examples/stack-layout/LayoutAlignment.tsx
@@ -0,0 +1,59 @@
+import { ReactElement, useState, ChangeEventHandler } from "react";
+import {
+  StackLayout,
+  FlexItem,
+  FLEX_ALIGNMENT_BASE as flexAlignment,
+  FormField,
+  FormFieldLabel,
+  RadioButtonGroup,
+  RadioButton,
+} from "@salt-ds/core";
+import styles from "./index.module.css";
+import layoutAlignmentStyles from "./LayoutAlignment.module.css";
+
+type FlexAlignment = typeof flexAlignment[number];
+type FlexAlign = FlexAlignment | "stretch" | "baseline";
+
+const FlexAlignmentOptions = [...flexAlignment, "stretch", "baseline"];
+
+export const LayoutAlignment = (): ReactElement => {
+  const [align, setAlign] = useState<FlexAlign>("center");
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const { value } = event.target;
+    setAlign(value as FlexAlign);
+  };
+
+  return (
+    <div className={layoutAlignmentStyles.container}>
+      <StackLayout align={align}>
+        {Array.from({ length: 4 }, (_, index) => (
+          <FlexItem key={index} className={styles.flexItem}>
+            {index + 1}
+          </FlexItem>
+        ))}
+      </StackLayout>
+
+      <FormField>
+        <FormFieldLabel>Align</FormFieldLabel>
+        <RadioButtonGroup
+          direction={"horizontal"}
+          aria-label="Align Controls"
+          name="align"
+          onChange={handleChange}
+          value={align}
+        >
+          {FlexAlignmentOptions.map((alignment) => (
+            <RadioButton
+              key={alignment}
+              label={`${alignment.charAt(0).toUpperCase()}${alignment.slice(
+                1
+              )}`}
+              value={alignment}
+            />
+          ))}
+        </RadioButtonGroup>
+      </FormField>
+    </div>
+  );
+};
