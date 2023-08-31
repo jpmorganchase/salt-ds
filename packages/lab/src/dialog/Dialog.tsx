2fa9f367f26afa2beb340ad0074ffb310a6e69f9
R662301 <lily.cooksley@jpmchase.com>
2023-08-31 14:51:56 +0100

    add SaltProvider to Dialog

--- packages/lab/src/dialog/Dialog.tsx
+++ packages/lab/src/dialog/Dialog.tsx
@@ -14,6 +14,7 @@ import {
 } from "@floating-ui/react";
 import {
   makePrefixer,
+  SaltProvider,
   useForkRef,
   useId,
   ValidationStatus,
@@ -90,6 +91,7 @@ export const Dialog = forwardRef<HTMLDivElement, DialogProps>(function Dialog(
 
   return (
     <FloatingPortal>
+      <SaltProvider>
       {showComponent && (
         <FloatingOverlay className={withBaseName("overlay")} lockScroll>
           <FloatingFocusManager
@@ -128,6 +130,7 @@ export const Dialog = forwardRef<HTMLDivElement, DialogProps>(function Dialog(
           </FloatingFocusManager>
         </FloatingOverlay>
       )}
+      </SaltProvider>
     </FloatingPortal>
   );
 });
