f6a06c7cfd68e9be72c20a56fdc92c322afbcb6e
lilyvc <45168453+lilyvc@users.noreply.github.com>
2023-09-05 13:51:23 +0100

    Dialog docs to site (#2306)
    Co-authored-by: Liberty Curtis <91206958+libertymayc@users.noreply.github.com>
    Co-authored-by: Zarko Pernar <zarko.pernar@gmail.com>

---
+++ site/src/examples/dialog/Success.tsx
@@ -0,0 +1,50 @@
+import { ReactElement, useState } from "react";
+import { Button } from "@salt-ds/core";
+import {
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+} from "@salt-ds/lab";
+
+export const Success = (): ReactElement => {
+  const [open, setOpen] = useState(false);
+
+  const handleRequestOpen = () => {
+    setOpen(true);
+  };
+
+  const onOpenChange = (value: boolean) => {
+    setOpen(value);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button data-testid="dialog-button" onClick={handleRequestOpen}>
+        Open success dialog
+      </Button>
+      <Dialog
+        style={{ width: 500 }}
+        open={open}
+        onOpenChange={onOpenChange}
+        role="alertdialog"
+        status="success"
+      >
+        <DialogTitle>File uploaded</DialogTitle>
+        <DialogContent>
+          File has been successfully uploaded to the shared drive.
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+          <Button variant="cta" onClick={handleClose}>
+            Go to file
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
