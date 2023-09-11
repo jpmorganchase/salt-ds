d40d8cd2a5aa296587e073b75a9a54cedef21170
R662301 <lily.cooksley@jpmchase.com>
2023-09-11 12:41:35 +0100

    revert changes to example

--- site/src/examples/dropdown/Default.tsx
+++ site/src/examples/dropdown/Default.tsx
@@ -2,10 +2,4 @@ import { ReactElement, SyntheticEvent } from "react";
 import { DropdownNext } from "@salt-ds/lab";
 import { StateNames } from "./exampleData";
 
-const handleSelect = (_event: SyntheticEvent, data: { value: string }) => {
-  console.log("selected item", data.value);
-};
-
-export const DefaultSelected = (): ReactElement => (
-  <DropdownNext source={StateNames} onSelect={handleSelect} />
-);
+export const Default = (): ReactElement => <DropdownNext source={StateNames} />;
