36d3c153dcd4ff61bb07c69a1f234b824bab5b5d
lilyvc <45168453+lilyvc@users.noreply.github.com>
2023-09-11 10:09:23 +0000

    Add onSelect prop to DropdownNext

--- site/src/examples/dropdown/Default.tsx
+++ site/src/examples/dropdown/Default.tsx
@@ -1,5 +1,11 @@
-import { ReactElement } from "react";
+import { ReactElement, SyntheticEvent } from "react";
 import { DropdownNext } from "@salt-ds/lab";
 import { StateNames } from "./exampleData";
 
-export const Default = (): ReactElement => <DropdownNext source={StateNames} />;
+const handleSelect = (_event: SyntheticEvent, data: { value: string }) => {
+  console.log("selected item", data.value);
+};
+
+export const DefaultSelected = (): ReactElement => (
+  <DropdownNext source={StateNames} onSelect={handleSelect} />
+);
--- site/src/examples/dropdown/DefaultSelected.tsx
+++ site/src/examples/dropdown/DefaultSelected.tsx
@@ -1,7 +1,15 @@
-import { ReactElement } from "react";
+import { ReactElement, SyntheticEvent } from "react";
 import { DropdownNext } from "@salt-ds/lab";
 import { StateNames } from "./exampleData";
 
+const handleSelect = (_event: SyntheticEvent, data: { value: string }) => {
+  console.log("selected item", data.value);
+};
+
 export const DefaultSelected = (): ReactElement => (
-  <DropdownNext source={StateNames} defaultSelected="California" />
+  <DropdownNext
+    source={StateNames}
+    defaultSelected="California"
+    onSelect={handleSelect}
+  />
 );
