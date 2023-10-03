21c1ae6a5d5ccb6bbc4cf663eb6626ec85e32cbb
Lily Cooksley <lilycooksley@gmail.com>
2023-10-03 14:13:59 +0100

    add closing tag

--- site/src/examples/link/OpenInANewTab.tsx
+++ site/src/examples/link/OpenInANewTab.tsx
@@ -4,34 +4,32 @@ import { GithubIcon } from "@salt-ds/icons";
 
 export const OpenInANewTab = (): ReactElement => (
   <StackLayout>
- <p>
-    Using default icon:
-    {" "}
-    <Link
-      href="https://www.saltdesignsystem.com"
-      target="_blank"
-      rel="noopener"
-    >
-      Open the Salt website in a new tab
-    </Link>
-  </p>
-  <p>
-    Using a custom icon:
-    {" "}
-    <Link
-      href="https://github.com/jpmorganchase/salt-ds"
-      target="_blank"
-      rel="noopener"
-      IconComponent={GithubIcon}
-    >
-      Open the Salt GitHub repository in a new tab
-    </Link>
-  </p>
-  <p>
-    With the icon removed:
-    {" "}
-    <Link href="" target="_blank" rel="noopener" IconComponent={null}>
-      Open this page in a new tab
-    </Link>
+    <p>
+      Using default icon:{" "}
+      <Link
+        href="https://www.saltdesignsystem.com"
+        target="_blank"
+        rel="noopener"
+      >
+        Open the Salt website in a new tab
+      </Link>
+    </p>
+    <p>
+      Using a custom icon:{" "}
+      <Link
+        href="https://github.com/jpmorganchase/salt-ds"
+        target="_blank"
+        rel="noopener"
+        IconComponent={GithubIcon}
+      >
+        Open the Salt GitHub repository in a new tab
+      </Link>
+    </p>
+    <p>
+      With the icon removed:{" "}
+      <Link href="" target="_blank" rel="noopener" IconComponent={null}>
+        Open this page in a new tab
+      </Link>
+    </p>
   </StackLayout>
 );
