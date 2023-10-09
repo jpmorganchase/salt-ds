68a51b5d2d81c3de1efba04e8da9064a471ce507
Fernanda Castillo <fersitacastillo123@gmail.com>
2023-10-09 13:01:23 +0100

    add examples to nav item docs

---
+++ site/src/examples/navigation-item/WithNestedItems.tsx
@@ -0,0 +1,40 @@
+import { ReactElement, useState } from "react";
+import { NavigationItem } from "@salt-ds/lab";
+import { FlexLayout } from "@salt-ds/core";
+
+export const WithNestedItems = (): ReactElement => {
+  const [horizontalActive, setHorizontalActive] = useState(false);
+
+  const [verticalActive, setVerticalActive] = useState(false);
+
+  const handleHorizontalActiveToggle = () => {
+    setHorizontalActive((current) => !current);
+  };
+
+  const handleVerticalActiveToggle = () => {
+    setVerticalActive((current) => !current);
+  };
+
+  return (
+    <FlexLayout align="center">
+      <NavigationItem
+        active={horizontalActive}
+        onExpand={handleHorizontalActiveToggle}
+        href="#"
+        parent
+      >
+        Label
+      </NavigationItem>
+
+      <NavigationItem
+        active={verticalActive}
+        onExpand={handleVerticalActiveToggle}
+        href="#"
+        orientation="vertical"
+        parent
+      >
+        Label
+      </NavigationItem>
+    </FlexLayout>
+  );
+};
