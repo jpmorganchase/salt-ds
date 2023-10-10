c8b7d6b95702dca3a472e8be6adf515470420ece
R662301 <lily.cooksley@jpmchase.com>
2023-10-10 16:33:57 +0100

    HD spinner svg

--- packages/core/src/spinner/svgSpinners/SpinnerSVG.tsx
+++ packages/core/src/spinner/svgSpinners/SpinnerSVG.tsx
@@ -19,27 +19,15 @@ export const SpinnerSVG = (props: {
       {...rest}
     >
       <defs>
-        <linearGradient id={`${id}-1`} x1="100%" x2="0%" y1="78%" y2="78%">
-          <stop className={withBaseName("gradientStop")} offset="0%" />
-          <stop className={withBaseName("gradientStop")} offset="100%" />
+        {/* There is no gradient here */}
+        <linearGradient id={`${id}-1`} x1="0%" y1="0%" x2="100%" y2="100%">
+          <stop className={withBaseName("gradientStop")} />
         </linearGradient>
 
-        <linearGradient
-          id={`${id}-2`}
-          gradientUnits="userSpaceOnUse"
-          x1="1"
-          y1="14"
-          x2="27"
-          y2="14"
-        >
+        <linearGradient id={`${id}-2`} x1="0" y1="0" x2="100%" y2="0">
           <stop
             className={withBaseName("gradientStop")}
-            offset="2%"
-            stopOpacity="0"
-          />
-          <stop
-            className={withBaseName("gradientStop")}
-            offset="50%"
+            offset="0%"
             stopOpacity="1"
           />
           <stop
@@ -50,14 +38,21 @@ export const SpinnerSVG = (props: {
         </linearGradient>
       </defs>
       <g fill="none" fillRule="evenodd">
+        {/* This first path draws the top half of the circle, with block color/ no gradient*/}
         <path
-          d="M14,1 a13,13 0 1,1 0,26"
+          // adjusted for bigger radius
+          d="M26.7,14 a12.7,12.7 0 1,0 -25.4,0"
+          // for radius of 1.4 to fit exactly
+          // d="M27.3,14 a13.3,13.3 0 1,0 -26.6,0"
           stroke={`url(#${id}-1)`}
           strokeWidth="1.4"
           fill="none"
         />
+        {/* This second path draws the left half of the circle with a gradient that is opaque on the left and transparent at the end */}
+        {/* TODO: make it only 1/4 by chaning end position */}
         <path
-          d="M1,14 a13,13 0 0,1 26,0"
+          d="M14,1.3 a12.7,12.7 0 1,0 0,25.4"
+          // d="M14,0.7 a13.3,13.3 0 1,0 0,26.6"
           stroke={`url(#${id}-2)`}
           strokeWidth="1.4"
           fill="none"
