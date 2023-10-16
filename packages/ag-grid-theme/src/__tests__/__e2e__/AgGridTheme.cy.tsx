712316db111b1683c0960591c0433e850dad1fbc
Alina Visan <alycry.alina@gmail.com>
2023-10-16 13:37:20 +0300

    re-structure folders

---
+++ packages/ag-grid-theme/src/__tests__/__e2e__/AgGridTheme.cy.tsx
@@ -0,0 +1,16 @@
+import { composeStories } from "@storybook/react";
+import * as agGridStories from "@stories/ag-grid-theme.stories";
+
+const { BasicGrid } = composeStories(agGridStories);
+
+describe("Given Ag Grid Theme", () => {
+  describe("WHEN the Default dtory is mounted", () => {
+
+    console.log('agGridStories', agGridStories);
+    
+    it("should match screenshot", () => {
+      cy.mount(<BasicGrid />);
+      cy.screenshot()
+    });
+  });
+});
