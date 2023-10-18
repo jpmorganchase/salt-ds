1adde6945b9086e2f8c7e66ed475715342de3fed
Alina Visan <alycry.alina@gmail.com>
2023-10-18 18:25:09 +0300

    updates

--- packages/ag-grid-theme/src/__tests__/__e2e__/AgGridTheme.cy.tsx
+++ packages/ag-grid-theme/src/__tests__/__e2e__/AgGridTheme.cy.tsx
@@ -38,7 +38,11 @@ describe("Given Ag Grid Theme", () => {
     it.skip("THEN should match screenshot", () => {
       cy.mount(<BasicGrid />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
     describe("WHEN column menu is open", () => {
       describe("AND general tab is selected", () => {
@@ -73,7 +77,11 @@ describe("Given Ag Grid Theme", () => {
     it.skip("THEN should match screenshot", () => {
       cy.mount(<CheckboxSelection />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
     describe("WHEN editable-cell is focused", () => {
       it("THEN should match screenshot", () => {
@@ -111,28 +119,44 @@ describe("Given Ag Grid Theme", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<Coloration />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the ColumnGroup story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<ColumnGroup />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the ColumnSpanning story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<ColumnSpanning />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe("WHEN the ContextMenu story is mounted", () => {
     it.skip("THEN should match screenshot", () => {
       cy.mount(<ContextMenu />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
     describe("WHEN cell context menu is open", () => {
       it("THEN should match screenshot", () => {
@@ -147,7 +171,11 @@ describe("Given Ag Grid Theme", () => {
     it.skip("THEN should match screenshot", () => {
       cy.mount(<CustomFilter />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
     describe("WHEN custom filter is focused", () => {
       it("THEN should match screenshot", () => {
@@ -164,28 +192,44 @@ describe("Given Ag Grid Theme", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<DragRowOrder />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the Icons story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<Icons />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the FloatingFilter story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<FloatingFilter />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe("WHEN the HDCompact story is mounted", () => {
     it.skip("THEN should match screenshot", () => {
       cy.mount(<HDCompact />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
     describe("WHEN column menu is open", () => {
       describe("AND general tab is selected", () => {
@@ -221,7 +265,11 @@ describe("Given Ag Grid Theme", () => {
     it.skip("THEN should match screenshot", () => {
       cy.mount(<HDCompactDark />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
     describe("WHEN column menu is open", () => {
       describe("AND general tab is selected", () => {
@@ -255,119 +303,187 @@ describe("Given Ag Grid Theme", () => {
   describe.skip("WHEN the InfiniteScroll story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<InfiniteScroll />);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the LoadingOverlay story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<LoadingOverlay />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the MasterDetail story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<MasterDetail />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the MasterDetailDark story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<MasterDetailDark />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the NoDataOverlay story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<NoDataOverlay />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the Pagination story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<Pagination />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the ParentChildRows story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<ParentChildRows />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the RowGrouping story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<RowGrouping />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the RowGroupPanel story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<RowGroupPanel />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the PinnedRows story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<PinnedRows />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the StatusBar story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<StatusBar />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the StatusBarDark story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<StatusBarDark />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the VariantSecondary story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<VariantSecondary />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the VariantSecondaryDark story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<VariantSecondaryDark />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the VariantZebra story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<VariantZebra />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the VariantZebraDark story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<VariantZebraDark />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
   describe.skip("WHEN the WrappedHeader story is mounted", () => {
     it("THEN should match screenshot", () => {
       cy.mount(<WrappedHeader />);
       cy.wait(500);
-      cy.matchImage();
+      cy.matchImage({
+        forceDeviceScaleFactor: false,
+        maxDiffThreshold: 0.1,
+        updateImages: true,
+      });
     });
   });
 });
