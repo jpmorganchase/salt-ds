4db79a877eaa4b9957a8b34f85c9a2c334b01290
Alina Visan <alycry.alina@gmail.com>
2023-10-18 15:22:47 +0300

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -61,7 +61,7 @@ export default defineConfig({
   viewportHeight: 1024,
   video: false,
   env: {
-    pluginVisualRegressionDiffConfig: { threshold: 0.1 }
+    pluginVisualRegressionDiffConfig: { threshold: 0.1 },
   },
   e2e: {
     setupNodeEvents(on, config) {
