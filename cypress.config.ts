5e87c2cc06ee2bfe8d44456a03a48dbcb7c40c49
Alina Visan <alycry.alina@gmail.com>
2023-10-20 22:27:34 +0300

    clean

--- cypress.config.ts
+++ cypress.config.ts
@@ -72,65 +72,7 @@ export default defineConfig({
           return null;
         },
       });
-      // let's increase the browser window size when running headlessly
-      // this will produce higher resolution images and videos
-      // https://on.cypress.io/browser-launch-api
-      on(
-        "before:browser:launch",
-        (
-          browser = {
-            name: "",
-            family: "chromium",
-            channel: "",
-            displayName: "",
-            version: "",
-            majorVersion: "",
-            path: "",
-            isHeaded: false,
-            isHeadless: false,
-          },
-          launchOptions
-        ) => {
-          console.log(
-            "launching browser %s is headless? %s",
-            browser.name,
-            browser.isHeadless
-          );
-
-          // the browser width and height we want to get
-          // our screenshots and videos will be of that resolution
-          const width = 1280;
-          const height = 1024;
-
-          console.log(
-            "setting the browser window size to %d x %d",
-            width,
-            height
-          );
-
-          if (browser.name === "chrome" && browser.isHeadless) {
-            launchOptions.args.push(`--window-size=${width},${height}`);
-
-            // force screen to be non-retina and just use our given resolution
-            launchOptions.args.push("--force-device-scale-factor=1");
-          }
-
-          if (browser.name === "electron" && browser.isHeadless) {
-            // might not work on CI for some reason
-            launchOptions.preferences.width = width;
-            launchOptions.preferences.height = height;
-            launchOptions.preferences.resizable = false;
-          }
-
-          if (browser.name === "firefox" && browser.isHeadless) {
-            launchOptions.args.push(`--width=${width}`);
-            launchOptions.args.push(`--height=${height}`);
-          }
-
-          // IMPORTANT: return the updated browser launch options
-          return launchOptions;
-        }
-      );
+    
       return config;
     },
     devServer: {
