2ddb02ae3ed93807909dba22c826e0183764467c
Alina Visan <alycry.alina@gmail.com>
2023-10-23 22:09:22 +0300

    updates

--- packages/ag-grid-theme/scripts/build.mjs
+++ packages/ag-grid-theme/scripts/build.mjs
@@ -4,7 +4,7 @@ import fs from "fs-extra";
 import { fileURLToPath, pathToFileURL } from "url";
 import { createRequire } from "module";
 import { deleteSync } from "del";
-import sass from "sass";
+import * as sass from "sass";
 import postcss from "postcss";
 import cssnano from "cssnano";
 import url from "postcss-url";
