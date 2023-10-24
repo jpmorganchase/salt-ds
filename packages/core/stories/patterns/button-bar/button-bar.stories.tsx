ae95d2a799258822fc90bbb02def25bb3888b911
R662301 <lily.cooksley@jpmchase.com>
2023-10-24 14:14:40 +0100

    button bar examples

--- packages/core/stories/patterns/button-bar/button-bar.stories.tsx
+++ packages/core/stories/patterns/button-bar/button-bar.stories.tsx
@@ -1,4 +1,12 @@
-import { Button, FlexItem, FlowLayout, SplitLayout } from "@salt-ds/core";
+import { Button, FlexItem, FlexLayout, SplitLayout } from "@salt-ds/core";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@salt-ds/lab";
+import { ExportIcon, ImportIcon } from "@salt-ds/icons";
+
 import { Meta } from "@storybook/react";
 
 export default {
@@ -7,41 +15,165 @@ export default {
 
 export const ButtonBar = () => {
   return (
-    <SplitLayout direction="row">
-      <FlexItem>
-        <Button variant="cta">Save</Button>
-        <Button>Cancel</Button>
-      </FlexItem>
-      <FlexItem>
-        <Button variant="secondary">Delete</Button>
-      </FlexItem>
-    </SplitLayout>
+    <div style={{ width: "50vw" }}>
+      <FlexLayout justify="end" style={{ width: "100%" }} gap={1}>
+        <FlexItem>
+          <Button>Cancel</Button>
+        </FlexItem>
+        <FlexItem>
+          <Button variant="cta">Save</Button>
+        </FlexItem>
+      </FlexLayout>
+    </div>
   );
 };
 
 export const WithSecondary = () => {
   const startItem = (
-    <FlowLayout>
+    <FlexLayout gap={1}>
       <FlexItem>
-        <Button variant="secondary">Start/secondary</Button>
+        <ExportIcon />
+        <Button variant="secondary">Export</Button>
       </FlexItem>
-    </FlowLayout>
+      <FlexItem>
+        <ImportIcon />
+        <Button variant="secondary">Import</Button>
+      </FlexItem>
+    </FlexLayout>
   );
 
   const endItem = (
-    <FlowLayout>
+    <FlexLayout gap={1}>
       <FlexItem>
-        <Button>End/primary</Button>
+        <Button>Cancel</Button>
       </FlexItem>
-    </FlowLayout>
+      <FlexItem>
+        <Button variant="cta">Save</Button>
+      </FlexItem>
+    </FlexLayout>
   );
 
   return (
-    <SplitLayout
-      startItem={startItem}
-      endItem={endItem}
-      style={{minWidth: '30vw'}}
-      direction={{ xs: "column", sm: "row" }}
-    />
+    <div style={{ width: "50vw" }}>
+      <SplitLayout
+        startItem={startItem}
+        endItem={endItem}
+        style={{ width: "100%" }}
+      />
+    </div>
+  );
+};
+
+export const FullPage = () => {
+  const startItem = (
+    <FlexLayout gap={1}>
+      <FlexItem>
+        <Button>Previous</Button>
+      </FlexItem>
+      <FlexItem>
+        <Button variant="cta">Continue</Button>
+      </FlexItem>
+    </FlexLayout>
+  );
+
+  const endItem = (
+    <FlexLayout gap={1}>
+      <FlexItem>
+        <Button variant="secondary">Upload file</Button>
+      </FlexItem>
+    </FlexLayout>
+  );
+
+  return (
+    <div style={{ width: "50vw" }}>
+      <SplitLayout
+        startItem={startItem}
+        endItem={endItem}
+        style={{ width: "100%" }}
+      />
+    </div>
+  );
+};
+
+export const DestructiveActions = () => {
+  const startItem = (
+    <FlexLayout gap={1}>
+      <FlexItem>
+        <Button variant="secondary">Delete</Button>
+      </FlexItem>
+    </FlexLayout>
+  );
+
+  const endItem = (
+    <FlexLayout gap={1}>
+      <FlexItem>
+        <Button>Cancel</Button>
+      </FlexItem>
+      <FlexItem>
+        <Button variant="cta">Save changes</Button>
+      </FlexItem>
+    </FlexLayout>
+  );
+
+  return (
+    <div style={{ width: "50vw" }}>
+      <SplitLayout
+        startItem={startItem}
+        endItem={endItem}
+        style={{ width: "100%" }}
+      />
+    </div>
+  );
+};
+
+// stacks the wrong way
+export const Stacked = () => {
+  const startItem = (
+    <FlexLayout gap={1} direction={{ xs: "column", sm: "row" }}>
+      <FlexItem>
+        <Button variant="secondary">Secondary</Button>
+      </FlexItem>
+    </FlexLayout>
+  );
+
+  const endItem = (
+    <FlexLayout gap={1} direction={{ xs: "column", sm: "row" }}>
+      <FlexItem>
+        <Button>Primary</Button>
+      </FlexItem>
+      <FlexItem>
+        <Button variant="cta">CTA</Button>
+      </FlexItem>
+    </FlexLayout>
+  );
+  return (
+    <div style={{ width: "50vw" }}>
+      <SplitLayout
+        startItem={startItem}
+        endItem={endItem}
+        style={{ width: "100%" }}
+        gap={1}
+        // stacks at breakpoint xs
+        direction={{ xs: "column", sm: "row" }}
+      />
+    </div>
+  );
+};
+
+export const InDialog = () => {
+  return (
+    <Dialog
+      style={{
+        width: 500,
+      }}
+      role="alertdialog"
+    >
+      <DialogTitle>Info</DialogTitle>
+      <DialogContent>This is the content of the dialog.</DialogContent>
+      <DialogActions>
+        <Button>Cancel</Button>
+        <Button variant="cta">Ok</Button>
+      </DialogActions>
+    </Dialog>
   );
 };
