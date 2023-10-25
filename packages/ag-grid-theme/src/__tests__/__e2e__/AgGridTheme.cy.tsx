600a3ab218688286168794c2b88d01c6e6cc19a3
Alina Visan <alycry.alina@gmail.com>
2023-10-25 16:48:09 +0300

    updates

--- packages/ag-grid-theme/src/__tests__/__e2e__/AgGridTheme.cy.tsx
+++ packages/ag-grid-theme/src/__tests__/__e2e__/AgGridTheme.cy.tsx
@@ -4,42 +4,14 @@ import * as agGridStories from "@stories/ag-grid-theme.stories";
 const {
   BasicGrid,
   CheckboxSelection,
-  Coloration,
-  ColumnGroup,
-  ColumnSpanning,
   ContextMenu,
   CustomFilter,
-  DragRowOrder,
-  Icons,
-  FloatingFilter,
   HDCompact,
   HDCompactDark,
-  InfiniteScroll,
-  LoadingOverlay,
-  MasterDetail,
-  MasterDetailDark,
-  NoDataOverlay,
-  Pagination,
-  ParentChildRows,
-  RowGrouping,
-  RowGroupPanel,
-  PinnedRows,
-  StatusBar,
-  StatusBarDark,
-  VariantSecondary,
-  VariantSecondaryDark,
-  VariantZebra,
-  VariantZebraDark,
-  WrappedHeader,
 } = composeStories(agGridStories);
 
 describe("Given Ag Grid Theme", () => {
   describe("WHEN the Default story is mounted", () => {
-    it.skip("THEN should match screenshot", () => {
-      cy.mount(<BasicGrid />);
-      cy.wait(500);
-      cy.matchImage();
-    });
     describe("WHEN column menu is open", () => {
       describe("AND general tab is selected", () => {
         it("THEN should match screenshot", () => {
@@ -70,11 +42,6 @@ describe("Given Ag Grid Theme", () => {
     });
   });
   describe("WHEN the CheckboxSelection story is mounted", () => {
-    it.skip("THEN should match screenshot", () => {
-      cy.mount(<CheckboxSelection />);
-      cy.wait(500);
-      cy.matchImage();
-    });
     describe("WHEN editable-cell is focused", () => {
       it("THEN should match screenshot", () => {
         cy.mount(<CheckboxSelection />);
@@ -107,33 +74,7 @@ describe("Given Ag Grid Theme", () => {
     });
   });
 
-  describe.skip("WHEN the Coloration story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<Coloration />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the ColumnGroup story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<ColumnGroup />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the ColumnSpanning story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<ColumnSpanning />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
   describe("WHEN the ContextMenu story is mounted", () => {
-    it.skip("THEN should match screenshot", () => {
-      cy.mount(<ContextMenu />);
-      cy.wait(500);
-      cy.matchImage();
-    });
     describe("WHEN cell context menu is open", () => {
       it("THEN should match screenshot", () => {
         cy.mount(<ContextMenu />);
@@ -144,11 +85,6 @@ describe("Given Ag Grid Theme", () => {
     });
   });
   describe("WHEN the CustomFilter story is mounted", () => {
-    it.skip("THEN should match screenshot", () => {
-      cy.mount(<CustomFilter />);
-      cy.wait(500);
-      cy.matchImage();
-    });
     describe("WHEN custom filter is focused", () => {
       it("THEN should match screenshot", () => {
         cy.mount(<CustomFilter />);
@@ -158,27 +94,6 @@ describe("Given Ag Grid Theme", () => {
           .parents(".ag-floating-filter")
           .matchImage({ maxDiffThreshold: 0.1 });
       });
-    });
-  });
-  describe.skip("WHEN the DragRowOrder story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<DragRowOrder />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the Icons story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<Icons />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the FloatingFilter story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<FloatingFilter />);
-      cy.wait(500);
-      cy.matchImage();
     });
   });
   describe("WHEN the HDCompact story is mounted", () => {
@@ -250,124 +165,6 @@ describe("Given Ag Grid Theme", () => {
           cy.get(".ag-menu").matchImage({ maxDiffThreshold: 0.1 });
         });
       });
-    });
-  });
-  describe.skip("WHEN the InfiniteScroll story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<InfiniteScroll />);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the LoadingOverlay story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<LoadingOverlay />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the MasterDetail story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<MasterDetail />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the MasterDetailDark story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<MasterDetailDark />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the NoDataOverlay story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<NoDataOverlay />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the Pagination story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<Pagination />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the ParentChildRows story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<ParentChildRows />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the RowGrouping story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<RowGrouping />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the RowGroupPanel story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<RowGroupPanel />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the PinnedRows story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<PinnedRows />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the StatusBar story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<StatusBar />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the StatusBarDark story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<StatusBarDark />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the VariantSecondary story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<VariantSecondary />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the VariantSecondaryDark story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<VariantSecondaryDark />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the VariantZebra story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<VariantZebra />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the VariantZebraDark story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<VariantZebraDark />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the WrappedHeader story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<WrappedHeader />);
-      cy.wait(500);
-      cy.matchImage();
     });
   });
 });
