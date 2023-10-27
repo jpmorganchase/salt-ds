34941b232a6b217bb7b585e77c63e55385022472
Alina Visan <alycry.alina@gmail.com>
2023-10-27 14:12:05 +0300

    Update Default.tsx

--- packages/ag-grid-theme/src/examples/Default.tsx
+++ packages/ag-grid-theme/src/examples/Default.tsx
@@ -32,8 +32,8 @@ const Default = (props: { defaultTheme: string }) => {
             {
               headerName: "Capital",
               field: "capital",
-              tooltipField: 'capital',
-              headerTooltip: 'Capital'
+              tooltipField: "capital",
+              headerTooltip: "Capital",
             },
           ]}
           rowData={dataGridExampleData}
