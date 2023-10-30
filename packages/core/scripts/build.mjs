b034fac2a1735f196e96d368d9b482860ed43e4b
David W J Reid <reid5587@gmail.com>
2023-10-30 11:27:17 +0000

    refactor: output to a single `salt-core.css` file

--- packages/core/scripts/build.mjs
+++ packages/core/scripts/build.mjs
@@ -1,23 +1,50 @@
 import esbuild from "esbuild";
 import path from "node:path";
+import fs from "node:fs";
 import { deleteSync } from "del";
 import { fileURLToPath } from "node:url";
 import glob from "fast-glob";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const buildFolder = path.join(__dirname, "../../../dist/salt-ds-core/css");
-
+const absWorkingDir = path.resolve(__dirname, "..");
+const entryFile = "src/salt-core.css";
 deleteSync([buildFolder], { force: true });
 
-esbuild.build({
-  absWorkingDir: path.resolve(__dirname, ".."),
-  entryPoints: glob.sync(["src/**/*.css"]),
-  assetNames: "[dir]/[name]",
-  outdir: buildFolder,
-  loader: {
-    ".ttf": "file",
-  },
-  write: true,
-  bundle: true,
-  logLevel: "info",
-});
+createCssEntryFile();
+
+esbuild
+  .build({
+    absWorkingDir,
+    entryPoints: [entryFile],
+    outdir: buildFolder,
+    loader: {
+      ".ttf": "file",
+    },
+    write: true,
+    bundle: true,
+    logLevel: "info",
+  })
+  .then(() => {
+    deleteSync(entryFile, { force: true });
+  });
+
+function createCssEntryFile() {
+  const cssFiles = glob.sync(["src/**/*.css"]);
+
+  const entry = fs.createWriteStream(entryFile, {
+    flags: "a", // preserve existing file data
+  });
+
+  const writeLine = (line, index) =>
+    index == 0 ? entry.write(line) : entry.write(`\n${line}`);
+
+  cssFiles.forEach((cssFile, index) => {
+    writeLine(
+      `@import "${path.resolve(path.relative(absWorkingDir, cssFile))}";`,
+      index
+    );
+  });
+
+  entry.end();
+}
