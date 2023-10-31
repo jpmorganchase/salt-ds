1a5fc759399eac711e2bca1c3ac5b0a15d03005d
Alina Visan <alycry.alina@gmail.com>
2023-10-31 16:55:28 +0200

    fix build

---
+++ packages/ag-grid-theme/scripts/build.mjs
@@ -0,0 +1,43 @@
+import esbuild from "esbuild";
+import path from "node:path";
+import { deleteSync } from "del";
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const buildFolder = path.join(__dirname, "../../../dist/salt-ds-ag-grid-theme");
+
+deleteSync([buildFolder], { force: true });
+
+esbuild
+  .build({
+    absWorkingDir: path.resolve(__dirname, ".."),
+    entryPoints: ["index.css", "css/salt-ag-grid-theme.css", "css/salt-icons.css"],
+    assetNames: "[dir]/[name]",
+    outdir: buildFolder,
+    loader: {
+      ".woff": "file",
+    },
+    write: true,
+    bundle: true,
+    logLevel: "info",
+  })
+  .then(() => {
+    // File destination.txt will be created or overwritten by default.
+    fs.copyFile(
+      path.resolve(__dirname, "../package.json"),
+      path.join(buildFolder, "package.json"),
+      (err) => {
+        if (err) throw err;
+        console.log(
+          `${path.relative(
+            process.cwd(),
+            path.resolve(__dirname, "../package.json")
+          )} copied to ${path.relative(
+            process.cwd(),
+            path.join(buildFolder, "package.json")
+          )}`
+        );
+      }
+    );
+  });
