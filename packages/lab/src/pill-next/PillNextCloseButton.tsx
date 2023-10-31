f975416441fa8b1d8e998fa2d3ef87c721ed6d7e
Fernanda Castillo <fersitacastillo123@gmail.com>
2023-10-31 11:08:18 +0000

    updates from feedback

--- packages/lab/src/pill-next/PillNextCloseButton.tsx
+++ packages/lab/src/pill-next/PillNextCloseButton.tsx
@@ -1,9 +1,4 @@
-import {
-  forwardRef,
-  KeyboardEvent,
-  MouseEvent,
-  ComponentPropsWithoutRef,
-} from "react";
+import { forwardRef, MouseEvent, ComponentPropsWithoutRef } from "react";
 import clsx from "clsx";
 import { useWindow } from "@salt-ds/window";
 import { useComponentCssInjection } from "@salt-ds/styles";
@@ -15,7 +10,7 @@ export interface PillNextCloseButtonProps
   extends ComponentPropsWithoutRef<"button"> {
   /* If true the close button will be disabled */
   disabled?: boolean;
-  onClose: () => void;
+  onClose: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
 const withButtonBaseName = makePrefixer("saltPillNextCloseButton");
@@ -37,30 +32,12 @@ export const PillNextCloseButton = forwardRef<
   // we do not want to spread tab index in this case because the button element
   // does not require tabindex="0" attribute
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { tabIndex, onKeyUp, onClick, ...restCloseButtonProps } = buttonProps;
+  const { tabIndex, onClick, ...restCloseButtonProps } = buttonProps;
 
-  const handleKeyUp = (e: KeyboardEvent<HTMLButtonElement>) => {
-    if (!disabled) {
-      switch (e.key) {
-        case "Backspace":
-        case "Delete":
-          e.stopPropagation();
-          handleClose();
-          break;
-        default:
-          onKeyUp(e);
-      }
-    }
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    onClose?.(event);
+    onClick?.(event);
   };
-
-  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
-    if (!disabled) {
-      handleClose();
-      onClick?.(e);
-    }
-  };
-
-  const handleClose = () => onClose?.();
 
   return (
     <Button
@@ -69,8 +46,8 @@ export const PillNextCloseButton = forwardRef<
       className={clsx(withButtonBaseName(), {
         [withButtonBaseName("disabled")]: disabled,
       })}
+      disabled={disabled}
       onClick={handleClick}
-      onKeyUp={handleKeyUp}
       {...restCloseButtonProps}
     >
       <CloseSmallIcon />
