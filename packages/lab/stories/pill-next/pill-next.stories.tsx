2a3c77dc743f48cc249b94827aea40637723e919
Fernanda Castillo <fersitacastillo123@gmail.com>
2023-11-01 13:01:59 +0000

    add docs and example for closable pill

--- packages/lab/stories/pill-next/pill-next.stories.tsx
+++ packages/lab/stories/pill-next/pill-next.stories.tsx
@@ -1,6 +1,9 @@
 import { PillNext } from "@salt-ds/lab";
 import { FavoriteIcon } from "@salt-ds/icons";
 import { Meta, StoryFn } from "@storybook/react";
+import { useState } from "react";
+import { shortColorData } from "./../assets/exampleData";
+import { Button, FlowLayout } from "@salt-ds/core";
 
 export default {
   title: "Lab/Pill Next",
@@ -19,6 +22,40 @@ export const Disabled: StoryFn<typeof PillNext> = () => {
     <PillNext disabled onClick={() => console.log("Click")}>
       Disabled Pill
     </PillNext>
+  );
+};
+
+export const Closable: StoryFn<typeof PillNext> = () => {
+  const [colors, setColor] = useState(shortColorData);
+
+  const removeColor = (color: string) => {
+    const filteredColors = colors.filter((colorItem) => colorItem !== color);
+    console.log(`Closed ${color}`);
+    setColor(filteredColors);
+  };
+  return (
+    <>
+      <FlowLayout gap={0.5} style={{ maxWidth: "400px" }}>
+        <FlowLayout
+          gap={1}
+          style={{ flexBasis: "100%", marginBottom: "1.5rem" }}
+          align={"center"}
+          justify={"space-between"}
+        >
+          <Button onClick={() => setColor(shortColorData)}>reset</Button>
+        </FlowLayout>
+        {colors.map((color, index) => (
+          <PillNext
+            key={color}
+            disabled={index < 3}
+            onClick={() => console.log(`Clicked ${color}`)}
+            onClose={() => removeColor(color)}
+          >
+            {color}
+          </PillNext>
+        ))}
+      </FlowLayout>
+    </>
   );
 };
 
