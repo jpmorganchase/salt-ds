80d10afc5457d9722592aecdfae3fcf9c4a832fb
Alina Visan <alycry.alina@gmail.com>
2023-11-03 14:49:03 +0200

    Update CheckboxSelection.tsx

--- packages/ag-grid-theme/src/examples/CheckboxSelection.tsx
+++ packages/ag-grid-theme/src/examples/CheckboxSelection.tsx
@@ -28,7 +28,7 @@ const CheckboxSelection = (props: AgGridReactProps) => {
 };
 
 CheckboxSelection.parameters = {
-  chromatic: { disableSnapshot: false, delay: 200 },
+  chromatic: { disableSnapshot: false, delay: 500 },
 };
 
 export default CheckboxSelection;
