5724c7bb672342bca118bb704d9122a10d670f1f
David W J Reid <reid5587@gmail.com>
2023-11-06 14:28:27 +0000

    fix: ensure CSS entry file is created before building

--- packages/core/scripts/build.mjs
+++ packages/core/scripts/build.mjs
@@ -9,28 +9,15 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const buildFolder = path.join(__dirname, "../../../dist/salt-ds-core/css");
 const absWorkingDir = path.resolve(__dirname, "..");
 const entryFile = "src/salt-core.css";
+
+/** MAIN */
 deleteSync([buildFolder], { force: true });
-
 createCssEntryFile();
-
-esbuild
-  .build({
-    absWorkingDir,
-    entryPoints: [entryFile],
-    outdir: buildFolder,
-    loader: {
-      ".ttf": "file",
-    },
-    write: true,
-    bundle: true,
-    logLevel: "info",
-  })
-  .then(() => {
-    deleteSync(entryFile, { force: true });
-  });
+runBuild();
 
 function createCssEntryFile() {
   const cssFiles = glob.sync(["src/**/*.css"]);
+  console.log(`Merging ${cssFiles.length} into entry file...`);
 
   const entry = fs.createWriteStream(entryFile, {
     flags: "a", // preserve existing file data
@@ -47,4 +34,22 @@ function createCssEntryFile() {
   });
 
   entry.end();
+}
+
+function runBuild() {
+  esbuild
+    .build({
+      absWorkingDir,
+      entryPoints: [entryFile],
+      outdir: buildFolder,
+      loader: {
+        ".ttf": "file",
+      },
+      write: true,
+      bundle: true,
+      logLevel: "info",
+    })
+    .then(() => {
+      deleteSync(entryFile, { force: true });
+    });
 }
