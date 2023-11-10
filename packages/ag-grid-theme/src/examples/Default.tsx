27a5a9dfe9b69fb8777d6e6017752f637c5e7a5b
Alina Visan <alycry.alina@gmail.com>
2023-11-10 17:34:39 +0200

    Update Default.tsx

--- packages/ag-grid-theme/src/examples/Default.tsx
+++ packages/ag-grid-theme/src/examples/Default.tsx
@@ -47,9 +47,9 @@ Default.parameters = {
   chromatic: { disableSnapshot: false, delay: 200 },
 };
 
-export const DefaultUitk = Default.bind({defaultTheme: "uitk"})
+export const DefaultUitk = Default.bind({ defaultTheme: "uitk" });
 DefaultUitk.parameters = {
   chromatic: { disableSnapshot: false, delay: 200 },
-}
+};
 
 export default Default;
