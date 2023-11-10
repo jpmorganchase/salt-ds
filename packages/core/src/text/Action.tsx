12e185a8e300569176f8bfbc982cab3140cde4d1
Felicia Chong <felckh@gmail.com>
2023-11-10 14:47:46 +0000

    address feedback 2

--- packages/core/src/text/Action.tsx
+++ packages/core/src/text/Action.tsx
@@ -1,21 +1,12 @@
 import { forwardRef } from "react";
-import { makePrefixer } from "../utils";
-import { clsx } from "clsx";
 import { Text, TextProps } from "./Text";
-
-const withBaseName = makePrefixer("saltText");
 
 export const Action = forwardRef<
   HTMLSpanElement,
   Omit<TextProps<"span">, "as">
 >(function Action({ children, className, ...rest }, ref) {
   return (
-    <Text
-      as="span"
-      className={clsx(className, withBaseName("action"))}
-      ref={ref}
-      {...rest}
-    >
+    <Text as="span" styleAs="action" ref={ref} {...rest}>
       {children}
     </Text>
   );
