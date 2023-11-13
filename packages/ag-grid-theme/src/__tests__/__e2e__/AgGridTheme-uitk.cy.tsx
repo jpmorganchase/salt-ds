7ff50c549689197aa302848a939c2ec557c69a36
Alina Visan <alycry.alina@gmail.com>
2023-11-13 15:37:04 +0200

    clean

--- packages/ag-grid-theme/src/__tests__/__e2e__/AgGridTheme-uitk.cy.tsx
+++ packages/ag-grid-theme/src/__tests__/__e2e__/AgGridTheme-uitk.cy.tsx
@@ -4,29 +4,10 @@ import * as agGridStories from "@stories/ag-grid-theme.stories";
 const {
   BasicGrid,
   CheckboxSelection,
-  Coloration,
-  ColumnGroup,
-  ColumnSpanning,
   ContextMenu,
   CustomFilter,
-  DragRowOrder,
-  Icons,
-  FloatingFilter,
   HDCompact,
   HDCompactDark,
-  InfiniteScroll,
-  LoadingOverlay,
-  MasterDetail,
-  MasterDetailDark,
-  NoDataOverlay,
-  Pagination,
-  ParentChildRows,
-  RowGrouping,
-  RowGroupPanel,
-  PinnedRows,
-  StatusBar,
-  StatusBarDark,
-  WrappedHeader,
 } = composeStories(agGridStories);
 
 describe("Given Ag Grid Theme - uitk", () => {
@@ -92,28 +73,6 @@ describe("Given Ag Grid Theme - uitk", () => {
       });
     });
   });
-
-  describe.skip("WHEN the Coloration story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<Coloration defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the ColumnGroup story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<ColumnGroup defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the ColumnSpanning story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<ColumnSpanning defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
   describe("WHEN the ContextMenu story is mounted", () => {
     describe("WHEN cell context menu is open", () => {
       it("THEN should match screenshot", () => {
@@ -136,33 +95,7 @@ describe("Given Ag Grid Theme - uitk", () => {
       });
     });
   });
-  describe.skip("WHEN the DragRowOrder story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<DragRowOrder defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the Icons story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<Icons defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the FloatingFilter story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<FloatingFilter defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
   describe("WHEN the HDCompact story is mounted", () => {
-    it.skip("THEN should match screenshot", () => {
-      cy.mount(<HDCompact defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
     describe("WHEN column menu is open", () => {
       describe("AND general tab is selected", () => {
         it("THEN should match screenshot", () => {
@@ -194,11 +127,6 @@ describe("Given Ag Grid Theme - uitk", () => {
     });
   });
   describe("WHEN the HDCompactDark story is mounted", () => {
-    it.skip("THEN should match screenshot", () => {
-      cy.mount(<HDCompactDark defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
     describe("WHEN column menu is open", () => {
       describe("AND general tab is selected", () => {
         it("THEN should match screenshot", () => {
@@ -226,96 +154,6 @@ describe("Given Ag Grid Theme - uitk", () => {
           cy.get(".ag-menu").matchImage({ maxDiffThreshold: 0.1 });
         });
       });
-    });
-  });
-  describe.skip("WHEN the InfiniteScroll story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<InfiniteScroll defaultTheme="uitk" />);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the LoadingOverlay story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<LoadingOverlay defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the MasterDetail story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<MasterDetail defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the MasterDetailDark story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<MasterDetailDark defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the NoDataOverlay story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<NoDataOverlay defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the Pagination story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<Pagination defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the ParentChildRows story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<ParentChildRows defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the RowGrouping story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<RowGrouping defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the RowGroupPanel story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<RowGroupPanel defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the PinnedRows story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<PinnedRows defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the StatusBar story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<StatusBar defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the StatusBarDark story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<StatusBarDark defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
-    });
-  });
-  describe.skip("WHEN the WrappedHeader story is mounted", () => {
-    it("THEN should match screenshot", () => {
-      cy.mount(<WrappedHeader defaultTheme="uitk" />);
-      cy.wait(500);
-      cy.matchImage();
     });
   });
 });
