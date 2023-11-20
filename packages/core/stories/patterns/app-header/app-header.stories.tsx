9c8ab85840b71005d3e09b2e41312b58dca439ff
R662301 <lily.cooksley@jpmchase.com>
2023-11-20 15:23:36 +0000

    simplify

--- packages/core/stories/patterns/app-header/app-header.stories.tsx
+++ packages/core/stories/patterns/app-header/app-header.stories.tsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
+import { FC, HTMLAttributes, useState } from "react";
 import {
   Button,
   FlexItem,
   FlexLayout,
-  BorderItem,
-  BorderLayout,
   StackLayout,
+  BorderLayout,
+  BorderItem,
   useResponsiveProp,
 } from "@salt-ds/core";
-import { Drawer, NavigationItem } from "@salt-ds/lab"; // Assuming you're using Material-UI for UI components
+import { Drawer, NavigationItem } from "@salt-ds/lab";
 import {
   SymphonyIcon,
   StackoverflowIcon,
@@ -22,11 +22,16 @@ export default {
   title: "Patterns/App Header",
 } as Meta;
 
-export const AppHeader = () => {
-  const [drawerOpen, setDrawerOpen] = useState(false);
-  const items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
-  const [active, setActive] = useState(items[0]);
+interface AppHeaderProps extends HTMLAttributes<HTMLElement> {
+  /**
+   * Items included in the App Header
+   */
+  items?: string[];
+}
 
+const AppHeader: FC<AppHeaderProps> = ({ items }) => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [active, setActive] = useState(items?.[0]);
   const isMobile = useResponsiveProp({ xs: true, sm: false }, false);
 
   const handleDrawerToggle = () => {
@@ -36,13 +41,14 @@ export const AppHeader = () => {
   return (
     <header>
       {isMobile ? (
+        // Mobile Header
         <FlexLayout style={{ height: "44px" }} justify="space-between" gap={3}>
           <StackLayout
-            direction={"row"}
+            direction="row"
             style={{
               width: "100%",
               backgroundColor: "var(--salt-container-primary-background)",
-              zIndex: "999999999",
+              zIndex: 999999999,
               paddingRight: "1em",
               borderBottom:
                 "var(--salt-size-border) var(--salt-container-borderStyle) var(--salt-container-secondary-borderColor)",
@@ -66,24 +72,22 @@ export const AppHeader = () => {
                 {drawerOpen ? <CloseIcon /> : <MenuIcon />}
               </Button>
             </FlexItem>
-
             <FlexItem align="center">App logo</FlexItem>
           </StackLayout>
+
           <Drawer
             style={{ paddingTop: "44px", paddingLeft: "0" }}
             open={drawerOpen}
           >
             <nav>
               <ul style={{ listStyle: "none", padding: "0" }}>
-                {items.map((item) => (
+                {items?.map((item) => (
                   <li key={item}>
                     <NavigationItem
                       orientation="vertical"
                       active={active === item}
                       href="#"
-                      onClick={() => {
-                        setActive(item);
-                      }}
+                      onClick={() => setActive(item)}
                     >
                       {item}
                     </NavigationItem>
@@ -94,6 +98,7 @@ export const AppHeader = () => {
           </Drawer>
         </FlexLayout>
       ) : (
+        // Desktop Header
         <FlexLayout
           style={{ height: "44px", paddingLeft: "1em", paddingRight: "1em" }}
           justify="space-between"
@@ -102,14 +107,12 @@ export const AppHeader = () => {
           <FlexItem align="center">App logo</FlexItem>
           <nav>
             <ul style={{ display: "flex", listStyle: "none", margin: "0" }}>
-              {items.map((item) => (
+              {items?.map((item) => (
                 <li key={item}>
                   <NavigationItem
                     active={active === item}
                     href="#"
-                    onClick={() => {
-                      setActive(item);
-                    }}
+                    onClick={() => setActive(item)}
                   >
                     {item}
                   </NavigationItem>
@@ -118,7 +121,7 @@ export const AppHeader = () => {
             </ul>
           </nav>
           <FlexItem align="center">
-            <StackLayout direction={"row"} gap={1}>
+            <StackLayout direction="row" gap={1}>
               <Button variant="secondary">
                 <SymphonyIcon />
               </Button>
@@ -136,15 +139,13 @@ export const AppHeader = () => {
   );
 };
 
-AppHeader.parameters = {
-  layout: "fullscreen",
-};
-
 export const FullPage = () => {
+  const items = ["Home", "About", "Services", "Contact", "Blog"];
+
   return (
     <BorderLayout style={{ height: "100vh" }}>
       <BorderItem position="north">
-        <AppHeader />
+        <AppHeader items={items} />
       </BorderItem>
       <BorderItem
         style={{ backgroundColor: "var(--salt-color-teal-30)", padding: "1em" }}
