4f3cc05de81711ce6589f40e977b8473248ba737
Felicia Chong <felckh@gmail.com>
2023-11-20 10:16:52 +0000

    add Notation and Action to site examples

--- packages/core/src/text/Display.tsx
+++ packages/core/src/text/Display.tsx
@@ -4,9 +4,12 @@ import { Text, TextProps } from "./Text";
 export const Display1 = forwardRef<
   HTMLSpanElement,
   Omit<TextProps<"span">, "as">
->(function Display1({ children, className, ...rest }, ref) {
+>(function Display1(
+  { children, className, styleAs = "display1", ...rest },
+  ref
+) {
   return (
-    <Text as="span" styleAs="display1" ref={ref} {...rest}>
+    <Text as="span" styleAs={styleAs} ref={ref} {...rest}>
       {children}
     </Text>
   );
@@ -15,9 +18,12 @@ export const Display1 = forwardRef<
 export const Display2 = forwardRef<
   HTMLSpanElement,
   Omit<TextProps<"span">, "as">
->(function Display2({ children, className, ...rest }, ref) {
+>(function Display2(
+  { children, className, styleAs = "display2", ...rest },
+  ref
+) {
   return (
-    <Text as="span" styleAs="display2" ref={ref} {...rest}>
+    <Text as="span" styleAs={styleAs} ref={ref} {...rest}>
       {children}
     </Text>
   );
@@ -26,9 +32,12 @@ export const Display2 = forwardRef<
 export const Display3 = forwardRef<
   HTMLSpanElement,
   Omit<TextProps<"span">, "as">
->(function Display3({ children, className, ...rest }, ref) {
+>(function Display3(
+  { children, className, styleAs = "display3", ...rest },
+  ref
+) {
   return (
-    <Text as="span" styleAs="display3" ref={ref} {...rest}>
+    <Text as="span" styleAs={styleAs} ref={ref} {...rest}>
       {children}
     </Text>
   );
