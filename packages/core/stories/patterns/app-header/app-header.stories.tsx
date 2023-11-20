a24137f1f42f590c6899be6bfb1153f7d3d07647
R662301 <lily.cooksley@jpmchase.com>
2023-11-20 15:29:36 +0000

    combine examples

--- packages/core/stories/patterns/app-header/app-header.stories.tsx
+++ packages/core/stories/patterns/app-header/app-header.stories.tsx
@@ -22,69 +22,99 @@ export default {
   title: "Patterns/App Header",
 } as Meta;
 
-interface AppHeaderProps extends HTMLAttributes<HTMLElement> {
-  /**
-   * Items included in the App Header
-   */
-  items?: string[];
-}
+export const AppHeaderInPage = () => {
+  interface AppHeaderProps extends HTMLAttributes<HTMLElement> {
+    /**
+     * Items included in the App Header
+     */
+    items?: string[];
+  }
 
-const AppHeader: FC<AppHeaderProps> = ({ items }) => {
-  const [drawerOpen, setDrawerOpen] = useState(false);
-  const [active, setActive] = useState(items?.[0]);
-  const isMobile = useResponsiveProp({ xs: true, sm: false }, false);
+  const AppHeader: FC<AppHeaderProps> = ({ items }) => {
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const [active, setActive] = useState(items?.[0]);
+    const isMobile = useResponsiveProp({ xs: true, sm: false }, false);
 
-  const handleDrawerToggle = () => {
-    setDrawerOpen(!drawerOpen);
-  };
+    const handleDrawerToggle = () => {
+      setDrawerOpen(!drawerOpen);
+    };
 
-  return (
-    <header>
-      {isMobile ? (
-        // Mobile Header
-        <FlexLayout style={{ height: "44px" }} justify="space-between" gap={3}>
-          <StackLayout
-            direction="row"
-            style={{
-              width: "100%",
-              backgroundColor: "var(--salt-container-primary-background)",
-              zIndex: 999999999,
-              paddingRight: "1em",
-              borderBottom:
-                "var(--salt-size-border) var(--salt-container-borderStyle) var(--salt-container-secondary-borderColor)",
-            }}
+    return (
+      <header>
+        {isMobile ? (
+          // Mobile Header
+          <FlexLayout
+            style={{ height: "44px" }}
+            justify="space-between"
+            gap={3}
           >
-            <FlexItem
+            <StackLayout
+              direction="row"
               style={{
-                justifyContent: "center",
-                display: "flex",
-                height: "44px",
-                width: "44px",
-                borderRight:
+                width: "100%",
+                backgroundColor: "var(--salt-container-primary-background)",
+                zIndex: 999999999,
+                paddingRight: "1em",
+                borderBottom:
                   "var(--salt-size-border) var(--salt-container-borderStyle) var(--salt-container-secondary-borderColor)",
               }}
             >
-              <Button
-                onClick={handleDrawerToggle}
-                variant={drawerOpen ? "primary" : "secondary"}
-                style={{ alignSelf: "center" }}
+              <FlexItem
+                style={{
+                  justifyContent: "center",
+                  display: "flex",
+                  height: "44px",
+                  width: "44px",
+                  borderRight:
+                    "var(--salt-size-border) var(--salt-container-borderStyle) var(--salt-container-secondary-borderColor)",
+                }}
               >
-                {drawerOpen ? <CloseIcon /> : <MenuIcon />}
-              </Button>
-            </FlexItem>
-            <FlexItem align="center">App logo</FlexItem>
-          </StackLayout>
+                <Button
+                  onClick={handleDrawerToggle}
+                  variant={drawerOpen ? "primary" : "secondary"}
+                  style={{ alignSelf: "center" }}
+                >
+                  {drawerOpen ? <CloseIcon /> : <MenuIcon />}
+                </Button>
+              </FlexItem>
+              <FlexItem align="center">App logo</FlexItem>
+            </StackLayout>
 
-          <Drawer
-            style={{ paddingTop: "44px", paddingLeft: "0" }}
-            open={drawerOpen}
+            <Drawer
+              style={{ paddingTop: "44px", paddingLeft: "0" }}
+              open={drawerOpen}
+            >
+              <nav>
+                <ul style={{ listStyle: "none", padding: "0" }}>
+                  {items?.map((item) => (
+                    <li key={item}>
+                      <NavigationItem
+                        orientation="vertical"
+                        active={active === item}
+                        href="#"
+                        onClick={() => setActive(item)}
+                      >
+                        {item}
+                      </NavigationItem>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </Drawer>
+          </FlexLayout>
+        ) : (
+          // Desktop Header
+          <FlexLayout
+            style={{ height: "44px", paddingLeft: "1em", paddingRight: "1em" }}
+            justify="space-between"
+            gap={3}
           >
+            <FlexItem align="center">App logo</FlexItem>
             <nav>
-              <ul style={{ listStyle: "none", padding: "0" }}>
+              <ul style={{ display: "flex", listStyle: "none", margin: "0" }}>
                 {items?.map((item) => (
                   <li key={item}>
                     <NavigationItem
-                      orientation="vertical"
                       active={active === item}
                       href="#"
                       onClick={() => setActive(item)}
@@ -95,51 +125,25 @@ const AppHeader: FC<AppHeaderProps> = ({ items }) => {
                 ))}
               </ul>
             </nav>
-          </Drawer>
-        </FlexLayout>
-      ) : (
-        // Desktop Header
-        <FlexLayout
-          style={{ height: "44px", paddingLeft: "1em", paddingRight: "1em" }}
-          justify="space-between"
-          gap={3}
-        >
-          <FlexItem align="center">App logo</FlexItem>
-          <nav>
-            <ul style={{ display: "flex", listStyle: "none", margin: "0" }}>
-              {items?.map((item) => (
-                <li key={item}>
-                  <NavigationItem
-                    active={active === item}
-                    href="#"
-                    onClick={() => setActive(item)}
-                  >
-                    {item}
-                  </NavigationItem>
-                </li>
-              ))}
-            </ul>
-          </nav>
-          <FlexItem align="center">
-            <StackLayout direction="row" gap={1}>
-              <Button variant="secondary">
-                <SymphonyIcon />
-              </Button>
-              <Button variant="secondary">
-                <StackoverflowIcon />
-              </Button>
-              <Button variant="secondary">
-                <GithubIcon />
-              </Button>
-            </StackLayout>
-          </FlexItem>
-        </FlexLayout>
-      )}
-    </header>
-  );
-};
+            <FlexItem align="center">
+              <StackLayout direction="row" gap={1}>
+                <Button variant="secondary">
+                  <SymphonyIcon />
+                </Button>
+                <Button variant="secondary">
+                  <StackoverflowIcon />
+                </Button>
+                <Button variant="secondary">
+                  <GithubIcon />
+                </Button>
+              </StackLayout>
+            </FlexItem>
+          </FlexLayout>
+        )}
+      </header>
+    );
+  };
 
-export const FullPage = () => {
   const items = ["Home", "About", "Services", "Contact", "Blog"];
 
   return (
@@ -175,6 +179,6 @@ export const FullPage = () => {
   );
 };
 
-FullPage.parameters = {
+AppHeaderInPage.parameters = {
   layout: "fullscreen",
 };
