90528b9b56ea312479746e4e4e897458b9cc91bd
Felicia Chong <felckh@gmail.com>
2023-11-23 12:15:39 +0000

    add tests & stories

--- packages/lab/src/__tests__/__e2e__/overlay/Overlay.cy.tsx
+++ packages/lab/src/__tests__/__e2e__/overlay/Overlay.cy.tsx
@@ -3,15 +3,65 @@ import * as overlayStories from "@stories/overlay/overlay.stories";
 import { checkAccessibility } from "../../../../../../cypress/tests/checkAccessibility";
 
 const composedStories = composeStories(overlayStories);
-const { OverlayTop, OverlayRight, OverlayBottom, OverlayLeft } =
-  composedStories;
+const { Default, OverlayRight, OverlayBottom, OverlayLeft } = composedStories;
 
 describe("GIVEN an Overlay", () => {
   checkAccessibility(composedStories);
 
+  describe("WHEN rendered", () => {
+    it("THEN it should show Overlay on anchor element click", () => {
+      cy.mount(<Default />);
+
+      cy.realPress("Tab").realPress("Enter");
+      cy.findByRole("dialog").should("be.visible");
+      // focus remains on anchor element
+      cy.findByRole("button", { name: /Toggle Overlay/i }).should("be.focused");
+    });
+
+    it("THEN it should dismiss on Esc key press", () => {
+      cy.mount(<Default />);
+
+      cy.realPress("Tab").realPress("Enter");
+      cy.findByRole("dialog").should("be.visible");
+      cy.realPress("Escape");
+      cy.findByRole("dialog").should("not.exist");
+      // focus goes back to anchor element
+      cy.findByRole("button", { name: /Toggle Overlay/i }).should("be.focused");
+    });
+
+    it("THEN it should remain open until outside Overlay click or close button click", () => {
+      cy.mount(<Default />);
+
+      cy.findByRole("button", { name: /Toggle Overlay/i }).click();
+      cy.findByRole("dialog").should("be.visible");
+      cy.get(".saltOverlay-closeButton").click();
+      cy.findByRole("dialog").should("not.exist");
+
+      cy.findByRole("button", { name: /Toggle Overlay/i }).click();
+      cy.findByRole("dialog").should("be.visible");
+      cy.get("body").click(0, 0); // click outside of Overlay
+      cy.findByRole("dialog").should("not.exist");
+    });
+
+    xit("THEN it should trap focus within Overlay once opened", () => {
+      cy.mount(<Default open />);
+
+      cy.findByRole("dialog").should("be.visible");
+      cy.realPress("Tab");
+      cy.findAllByRole("button").eq(1).should("be.focused");
+      cy.realPress("Tab");
+      cy.findAllByRole("button").eq(2).should("be.focused");
+      cy.realPress("Tab");
+      cy.findAllByRole("button")
+        .eq(1)
+        .should("be.focused")
+        .should("have.attr", "aria-label", "Close Overlay");
+    });
+  });
+
   describe("WHEN mounted top", () => {
     it("THEN it should appear on top of anchor element", () => {
-      cy.mount(<OverlayTop open />);
+      cy.mount(<Default open />);
 
       cy.findByRole("dialog").then(($el) => {
         const position = $el[0].getBoundingClientRect().y;
@@ -21,6 +71,7 @@ describe("GIVEN an Overlay", () => {
       });
     });
   });
+
   describe("WHEN mounted right", () => {
     it("THEN it should appear on right of anchor element", () => {
       cy.mount(<OverlayRight open />);
@@ -33,6 +84,7 @@ describe("GIVEN an Overlay", () => {
       });
     });
   });
+
   describe("WHEN mounted bottom", () => {
     it("THEN it should appear on bottom of anchor element", () => {
       cy.mount(<OverlayBottom open />);
@@ -45,6 +97,7 @@ describe("GIVEN an Overlay", () => {
       });
     });
   });
+
   describe("WHEN mounted left", () => {
     it("THEN it should appear on left of anchor element", () => {
       cy.mount(<OverlayLeft open />);
--- packages/lab/stories/overlay/overlay.stories.tsx
+++ packages/lab/stories/overlay/overlay.stories.tsx
@@ -1,4 +1,4 @@
-import { Button, Tooltip } from "@salt-ds/core";
+import { Button, StackLayout, Tooltip } from "@salt-ds/core";
 import { Overlay } from "@salt-ds/lab";
 import { Meta, StoryFn } from "@storybook/react";
 
@@ -9,42 +9,72 @@ export default {
   component: Overlay,
 } as Meta<typeof Overlay>;
 
-const OverlayTemplate: StoryFn<typeof Overlay> = (props) => {
-  const OverlayContent = () => (
-    <>
-      <h3 className="content-heading">Title</h3>
-      <div className="content-body">Content of Overlay</div>
-      {/* <CheckboxGroupExample /> */}
-      <br />
-      <Tooltip content={"im a tooltip"}>
-        <Button>hover me</Button>
-      </Tooltip>
-    </>
-  );
+const OverlayContent = (
+  <>
+    <h3 className="content-heading">Title</h3>
+    <div className="content-body">Content of Overlay</div>
+    <br />
+    <Tooltip content={"im a tooltip"}>
+      <Button>hover me</Button>
+    </Tooltip>
+  </>
+);
 
+const OverlayTemplate: StoryFn<typeof Overlay> = (props) => {
   return (
-    <Overlay {...props} content={<OverlayContent />}>
-      <Button>open overlay</Button>
+    <Overlay {...props} content={props.content}>
+      <Button>Toggle Overlay</Button>
     </Overlay>
   );
 };
 
-export const OverlayTop = OverlayTemplate.bind({});
-OverlayTop.args = {
+export const Default = OverlayTemplate.bind({});
+Default.args = {
   placement: "top",
+  content: OverlayContent,
 };
 
 export const OverlayRight = OverlayTemplate.bind({});
 OverlayRight.args = {
   placement: "right",
+  content: OverlayContent,
 };
 
 export const OverlayBottom = OverlayTemplate.bind({});
 OverlayBottom.args = {
   placement: "bottom",
+  content: OverlayContent,
 };
 
 export const OverlayLeft = OverlayTemplate.bind({});
 OverlayLeft.args = {
   placement: "left",
+  content: OverlayContent,
+};
+
+export const LongContent = OverlayTemplate.bind({});
+LongContent.args = {
+  placement: "right",
+  style: {
+    width: 300,
+    height: 200,
+    overflow: "auto",
+  },
+  content: (
+    <StackLayout>
+      <div>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
+      </div>
+      <div>
+        It has survived not only five centuries, but also the leap into
+        electronic typesetting, remaining essentially unchanged. It was
+        popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing software
+        like Aldus PageMaker including versions of Lorem Ipsum.
+      </div>
+    </StackLayout>
+  ),
 };
