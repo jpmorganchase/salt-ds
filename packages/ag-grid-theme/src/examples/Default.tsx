66ede9b389d838989385a58ba88040fa9f66f2cf
Alina Visan <alycry.alina@gmail.com>
2023-11-29 13:58:38 +0200

    Update Default.tsx

--- packages/ag-grid-theme/src/examples/Default.tsx
+++ packages/ag-grid-theme/src/examples/Default.tsx
@@ -1,52 +1,20 @@
-import { useEffect, useRef, useState } from "react";
 import { AgGridReact } from "ag-grid-react";
-import { StackLayout, useDensity, useTheme } from "@salt-ds/core";
-import { ColumnApi, GridApi, GridReadyEvent } from "ag-grid-community";
+import { StackLayout } from "@salt-ds/core";
 import dataGridExampleData from "../dependencies/dataGridExampleData";
-// import { useAgGridHelpers } from "../dependencies/useAgGridHelpers";
+import { useAgGridHelpers } from "../dependencies/useAgGridHelpers";
 import { useAgGridThemeSwitcher } from "../dependencies/ThemeSwitcher";
 
 const Default = (props: { defaultTheme: string }) => {
   const { defaultTheme = "salt" } = props;
-  const { switcher } = useAgGridThemeSwitcher(defaultTheme);
-  // const { agGridProps, containerProps } = useAgGridHelpers({
-  //   agThemeName: `ag-theme-${themeName}`,
-  // });
-
-  const [isGridReady, setGridReady] = useState(false);
-  const { mode } = useTheme();
-  const density = useDensity();
-
-  const apiRef = useRef<{ api: GridApi; columnApi: ColumnApi }>();
-  const onGridReady = ({ api, columnApi }: GridReadyEvent) => {
-    apiRef.current = { api, columnApi };
-    api.sizeColumnsToFit();
-    setGridReady(true);
-  };
-
-  useEffect(() => {
-    // setHeaderHeight doesn't work if not in setTimeout
-    setTimeout(() => {
-      if (isGridReady) {
-        // const rowHeight = apiRef.current!.api.getRowHeight()
-        console.log("resetRowHeights");
-
-        apiRef.current?.api.resetRowHeights();
-        // apiRef.current!.api.setHeaderHeight(rowHeight);
-        // apiRef.current!.api.setFloatingFiltersHeight(rowHeight);
-      }
-    }, 0);
-  }, [density, isGridReady]);
+  const { themeName, switcher } = useAgGridThemeSwitcher(defaultTheme);
+  const { agGridProps, containerProps } = useAgGridHelpers({
+    agThemeName: `ag-theme-${themeName}`,
+  });
 
   return (
     <StackLayout gap={4}>
       {switcher}
-
-      <div
-        className={`ag-theme-salt-${mode}`}
-        style={{ height: 500, width: "800px" }}
-      >
-        {/* <div {...containerProps}> */}
+      <div {...containerProps}>
         <AgGridReact
           columnDefs={[
             {
@@ -56,7 +24,6 @@ const Default = (props: { defaultTheme: string }) => {
                 buttons: ["reset", "apply"],
               },
               editable: false,
-              autoHeight: true,
             },
             {
               headerName: "Code",
@@ -69,8 +36,7 @@ const Default = (props: { defaultTheme: string }) => {
           ]}
           rowData={dataGridExampleData}
           rowSelection="single"
-          onGridReady={onGridReady}
-          // {...agGridProps}
+          {...agGridProps}
         />
       </div>
     </StackLayout>
