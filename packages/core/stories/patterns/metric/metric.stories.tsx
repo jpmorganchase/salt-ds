a6a4cffcf0865a95c0bd02ddb9dbf83dd823c5d3
Lily Cooksley <lilycooksley@gmail.com>
2023-11-29 15:54:37 +0000

    initial metric pattern examples

---
+++ packages/core/stories/patterns/metric/metric.stories.tsx
@@ -0,0 +1,172 @@
+import {
+  StackLayout,
+  Text,
+  Display1,
+  Display2,
+  Display3,
+  Link,
+} from "@salt-ds/core";
+import { ArrowUpIcon, ArrowDownIcon } from "@salt-ds/icons";
+import { Meta } from "@storybook/react";
+
+export default {
+  title: "Patterns/Metric",
+} as Meta;
+
+export const Metric = () => {
+  return (
+    <StackLayout gap={1}>
+      <Text>
+        <b>Revenue YTD</b>
+      </Text>
+      <Display1>$801.9B</Display1>
+    </StackLayout>
+  );
+};
+
+export const HorizontalMetric = () => {
+  return (
+    <StackLayout direction="row" gap={1}>
+      <Text>
+        <b>Revenue YTD</b>
+      </Text>
+      <Display1 style={{ lineHeight: "0.8" }}>$801.9B</Display1>
+    </StackLayout>
+  );
+};
+
+export const Subtitle = () => {
+  return (
+    <StackLayout gap={1}>
+      <Text>
+        <b>Revenue YTD</b>
+      </Text>
+      <Text variant="secondary">Total Value</Text>
+      <Display1>$801.9B</Display1>
+    </StackLayout>
+  );
+};
+
+export const Subvalue = () => {
+  return (
+    <StackLayout gap={1}>
+      <Text>
+        <b>Revenue YTD</b>
+      </Text>
+      <Text variant="secondary">Total Value</Text>
+      <Display1>$801.9B</Display1>
+      <Text style={{ color: "var(--salt-status-positive-foreground)" }}>
+        +10.1 (+1.23%)
+      </Text>
+    </StackLayout>
+  );
+};
+
+export const LinkSubtitle = () => {
+  return (
+    <StackLayout gap={1}>
+      <Text>
+        <b>Revenue YTD</b>
+      </Text>
+      <Link variant="secondary">Total Value</Link>
+      <Display1>$801.9B</Display1>
+      <Text style={{ color: "var(--salt-status-positive-foreground)" }}>
+        +10.1 (+1.23%)
+      </Text>
+    </StackLayout>
+  );
+};
+
+export const Indicators = () => {
+  return (
+    <StackLayout direction={"row"} gap={8}>
+      <StackLayout gap={1}>
+        <Text>
+          <b>Revenue YTD</b>
+        </Text>
+        <Text variant="secondary">Total Value</Text>
+        <Display1>
+          $801.9B
+          <ArrowUpIcon
+            style={{ fill: "var(--salt-status-positive-foreground)" }}
+            size={2}
+          />
+        </Display1>
+        <Text style={{ color: "var(--salt-status-positive-foreground)" }}>
+          +10.1 (+1.23%)
+        </Text>
+      </StackLayout>
+      <StackLayout gap={1}>
+        <Text>
+          <b>Revenue YTD</b>
+        </Text>
+        <Text variant="secondary">Total Value</Text>
+        <Display1>
+          $801.9B
+          <ArrowDownIcon
+            style={{ fill: "var(--salt-status-negative-foreground)" }}
+            size={2}
+          />
+        </Display1>
+        <Text style={{ color: "var(--salt-status-negative-foreground)" }}>
+          -10.1 (+1.23%)
+        </Text>
+      </StackLayout>
+    </StackLayout>
+  );
+};
+
+export const Hierarchical = () => {
+  return (
+    <StackLayout direction={"row"} gap={8} align="end">
+      <StackLayout gap={1}>
+        <Text>
+          <b>Revenue YTD</b>
+        </Text>
+        <Text variant="secondary">Total Value</Text>
+        <Display3>
+          $801.9B
+          <ArrowUpIcon
+            style={{ fill: "var(--salt-status-positive-foreground)" }}
+            size={1}
+          />
+        </Display3>
+        <Text style={{ color: "var(--salt-status-positive-foreground)" }}>
+          +10.1 (+1.23%)
+        </Text>
+      </StackLayout>
+      <StackLayout gap={1}>
+        <Text>
+          <b>Revenue YTD</b>
+        </Text>
+        <Text variant="secondary">Total Value</Text>
+        <Display2>
+          $801.9B
+          <ArrowUpIcon
+            style={{ fill: "var(--salt-status-positive-foreground)" }}
+            size={2}
+          />
+        </Display2>
+        <Text style={{ color: "var(--salt-status-positive-foreground)" }}>
+          +10.1 (+1.23%)
+        </Text>
+      </StackLayout>
+      <StackLayout gap={1}>
+        <Text>
+          <b>Revenue YTD</b>
+        </Text>
+        <Text variant="secondary">Total Value</Text>
+        <Display1>
+          $801.9B
+          <ArrowUpIcon
+            style={{ fill: "var(--salt-status-positive-foreground)" }}
+            size={2}
+          />
+        </Display1>
+        <Text style={{ color: "var(--salt-status-positive-foreground)" }}>
+          +10.1 (+1.23%)
+        </Text>
+      </StackLayout>
+    </StackLayout>
+  );
+};
