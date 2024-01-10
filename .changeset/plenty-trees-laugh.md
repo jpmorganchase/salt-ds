6dd12fa231ddfeb52d5499e9b41cd810f102ae05
Fernanda Castillo <fersitacastillo123@gmail.com>
2024-01-10 16:49:50 +0000

    fix tooltip disabled status evaluator

---
+++ .changeset/plenty-trees-laugh.md
@@ -0,0 +1,5 @@
+---
+"@salt-ds/core": patch
+---
+
+Fix logic for tooltip disabled to evaluate disabledProp is passed
--- packages/core/src/tooltip/Tooltip.tsx
+++ packages/core/src/tooltip/Tooltip.tsx
@@ -89,7 +89,8 @@ export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
       validationStatus: formFieldValidationStatus,
     } = useFormFieldProps();
 
-    const disabled = formFieldDisabled ?? disabledProp;
+    const disabled = Boolean(formFieldDisabled) || disabledProp;
+
     const status =
       formFieldValidationStatus !== undefined &&
       VALIDATION_NAMED_STATUS.includes(formFieldValidationStatus)
