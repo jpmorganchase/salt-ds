860fdaf60063f7b2cc519d2157b106e76c6b1dac
Felicia Chong <felckh@gmail.com>
2024-01-10 15:48:18 +0000

    address feedback

--- site/src/examples/overlay/Default.tsx
+++ site/src/examples/overlay/Default.tsx
@@ -8,24 +8,6 @@ import {
 } from "@salt-ds/lab";
 import { Tooltip, Button } from "@salt-ds/core";
 
-const OverlayContent = ({ id }: { id: string }) => {
-  return (
-    <>
-      <h3 id={`${id}-header`} className="content-heading">
-        Title
-      </h3>
-      <div id={`${id}-content`}>
-        Content of Overlay
-        <br />
-        <br />
-        <Tooltip content={"im a tooltip"}>
-          <Button>hover me</Button>
-        </Tooltip>
-      </div>
-    </>
-  );
-};
-
 export const Default = (props: OverlayProps): ReactElement => {
   const { style, ...rest } = props;
   const id = "overlay-default";
@@ -36,7 +18,17 @@ export const Default = (props: OverlayProps): ReactElement => {
         <Button>Show Overlay</Button>
       </OverlayTrigger>
       <OverlayPanel style={style}>
-        <OverlayContent id={id ?? ""} />
+        <h3 id={`${id}-header`} className="content-heading">
+          Title
+        </h3>
+        <div id={`${id}-content`}>
+          Content of Overlay
+          <br />
+          <br />
+          <Tooltip content={"im a tooltip"}>
+            <Button>hover me</Button>
+          </Tooltip>
+        </div>
       </OverlayPanel>
     </Overlay>
   );
