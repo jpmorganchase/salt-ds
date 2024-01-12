513f5fd138923ece39a880401ba0b728c4c4cbff
Fernanda Castillo <fersitacastillo123@gmail.com>
2024-01-12 09:47:36 +0000

    updates from feedback

--- packages/lab/src/pill-next/PillNext.tsx
+++ packages/lab/src/pill-next/PillNext.tsx
@@ -7,53 +7,55 @@ import pillCss from "./PillNext.css";
 
 const withBaseName = makePrefixer("saltPillNext");
 
-export const PillNext = forwardRef<
-  HTMLButtonElement,
-  ComponentPropsWithoutRef<"button">
->(function PillNext(
-  {
-    children,
-    className,
-    disabled,
-    onKeyUp,
-    onKeyDown,
-    onClick,
-    onBlur,
-    ...rest
-  },
-  ref
-) {
-  const targetWindow = useWindow();
-  useComponentCssInjection({
-    testId: "salt-pill-next",
-    css: pillCss,
-    window: targetWindow,
-  });
-  const { buttonProps, active } = useButton<HTMLButtonElement>({
-    disabled,
-    onKeyUp,
-    onKeyDown,
-    onClick,
-    onBlur,
-  });
-  // we do not want to spread tab index in this case because the button element
-  // does not require tabindex="0" attribute
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { tabIndex, ...restButtonProps } = buttonProps;
-  return (
-    <button
-      data-testid="pill"
-      ref={ref}
-      className={clsx(
-        withBaseName(),
-        withBaseName("clickable"),
-        { [withBaseName("active")]: active },
-        className
-      )}
-      {...restButtonProps}
-      {...rest}
-    >
-      {children}
-    </button>
-  );
-});
+/* eslint-disable @typescript-eslint/no-empty-interface */
+export interface PillNextProps extends ComponentPropsWithoutRef<"button"> {}
+
+export const PillNext = forwardRef<HTMLButtonElement, PillNextProps>(
+  function PillNext(
+    {
+      children,
+      className,
+      disabled,
+      onKeyUp,
+      onKeyDown,
+      onClick,
+      onBlur,
+      ...rest
+    },
+    ref
+  ) {
+    const targetWindow = useWindow();
+    useComponentCssInjection({
+      testId: "salt-pill-next",
+      css: pillCss,
+      window: targetWindow,
+    });
+    const { buttonProps, active } = useButton<HTMLButtonElement>({
+      disabled,
+      onKeyUp,
+      onKeyDown,
+      onClick,
+      onBlur,
+    });
+    // we do not want to spread tab index in this case because the button element
+    // does not require tabindex="0" attribute
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { tabIndex, ...restButtonProps } = buttonProps;
+    return (
+      <button
+        data-testid="pill"
+        ref={ref}
+        className={clsx(
+          withBaseName(),
+          withBaseName("clickable"),
+          { [withBaseName("active")]: active },
+          className
+        )}
+        {...restButtonProps}
+        {...rest}
+      >
+        {children}
+      </button>
+    );
+  }
+);
