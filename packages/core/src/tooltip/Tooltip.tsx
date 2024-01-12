48f6c20c8ac31c888273dcdf0921dbf1b6d0a180
Fernanda Castillo <fersitacastillo123@gmail.com>
2024-01-12 15:14:45 +0000

    fix tooltip disabled status evaluator (#2901)
    Co-authored-by: Zhihao Cui <davidcui2@me.com>

--- packages/core/src/tooltip/Tooltip.tsx
+++ packages/core/src/tooltip/Tooltip.tsx
@@ -89,7 +89,8 @@ export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
       validationStatus: formFieldValidationStatus,
     } = useFormFieldProps();
 
-    const disabled = formFieldDisabled ?? disabledProp;
+    const disabled = disabledProp || formFieldDisabled;
+
     const status =
       formFieldValidationStatus !== undefined &&
       VALIDATION_NAMED_STATUS.includes(formFieldValidationStatus)
