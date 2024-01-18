a299eb2e5cd80f72ee582466f4429804e1e0b5d8
Alina Visan <alycry.alina@gmail.com>
2024-01-18 12:34:54 +0200

    fix imports

--- site/src/examples/pagination/Truncation.tsx
+++ site/src/examples/pagination/Truncation.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from "react";
-import { Pagination, Paginator } from "@salt-ds/lab";
+import { Pagination, Paginator } from "@salt-ds/core";
 
 export const Truncation = (): ReactElement => (
   <Pagination count={20} defaultPage={10}>
