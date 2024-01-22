caa3a7fc7c75a1af38d067b655a7e68232922454
origami-z <davidcui2@me.com>
2024-01-22 11:38:56 +0000

    Adds QA story for Chromatic

---
+++ packages/countries/stories/CountrySymbol.qa.stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, StoryFn } from "@storybook/react";
+import { StackLayout } from "@salt-ds/core";
+import { MX } from "@salt-ds/countries";
+import "@salt-ds/countries/saltCountries.css";
+
+export default {
+  title: "Country Symbols/Country Symbol/QA",
+} as Meta;
+
+export const CountrySymbolSizes: StoryFn = () => {
+  return (
+    <StackLayout direction="row">
+      <MX size={1} />
+      <MX size={2} />
+      <MX size={3} />
+      <MX size={4} />
+      <MX size={5} />
+    </StackLayout>
+  );
+};
+
+CountrySymbolSizes.parameters = {
+  chromatic: { disableSnapshot: false },
+};
+
+export const CssBackground: StoryFn = () => {
+  return (
+    <StackLayout direction="row">
+      <div className="saltCountry-AD" />
+      <div className="saltCountry-MX" />
+      <div className="saltCountry-GB-SCT" />
+    </StackLayout>
+  );
+};
+
+CssBackground.parameters = {
+  chromatic: { disableSnapshot: false },
+};
