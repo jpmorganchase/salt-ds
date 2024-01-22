73065866aa174f4f0b0f7e780b20ba4efc272caa
R662301 <lily.cooksley@jpmchase.com>
2024-01-22 14:36:05 +0000

    add covered border story

--- packages/core/stories/scrim/scrim.stories.tsx
+++ packages/core/stories/scrim/scrim.stories.tsx
@@ -120,3 +120,36 @@ export const Fixed: StoryFn<typeof Scrim> = () => {
     </Card>
   );
 };
+
+export const CoveredBorder: StoryFn<typeof Scrim> = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div style={{ position: "relative", width: "512px" }}>
+      <Card>
+        <StackLayout>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+          <Button onClick={handleOpen}>Click to open scrim</Button>
+        </StackLayout>
+      </Card>
+      <Scrim open={open}>
+        <Button onClick={handleClose}>Click to close scrim</Button>
+      </Scrim>
+    </div>
+  );
+};
