340ff478c47bd7fc2e2108e7b93b7f215008ae44
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2024-01-22 11:12:48 +0000

    Return orientation from useToggleButtonGroup (#2940)

--- packages/core/src/toggle-button-group/ToggleButtonGroupContext.ts
+++ packages/core/src/toggle-button-group/ToggleButtonGroupContext.ts
@@ -1,15 +1,15 @@
 import { SyntheticEvent, useContext } from "react";
 import { createContext } from "../utils";
+
+export type Value = string | readonly string[] | number | undefined;
+
 export interface ToggleButtonGroupContextValue {
   disabled?: boolean;
   select: (event: SyntheticEvent<HTMLButtonElement>) => void;
-  isSelected: (
-    id: string | ReadonlyArray<string> | number | undefined
-  ) => boolean;
-  focus: (id: string | ReadonlyArray<string> | number | undefined) => void;
-  isFocused: (
-    id: string | ReadonlyArray<string> | number | undefined
-  ) => boolean;
+  isSelected: (id: Value) => boolean;
+  focus: (id: Value) => void;
+  isFocused: (id: Value) => boolean;
+  orientation: "horizontal" | "vertical";
 }
 
 export const ToggleButtonGroupContext = createContext<
