fc22b45a539eb7dc1908bf60d6cf2b1999d4363d
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2024-01-31 11:27:29 +0000

    Change examples to use defaultValue

--- site/src/examples/multiline-input/Primary.tsx
+++ site/src/examples/multiline-input/Primary.tsx
@@ -2,5 +2,5 @@ import { ReactElement } from "react";
 import { MultilineInput } from "@salt-ds/core";
 
 export const Primary = (): ReactElement => (
-  <MultilineInput value="Value" style={{ maxWidth: "256px" }} />
+  <MultilineInput defaultValue="Value" style={{ maxWidth: "256px" }} />
 );
