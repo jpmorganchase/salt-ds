953bf8e926384af3bdee71d59ada84dc74bfbd44
Fernanda Castillo <fersitacastillo123@gmail.com>
2024-02-02 12:20:48 +0000

    updates from feedback

--- packages/lab/src/__tests__/__e2e__/progress/CircularProgress.cy.tsx
+++ packages/lab/src/__tests__/__e2e__/progress/CircularProgress.cy.tsx
@@ -1,22 +1,17 @@
 import { CircularProgress } from "../../../progress";
 
 describe("GIVEN a CircularProgress", () => {
-  it("SHOULD render progress circle", () => {
-    cy.mount(<CircularProgress />);
-    cy.get('[data-testid="circular-progress"]').should("exist");
-  });
-
   it("SHOULD render progress circle with correct value and percentage", () => {
     cy.mount(<CircularProgress value={50} />);
-    cy.get("[aria-valuemax=100]").should("exist");
-    cy.get("[aria-valuemin=0]").should("exist");
-    cy.get("[aria-valuenow=50]").should("exist");
+    cy.findByRole("progressbar").should("have.attr", "aria-valuemax", "100");
+    cy.findByRole("progressbar").should("have.attr", "aria-valuemin", "0");
+    cy.findByRole("progressbar").should("have.attr", "aria-valuenow", "50");
   });
 
   it("SHOULD render progress circle with correct min-max values", () => {
     cy.mount(<CircularProgress min={20} max={40} value={30} />);
-    cy.get("[aria-valuemax=40]").should("exist");
-    cy.get("[aria-valuemin=20]").should("exist");
-    cy.get('[data-testid="circular-progress"]').contains("50 %");
+    cy.findByRole("progressbar").should("have.attr", "aria-valuemax", "40");
+    cy.findByRole("progressbar").should("have.attr", "aria-valuemin", "20");
+    cy.findByRole("progressbar").contains("50 %");
   });
 });
--- packages/lab/src/__tests__/__e2e__/progress/LinearProgress.cy.tsx
+++ packages/lab/src/__tests__/__e2e__/progress/LinearProgress.cy.tsx
@@ -1,22 +1,17 @@
 import { LinearProgress } from "../../../progress";
 
 describe("GIVEN a LinearProgress", () => {
-  it("SHOULD render progress bar", () => {
-    cy.mount(<LinearProgress />);
-    cy.get('[data-testid="linear-progress"]').should("exist");
-  });
-
   it("SHOULD render progress bar with correct value with correct value and percentage", () => {
     cy.mount(<LinearProgress value={50} />);
-    cy.get("[aria-valuemax=100]").should("exist");
-    cy.get("[aria-valuemin=0]").should("exist");
-    cy.get("[aria-valuenow=50]").should("exist");
+    cy.findByRole("progressbar").should("have.attr", "aria-valuemax", "100");
+    cy.findByRole("progressbar").should("have.attr", "aria-valuemin", "0");
+    cy.findByRole("progressbar").should("have.attr", "aria-valuenow", "50");
   });
 
   it("SHOULD render progress bar with correct min-max values", () => {
     cy.mount(<LinearProgress min={20} max={40} value={30} />);
-    cy.get("[aria-valuemax=40]").should("exist");
-    cy.get("[aria-valuemin=20]").should("exist");
-    cy.get('[data-testid="linear-progress"]').contains("50 %");
+    cy.findByRole("progressbar").should("have.attr", "aria-valuemax", "40");
+    cy.findByRole("progressbar").should("have.attr", "aria-valuemin", "20");
+    cy.findByRole("progressbar").contains("50 %");
   });
 });
