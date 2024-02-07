d1927f91f635a68953ff1b60b0692576dd7f90ae
Luke Atkinson-Coyle <luke.atkinson-coyle@outlook.com>
2024-02-07 16:21:53 +0000

    fix test

--- packages/lab/src/__tests__/__e2e__/dialog/Dialog.cy.tsx
+++ packages/lab/src/__tests__/__e2e__/dialog/Dialog.cy.tsx
@@ -3,14 +3,14 @@ import * as dialogStories from "@stories/dialog/dialog.stories";
 
 const composedStories = composeStories(dialogStories);
 
-const { Default, InfoStatus } = composedStories;
+const { Default } = composedStories;
 
 describe("GIVEN a Dialog", () => {
   describe("WHEN no props are provided", () => {
     it("THEN it should display a dialog by default", () => {
       cy.mount(<Default />);
 
-      cy.get(".saltButton").click();
+      cy.findByRole("button").click();
 
       cy.findByRole("dialog").should("be.visible");
       cy.get(".saltDialogTitle").should("be.visible");
@@ -20,7 +20,7 @@ describe("GIVEN a Dialog", () => {
 
     it("THEN it should add the accent class to the title component", () => {
       cy.mount(<Default />);
-      cy.get(".saltButton").click();
+      cy.findByRole("button").click();
       cy.findByRole("dialog").should("be.visible");
       cy.get(".saltDialogTitle-withAccent").should("exist");
     });
@@ -28,7 +28,7 @@ describe("GIVEN a Dialog", () => {
     it("THEN it should display animations by default", () => {
       cy.mount(<Default />);
 
-      cy.get(".saltButton").click();
+      cy.findByRole("button").click();
 
       cy.findByRole("dialog").should("have.class", "saltDialog-enterAnimation");
     });
@@ -42,7 +42,7 @@ describe("GIVEN a Dialog", () => {
       () => {
         cy.mount(<Default />);
 
-        cy.get(".saltButton").click();
+        cy.findByRole("button").click();
 
         cy.findByRole("dialog").should("have.class", "saltDialog-medium-xl");
       }
@@ -59,7 +59,7 @@ describe("GIVEN a Dialog", () => {
       () => {
         cy.mount(<Default size={"large"} />);
 
-        cy.get(".saltButton").click();
+        cy.findByRole("button").click();
 
         cy.findByRole("dialog").should("have.class", "saltDialog-large-xl");
       }
@@ -74,9 +74,9 @@ describe("GIVEN a Dialog", () => {
       () => {
         cy.mount(<Default size={"small"} />);
 
-        cy.get(".saltButton").click();
+        cy.findByRole("button").click();
 
-        cy.get(".saltDialog").should("have.class", "saltDialog-small-xs");
+        cy.findByRole("dialog").should("have.class", "saltDialog-small-xs");
       }
     );
   });
@@ -85,7 +85,7 @@ describe("GIVEN a Dialog", () => {
     it("THEN it should close when the close button is clicked", () => {
       cy.mount(<Default />);
 
-      cy.get(".saltButton").click();
+      cy.findByRole("button").click();
 
       cy.findByRole("dialog").should("be.visible");
 
@@ -97,27 +97,27 @@ describe("GIVEN a Dialog", () => {
     it("THEN it should close when the ESC key is pressed", () => {
       cy.mount(<Default />);
 
-      cy.get(".saltButton").click();
+      cy.findByRole("button").click();
 
-      cy.get(".saltDialog").should("be.visible");
+      cy.findByRole("dialog").should("be.visible");
 
       cy.realPress("Escape");
 
-      cy.get(".saltDialog").should("not.exist");
+      cy.findByRole("dialog").should("not.exist");
     });
 
     it("THEN it should close when clicking outside the dialog", () => {
-      cy.mount(<InfoStatus />);
-      cy.get(".saltButton").click();
-      cy.get(".saltDialog").should("be.visible");
-      cy.get(".saltScrim").click("left");
-      cy.get(".saltDialog").should("not.exist");
+      cy.mount(<Default />);
+      cy.findByRole("button").click();
+      cy.findByRole("dialog").should("be.visible");
+      cy.get(".saltScrim").click("left", { force: true });
+      cy.findByRole("dialog").should("not.exist");
     });
 
     it("THEN it should trap focus inside the Dialog", () => {
       cy.mount(<Default />);
-      cy.get(".saltButton").click();
-      cy.get(".saltDialog").should("be.visible");
+      cy.findByRole("button").click();
+      cy.findByRole("dialog").should("be.visible");
       cy.findAllByRole("button").eq(0).should("be.focused");
       cy.realPress("Tab");
       cy.findAllByRole("button").eq(1).should("be.focused");
