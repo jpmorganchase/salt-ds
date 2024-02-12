123368b179e87a41bf39c45a706bd5ee2c164a70
Zhihao Cui <davidcui2@me.com>
2024-02-12 11:47:27 +0000

    Prettier

--- packages/core/stories/accordion/accordion.stories.tsx
+++ packages/core/stories/accordion/accordion.stories.tsx
@@ -19,31 +19,31 @@ export default {
   component: Accordion,
   // Default is centered, but accordion will jump around when interacting
   parameters: {
-    layout: 'padded',
+    layout: "padded",
   },
 } as Meta<typeof Accordion>;
 
 export const Default: StoryFn<AccordionProps> = (props) => (
-    <Accordion {...props}>
-      <AccordionHeader>Internal form</AccordionHeader>
-      <AccordionPanel>
-        <FlowLayout>
-          Please fill out the following details.
-          <FormField labelPlacement="left">
-            <FormLabel>Disclosure ID</FormLabel>
-            <Input />
-          </FormField>
-          <FormField labelPlacement="left">
-            <FormLabel>Email</FormLabel>
-            <Input />
-          </FormField>
-          <FormField labelPlacement="left">
-            <FormLabel>Justification</FormLabel>
-            <Input />
-          </FormField>
-        </FlowLayout>
-      </AccordionPanel>
-    </Accordion>
+  <Accordion {...props}>
+    <AccordionHeader>Internal form</AccordionHeader>
+    <AccordionPanel>
+      <FlowLayout>
+        Please fill out the following details.
+        <FormField labelPlacement="left">
+          <FormLabel>Disclosure ID</FormLabel>
+          <Input />
+        </FormField>
+        <FormField labelPlacement="left">
+          <FormLabel>Email</FormLabel>
+          <Input />
+        </FormField>
+        <FormField labelPlacement="left">
+          <FormLabel>Justification</FormLabel>
+          <Input />
+        </FormField>
+      </FlowLayout>
+    </AccordionPanel>
+  </Accordion>
 );
 
 Default.args = {
@@ -51,30 +51,30 @@ Default.args = {
 };
 
 export const DefaultGroup: StoryFn<AccordionGroupProps> = (props) => (
-    <AccordionGroup {...props}>
-      {Array.from({ length: 3 }, (_, i) => i + 1).map((i) => (
-        <Accordion value={`accordion-${i}`} key={`accordion-${i}`}>
-          <AccordionHeader>Internal form</AccordionHeader>
-          <AccordionPanel>
-            <FlowLayout>
-              Please fill out the following details.
-              <FormField labelPlacement="left">
-                <FormLabel>Disclosure ID</FormLabel>
-                <Input />
-              </FormField>
-              <FormField labelPlacement="left">
-                <FormLabel>Email</FormLabel>
-                <Input />
-              </FormField>
-              <FormField labelPlacement="left">
-                <FormLabel>Justification</FormLabel>
-                <Input />
-              </FormField>
-            </FlowLayout>
-          </AccordionPanel>
-        </Accordion>
-      ))}
-    </AccordionGroup>
+  <AccordionGroup {...props}>
+    {Array.from({ length: 3 }, (_, i) => i + 1).map((i) => (
+      <Accordion value={`accordion-${i}`} key={`accordion-${i}`}>
+        <AccordionHeader>Internal form</AccordionHeader>
+        <AccordionPanel>
+          <FlowLayout>
+            Please fill out the following details.
+            <FormField labelPlacement="left">
+              <FormLabel>Disclosure ID</FormLabel>
+              <Input />
+            </FormField>
+            <FormField labelPlacement="left">
+              <FormLabel>Email</FormLabel>
+              <Input />
+            </FormField>
+            <FormField labelPlacement="left">
+              <FormLabel>Justification</FormLabel>
+              <Input />
+            </FormField>
+          </FlowLayout>
+        </AccordionPanel>
+      </Accordion>
+    ))}
+  </AccordionGroup>
 );
 
 export const ExclusiveGroup: StoryFn<AccordionGroupProps> = (props) => {
@@ -86,45 +86,13 @@ export const ExclusiveGroup: StoryFn<AccordionGroupProps> = (props) => {
   };
 
   return (
-      <AccordionGroup {...props}>
-        {Array.from({ length: 3 }, (_, i) => i + 1).map((i) => (
-          <Accordion
-            value={`accordion-${i}`}
-            expanded={expanded === `accordion-${i}`}
-            onToggle={onChange}
-            key={`accordion-${i}`}
-          >
-            <AccordionHeader>Internal form</AccordionHeader>
-            <AccordionPanel>
-              <FlowLayout>
-                Please fill out the following details.
-                <FormField labelPlacement="left">
-                  <FormLabel>Disclosure ID</FormLabel>
-                  <Input />
-                </FormField>
-                <FormField labelPlacement="left">
-                  <FormLabel>Email</FormLabel>
-                  <Input />
-                </FormField>
-                <FormField labelPlacement="left">
-                  <FormLabel>Justification</FormLabel>
-                  <Input />
-                </FormField>
-              </FlowLayout>
-            </AccordionPanel>
-          </Accordion>
-        ))}
-      </AccordionGroup>
-  );
-};
-
-export const Disabled: StoryFn<AccordionGroupProps> = (props) => (
     <AccordionGroup {...props}>
       {Array.from({ length: 3 }, (_, i) => i + 1).map((i) => (
         <Accordion
           value={`accordion-${i}`}
+          expanded={expanded === `accordion-${i}`}
+          onToggle={onChange}
           key={`accordion-${i}`}
-          disabled={i === 2}
         >
           <AccordionHeader>Internal form</AccordionHeader>
           <AccordionPanel>
@@ -147,6 +115,38 @@ export const Disabled: StoryFn<AccordionGroupProps> = (props) => (
         </Accordion>
       ))}
     </AccordionGroup>
+  );
+};
+
+export const Disabled: StoryFn<AccordionGroupProps> = (props) => (
+  <AccordionGroup {...props}>
+    {Array.from({ length: 3 }, (_, i) => i + 1).map((i) => (
+      <Accordion
+        value={`accordion-${i}`}
+        key={`accordion-${i}`}
+        disabled={i === 2}
+      >
+        <AccordionHeader>Internal form</AccordionHeader>
+        <AccordionPanel>
+          <FlowLayout>
+            Please fill out the following details.
+            <FormField labelPlacement="left">
+              <FormLabel>Disclosure ID</FormLabel>
+              <Input />
+            </FormField>
+            <FormField labelPlacement="left">
+              <FormLabel>Email</FormLabel>
+              <Input />
+            </FormField>
+            <FormField labelPlacement="left">
+              <FormLabel>Justification</FormLabel>
+              <Input />
+            </FormField>
+          </FlowLayout>
+        </AccordionPanel>
+      </Accordion>
+    ))}
+  </AccordionGroup>
 );
 
 const statuses: AccordionProps["status"][] = [
@@ -157,32 +157,32 @@ const statuses: AccordionProps["status"][] = [
 ];
 
 export const Status: StoryFn<AccordionGroupProps> = (props) => (
-    <AccordionGroup {...props}>
-      {Array.from({ length: 3 }, (_, i) => i + 1).map((i) => (
-        <Accordion
-          value={`accordion-${i}`}
-          key={`accordion-${i}`}
-          status={statuses[i]}
-        >
-          <AccordionHeader>Internal form</AccordionHeader>
-          <AccordionPanel>
-            <FlowLayout>
-              Please fill out the following details.
-              <FormField labelPlacement="left">
-                <FormLabel>Disclosure ID</FormLabel>
-                <Input />
-              </FormField>
-              <FormField labelPlacement="left">
-                <FormLabel>Email</FormLabel>
-                <Input />
-              </FormField>
-              <FormField labelPlacement="left">
-                <FormLabel>Justification</FormLabel>
-                <Input />
-              </FormField>
-            </FlowLayout>
-          </AccordionPanel>
-        </Accordion>
-      ))}
-    </AccordionGroup>
+  <AccordionGroup {...props}>
+    {Array.from({ length: 3 }, (_, i) => i + 1).map((i) => (
+      <Accordion
+        value={`accordion-${i}`}
+        key={`accordion-${i}`}
+        status={statuses[i]}
+      >
+        <AccordionHeader>Internal form</AccordionHeader>
+        <AccordionPanel>
+          <FlowLayout>
+            Please fill out the following details.
+            <FormField labelPlacement="left">
+              <FormLabel>Disclosure ID</FormLabel>
+              <Input />
+            </FormField>
+            <FormField labelPlacement="left">
+              <FormLabel>Email</FormLabel>
+              <Input />
+            </FormField>
+            <FormField labelPlacement="left">
+              <FormLabel>Justification</FormLabel>
+              <Input />
+            </FormField>
+          </FlowLayout>
+        </AccordionPanel>
+      </Accordion>
+    ))}
+  </AccordionGroup>
 );
