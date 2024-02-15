fa7659502b83e3b50c98962fceee7ec341656235
R662301 <lily.cooksley@jpmchase.com>
2024-02-15 18:08:30 +0400

    simplify examples

--- site/src/examples/card/Accent.tsx
+++ site/src/examples/card/Accent.tsx
@@ -1,65 +1,39 @@
-import { ReactElement, useState, ChangeEvent } from "react";
+import { useState } from "react";
 import {
   H3,
   Text,
   StackLayout,
   Card,
   CardProps,
-  RadioButton,
   RadioButtonGroup,
+  RadioButton,
   Link,
 } from "@salt-ds/core";
 
-export const Accent = (): ReactElement => {
+export const Accent = () => {
   const [placement, setPlacement] = useState<CardProps["accent"]>("top");
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setPlacement(event.target.value as CardProps["accent"]);
-  };
 
   return (
     <StackLayout style={{ width: "266px" }} align="center">
       <Card accent={placement} hoverable>
-        <StackLayout>
-          <StackLayout gap={1}>
-            <H3>Sustainable investing products</H3>
-            <Text>
-              We have a commitment to provide a wide range of investment
-              solutions to enable you to align your financial goals to your
-              values.
-            </Text>
-          </StackLayout>
-          <Link href="#" IconComponent={null}>
-            Learn more
-          </Link>
+        <StackLayout gap={1}>
+          <H3>Sustainable investing products</H3>
+          <Text>
+            We offer a variety of investment solutions that let you align your
+            financial goals with your values.
+          </Text>
+          <Link href="#">Learn more</Link>
         </StackLayout>
       </Card>
-      <RadioButtonGroup direction={"horizontal"} defaultValue="top">
-        <RadioButton
-          key="top"
-          label="top"
-          value="top"
-          onChange={handleChange}
-        />
-        <RadioButton
-          key="right"
-          label="right"
-          value="right"
-          onChange={handleChange}
-        />
-        <RadioButton
-          key="bottom"
-          label="bottom"
-          value="bottom"
-          onChange={handleChange}
-          checked
-        />
-        <RadioButton
-          key="left"
-          label="left"
-          value="left"
-          onChange={handleChange}
-        />
+      <RadioButtonGroup
+        value={placement}
+        onChange={(e) => setPlacement(e.target.value as CardProps["accent"])}
+        direction="horizontal"
+      >
+        <RadioButton label="top" value="top" />
+        <RadioButton label="right" value="right" />
+        <RadioButton label="bottom" value="bottom" />
+        <RadioButton label="left" value="left" />
       </RadioButtonGroup>
     </StackLayout>
   );
--- site/src/examples/card/CustomPadding.tsx
+++ site/src/examples/card/CustomPadding.tsx
@@ -1,77 +1,49 @@
-import { ReactElement, useState, ChangeEvent } from "react";
+import { ReactElement, useState, CSSProperties } from "react";
 import {
   H3,
   Text,
   StackLayout,
   Card,
-  RadioButton,
   RadioButtonGroup,
+  RadioButton,
 } from "@salt-ds/core";
 
 export const CustomPadding = (): ReactElement => {
-  const [padding, setPadding] = useState<string>("var(--salt-spacing-200)");
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setPadding(event.target.value);
-  };
+  const [padding, setPadding] = useState("var(--salt-spacing-200)");
 
   return (
     <StackLayout align="center">
       <Card
         style={
-          {
-            "--saltCard-padding": padding,
-            width: "260px",
-          } as React.CSSProperties
+          { "--saltCard-padding": padding, width: "260px" } as CSSProperties
         }
       >
         <img
           src="/img/examples/cardExample.jpeg"
-          style={{
-            paddingBottom: padding,
-            width: "100%",
-          }}
+          style={{ width: "100%", paddingBottom: padding }}
         />
         <StackLayout
           gap={1}
           // Apply padding around the content below the image for a full width image
-          style={{ padding: padding == "0" ? "var(--salt-spacing-200)" : 0 }}
+          style={{
+            padding: padding === "0" ? "var(--salt-spacing-200)" : undefined,
+          }}
         >
           <H3>Sustainable investing products</H3>
           <Text>
-            We have a commitment to provide a wide range of investment solutions
-            to enable you to align your financial goals to your values.
+            Investment solutions to align your financial goals with your values.
           </Text>
         </StackLayout>
       </Card>
       <RadioButtonGroup
-        direction={"horizontal"}
-        defaultValue="var(--salt-spacing-200)"
+        value={padding}
+        onChange={(e) => setPadding(e.target.value)}
+        direction="horizontal"
       >
-        <RadioButton
-          key="default"
-          label="default"
-          value="var(--salt-spacing-200)"
-          onChange={handleChange}
-        />
-        <RadioButton
-          key="spacing-300"
-          label="spacing-300"
-          value="var(--salt-spacing-300)"
-          onChange={handleChange}
-        />
-        <RadioButton
-          key="spacing-100"
-          label="spacing-100"
-          value="var(--salt-spacing-100)"
-          onChange={handleChange}
-        />
-        <RadioButton
-          key="fullWidthImage"
-          label="full width image"
-          value="0"
-          onChange={handleChange}
-        />
+        <RadioButton label="default" value="var(--salt-spacing-200)" />
+        <RadioButton label="spacing-300" value="var(--salt-spacing-300)" />
+        <RadioButton label="spacing-100" value="var(--salt-spacing-100)" />
+        <RadioButton label="full width image" value="0" />
       </RadioButtonGroup>
     </StackLayout>
   );
