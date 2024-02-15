8c68b616a182efc9a33bc3c280673e7334965178
R662301 <lily.cooksley@jpmchase.com>
2024-02-15 17:32:10 +0400

    add tests

---
+++ packages/lab/src/__tests__/__e2e__/link-card/LinkCard.cy.tsx
@@ -0,0 +1,23 @@
+import { composeStories } from "@storybook/react";
+import * as linkCardStories from "@stories/link-card/link-card.stories";
+import { checkAccessibility } from "../../../../../../cypress/tests/checkAccessibility";
+
+const composedStories = composeStories(linkCardStories);
+const { Default, AccentPlacement } = composedStories;
+
+describe("Given a Link Card", () => {
+  checkAccessibility(composedStories);
+
+  it("should render children", () => {
+    cy.mount(<Default />);
+    cy.findByText("Sustainable investing products").should("be.visible");
+    cy.findByText(
+      "We have a commitment to provide a wide range of investment solutions to enable you to align your financial goals to your values."
+    ).should("be.visible");
+  });
+
+  it("should navigate to the correct href", () => {
+    cy.mount(<Default />);
+    cy.get("a").should("have.attr", "href", "#");
+  });
+});
