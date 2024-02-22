fc915775ab7a7167a7be2742fe651d9681e443a5
feliciackh <54270624+feliciackh@users.noreply.github.com>
2024-02-22 11:43:52 +0000

    Drawer to RC (#2960)
    Co-authored-by: Luke Atkinson-Coyle <luke.atkinson-coyle@outlook.com>
    Co-authored-by: lukeac123 <96307810+lukeac123@users.noreply.github.com>
    Co-authored-by: Fernanda Castillo <fersitacastillo123@gmail.com>
    Co-authored-by: mikearildbrown <106546872+mikearildbrown@users.noreply.github.com>

--- packages/lab/src/drawer/index.ts
+++ packages/lab/src/drawer/index.ts
@@ -1,2 +1,2 @@
 export * from "./Drawer";
-export * from "./useDrawer";
+export * from "./DrawerCloseButton";
---
+++ site/src/examples/drawer/Default.tsx
@@ -0,0 +1,33 @@
+import { ReactElement, useState } from "react";
+
+import { Drawer, DrawerCloseButton } from "@salt-ds/lab";
+import { Button, StackLayout } from "@salt-ds/core";
+
+export const Default = (): ReactElement => {
+  const [openPrimary, setOpenPrimary] = useState(false);
+  const [openSecondary, setOpenSecondary] = useState(false);
+
+  return (
+    <StackLayout>
+      <Button onClick={() => setOpenPrimary(true)}>Open Primary Drawer</Button>
+      <Drawer
+        open={openPrimary}
+        onOpenChange={(newOpen) => setOpenPrimary(newOpen)}
+        style={{ width: 200 }}
+      >
+        <DrawerCloseButton onClick={() => setOpenPrimary(false)} />
+      </Drawer>
+      <Button onClick={() => setOpenSecondary(true)}>
+        Open Secondary Drawer
+      </Button>
+      <Drawer
+        open={openSecondary}
+        onOpenChange={(newOpen) => setOpenSecondary(newOpen)}
+        variant="secondary"
+        style={{ width: 200 }}
+      >
+        <DrawerCloseButton onClick={() => setOpenSecondary(false)} />
+      </Drawer>
+    </StackLayout>
+  );
+};
