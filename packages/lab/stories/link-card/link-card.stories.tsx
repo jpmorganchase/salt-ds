5e108edf67f9f66c32b2157295f717e25b0e36d9
lilyvc <45168453+lilyvc@users.noreply.github.com>
2024-02-29 11:21:41 +0000

    add args to stories

--- packages/lab/stories/link-card/link-card.stories.tsx
+++ packages/lab/stories/link-card/link-card.stories.tsx
@@ -17,30 +17,30 @@ export default {
   component: LinkCard,
 } as Meta<typeof LinkCard>;
 
-const exampleData = {
-  title: "Sustainable investing products",
-  content:
-    "We have a commitment to provide a wide range of investment solutions to enable you to align your financial goals to your values.",
-};
-
-export const Default: StoryFn<typeof Card> = () => (
-  <LinkCard style={{ width: "260px" }} href="#" target="_blank">
+export const Default: StoryFn<typeof LinkCard> = (args) => (
+  <LinkCard {...args} style={{ width: "260px" }} href="#" target="_blank">
     <StackLayout gap={1}>
-      <H3>{exampleData.title}</H3>
-      <Text>{exampleData.content}</Text>
+      <H3>Sustainable investing products</H3>
+      <Text>
+        We have a commitment to provide a wide range of investment solutions to
+        enable you to align your financial goals to your values.
+      </Text>
     </StackLayout>
   </LinkCard>
 );
 
-export const AccentPlacement: StoryFn<typeof Card> = () => {
+export const AccentPlacement: StoryFn<typeof LinkCard> = (args) => {
   const [placement, setPlacement] = useState<LinkCardProps["accent"]>("bottom");
 
   return (
     <StackLayout style={{ width: "266px" }}>
-      <LinkCard accent={placement}>
+      <LinkCard {...args} accent={placement}>
         <StackLayout gap={1}>
-          <H3>{exampleData.title}</H3>
-          <Text>{exampleData.content}</Text>
+          <H3>Sustainable investing products</H3>
+          <Text>
+            We have a commitment to provide a wide range of investment solutions
+            to enable you to align your financial goals to your values.
+          </Text>
         </StackLayout>
       </LinkCard>
       <RadioButtonGroup
@@ -59,7 +59,7 @@ export const AccentPlacement: StoryFn<typeof Card> = () => {
   );
 };
 
-export const Variant: StoryFn<typeof Card> = () => {
+export const Variant: StoryFn<typeof LinkCard> = (args) => {
   const variants = ["primary", "secondary"] as const;
   return (
     <StackLayout style={{ width: "266px" }}>
@@ -67,10 +67,14 @@ export const Variant: StoryFn<typeof Card> = () => {
         return (
           <StackLayout align="end" key={variant}>
             <StackLayout direction="row">
-              <LinkCard variant={variant}>
+              <LinkCard {...args} variant={variant}>
                 <StackLayout gap={1}>
-                  <H3>{exampleData.title}</H3>
-                  <Text>{exampleData.content}</Text>
+                  <H3>Sustainable investing products</H3>
+                  <Text>
+                    We have a commitment to provide a wide range of investment
+                    solutions to enable you to align your financial goals to
+                    your values.
+                  </Text>
                 </StackLayout>
               </LinkCard>
             </StackLayout>
