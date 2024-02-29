5953a343a58d6920cabb2e8c39dc0e70f85bfda7
lilyvc <45168453+lilyvc@users.noreply.github.com>
2024-02-29 11:23:59 +0000

    add qa stories

---
+++ packages/lab/stories/link-card/link-card.qa.stories.tsx
@@ -0,0 +1,62 @@
+import { H1, Text } from "@salt-ds/core";
+
+import { LinkCard } from "@salt-ds/lab";
+import { Meta, StoryFn } from "@storybook/react";
+import {
+  QAContainer,
+  QAContainerNoStyleInjection,
+  QAContainerNoStyleInjectionProps,
+  QAContainerProps,
+} from "docs/components";
+
+export default {
+  title: "Lab/Link Card/QA",
+  component: LinkCard,
+} as Meta<typeof LinkCard>;
+
+export const AllExamplesUsingText: StoryFn<
+  QAContainerProps & { className?: string }
+> = (props) => {
+  return (
+    <QAContainer itemPadding={10} itemWidthAuto {...props}>
+      <LinkCard>
+        <H1>Card with density</H1>
+        <Text>Content</Text>
+      </LinkCard>
+      <LinkCard variant="secondary">
+        <H1>Secondary card with density</H1>
+        <Text>Content</Text>
+      </LinkCard>
+      <LinkCard accent="top">
+        <H1>Card with accent and density</H1>
+        <Text>Content</Text>
+      </LinkCard>
+      <LinkCard variant="secondary" accent="bottom">
+        <H1>Secondary card with accent and density</H1>
+        <Text>Content</Text>
+      </LinkCard>
+    </QAContainer>
+  );
+};
+AllExamplesUsingText.parameters = {
+  chromatic: { disableSnapshot: false },
+};
+
+export const NoStyleInjectionGrid: StoryFn<QAContainerNoStyleInjectionProps> = (
+  props
+) => (
+  <QAContainerNoStyleInjection itemPadding={10} itemWidthAuto {...props}>
+    <LinkCard>
+      <H1>Card with density</H1>
+      <Text>Content</Text>
+    </LinkCard>
+    <LinkCard variant="secondary">
+      <H1>Secondary card with density</H1>
+      <Text>Content</Text>
+    </LinkCard>
+  </QAContainerNoStyleInjection>
+);
+
+NoStyleInjectionGrid.parameters = {
+  chromatic: { disableSnapshot: false },
+};
--- packages/lab/stories/link-card/link-card.stories.tsx
+++ packages/lab/stories/link-card/link-card.stories.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Meta, StoryFn } from "@storybook/react";
 import {
-  Card,
   H3,
   Text,
   StackLayout,
