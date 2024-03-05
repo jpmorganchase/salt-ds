5d1a1a2fbbfcca5c52e077b379b3db8d6ef54fd2
lilyvc <45168453+lilyvc@users.noreply.github.com>
2024-03-05 11:31:16 +0000

    fix story

--- site/src/examples/card/ToggleGroup.tsx
+++ site/src/examples/card/ToggleGroup.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from "react";
 import { InteractableCard, InteractableCardGroup } from "@salt-ds/lab";
-import { Checkbox, H3, StackLayout, Text } from "@salt-ds/core";
+import { H3, StackLayout, Text } from "@salt-ds/core";
 import { BankIcon, CreditCardIcon, DiamondIcon } from "@salt-ds/icons";
 
 export const ToggleGroup = (): ReactElement => (
@@ -26,36 +26,6 @@ export const ToggleGroup = (): ReactElement => (
             <H3>Bank wire</H3>
           </StackLayout>
           <Text>Link your bank to your payments account.</Text>
-        </StackLayout>
-      </InteractableCard>
-      <InteractableCard value="crypto" accent="top" style={{ width: "180px" }}>
-        <StackLayout gap={1}>
-          <StackLayout gap={1} direction="row" align="center">
-            <DiamondIcon size={2} />
-            <H3>Cryptocurrency</H3>
-          </StackLayout>
-          <Text>Link your crypto to your payments account.</Text>
-        </StackLayout>
-      </InteractableCard>
-    </InteractableCardGroup>
-    <InteractableCardGroup selectionVariant="multiselect">
-      <InteractableCard value="card" accent="top" style={{ width: "180px" }}>
-        <StackLayout gap={1}>
-          <StackLayout gap={1} direction="row" align="center">
-            <CreditCardIcon size={2} />
-            <H3>Credit Card</H3>
-          </StackLayout>
-          <Text>Link credit card to your payments account.</Text>
-        </StackLayout>
-      </InteractableCard>
-      <InteractableCard value="wire" accent="top" style={{ width: "180px" }}>
-        <StackLayout gap={1}>
-          <StackLayout gap={1} direction="row" align="center">
-            <BankIcon size={2} />
-            <H3>Bank wire</H3>
-          </StackLayout>
-          <Text>Link your bank to your payments account.</Text>
-          <Checkbox>Select</Checkbox>
         </StackLayout>
       </InteractableCard>
       <InteractableCard value="crypto" accent="top" style={{ width: "180px" }}>
