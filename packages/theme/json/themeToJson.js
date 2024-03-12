5fcb97f58e1cf4de66b67dd0a30812e0586b60e2
libertymayc <liberty.curtis@live.co.uk>
2024-03-12 16:42:08 -0400

    include density and mode in json

--- packages/theme/json/themeToJson.js
+++ packages/theme/json/themeToJson.js
@@ -1,39 +1,6 @@
-const fs = require("fs");
-const path = require("path");
 const util = require("util");
-
-function getCssVariablesFromDir(dirPath) {
-  const cssVariableRegex = /([a-zA-Z0-9_-]+)\s*:\s*([^;]+)/g;
-  const cssVariables = {};
-
-  const files = fs.readdirSync(dirPath);
-
-  files.forEach((file) => {
-    const filePath = path.join(dirPath, file);
-    const stats = fs.statSync(filePath);
-
-    if (stats.isDirectory()) {
-      // Recursively process subdirectories
-      Object.assign(cssVariables, getCssVariablesFromDir(filePath));
-    } else if (
-      stats.isFile() &&
-      path.extname(file) === ".css" &&
-      !filePath.includes("animation")
-    ) {
-      // Process CSS files
-      const cssContent = fs.readFileSync(filePath, "utf8");
-      let match;
-
-      while ((match = cssVariableRegex.exec(cssContent)) !== null) {
-        const variableName = match[1];
-        const variableValue = match[2].trim();
-        cssVariables[variableName] = variableValue;
-      }
-    }
-  });
-
-  return cssVariables;
-}
+const path = require("path");
+const getCssVariablesFromDir = require("./getCssVariablesFromDir");
 
 /* Removes surrounding CSS var('...') function from a token */
 function stripVarFunc(cssVar) {
@@ -45,156 +12,169 @@ function removePrefix(cssVar) {
   return cssVar.replace("--salt-", "");
 }
 
-/* Adds curly brackets around token */
-function formatValue(cssVar) {
-  return `{${removePrefix(cssVar)}}`;
+const jsonTokens = { palette: {}, foundations: {} };
+/*
+ * Adds token to JSON schema
+ * @param key - Mode or density the token belongs to, or 'general' if it has the same value in all modes and densities
+ * @param themeLevel - One of 'palette', 'foundations', 'characteristics'
+ * @param semantic - Key within theme level, e.g. 'accent', 'color', 'size', etc
+ * @param tokenName - Name of the token
+ * @param type - Token type, see https://design-tokens.github.io/community-group/format/#types
+ * @tokenValue - Value of token (alias, e.g. {color.white}, or raw value, e.g. '4px')
+ **/
+function addToJson(key, themeLevel, semantic, tokenName, type, tokenValue) {
+  if (key !== "general") {
+    // In this case, value will belong to either a mode or density,
+    // ensure to keep all values within the field
+    tokenValue = {
+      [key]: tokenValue,
+      ...(jsonTokens[themeLevel][semantic] &&
+        jsonTokens[themeLevel][semantic][tokenName] && {
+          ...jsonTokens[themeLevel][semantic][tokenName].$value,
+        }),
+    };
+  }
+  jsonTokens[themeLevel][semantic] = {
+    ...jsonTokens[themeLevel][semantic],
+    [tokenName]: {
+      $value: tokenValue,
+      $type: type,
+    },
+  };
+}
+
+function formatFoundationValue(semantic, tokenValue) {
+  const alias = removePrefix(stripVarFunc(tokenValue));
+
+  let foundationSemantic;
+  switch (semantic) {
+    case "corner":
+      foundationSemantic = "curve";
+    case "opacity":
+      foundationSemantic = "opacity";
+    default:
+      foundationSemantic = "color";
+  }
+
+  return `{foundations.${foundationSemantic}.${alias}}`;
 }
 
 function format(variables) {
-  const jsonTokens = { palette: {}, foundations: {} };
+  for (const key in variables) {
+    for (const token of Object.entries(variables[key])) {
+      var tokenName = removePrefix(token[0]);
+      var tokenValue = token[1];
+      let type;
 
-  for (const token of Object.entries(variables)) {
-    var tokenName = removePrefix(token[0]);
-    var tokenValue = token[1];
-    let type;
-    let rawValue;
-    let calculation;
-
-    if (tokenName.startsWith("palette")) {
-      const semantic = tokenName.split("-")[1];
-      if (semantic === "corner") {
-        type = "dimension";
-      } else if (semantic === "opacity") {
-        type = "number";
+      if (tokenName.startsWith("palette")) {
+        const semantic = tokenName.split("-")[1];
+        switch (semantic) {
+          case "corner":
+            type = "dimension";
+            break;
+          case "opacity":
+            type = "number";
+            break;
+          default:
+            type = "color";
+        }
+        tokenValue =
+          !tokenValue !== "transparent"
+            ? formatFoundationValue(semantic, tokenValue)
+            : tokenValue;
+        addToJson(key, "palette", semantic, tokenName, type, tokenValue);
       } else {
-        type = "color";
-      }
-      const aliasToken = stripVarFunc(tokenValue);
-      let rawValue = variables[`${aliasToken}`];
-      if (rawValue && rawValue.includes("opacity")) {
-        let opacityToken = rawValue.split(",")[3];
-        let opacityValue = variables[stripVarFunc(opacityToken)];
-        rawValue =
-          rawValue.split(",").slice(0, 3).join(",") +
-          `, ${
-            opacityValue.includes("opacity")
-              ? variables[stripVarFunc(opacityValue)]
-              : opacityValue
-          })`;
-      }
-      jsonTokens["palette"][semantic] = {
-        [tokenName]: {
-          $value: !rawValue
-            ? "transparent"
-            : `{foundations.${
-                semantic === "corner"
-                  ? "curve"
-                  : semantic === "opacity"
-                  ? "opacity"
-                  : "color"
-              }.${removePrefix(aliasToken)}}`,
-          $type: type,
-          ...(rawValue && {
-            $rawValue: rawValue,
-          }),
-        },
-        ...jsonTokens["palette"][semantic],
-      };
-    } else {
-      const semantic = tokenName.split("-")[0];
-      if (["curve", "size"].includes(semantic)) {
-        type = "dimension";
-      } else if (semantic === "spacing") {
-        type = "dimension";
-        if (tokenValue.includes("calc")) {
-          let [multiplier, multipleToken] = tokenValue
-            .replace("calc(", "")
-            .replace(")", "")
-            .split(" * ");
-          multipleToken = variables[stripVarFunc(multipleToken)].replace(
-            "px",
-            ""
-          );
-          calculation = removePrefix(stripVarFunc(tokenValue))
-            .replace("spacing", `{spacing`)
-            .replace("100", "100}")
-            .replace("calc(", "");
-          tokenValue = `${multiplier * multipleToken}`;
+        const semantic = tokenName.split("-")[0];
+        switch (semantic) {
+          case "curve":
+            type = "dimension";
+            break;
+          case "size":
+            type = "dimension";
+            break;
+          case "spacing":
+            type = "dimension";
+            if (tokenValue.includes("calc")) {
+              let [multiplier, multipleToken] = tokenValue
+                .replace("calc(", "")
+                .replace(")", "")
+                .split(" * ");
+              multipleToken = variables["general"][
+                stripVarFunc(multipleToken)
+              ].replace("px", "");
+              tokenValue = removePrefix(stripVarFunc(tokenValue))
+                .replace("spacing", `{spacing`)
+                .replace("100", "100}")
+                .replace("calc(", "");
+            }
+            break;
+          case "opacity":
+            type = "number";
+            break;
+          case "zIndex":
+            type = "number";
+            break;
+          case "duration":
+            type = "duration";
+            break;
+          case "typography":
+            const attribute = tokenName.split("-")[1];
+            type = attribute;
+            break;
+          case "shadow":
+            if (tokenName.includes("color")) {
+              type = "color";
+            } else {
+              type = "shadow";
+              const [offsetX, offsetY, blur, spread, color] =
+                tokenValue.split(" ");
+              tokenValue = {
+                offsetX: offsetX,
+                offsetY: offsetY,
+                blur: blur,
+                spread: spread,
+                color: `{foundations.color.${removePrefix(
+                  stripVarFunc(color)
+                )}}`,
+              };
+            }
+            break;
+          case "color":
+            if (tokenName.includes("fade")) {
+              type = "color";
+              const colorToken = `${tokenName.split("-fade")[0]}`;
+              const opacityValue = stripVarFunc(tokenValue.split(",")[3]);
+              const foundationOpacityValue =
+                variables["general"][`${opacityValue}`];
+              // e.g. --salt-palette-opacity-disabled -> --salt-opacity-40
+              if (foundationOpacityValue.includes("opacity")) {
+                tokenValue = {
+                  color: `{foundations.color.${colorToken}}`,
+                  opacity: `{foundations.opacity.${removePrefix(
+                    stripVarFunc(foundationOpacityValue)
+                  )}}`,
+                };
+              }
+              // eg. --salt-opacity-8 directly used in --salt-color-black-fade-background-hover
+              else {
+                tokenValue = {
+                  color: `{foundations.color.${colorToken}}`,
+                  opacity: `{foundations.opacity.${removePrefix(
+                    opacityValue
+                  )}}`,
+                };
+              }
+            } else {
+              type = "color";
+            }
+            break;
+          default:
+            type = "undefined";
         }
-      } else if (["opacity", "zIndex"].includes(semantic)) {
-        type = "number";
-      } else if (semantic === "duration") {
-        type = "duration";
-      } else if (semantic === "typography") {
-        const attribute = tokenName.split("-")[1];
-        type = attribute;
-      } else if (semantic === "shadow") {
-        if (tokenName.includes("color")) {
-          type = "color";
-        } else {
-          type = "shadow";
-          const [offsetX, offsetY, blur, spread, color] = tokenValue.split(" ");
-          tokenValue = {
-            offsetX: offsetX,
-            offsetY: offsetY,
-            blur: blur,
-            spread: spread,
-            color: `{foundations.color.${removePrefix(stripVarFunc(color))}}`,
-          };
-          const rawColor = variables[stripVarFunc(color)];
-          rawValue = `${offsetX}, ${offsetY}, ${blur}, ${spread}, ${rawColor}`;
+
+        if (type !== "undefined") {
+          addToJson(key, "foundations", semantic, tokenName, type, tokenValue);
         }
-      } else if (semantic === "color") {
-        if (tokenName.includes("fade")) {
-          type = "color";
-
-          const colorToken = `${tokenName.split("-fade")[0]}`;
-          const rawColorValue = variables[`--salt-${colorToken}`];
-
-          const opacityValue = stripVarFunc(tokenValue.split(",")[3]);
-          const foundationOpacityValue = variables[`${opacityValue}`];
-
-          // e.g. --salt-palette-opacity-disabled -> --salt-opacity-40
-          if (foundationOpacityValue.includes("opacity")) {
-            const opacityAlias = stripVarFunc(foundationOpacityValue);
-            let rawOpacityValue = variables[`${opacityAlias}`];
-            tokenValue = {
-              color: `{foundations.color.${colorToken}}`,
-              opacity: `{foundations.opacity.${removePrefix(opacityAlias)}}`,
-            };
-            rawValue = `${rawColorValue
-              .replace("rgb", "rgba")
-              .replace(")", "")}, ${rawOpacityValue})`;
-          }
-          // eg. --salt-opacity-8 directly used in --salt-color-black-fade-background-hover
-          else {
-            tokenValue = {
-              color: `{foundations.color.${colorToken}}`,
-              opacity: `{foundations.opacity.${removePrefix(opacityValue)}}`,
-            };
-            rawValue = `${rawColorValue
-              .replace("rgb", "rgba")
-              .replace(")", "")}, ${foundationOpacityValue})`;
-          }
-        } else {
-          type = "color";
-        }
-      }
-
-      if (type) {
-        jsonTokens["foundations"][semantic] = {
-          [tokenName]: {
-            $value: tokenValue,
-            $type: type,
-            ...(rawValue && {
-              $rawValue: rawValue,
-            }),
-            ...(calculation && {
-              $calculation: calculation,
-            }),
-          },
-          ...jsonTokens["foundations"][semantic],
-        };
       }
     }
   }
@@ -209,7 +189,47 @@ function themeToJson() {
   const foundationVariables = getCssVariablesFromDir(
     path.resolve(__dirname, "../css/foundations")
   );
-  format({ ...paletteVariables, ...foundationVariables });
+  const characteristicVariables = {};
+  // getCssVariablesFromDir(
+  //   path.resolve(__dirname, "../css/characteristics")
+  // );
+  format({
+    light: {
+      ...paletteVariables.light,
+      ...foundationVariables.light,
+      ...characteristicVariables.light,
+    },
+    dark: {
+      ...paletteVariables.dark,
+      ...foundationVariables.dark,
+      ...characteristicVariables.dark,
+    },
+    hd: {
+      ...paletteVariables.hd,
+      ...foundationVariables.hd,
+      ...characteristicVariables.hd,
+    },
+    md: {
+      ...paletteVariables.md,
+      ...foundationVariables.md,
+      ...characteristicVariables.md,
+    },
+    td: {
+      ...paletteVariables.td,
+      ...foundationVariables.td,
+      ...characteristicVariables.td,
+    },
+    ld: {
+      ...paletteVariables.ld,
+      ...foundationVariables.ld,
+      ...characteristicVariables.ld,
+    },
+    general: {
+      ...paletteVariables.general,
+      ...foundationVariables.general,
+      ...characteristicVariables.general,
+    },
+  });
 }
 
 themeToJson();
