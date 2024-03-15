37ca90488ac94b1a5dbf23dc0f45ebf25d787e82
Fernanda Castillo <fersitacastillo123@gmail.com>
2024-03-15 12:10:46 +0000

    updates from feedback

--- site/src/examples/progress/WithBuffer.tsx
+++ site/src/examples/progress/WithBuffer.tsx
@@ -14,39 +14,26 @@ export const WithBuffer = (): ReactElement => {
   const [selectedType, setSelectedType] = useState("circular");
 
   return (
-    <FlexLayout direction="column" style={{ height: "100%" }}>
-      <H3 style={{ textAlign: "center" }}> value = 38, buffer value = 60</H3>
-      <FlexItem>
-        <FlowLayout justify="center" className="controls" gap={1}>
-          <RadioButtonGroup direction="horizontal" defaultChecked>
-            <RadioButton
-              key="circular"
-              label="Circular"
-              value="circular"
-              checked
-              onChange={() => setSelectedType("circular")}
-            />
-            <RadioButton
-              key="linear"
-              label="Linear"
-              value="linear"
-              onChange={() => setSelectedType("linear")}
-            />
-          </RadioButtonGroup>
-        </FlowLayout>
-      </FlexItem>
+    <FlexLayout direction="column" align="center" style={{ height: "100%" }}>
+      <H3> value = 38, buffer value = 60</H3>
+      <FlowLayout justify="center" gap={1}>
+        <RadioButtonGroup
+          direction="horizontal"
+          value={selectedType}
+          aria-label="Progress type control"
+          onChange={(e) => setSelectedType(e.target.value)}
+        >
+          <RadioButton label="Circular" value="circular" />
+          <RadioButton label="Linear" value="linear" />
+        </RadioButtonGroup>
+      </FlowLayout>
 
-      <FlexItem align="center" grow={1}>
+      <FlexItem style={{ margin: "auto" }}>
         {selectedType === "circular" && (
           <CircularProgress aria-label="Download" value={38} bufferValue={60} />
         )}
         {selectedType === "linear" && (
-          <LinearProgress
-            aria-label="Download"
-            value={38}
-            bufferValue={60}
-            style={{ height: "100%" }}
-          />
+          <LinearProgress aria-label="Download" value={38} bufferValue={60} />
         )}
       </FlexItem>
     </FlexLayout>
