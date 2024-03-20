5c7e0c432ec357f912970bff9a1b0a38c1f75895
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2024-03-20 16:43:41 +0000

    Add related patterns to pattern page

---
+++ site/src/layouts/DetailPattern/RelatedPatterns.tsx
@@ -0,0 +1,42 @@
+import { Heading4 } from "../../components/mdx/h4";
+import { SiteState, useStore } from "@jpmorganchase/mosaic-store";
+import styles from "./RelatedPatterns.module.css";
+import { Link } from "@salt-ds/core";
+
+type Data = {
+  relatedPatterns: string[];
+};
+
+type CustomSiteState = SiteState & { data?: Data };
+
+function getHrefFromComponent(component: string) {
+  // Multiline Input -> multiline-input
+  // Input -> input
+  // File Drop Zone => file-drop-zone
+  const tag = component.replaceAll(/ /g, "-").toLowerCase();
+  return `/salt/patterns/${tag}`;
+}
+
+export function RelatedPatterns() {
+  const relatedPatterns =
+    useStore((state: CustomSiteState) => state.data?.relatedPatterns) ?? [];
+
+  if (relatedPatterns.length > 0) {
+    return (
+      <section className={styles.root}>
+        <Heading4>Related Patterns</Heading4>
+        <ul className={styles.list}>
+          {relatedPatterns
+            .sort((a, b) => a.localeCompare(b))
+            .map((pattern) => (
+              <li key={pattern}>
+                <Link href={getHrefFromComponent(pattern)}>{pattern}</Link>
+              </li>
+            ))}
+        </ul>
+      </section>
+    );
+  }
+
+  return null;
+}
