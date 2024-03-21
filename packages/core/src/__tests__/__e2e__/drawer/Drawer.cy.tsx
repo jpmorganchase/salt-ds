330622c66169aa09023eca1742553d8cc84cfdd5
origami-z <5257855+origami-z@users.noreply.github.com>
2024-03-21 10:49:18 +0000

    Fix drawer children unmount

--- packages/core/src/__tests__/__e2e__/drawer/Drawer.cy.tsx
+++ packages/core/src/__tests__/__e2e__/drawer/Drawer.cy.tsx
@@ -8,12 +8,17 @@ const { Default, OptionalCloseAction } = composedStories;
 describe("GIVEN a Drawer", () => {
   describe("WHEN a drawer with close button is open", () => {
     it("THEN it should close on close button click or outside Drawer click", () => {
+      cy.spy(console, "log").as("consoleSpy");
+
       cy.mount(<Default />);
 
       cy.findByRole("button", { name: "Open Primary Drawer" }).realClick();
+      cy.findByTestId("scrim").should("exist");
       cy.findByRole("dialog").should("be.visible");
+      cy.get("@consoleSpy").should("have.callCount", 1); // Strict mode unmounting
       cy.findByRole("button", { name: "Close Drawer" }).click();
       cy.findByRole("dialog").should("not.exist");
+      cy.get("@consoleSpy").should("have.callCount", 2); // Test unmount regression #3153
 
       cy.findByRole("button", { name: "Open Secondary Drawer" }).realClick();
       cy.findByRole("dialog").should("be.visible");
@@ -22,10 +27,11 @@ describe("GIVEN a Drawer", () => {
     });
 
     it("THEN it should dismiss on Esc key press", () => {
-      cy.mount(<Default />);
+      cy.mount(<Default disableScrim />);
 
       cy.findByRole("button", { name: "Open Primary Drawer" }).realClick();
       cy.findByRole("dialog").should("be.visible");
+      cy.findByTestId("scrim").should("not.exist"); // disableScrim
       cy.realPress("Escape");
       cy.findByRole("dialog").should("not.exist");
     });
