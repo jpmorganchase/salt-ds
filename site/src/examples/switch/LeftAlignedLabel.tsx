23c420196f59192003c7d0706b50cd0970ab1351
Liberty Curtis <91206958+libertymayc@users.noreply.github.com>
2024-03-21 14:08:45 -0400

    Update site/src/examples/switch/LeftAlignedLabel.tsx
    Co-authored-by: Josh Wooding <12938082+joshwooding@users.noreply.github.com>

--- site/src/examples/switch/LeftAlignedLabel.tsx
+++ site/src/examples/switch/LeftAlignedLabel.tsx
@@ -2,10 +2,10 @@ import { ReactElement } from "react";
 import { FormField, FormFieldLabel, Switch } from "@salt-ds/core";
 
 export const LeftAlignedLabel = (): ReactElement => (
-  <>
+  <div>
     <FormField labelPlacement="left">
       <FormFieldLabel>Active</FormFieldLabel>
       <Switch />
     </FormField>
-  </>
+  </div>
 );
