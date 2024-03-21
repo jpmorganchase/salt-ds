b19db7cf241d5ad2da55ea9b26f099eb6eaff4e5
Zhihao Cui <5257855+origami-z@users.noreply.github.com>
2024-03-21 14:27:49 +0000

    Fixed Drawer children being unmounted twice unexpectedly when closing (#3159)

--- packages/core/stories/drawer/drawer.stories.tsx
+++ packages/core/stories/drawer/drawer.stories.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, SyntheticEvent, useState } from "react";
+import { ChangeEvent, SyntheticEvent, useEffect, useState } from "react";
 import {
   Button,
   Card,
@@ -17,15 +17,25 @@ import {
   DrawerCloseButton,
   ComboBox,
   Option,
+  DrawerProps,
 } from "@salt-ds/core";
-import { Meta } from "@storybook/react";
+import { Meta, StoryFn } from "@storybook/react";
 
 export default {
   title: "Core/Drawer",
   component: Drawer,
 } as Meta<typeof Drawer>;
 
-export const Default = ({ position = "left", ...args }) => {
+const DummyContent = () => {
+  useEffect(() => {
+    return () => {
+      console.log(new Date().getTime(), "Dummy unmount");
+    };
+  }, []);
+  return <Text>Dummy</Text>;
+};
+
+export const Default: StoryFn<DrawerProps> = (args) => {
   const [openPrimary, setOpenPrimary] = useState(false);
   const [openSecondary, setOpenSecondary] = useState(false);
 
@@ -33,17 +43,20 @@ export const Default = ({ position = "left", ...args }) => {
     <StackLayout>
       <Button onClick={() => setOpenPrimary(true)}>Open Primary Drawer</Button>
       <Drawer
+        {...args}
         open={openPrimary}
         onOpenChange={(newOpen) => setOpenPrimary(newOpen)}
         id="primary"
         style={{ width: 200 }}
       >
         <DrawerCloseButton onClick={() => setOpenPrimary(false)} />
+        <DummyContent />
       </Drawer>
       <Button onClick={() => setOpenSecondary(true)}>
         Open Secondary Drawer
       </Button>
       <Drawer
+        {...args}
         open={openSecondary}
         onOpenChange={(newOpen) => setOpenSecondary(newOpen)}
         variant="secondary"
@@ -56,7 +69,7 @@ export const Default = ({ position = "left", ...args }) => {
   );
 };
 
-export const Position = ({ position = "left", ...args }) => {
+export const Position: StoryFn<DrawerProps> = (args) => {
   const [openLeft, setOpenLeft] = useState(false);
   const [openRight, setOpenRight] = useState(false);
   const [openTop, setOpenTop] = useState(false);
@@ -66,6 +79,7 @@ export const Position = ({ position = "left", ...args }) => {
     <StackLayout>
       <Button onClick={() => setOpenLeft(true)}>Open Left Drawer</Button>
       <Drawer
+        {...args}
         open={openLeft}
         onOpenChange={(newOpen) => setOpenLeft(newOpen)}
         id="left-drawer"
@@ -75,6 +89,7 @@ export const Position = ({ position = "left", ...args }) => {
       </Drawer>
       <Button onClick={() => setOpenRight(true)}>Open Right Drawer</Button>
       <Drawer
+        {...args}
         open={openRight}
         onOpenChange={(newOpen) => setOpenRight(newOpen)}
         position="right"
@@ -85,6 +100,7 @@ export const Position = ({ position = "left", ...args }) => {
       </Drawer>
       <Button onClick={() => setOpenTop(true)}>Open Top Drawer</Button>
       <Drawer
+        {...args}
         open={openTop}
         onOpenChange={(newOpen) => setOpenTop(newOpen)}
         position="top"
@@ -95,6 +111,7 @@ export const Position = ({ position = "left", ...args }) => {
       </Drawer>
       <Button onClick={() => setOpenBottom(true)}>Open Bottom Drawer</Button>
       <Drawer
+        {...args}
         open={openBottom}
         onOpenChange={(newOpen) => setOpenBottom(newOpen)}
         position="bottom"
