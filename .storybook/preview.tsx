5d13755b171dfc98fa101031878703615fe46496
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2024-03-21 14:34:56 +0000

    Add PT Mono to Storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -14,6 +14,7 @@ import "@fontsource/open-sans/700.css";
 import "@fontsource/open-sans/700-italic.css";
 import "@fontsource/open-sans/800.css";
 import "@fontsource/open-sans/800-italic.css";
+import "@fontsource/pt-mono";
 import "./styles.css";
 
 import { ComponentProps } from "react";
