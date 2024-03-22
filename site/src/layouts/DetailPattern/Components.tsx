b40e1134f4082c3cf3fc0e263c4fd30922cb87c4
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2024-03-22 14:00:54 +0000

    Clean site sidebar (#3166)

--- site/src/layouts/DetailPattern/Components.tsx
+++ site/src/layouts/DetailPattern/Components.tsx
@@ -1,7 +1,9 @@
-import { Heading4 } from "../../components/mdx/h4";
 import { SiteState, useStore } from "@jpmorganchase/mosaic-store";
+
+import { getHrefFromComponent } from "../../utils/getHrefFromComponent";
+import { LinkList } from "../../components/link-list/LinkList";
+
 import styles from "./Resources.module.css";
-import { Link } from "@salt-ds/core";
 
 type Data = {
   components: string[];
@@ -9,31 +11,19 @@ type Data = {
 
 type CustomSiteState = SiteState & { data?: Data };
 
-function getHrefFromComponent(component: string) {
-  // Multiline Input -> multiline-input
-  // Input -> input
-  // File Drop Zone => file-drop-zone
-  const tag = component.replaceAll(/ /g, "-").toLowerCase();
-  return `/salt/components/${tag}`;
-}
-
 export function Components() {
   const components =
     useStore((state: CustomSiteState) => state.data?.components) ?? [];
 
+  const componentLinks = components.map((component) => ({
+    href: getHrefFromComponent(component),
+    label: component,
+  }));
+
   if (components.length > 0) {
     return (
       <section className={styles.root}>
-        <Heading4>Components</Heading4>
-        <ul className={styles.list}>
-          {components
-            .sort((a, b) => a.localeCompare(b))
-            .map((component) => (
-              <li key={component}>
-                <Link href={getHrefFromComponent(component)}>{component}</Link>
-              </li>
-            ))}
-        </ul>
+        <LinkList heading="Components" links={componentLinks} />
       </section>
     );
   }
--- site/src/layouts/DetailPattern/RelatedPatterns.tsx
+++ site/src/layouts/DetailPattern/RelatedPatterns.tsx
@@ -1,7 +1,6 @@
-import { Heading4 } from "../../components/mdx/h4";
 import { SiteState, useStore } from "@jpmorganchase/mosaic-store";
 import styles from "./RelatedPatterns.module.css";
-import { Link } from "@salt-ds/core";
+import { LinkList } from "../../components/link-list/LinkList";
 
 type Data = {
   relatedPatterns: string[];
@@ -21,19 +20,15 @@ export function RelatedPatterns() {
   const relatedPatterns =
     useStore((state: CustomSiteState) => state.data?.relatedPatterns) ?? [];
 
+  const relatedPatternLinks = relatedPatterns.map((pattern) => ({
+    href: getHrefFromComponent(pattern),
+    label: pattern,
+  }));
+
   if (relatedPatterns.length > 0) {
     return (
       <section className={styles.root}>
-        <Heading4>Related patterns</Heading4>
-        <ul className={styles.list}>
-          {relatedPatterns
-            .sort((a, b) => a.localeCompare(b))
-            .map((pattern) => (
-              <li key={pattern}>
-                <Link href={getHrefFromComponent(pattern)}>{pattern}</Link>
-              </li>
-            ))}
-        </ul>
+        <LinkList heading="Related patterns" links={relatedPatternLinks} />
       </section>
     );
   }
