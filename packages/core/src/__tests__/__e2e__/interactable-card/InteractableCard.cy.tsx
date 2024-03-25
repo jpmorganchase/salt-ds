f820b55bf5deba92156702e93f5a200ec32c5599
lilyvc <45168453+lilyvc@users.noreply.github.com>
2024-03-25 12:01:35 +0000

    remove unused import

--- packages/core/src/__tests__/__e2e__/interactable-card/InteractableCard.cy.tsx
+++ packages/core/src/__tests__/__e2e__/interactable-card/InteractableCard.cy.tsx
@@ -10,7 +10,7 @@ import {
 import { useState, SyntheticEvent } from "react";
 
 const composedStories = composeStories(cardStories);
-const { Default, AccentVariations } = composedStories;
+const { Default } = composedStories;
 
 const ControlledGroup = ({
   onChange,
