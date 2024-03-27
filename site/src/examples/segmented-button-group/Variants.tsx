897bef38ee3e3ef826169a716f10acf025b8b5ca
Luke Atkinson-Coyle <luke.atkinson-coyle@outlook.com>
2024-03-27 11:26:54 +0000

    changes as per pr comments

--- site/src/examples/segmented-button-group/Variants.tsx
+++ site/src/examples/segmented-button-group/Variants.tsx
@@ -1,23 +1,43 @@
 import { ReactElement } from "react";
 import { Button, StackLayout } from "@salt-ds/core";
 import { SegmentedButtonGroup } from "@salt-ds/lab";
+import { CallIcon, MessageIcon, ChatGroupIcon } from "@salt-ds/icons";
 
 export const Variants = (): ReactElement => (
   <StackLayout>
     <SegmentedButtonGroup>
-      <Button>Button</Button>
-      <Button>Button</Button>
-      <Button>Button</Button>
+      <Button>
+        <MessageIcon /> Message
+      </Button>
+      <Button>
+        <ChatGroupIcon />
+        Chat
+      </Button>
+      <Button>
+        <CallIcon /> Call
+      </Button>
     </SegmentedButtonGroup>
     <SegmentedButtonGroup>
-      <Button variant="secondary">Button</Button>
-      <Button variant="secondary">Button</Button>
-      <Button variant="secondary">Button</Button>
+      <Button variant="secondary">
+        <MessageIcon /> Message
+      </Button>
+      <Button variant="secondary">
+        <ChatGroupIcon /> Chat
+      </Button>
+      <Button variant="secondary">
+        <CallIcon /> Call
+      </Button>
     </SegmentedButtonGroup>
     <SegmentedButtonGroup>
-      <Button variant="cta">Button</Button>
-      <Button variant="cta">Button</Button>
-      <Button variant="cta">Button</Button>
+      <Button variant="cta">
+        <MessageIcon /> Message
+      </Button>
+      <Button variant="cta">
+        <ChatGroupIcon /> Chat
+      </Button>
+      <Button variant="cta">
+        <CallIcon /> Call
+      </Button>
     </SegmentedButtonGroup>
   </StackLayout>
 );
