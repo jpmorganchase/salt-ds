a724d7598e73e3e4443a57c7a48c4e33c049f4a8
Fernanda Castillo <fersitacastillo123@gmail.com>
2024-03-27 11:53:10 +0000

    clean date input and add custom formatter example

--- packages/lab/stories/date-input/date-input.stories.tsx
+++ packages/lab/stories/date-input/date-input.stories.tsx
@@ -1,8 +1,11 @@
 import { DateInput, DateInputProps } from "@salt-ds/lab";
 import { Meta, StoryFn } from "@storybook/react";
+import { ChangeEvent, useState } from "react";
+import { getLocalTimeZone, today } from "@internationalized/date";
+const localTimeZone = getLocalTimeZone();
 
 export default {
-  title: "Lab/DateInput",
+  title: "Lab/Date Input",
   component: DateInput,
 } as Meta<typeof DateInput>;
 
@@ -10,7 +13,21 @@ const DateInputTemplate: StoryFn<DateInputProps> = (args) => {
   return <DateInput {...args} />;
 };
 
+const formatter = (input: string): string => {
+  const date = new Date(input);
+  // @ts-ignore evaluating validity of date
+  return isNaN(date)
+    ? input
+    : new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+      }).format(date);
+};
+
 export const Default = DateInputTemplate.bind({});
-Default.args = {
-  // dateparser: () =>
+Default.args = {};
+
+export const CustomFormatter = DateInputTemplate.bind({});
+CustomFormatter.args = {
+  dateFormatter: formatter,
+  placeholder: "yyyy",
 };
