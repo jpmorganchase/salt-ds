0fe48b4f2287cb32fa6c7381acdbc65a91ff7829
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2024-04-09 11:03:57 +0100

    Fix components submitting forms (#3218)

--- packages/lab/src/calendar/internal/CalendarDay.tsx
+++ packages/lab/src/calendar/internal/CalendarDay.tsx
@@ -62,6 +62,7 @@ export const CalendarDay = forwardRef<HTMLButtonElement, CalendarDayProps>(
         <button
           aria-label={formatDate(day)}
           disabled={disabled}
+          type="button"
           {...dayProps}
           ref={buttonRef}
           {...rest}
