706417429c4ff20a4f73891b5690cdd89508a1e3
lilyvc <45168453+lilyvc@users.noreply.github.com>
2024-04-22 13:32:07 +0100

    add shadow on scroll and center nav i tems

--- packages/core/stories/patterns/app-header/app-header.stories.tsx
+++ packages/core/stories/patterns/app-header/app-header.stories.tsx
@@ -1,4 +1,4 @@
-import { FC, HTMLAttributes, ReactNode, useState } from "react";
+import { FC, HTMLAttributes, ReactNode, useEffect, useState } from "react";
 import {
   Button,
   FlexItem,
@@ -25,21 +25,88 @@ export default {
 } as Meta;
 
 export const AppHeader = () => {
-  interface AppHeaderProps extends HTMLAttributes<HTMLElement> {
-    /**
-     * Items included in the App Header navigation
-     */
-    items?: string[];
-    /**
-     * Utilities included in the App Header
-     */
-    utilities?: { icon: ReactNode; key: string }[];
-  }
+  const [offset, setOffset] = useState(0);
+  const isMobile = useResponsiveProp({ xs: true, sm: false }, false);
 
-  const AppHeader: FC<AppHeaderProps> = ({ items, utilities }) => {
+  const setScroll = () => {
+    setOffset(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", setScroll);
+    return () => {
+      window.removeEventListener("scroll", setScroll);
+    };
+  }, []);
+
+  const DesktopAppHeader: FC<{
+    items?: string[];
+    utilities?: { icon: ReactNode; key: string }[];
+  }> = ({ items, utilities }) => {
+    const [active, setActive] = useState(items?.[0]);
+
+    return (
+      <header>
+        <FlexLayout
+          style={{
+            height: "calc(var(--salt-size-base) + var(--salt-spacing-200))",
+            paddingLeft: "var(--salt-spacing-200)",
+            paddingRight: "var(--salt-spacing-200)",
+            backgroundColor: "var(--salt-container-primary-background)",
+            position: "fixed",
+            width: "100%",
+            boxShadow: offset > 0 ? "var(--salt-shadow-1)" : "none",
+            borderBottom:
+              "var(--salt-size-border) var(--salt-container-borderStyle) var(--salt-separable-primary-borderColor)",
+          }}
+          justify="space-between"
+          gap={3}
+        >
+          <FlexItem align="center">
+            <Text>App logo</Text>
+          </FlexItem>
+          <nav>
+            <ul
+              style={{
+                display: "flex",
+                listStyle: "none",
+                padding: "0",
+                margin: "0",
+              }}
+            >
+              {items?.map((item) => (
+                <li key={item}>
+                  <NavigationItem
+                    active={active === item}
+                    href="#"
+                    onClick={() => setActive(item)}
+                  >
+                    {item}
+                  </NavigationItem>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <FlexItem align="center">
+            <StackLayout direction="row" gap={1}>
+              {utilities?.map((utility) => (
+                <Button key={utility.key} variant="secondary">
+                  {utility.icon}
+                </Button>
+              ))}
+            </StackLayout>
+          </FlexItem>
+        </FlexLayout>
+      </header>
+    );
+  };
+
+  const MobileAppHeader: FC<{
+    items?: string[];
+    utilities?: { icon: ReactNode; key: string }[];
+  }> = ({ items, utilities }) => {
     const [drawerOpen, setDrawerOpen] = useState(false);
     const [active, setActive] = useState(items?.[0]);
-    const isMobile = useResponsiveProp({ xs: true, sm: false }, false);
 
     const handleClick = (item: string) => {
       setActive(item);
@@ -48,150 +115,97 @@ export const AppHeader = () => {
 
     return (
       <header>
-        {isMobile ? (
-          // Mobile Header
-          <>
-            <StackLayout
-              direction="row"
-              style={{
-                width: "100%",
-                backgroundColor: "var(--salt-container-primary-background)",
-                zIndex: "calc(var(--salt-zIndex-drawer) + 1)",
-                position: "fixed",
-                inset: "0 0 auto 0",
-                paddingRight: "1em",
-                borderBottom:
-                  "var(--salt-size-border) var(--salt-separable-borderStyle) var(--salt-separable-primary-borderColor)",
-              }}
-            >
-              <FlexItem
-                style={{
-                  justifyContent: "center",
-                  display: "flex",
-                  height:
-                    "calc(var(--salt-size-base) + var(--salt-spacing-200))",
-                  width:
-                    "calc(var(--salt-size-base) + var(--salt-spacing-200))",
-                  borderRight:
-                    "var(--salt-size-border) var(--salt-separable-borderStyle) var(--salt-separable-primary-borderColor)",
-                }}
-              >
-                {!drawerOpen && (
-                  <Button
-                    onClick={() => setDrawerOpen(true)}
-                    style={{ alignSelf: "center" }}
-                    variant="secondary"
-                  >
-                    <MenuIcon />
-                  </Button>
-                )}
-
-                {drawerOpen && (
-                  <Button
-                    onClick={() => setDrawerOpen(false)}
-                    style={{ alignSelf: "center" }}
-                    variant="secondary"
-                  >
-                    <CloseIcon />
-                  </Button>
-                )}
-              </FlexItem>
-              <FlexItem align="center">
-                <Text>App logo</Text>
-              </FlexItem>
-            </StackLayout>
-            <Drawer
-              style={{
-                paddingTop:
-                  "calc(var(--salt-size-base) + var(--salt-spacing-200))",
-                paddingLeft: "0",
-              }}
-              open={drawerOpen}
-              onOpenChange={() => {
-                if (drawerOpen) {
-                  setDrawerOpen(false);
-                }
-              }}
-            >
-              <nav>
-                <ul style={{ listStyle: "none", padding: "0" }}>
-                  {items?.map((item) => (
-                    <li key={item}>
-                      <NavigationItem
-                        orientation="vertical"
-                        active={active === item}
-                        href="#"
-                        onClick={() => {
-                          handleClick(item);
-                        }}
-                      >
-                        {item}
-                      </NavigationItem>
-                    </li>
-                  ))}
-                  {utilities?.map((utility) => (
-                    <li key={utility.key}>
-                      <NavigationItem
-                        orientation="vertical"
-                        href="#"
-                        onClick={() => {
-                          setDrawerOpen(false);
-                        }}
-                      >
-                        {utility.icon}
-                        {utility.key}
-                      </NavigationItem>
-                    </li>
-                  ))}
-                </ul>
-              </nav>
-            </Drawer>
-          </>
-        ) : (
-          // Desktop Header
-          <FlexLayout
+        <StackLayout
+          direction="row"
+          style={{
+            width: "100%",
+            backgroundColor: "var(--salt-container-primary-background)",
+            zIndex: "calc(var(--salt-zIndex-drawer) + 1)",
+            position: "fixed",
+            borderBottom:
+              "var(--salt-size-border) var(--salt-separable-borderStyle) var(--salt-separable-primary-borderColor)",
+            boxShadow: offset > 0 ? "var(--salt-shadow-1)" : "none",
+          }}
+        >
+          <FlexItem
             style={{
+              justifyContent: "center",
+              display: "flex",
               height: "calc(var(--salt-size-base) + var(--salt-spacing-200))",
-              paddingLeft: "1em",
-              paddingRight: "1em",
-              backgroundColor: "var(--salt-container-primary-background)",
-              position: "fixed",
-              inset: "0 0 auto 0",
-              borderBottom:
-                "var(--salt-size-border) var(--salt-container-borderStyle) var(--salt-container-secondary-borderColor)",
+              width: "calc(var(--salt-size-base) + var(--salt-spacing-200))",
+              borderRight:
+                "var(--salt-size-border) var(--salt-separable-borderStyle) var(--salt-separable-primary-borderColor)",
             }}
-            justify="space-between"
-            gap={3}
           >
-            <FlexItem align="center">
-              <Text>App logo</Text>
-            </FlexItem>
-            <nav>
-              <ul style={{ display: "flex", listStyle: "none", margin: "0" }}>
-                {items?.map((item) => (
-                  <li key={item}>
-                    <NavigationItem
-                      active={active === item}
-                      href="#"
-                      onClick={() => setActive(item)}
-                    >
-                      {item}
-                    </NavigationItem>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-            <FlexItem align="center">
-              <StackLayout direction="row" gap={1}>
-                {utilities?.map((utility) => (
-                  <Button key={utility.key} variant="secondary">
+            {!drawerOpen && (
+              <Button
+                onClick={() => setDrawerOpen(true)}
+                style={{ alignSelf: "center" }}
+                variant="secondary"
+              >
+                <MenuIcon />
+              </Button>
+            )}
+
+            {drawerOpen && (
+              <Button
+                onClick={() => setDrawerOpen(false)}
+                style={{ alignSelf: "center" }}
+                variant="secondary"
+              >
+                <CloseIcon />
+              </Button>
+            )}
+          </FlexItem>
+          <FlexItem align="center">
+            <Text>App logo</Text>
+          </FlexItem>
+        </StackLayout>
+        <Drawer
+          style={{
+            paddingTop: "calc(var(--salt-size-base) + var(--salt-spacing-200))",
+            paddingLeft: "0",
+          }}
+          open={drawerOpen}
+          onOpenChange={() => {
+            if (drawerOpen) {
+              setDrawerOpen(false);
+            }
+          }}
+        >
+          <nav>
+            <ul style={{ listStyle: "none", padding: "0" }}>
+              {items?.map((item) => (
+                <li key={item}>
+                  <NavigationItem
+                    orientation="vertical"
+                    active={active === item}
+                    href="#"
+                    onClick={() => {
+                      handleClick(item);
+                    }}
+                  >
+                    {item}
+                  </NavigationItem>
+                </li>
+              ))}
+              {utilities?.map((utility) => (
+                <li key={utility.key}>
+                  <NavigationItem
+                    orientation="vertical"
+                    href="#"
+                    onClick={() => {
+                      setDrawerOpen(false);
+                    }}
+                  >
                     {utility.icon}
-                  </Button>
-                ))}
-              </StackLayout>
-            </FlexItem>
-          </FlexLayout>
-        )}
+                    {utility.key}
+                  </NavigationItem>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </Drawer>
       </header>
     );
   };
@@ -216,24 +230,39 @@ export const AppHeader = () => {
   return (
     <BorderLayout>
       <BorderItem position="north">
-        <AppHeader items={items} utilities={utilities} />
+        {isMobile ? (
+          <MobileAppHeader items={items} utilities={utilities} />
+        ) : (
+          <DesktopAppHeader items={items} utilities={utilities} />
+        )}
       </BorderItem>
       <BorderItem
         style={{
-          backgroundColor: "var(--salt-color-purple-30)",
-          paddingTop: "calc(var(--salt-size-base) + var(--salt-spacing-200))",
+          marginTop: "calc(var(--salt-size-base) + var(--salt-spacing-200))",
         }}
         position="center"
       >
-        <Text style={{ height: "120vh" }}>Main content</Text>
+        {Array.from({ length: 12 }, (_, index) => (
+          <div
+            key={index}
+            style={{
+              padding: "var(--salt-spacing-400)",
+              margin: "var(--salt-spacing-400)",
+              backgroundColor: "var(--salt-color-gray-10",
+            }}
+          />
+        ))}
       </BorderItem>
-      <BorderItem
-        style={{
-          backgroundColor: "var(--salt-color-orange-30)",
-        }}
-        position="south"
-      >
-        <Text>Footer</Text>
+      <BorderItem position="south">
+        <div
+          style={{
+            padding: "var(--salt-spacing-200)",
+            margin: "var(--salt-spacing-200)",
+            backgroundColor: "var(--salt-color-gray-10",
+          }}
+        >
+          <Text>Footer</Text>
+        </div>
       </BorderItem>
     </BorderLayout>
   );
