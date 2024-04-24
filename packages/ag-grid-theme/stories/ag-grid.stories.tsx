75a6d4b5d02f7340ea48c0937576edd0b6443d25
origami-z <5257855+origami-z@users.noreply.github.com>
2024-04-24 14:50:00 +0100

    Fixed single cell focus ring not visible in range selection mode

--- packages/ag-grid-theme/stories/ag-grid.stories.tsx
+++ packages/ag-grid-theme/stories/ag-grid.stories.tsx
@@ -1,16 +1,34 @@
 import { AgGridReact } from "ag-grid-react";
+import { userEvent, within, expect } from "@storybook/test";
+import type { Meta, StoryObj } from "@storybook/react";
+
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-material.css";
 import "../uitk-ag-theme.css";
 import "../salt-ag-theme.css";
+
+import { Default } from "./examples";
 
 export default {
   title: "Ag Grid/Ag Grid Theme",
   component: AgGridReact,
 };
 
+export const BasicGrid: StoryObj<typeof AgGridReact> = () => {
+  return <Default />;
+};
+BasicGrid.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  const azCell = await canvas.findByText("AZ");
+
+  await userEvent.click(azCell);
+
+  // classname has focus ring, which we want to snapshot, #3290
+  await expect(azCell).toHaveClass("ag-cell-range-single-cell");
+};
+
 export {
-  Default as BasicGrid,
   CheckboxSelection,
   Coloration,
   ColumnGroup,
