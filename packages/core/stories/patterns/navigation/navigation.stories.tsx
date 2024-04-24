e8c171e206f1a8f3fbda066ddb0674f762349ef5
lilyvc <45168453+lilyvc@users.noreply.github.com>
2024-04-24 16:46:19 +0100

    add navigation pattern example

---
+++ packages/core/stories/patterns/navigation/navigation.stories.tsx
@@ -0,0 +1,210 @@
+import { useEffect, useState } from "react";
+import {
+  Button,
+  FlexItem,
+  FlexLayout,
+  StackLayout,
+  BorderLayout,
+  BorderItem,
+  NavigationItem,
+} from "@salt-ds/core";
+import {
+  SymphonyIcon,
+  StackoverflowIcon,
+  GithubIcon,
+  LineChartIcon,
+  NotificationIcon,
+  PinIcon,
+  ReceiptIcon,
+  SearchIcon,
+  UserIcon,
+} from "@salt-ds/icons";
+import { Meta } from "@storybook/react";
+import logo from "../../assets/logo.svg";
+
+export default {
+  title: "Patterns/Navigation",
+} as Meta;
+
+const Item = () => {
+  return (
+    <div
+      style={{
+        padding: "calc(var(--salt-spacing-400)*4)",
+        margin: "var(--salt-spacing-400)",
+        backgroundColor: "var(--salt-color-gray-10)",
+      }}
+    />
+  );
+};
+
+export const Navigation = () => {
+
+  const verticalNavigationData = [{
+    name: "Overview",
+    href: "#",
+    icon: <PinIcon />
+  }, {
+    name: "Data analysis",
+    href: "#",
+    icon: <LineChartIcon />
+  }, {
+    name: "Market monitor",
+    href: "#",
+    icon: <NotificationIcon />
+  }, {
+    name: "Checks",
+    href: "#",
+    icon: <SearchIcon />
+  }, {
+    name: "Operations",
+    href: "#",
+    icon: <UserIcon />
+  }, {
+    name: "Trades",
+    href: "#",
+    icon: <ReceiptIcon />
+  }];
+
+  const headerItems = ["Home", "About", "Services", "Contact", "Blog"];
+
+  const headerUtilities = [
+    {
+      icon: <SymphonyIcon />,
+      key: "Symphony",
+    },
+    {
+      icon: <StackoverflowIcon />,
+      key: "Stack Overflow",
+    },
+    {
+      icon: <GithubIcon />,
+      key: "GitHub",
+    },
+  ];
+  const [activeVerticalNav, setActiveVerticalNav] = useState(verticalNavigationData[0].name);
+  const [activeHeaderNav, setActiveHeaderNAv] = useState(headerItems[0]);
+  const [offset, setOffset] = useState(0);
+
+  const setScroll = () => {
+    setOffset(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", setScroll);
+    return () => {
+      window.removeEventListener("scroll", setScroll);
+    };
+  }, []);
+
+  return <BorderLayout>
+    <BorderItem position="north">
+      <header>
+        <FlexLayout
+          style={{
+            paddingLeft: "var(--salt-spacing-300)",
+            paddingRight: "var(--salt-spacing-300)",
+            backgroundColor: "var(--salt-container-primary-background)",
+            position: "fixed",
+            width: "100%",
+            boxShadow:
+              offset > 0 ? "var(--salt-overlayable-shadow-scroll)" : "none",
+            borderBottom:
+              "var(--salt-size-border) var(--salt-container-borderStyle) var(--salt-separable-primary-borderColor)",
+          }}
+          justify="space-between"
+          gap={3}
+        >
+          <FlexItem align="center">
+            <img
+              alt="logo"
+              src={logo}
+              style={{
+                display: "block",
+                height: "calc(var(--salt-size-base) - var(--salt-spacing-150))",
+              }}
+            />
+          </FlexItem>
+          <nav>
+            <ul
+              style={{
+                display: "flex",
+                listStyle: "none",
+                padding: "0",
+                margin: "0",
+              }}
+            >
+              {headerItems?.map((item) => (
+                <li key={item}>
+                  <NavigationItem
+                    active={activeHeaderNav === item}
+                    href="#"
+                    onClick={(event) => {
+                      // prevent default to avoid navigation in storybook example
+                      event.preventDefault();
+                      setActiveHeaderNAv(item)
+                    }}
+                  >
+                    {item}
+                  </NavigationItem>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <FlexItem align="center">
+            <StackLayout direction="row" gap={1}>
+              {headerUtilities?.map((utility) => (
+                <Button key={utility.key} variant="secondary">
+                  {utility.icon}
+                </Button>
+              ))}
+            </StackLayout>
+          </FlexItem>
+        </FlexLayout>
+      </header>
+    </BorderItem>
+    <BorderItem position="west" style={{
+      marginTop: "calc(var(--salt-spacing-300) * 2)",
+      // Margin should be the height of the header
+      position: "fixed"
+    }}>
+      <aside style={{
+        width: "250px"
+      }}>
+        <nav>
+          <StackLayout gap="var(--salt-size-border)" as="ul" style={{
+            listStyle: "none",
+            margin: 0,
+            padding: 0
+          }}>
+            {verticalNavigationData.map(item => <li style={{
+              listStyle: "none"
+            }} key={item.name}>
+              <NavigationItem active={activeVerticalNav === item.name} href={item.href} orientation="vertical" onClick={event => {
+                // prevent default to avoid navigation in storybook example
+                event.preventDefault();
+                setActiveVerticalNav(item.name);
+              }}>
+                {item.icon}
+                {item.name}
+              </NavigationItem>
+            </li>)}
+          </StackLayout>
+        </nav>
+      </aside>
+    </BorderItem>
+    <BorderItem position="center" style={{
+      marginTop: "calc(var(--salt-spacing-300) * 2)",
+      marginLeft: "250px"
+    }}>
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+    </BorderItem>
+  </BorderLayout>;
+}
+
+Navigation.parameters = {
+  layout: "fullscreen",
+};
