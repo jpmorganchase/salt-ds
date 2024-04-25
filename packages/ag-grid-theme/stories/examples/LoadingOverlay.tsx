6c64d68107f093b049814cb2bb5ea62fda694152
origami-z <5257855+origami-z@users.noreply.github.com>
2024-04-25 10:42:51 +0100

    Fix loading overlay and no data overlay

--- packages/ag-grid-theme/stories/examples/LoadingOverlay.tsx
+++ packages/ag-grid-theme/stories/examples/LoadingOverlay.tsx
@@ -1,10 +1,19 @@
-import { CSSProperties } from "react";
+import { Card, Spinner, StackLayout, Text } from "@salt-ds/core";
 import { AgGridReact, AgGridReactProps } from "ag-grid-react";
-import { Card, Spinner, StackLayout } from "@salt-ds/core";
-import dataGridExampleData from "../dependencies/dataGridExampleData";
+import { useAgGridThemeSwitcher } from "../dependencies/ThemeSwitcher";
 import dataGridExampleColumns from "../dependencies/dataGridExampleColumns";
 import { useAgGridHelpers } from "../dependencies/useAgGridHelpers";
-import { useAgGridThemeSwitcher } from "../dependencies/ThemeSwitcher";
+
+const CustomOverlay = () => (
+  <Card>
+    <StackLayout gap={2} align="center">
+      <Spinner />
+      <Text aria-atomic="true" aria-live="polite">
+        Loading...
+      </Text>
+    </StackLayout>
+  </Card>
+);
 
 const LoadingOverlay = (props: AgGridReactProps) => {
   const { switcher, themeName } = useAgGridThemeSwitcher();
@@ -12,48 +21,17 @@ const LoadingOverlay = (props: AgGridReactProps) => {
     agThemeName: `ag-theme-${themeName}`,
   });
 
-  const getModalStyle: CSSProperties = {
-    position: "absolute",
-    top: "50%",
-    width: "100%",
-    left: "50%",
-    height: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(255, 255, 255, 0.6)",
-    zIndex: "3",
-    textAlign: "center",
-    transform: "translate(-50%, -50%)",
-  };
-
   return (
     <StackLayout gap={4}>
       {switcher}
-      <div style={{ position: "relative" }}>
-        <div className="modal" style={getModalStyle}>
-          <Card style={{ minHeight: "auto" }}>
-            <div>
-              <Spinner style={{ margin: "0 auto" }} />
-              <div
-                aria-atomic="true"
-                aria-live="polite"
-                style={{ fontSize: "16px", marginTop: "18px" }}
-                tabIndex={0}
-              >
-                Loading...
-              </div>
-            </div>
-          </Card>
-        </div>
-        <div {...containerProps} tabIndex={-1}>
-          <AgGridReact
-            {...agGridProps}
-            {...props}
-            columnDefs={dataGridExampleColumns}
-            rowData={dataGridExampleData}
-          />
-        </div>
+      <div {...containerProps} tabIndex={-1}>
+        <AgGridReact
+          {...agGridProps}
+          {...props}
+          columnDefs={dataGridExampleColumns}
+          rowData={null}
+          loadingOverlayComponent={CustomOverlay}
+        />
       </div>
     </StackLayout>
   );
