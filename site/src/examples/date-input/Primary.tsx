9149295e92347b4a1546f23a90e65626c394e68f
Fernanda Castillo <fersitacastillo123@gmail.com>
2024-04-29 14:21:20 +0100

    update input story

--- site/src/examples/date-input/Primary.tsx
+++ site/src/examples/date-input/Primary.tsx
@@ -2,5 +2,5 @@ import { ReactElement } from "react";
 import { DateInput } from "@salt-ds/lab";
 
 export const Primary = (): ReactElement => (
-  <DateInput defaultValue="01 Jan 2000" style={{ width: "256px" }} />
+  <DateInput style={{ width: "256px" }} />
 );
