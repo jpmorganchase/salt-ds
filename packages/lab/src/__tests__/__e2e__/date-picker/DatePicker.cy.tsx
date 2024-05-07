c5c25985ae2d5523dd1c83cc0050da0cbb369922
Fernanda Castillo <fersitacastillo123@gmail.com>
2024-05-07 14:02:21 +0100

    add more tests, remove isVisible from month

--- packages/lab/src/__tests__/__e2e__/date-picker/DatePicker.cy.tsx
+++ packages/lab/src/__tests__/__e2e__/date-picker/DatePicker.cy.tsx
@@ -7,25 +7,34 @@ import {
   DateFormatter,
   DateValue,
   getLocalTimeZone,
+  startOfMonth,
 } from "@internationalized/date";
 
 const composedStories = composeStories(datePickerStories);
 const { Default, Range } = composedStories;
 
 const testDate = new CalendarDate(2000, 2, 1);
+const testInput = "02-feb-2000";
 const localTimeZone = getLocalTimeZone();
 const currentLocale = navigator.languages[0];
 const formatDate = (date: DateValue, options?: Intl.DateTimeFormatOptions) => {
   const formatter = new DateFormatter(currentLocale, options);
   return formatter.format(date.toDate(localTimeZone));
 };
+const formatInput = (date: DateValue): string =>
+  new DateFormatter("EN-GB", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  }).format(date.toDate(localTimeZone));
+
 describe("GIVEN a DatePicker", () => {
   checkAccessibility(composedStories);
 
   describe("WHEN single datepicker is mounted", () => {
     it("THEN it should mount with the specified defaultStartDate", () => {
       cy.mount(<Default defaultStartDate={testDate} />);
-      cy.findByRole("textbox").should("have.value", "01 Feb 2000");
+      cy.findByRole("textbox").should("have.value", formatInput(testDate));
     });
     it("THEN should format a valid date with a different format on blur", () => {
       const onChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -33,9 +42,12 @@ describe("GIVEN a DatePicker", () => {
         event.persist();
       };
       cy.mount(<Default defaultStartDate={testDate} onChange={onChange} />);
-      cy.findByRole("textbox").click().clear().type("02-feb-2000");
+      cy.findByRole("textbox").click().clear().type(testInput);
       cy.findByRole("textbox").blur();
-      cy.findByRole("textbox").should("have.value", "02 Feb 2000");
+      cy.findByRole("textbox").should(
+        "have.value",
+        formatInput(testDate.add({ days: 1 }))
+      );
     });
     it("THEN should error and not format invalid dates on blur", () => {
       const onChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -67,8 +79,49 @@ describe("GIVEN a DatePicker", () => {
       cy.mount(<Default defaultStartDate={testDate} disabled />);
       cy.findByRole("button").should("be.disabled");
     });
+    it("THEN it should update the selected month when changing selected date", () => {
+      cy.mount(<Default defaultStartDate={testDate} />);
+      cy.findByRole("textbox").click().clear().type(testInput);
+      cy.findByRole("textbox").blur();
+      cy.findByRole("button", { name: "Open Calendar" }).realClick();
+      cy.findByRole("button", {
+        name: `Next Month, ${formatDate(
+          startOfMonth(testDate).add({ months: 1 })
+        )}`,
+      }).focus();
+    });
+    it("THEN it should clear the date if an empty input is submitted", () => {
+      cy.mount(
+        <Default
+          defaultStartDate={testDate}
+          // @ts-ignore
+          CalendarProps={{ visibleMonth: testDate }}
+        />
+      );
+      cy.findByRole("textbox").click().clear();
+      cy.findByRole("textbox").blur();
+      cy.findByRole("button", { name: "Open Calendar" }).realClick();
+      cy.findByRole("button", { name: formatDate(testDate) }).should(
+        "not.have.attr",
+        "aria-pressed",
+        "true"
+      );
+    });
   });
   describe("WHEN range datepicker is mounted", () => {
-    //
+    it("THEN it should mount with the specified defaultStartDate and defaultEndDate", () => {
+      cy.mount(
+        <Range
+          defaultStartDate={testDate}
+          defaultEndDate={testDate.add({ months: 1 })}
+        />
+      );
+      cy.findAllByRole("textbox")
+        .eq(0)
+        .should("have.value", formatInput(testDate));
+      cy.findAllByRole("textbox")
+        .eq(1)
+        .should("have.value", formatInput(testDate.add({ months: 1 })));
+    });
   });
 });
--- packages/lab/src/calendar/internal/CalendarMonth.tsx
+++ packages/lab/src/calendar/internal/CalendarMonth.tsx
@@ -32,7 +32,6 @@ export const CalendarMonth = forwardRef<HTMLDivElement, CalendarMonthProps>(
       renderDayContents,
       onMouseLeave,
       TooltipProps,
-      isVisible,
       ...rest
     } = props;
 
