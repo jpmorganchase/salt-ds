9f2c38cbfb39d590f884555eb4fa42fc761297cf
F745860 <vineet.dhiman@chase.com>
2024-05-08 15:32:07 +0530

    Added Status to Text Component added to the site

--- site/src/examples/text/Color.tsx
+++ site/src/examples/text/Color.tsx
@@ -6,5 +6,8 @@ export const Color = (): ReactElement => (
     <Text color="primary">This is primary color of Text</Text>
     <Text color="secondary">This is secondary color of Text</Text>
     <Text color="inherit">This is inherited color of Text</Text>
+    <Text color="success">This is success color of Text</Text>
+    <Text color="warning">This is warning color of Text</Text>
+    <Text color="error">This is error color of Text</Text>
   </StackLayout>
 );
