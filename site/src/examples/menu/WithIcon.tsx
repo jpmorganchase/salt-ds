b5d39e5bd7ade0f89fe761755ca1805dad70084c
Lily Cooksley <lilycooksley@gmail.com>
2024-05-10 10:33:36 +0100

    fix styling of story

--- site/src/examples/menu/WithIcon.tsx
+++ site/src/examples/menu/WithIcon.tsx
@@ -1,5 +1,12 @@
 import { ReactElement } from "react";
-import { Button, Menu, MenuItem, MenuPanel, MenuTrigger } from "@salt-ds/core";
+import {
+  Button,
+  Menu,
+  MenuItem,
+  MenuPanel,
+  MenuTrigger,
+  SegmentedButtonGroup,
+} from "@salt-ds/core";
 import {
   CopyIcon,
   ExportIcon,
@@ -9,26 +16,30 @@ import {
 
 export const WithIcon = (): ReactElement => {
   return (
-    <Menu>
-      <MenuTrigger>
-        <Button variant="secondary" aria-label="Open Menu">
-          <MicroMenuIcon aria-hidden />
-        </Button>
-      </MenuTrigger>
-      <MenuPanel>
-        <MenuItem>
-          <CopyIcon aria-hidden />
-          Copy
-        </MenuItem>
-        <MenuItem>
-          <ExportIcon aria-hidden />
-          Export
-        </MenuItem>
-        <MenuItem>
-          <SettingsIcon aria-hidden />
-          Settings
-        </MenuItem>
-      </MenuPanel>
-    </Menu>
+    <SegmentedButtonGroup>
+      <Button>Button</Button>
+      <Button>Button</Button>
+      <Menu>
+        <MenuTrigger>
+          <Button variant="secondary" aria-label="Open Menu">
+            <MicroMenuIcon aria-hidden />
+          </Button>
+        </MenuTrigger>
+        <MenuPanel>
+          <MenuItem>
+            <CopyIcon aria-hidden />
+            Copy
+          </MenuItem>
+          <MenuItem>
+            <ExportIcon aria-hidden />
+            Export
+          </MenuItem>
+          <MenuItem>
+            <SettingsIcon aria-hidden />
+            Settings
+          </MenuItem>
+        </MenuPanel>
+      </Menu>
+    </SegmentedButtonGroup>
   );
 };
