d26bcfcbb521e6608e0043daa149fb81056fdbb5
Lily Cooksley <lilycooksley@gmail.com>
2024-05-10 10:29:42 +0100

    ensure segmented button group only adds dividers to buttons

--- packages/core/stories/segmented-button-group/segmented-button-group.stories.tsx
+++ packages/core/stories/segmented-button-group/segmented-button-group.stories.tsx
@@ -4,8 +4,20 @@ import {
   Tooltip,
   SegmentedButtonGroup,
   SegmentedButtonGroupProps,
+  Menu,
+  MenuItem,
+  MenuPanel,
+  MenuTrigger,
 } from "@salt-ds/core";
-import { MessageIcon, ChatGroupIcon, CallIcon } from "@salt-ds/icons";
+import {
+  MessageIcon,
+  ChatGroupIcon,
+  CallIcon,
+  CopyIcon,
+  ExportIcon,
+  MicroMenuIcon,
+  SettingsIcon,
+} from "@salt-ds/icons";
 import { Meta, StoryFn } from "@storybook/react";
 
 export default {
@@ -58,5 +70,35 @@ export const Icons: StoryFn<SegmentedButtonGroupProps> = () => {
         );
       })}
     </StackLayout>
+  );
+};
+
+export const WithMenu: StoryFn<SegmentedButtonGroupProps> = () => {
+  return (
+    <SegmentedButtonGroup>
+      <Button>Button</Button>
+      <Button>Button</Button>
+      <Menu>
+        <MenuTrigger>
+          <Button variant="secondary" aria-label="Open Menu">
+            <MicroMenuIcon aria-hidden />
+          </Button>
+        </MenuTrigger>
+        <MenuPanel>
+          <MenuItem>
+            <CopyIcon aria-hidden />
+            Copy
+          </MenuItem>
+          <MenuItem>
+            <ExportIcon aria-hidden />
+            Export
+          </MenuItem>
+          <MenuItem>
+            <SettingsIcon aria-hidden />
+            Settings
+          </MenuItem>
+        </MenuPanel>
+      </Menu>
+    </SegmentedButtonGroup>
   );
 };
