9da71afd43ce7ce0220ee551024cd88ca3dd90c9
lilyvc <45168453+lilyvc@users.noreply.github.com>
2024-05-14 10:47:37 +0100

    revert changes to site example

--- site/src/examples/menu/WithIcon.tsx
+++ site/src/examples/menu/WithIcon.tsx
@@ -1,12 +1,5 @@
 import { ReactElement } from "react";
-import {
-  Button,
-  Menu,
-  MenuItem,
-  MenuPanel,
-  MenuTrigger,
-  SegmentedButtonGroup,
-} from "@salt-ds/core";
+import { Button, Menu, MenuItem, MenuPanel, MenuTrigger } from "@salt-ds/core";
 import {
   CopyIcon,
   ExportIcon,
@@ -16,30 +9,26 @@ import {
 
 export const WithIcon = (): ReactElement => {
   return (
-    <SegmentedButtonGroup>
-      <Button>Button</Button>
-      <Button>Button</Button>
-      <Menu>
-        <MenuTrigger>
-          <Button variant="secondary" aria-label="Open Menu">
-            <MicroMenuIcon aria-hidden />
-          </Button>
-        </MenuTrigger>
-        <MenuPanel>
-          <MenuItem>
-            <CopyIcon aria-hidden />
-            Copy
-          </MenuItem>
-          <MenuItem>
-            <ExportIcon aria-hidden />
-            Export
-          </MenuItem>
-          <MenuItem>
-            <SettingsIcon aria-hidden />
-            Settings
-          </MenuItem>
-        </MenuPanel>
-      </Menu>
-    </SegmentedButtonGroup>
+    <Menu>
+      <MenuTrigger>
+        <Button variant="secondary" aria-label="Open Menu">
+          <MicroMenuIcon aria-hidden />
+        </Button>
+      </MenuTrigger>
+      <MenuPanel>
+        <MenuItem>
+          <CopyIcon aria-hidden />
+          Copy
+        </MenuItem>
+        <MenuItem>
+          <ExportIcon aria-hidden />
+          Export
+        </MenuItem>
+        <MenuItem>
+          <SettingsIcon aria-hidden />
+          Settings
+        </MenuItem>
+      </MenuPanel>
+    </Menu>
   );
 };
