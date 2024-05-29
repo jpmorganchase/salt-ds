ed6d35e31d6bf6219becaccf766e13b92431b6ea
Joana Moreira <31695437+JoanaMMoreira@users.noreply.github.com>
2024-05-29 14:23:36 +0100

    Add Breadcrumbs pattern examples  (#3446)

---
+++ packages/core/stories/patterns/breadcrumbs/breadcrumbs.stories.tsx
@@ -0,0 +1,182 @@
+import { useState } from "react";
+import { Meta } from "@storybook/react";
+import {
+  Link,
+  Button,
+  Text,
+  FlowLayout,
+  Menu,
+  MenuItem,
+  MenuPanel,
+  MenuTrigger,
+  StackLayout,
+} from "@salt-ds/core";
+import { OverflowMenuIcon, ChevronRightIcon } from "@salt-ds/icons";
+
+export default {
+  title: "Patterns/Breadcrumbs",
+} as Meta;
+
+const Separator = () => <ChevronRightIcon aria-hidden />;
+
+export const Breadcrumbs = () => {
+  return (
+    <nav aria-label="Breadcrumb">
+      <FlowLayout
+        as="ul"
+        align="center"
+        gap={1}
+        style={{ listStyle: "none", minHeight: "var(--salt-size-base)" }}
+      >
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Link href="#">Home</Link>
+          <Separator />
+        </StackLayout>
+
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Link href="#">Level 2</Link>
+          <Separator />
+        </StackLayout>
+
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Link href="#">Level 3</Link>
+          <Separator />
+        </StackLayout>
+
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Link href="#">Level 4</Link>
+          <Separator />
+        </StackLayout>
+
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Link href="#">Level 5</Link>
+          <Separator />
+        </StackLayout>
+
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Text maxRows={1}>Current level</Text>
+        </StackLayout>
+      </FlowLayout>
+    </nav>
+  );
+};
+
+export const Wrapped = () => {
+  return (
+    <nav aria-label="Breadcrumb" style={{ maxWidth: 300 }}>
+      <FlowLayout
+        as="ul"
+        align="center"
+        gap={1}
+        style={{
+          listStyle: "none",
+          minHeight: "var(--salt-size-base)",
+        }}
+      >
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Link href="#">Home</Link>
+          <Separator />
+        </StackLayout>
+
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Link href="#">Level 2</Link>
+          <Separator />
+        </StackLayout>
+
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Link href="#">Level 3</Link>
+          <Separator />
+        </StackLayout>
+
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Link href="#">Level 4</Link>
+          <Separator />
+        </StackLayout>
+
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Link href="#">Level 5</Link>
+          <Separator />
+        </StackLayout>
+
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Text maxRows={1}>Current level</Text>
+        </StackLayout>
+      </FlowLayout>
+    </nav>
+  );
+};
+
+export const OverflowMenu = () => {
+  return (
+    <nav aria-label="Breadcrumb">
+      <FlowLayout as="ul" align="center" gap={1} style={{ listStyle: "none" }}>
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Link href="#">Home</Link>
+          <Separator />
+        </StackLayout>
+        <Menu>
+          <MenuTrigger>
+            <Button variant="secondary" aria-label="Open Menu">
+              <OverflowMenuIcon aria-hidden />
+            </Button>
+          </MenuTrigger>
+          <MenuPanel>
+            <MenuItem>Level 2</MenuItem>
+            <MenuItem>Level 3</MenuItem>
+            <MenuItem>Level 4</MenuItem>
+            <MenuItem>Level 5</MenuItem>
+          </MenuPanel>
+        </Menu>
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Separator />
+          <Text maxRows={1}>Current level</Text>
+        </StackLayout>
+      </FlowLayout>
+    </nav>
+  );
+};
+
+export const Expansion = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <nav aria-label="Breadcrumb">
+      <FlowLayout as="ul" align="center" gap={1} style={{ listStyle: "none" }}>
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Link href="#">Home</Link>
+          <Separator />
+        </StackLayout>
+        {isExpanded ? (
+          <>
+            <StackLayout as="li" direction="row" gap={1} align="center">
+              <Link href="#">Level 2</Link>
+              <Separator />
+            </StackLayout>
+
+            <StackLayout as="li" direction="row" gap={1} align="center">
+              <Link href="#">Level 3</Link>
+              <Separator />
+            </StackLayout>
+
+            <StackLayout as="li" direction="row" gap={1} align="center">
+              <Link href="#">Level 4</Link>
+              <Separator />
+            </StackLayout>
+
+            <StackLayout as="li" direction="row" gap={1} align="center">
+              <Link href="#">Level 5</Link>
+            </StackLayout>
+          </>
+        ) : (
+          <Button variant="secondary" onClick={() => setIsExpanded(true)}>
+            <OverflowMenuIcon aria-hidden />
+          </Button>
+        )}
+        <StackLayout as="li" direction="row" gap={1} align="center">
+          <Separator />
+          <Text maxRows={1}>Current level</Text>
+        </StackLayout>
+      </FlowLayout>
+    </nav>
+  );
+};
