ad593fbf79bf25258dcba8d2a938e0fc4e5dbc59
lukeac123 <96307810+lukeac123@users.noreply.github.com>
2024-05-31 15:35:54 +0100

    docs (#3501)

---
+++ site/src/examples/slider/RangeFormField.tsx
@@ -0,0 +1,38 @@
+import { ReactElement } from "react";
+import { Slider } from "@salt-ds/lab";
+import {
+  StackLayout,
+  FormField,
+  FormFieldLabel,
+  FlexLayout,
+} from "@salt-ds/core";
+
+export const RangeFormField = (): ReactElement => (
+  <StackLayout style={{ width: "400px" }}>
+    <FormField>
+      <FlexLayout gap={1}>
+        <FormFieldLabel style={{ width: "max-content" }}>
+          Field Label
+        </FormFieldLabel>
+        <Slider
+          aria-label="single"
+          defaultValue={[20, 80]}
+          min={0}
+          max={100}
+          style={{ flexGrow: 1 }}
+        />
+      </FlexLayout>
+    </FormField>
+    <FormField>
+      <FormFieldLabel> Field Label </FormFieldLabel>
+      <Slider
+        aria-label="single"
+        defaultValue={[20, 80]}
+        marks="bottom"
+        min={0}
+        max={100}
+        step={10}
+      />
+    </FormField>
+  </StackLayout>
+);
