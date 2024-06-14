0d33ec8c7f5be7317731f87b7bacdaa6ca903bb3
SudarshanramKK <156989415+Sudarshanram-krishnan@users.noreply.github.com>
2024-06-14 10:08:48 +0530

    Update mosaic.ts
    updated

--- site/src/types/mosaic.ts
+++ site/src/types/mosaic.ts
@@ -1,29 +1,28 @@
-import type {
-  MosaicAppProps,
-  MiddlewarePresetsProps,
+import type { 
+  MosaicAppProps, MiddlewarePresetsProps,
 } from "@jpmorganchase/mosaic-site-middleware";
 
 /**
  * In this file, define each of your own Middleware props, which combine together into MyAppProps
  *
  * Define the props returned by your middleware
- *
- * export interface ExampleMiddlewareProps {
- *   someProp?: string;
- * }
- *
+ */
+ export interface ExampleMiddlewareProps {
+    someProp?: string;
+  }
+ /*
  * Insert the options you want to pass to your middleware (optional)
  *
- * export interface ExampleMiddlewareOptions {
- *   someOption: number;
- * }
- *
+ */ 
+export interface ExampleMiddlewareOptions {
+    someOption: number;
+ }
+ /*
  * Then compose all your own Middleware props with the default MosaicAppProps
  *
  * export interface MyAppProps extends MosaicAppProps<MyMiddlewareProps> {};
  */
 
-export declare type MyMiddlewareProps =
-  MiddlewarePresetsProps /* & ExampleMiddlewareOptions */;
+export declare type MyMiddlewareProps = MiddlewarePresetsProps & ExampleMiddlewareOptions;
 
 export type MyAppProps = MosaicAppProps<MyMiddlewareProps>["props"];
