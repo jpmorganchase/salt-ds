1a1911ec58dba02cb9e0fd69b7da8ab658eb51a0
F745860 <vineet.dhiman@chase.com>
2024-06-17 16:28:57 +0530

    added divider examples to site

--- site/src/examples/ag-grid-theme/StatusBar.tsx
+++ site/src/examples/ag-grid-theme/StatusBar.tsx
@@ -1,8 +1,23 @@
-import { Text } from "@salt-ds/core";
+import { Text, StackLayout, Divider } from "@salt-ds/core";
 import { AgGridReact, AgGridReactProps } from "ag-grid-react";
 // refer to https://github.com/jpmorganchase/salt-ds/tree/main/site/src/examples/ag-grid-theme/data
 import { defaultColumns, defaultData } from "./data";
 import { useAgGridHelpers } from "./useAgGridHelpers";
+
+const CustomClickableStatusBar = () => {
+  return (
+    <StackLayout direction="row" align="center" gap={1}>
+      <Divider
+        orientation="vertical"
+        variant="secondary"
+        style={{ height: "var(--salt-text-fontSize)" }}
+      />
+      <Text color="secondary" style={{ lineHeight: "var(--salt-size-base)" }}>
+        Custom Component
+      </Text>
+    </StackLayout>
+  );
+};
 
 const statusBar = {
   statusPanels: [
@@ -13,6 +28,9 @@ const statusBar = {
     { statusPanel: "agFilteredRowCountComponent" },
     { statusPanel: "agSelectedRowCountComponent" },
     { statusPanel: "agAggregationComponent" },
+    {
+      statusPanel: CustomClickableStatusBar,
+    },
   ],
 };
 
