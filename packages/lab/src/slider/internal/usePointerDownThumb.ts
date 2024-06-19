c984220af4651ee783a21ba02c2cfcc0028ba3b9
Tom Hazledine <tom@tomhazledine.com>
2024-06-19 13:27:25 +0100

    unify usePointerDownThumb and usePointerDownTrack

--- packages/lab/src/slider/internal/usePointerDownThumb.ts
+++ packages/lab/src/slider/internal/usePointerDownThumb.ts
@@ -1,6 +1,6 @@
 import { RefObject, useState } from "react";
 import { SliderValue, SliderChangeHandler } from "../types";
-import { getValue, setValue } from "./utils";
+import { getNearestIndex, getValue, setValue } from "./utils";
 
 export function usePointerDownThumb(
   trackRef: RefObject<HTMLDivElement>,
@@ -9,28 +9,38 @@ export function usePointerDownThumb(
   step: number,
   value: SliderValue,
   onChange: SliderChangeHandler,
-  index: number,
+  index: number | null,
   activeThumb: number | undefined,
   setActiveThumb: (index: number | undefined) => void
 ) {
   const [pointerDown, setPointerDown] = useState(false);
 
-  const onDownThumb = () => {
-    document.addEventListener("pointermove", onPointerMove);
-    document.addEventListener("pointerup", onPointerUp);
+  const onDownThumb = (index: number) => {
+    const handlePointerMove = (e: PointerEvent) => onPointerMove(e, index);
+    const handlePointerUp = (e: PointerEvent) =>
+      _onPointerUp(e, handlePointerMove, handlePointerUp);
+
+    document.addEventListener("pointermove", handlePointerMove);
+    document.addEventListener("pointerup", handlePointerUp);
     setActiveThumb(index);
     setPointerDown(true);
   };
 
-  const onPointerUp = (event: PointerEvent) => {
+  const _onPointerUp = (
+    event: PointerEvent,
+    handlePointerMove: (e: PointerEvent) => void,
+    handlePointerUp: (e: PointerEvent) => void
+  ) => {
     event.preventDefault();
-    document.removeEventListener("pointermove", onPointerMove);
-    document.removeEventListener("pointerup", onPointerUp);
+    document.removeEventListener("pointermove", handlePointerMove);
+    document.removeEventListener("pointerup", handlePointerUp);
     setActiveThumb(undefined);
     setPointerDown(false);
   };
 
-  const onPointerMove = (event: PointerEvent): void => {
+  const onPointerMove = (event: PointerEvent, index: number): void => {
+    if (index === null) return;
+
     const { clientX } = event;
     let newValue: number | undefined = getValue(
       trackRef,
@@ -50,15 +60,38 @@ export function usePointerDownThumb(
   };
 
   return {
+    trackProps: {
+      onPointerDown(event: PointerEvent) {
+        const { clientX } = event;
+        const newValue: number = getValue(trackRef, min, max, step, clientX);
+        const nearestIndex = getNearestIndex(value, newValue);
+
+        setValue(
+          value,
+          newValue,
+          value.length > 1 ? nearestIndex : 0,
+          onChange
+        );
+        onDownThumb(nearestIndex);
+      },
+      onPointerUp() {
+        setActiveThumb(undefined);
+      },
+      onPointerOut() {
+        !pointerDown && setActiveThumb(undefined);
+      },
+    },
     thumbProps: {
-      onPointerDown() {
-        onDownThumb();
+      onPointerDown(event: PointerEvent) {
+        event.stopPropagation();
+        if (index === null) return;
+        onDownThumb(index);
       },
       onPointerOver() {
-        if (activeThumb === undefined) setActiveThumb(index);
+        if (activeThumb === undefined && index !== null) setActiveThumb(index);
       },
       onFocus() {
-        setActiveThumb(index);
+        if (index !== null) setActiveThumb(index);
       },
       onPointerUp() {
         setActiveThumb(undefined);
