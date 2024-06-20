bcb58a1f95daac746634b5cebef28e0c9b990e38
Fernanda Castillo <fersitacastillo123@gmail.com>
2024-06-20 12:08:00 +0100

    prettier

--- site/src/examples/tag/Bordered.tsx
+++ site/src/examples/tag/Bordered.tsx
@@ -1,4 +1,3 @@
-
 import { ReactElement } from "react";
 import { Tag } from "@salt-ds/lab";
 import { FlowLayout } from "@salt-ds/core";
@@ -6,6 +5,8 @@ import { FlowLayout } from "@salt-ds/core";
 export const Bordered = (): ReactElement => (
   <FlowLayout>
     <Tag bordered>Primary</Tag>
-    <Tag variant="secondary" bordered>Secondary</Tag>
+    <Tag variant="secondary" bordered>
+      Secondary
+    </Tag>
   </FlowLayout>
 );
--- site/src/examples/tag/WithIcon.tsx
+++ site/src/examples/tag/WithIcon.tsx
@@ -1,10 +1,16 @@
 import { ReactElement } from "react";
+import { FlowLayout } from "@salt-ds/core";
 import { Tag } from "@salt-ds/lab";
 import { LineChartIcon, ClockIcon } from "@salt-ds/icons";
 
 export const WithIcon = (): ReactElement => (
   <FlowLayout>
-    <Tag><LineChartIcon />Data</Tag>
-    <Tag variant="secondary"><ClockIcon /> Coming soon</Tag>
+    <Tag>
+      <LineChartIcon />
+      Data
+    </Tag>
+    <Tag variant="secondary">
+      <ClockIcon /> Coming soon
+    </Tag>
   </FlowLayout>
 );
