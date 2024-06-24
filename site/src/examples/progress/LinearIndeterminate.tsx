b2026f51060176492bc75b5462568e3050f0cbb3
origami-z <5257855+origami-z@users.noreply.github.com>
2024-06-24 14:08:46 +0100

    Remove site example change state

--- site/src/examples/progress/LinearIndeterminate.tsx
+++ site/src/examples/progress/LinearIndeterminate.tsx
@@ -1,71 +1,23 @@
-import { ReactElement, useEffect, useState } from "react";
 import {
   Button,
   LinearProgress,
-  LinearProgressProps,
+  Text,
   Toast,
   ToastContent,
-  ValidationStatus,
-  Text,
 } from "@salt-ds/core";
 import { CloseIcon } from "@salt-ds/icons";
+import { ReactElement } from "react";
 
 export const LinearIndeterminate = (): ReactElement => {
-  const [variant, setVariant] =
-    useState<LinearProgressProps["variant"]>("indeterminate");
-
-  const [value, setValue] = useState(0);
-
-  const [toastStatus, setToastStatus] = useState<{
-    header: string;
-    status: ValidationStatus;
-    message: string;
-  }>({
-    header: "File uploading",
-    status: "info",
-    message: "File upload to shared drive in progress.",
-  });
-
-  useEffect(() => {
-    const timer = setTimeout(() => setVariant("determinate"), 4000);
-    return () => clearTimeout(timer);
-  }, []);
-
-  useEffect(() => {
-    if (variant === "determinate" && value < 100) {
-      const interval = setInterval(
-        () => setValue((currentValue) => currentValue + 1),
-        20
-      );
-      return () => clearInterval(interval);
-    }
-  }, [value, variant]);
-
-  useEffect(() => {
-    if (value === 100) {
-      setToastStatus({
-        header: "Upload complete",
-        status: "success",
-        message: "File has successfully been uploaded to shared drive.",
-      });
-    }
-  }, [value]);
-
   return (
-    <Toast status={toastStatus.status}>
+    <Toast status="info">
       <ToastContent>
         <div>
           <Text>
-            <strong>{toastStatus.header}</strong>
+            <strong>File uploading</strong>
           </Text>
-          <div>{toastStatus.message}</div>
-          {value !== 100 && (
-            <LinearProgress
-              aria-label="Download"
-              variant={variant}
-              value={value}
-            />
-          )}
+          <Text>File upload to shared drive in progress.</Text>
+          <LinearProgress aria-label="Download" variant="indeterminate" />
         </div>
       </ToastContent>
       <Button variant="secondary" aria-label="Dismiss">
