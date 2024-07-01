d5fbd43284b7c25bfce7d3623d5ebcbb1285785e
Tom Hazledine <tom@tomhazledine.com>
2024-07-01 14:07:33 +0100

    Slider docs update

--- site/src/examples/slider/index.ts
+++ site/src/examples/slider/index.ts
@@ -1,5 +1,6 @@
 export * from "./Single";
 export * from "./Range";
+export * from "./CustomStep";
 export * from "./SingleFormField";
 export * from "./RangeFormField";
 export * from "./WithInput";
