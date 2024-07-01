f676509585047576cae3143cfd23ab2ad6e0bb75
Tom Hazledine <tom@tomhazledine.com>
2024-07-01 14:07:33 +0100

    Slider docs update

---
+++ site/src/examples/slider/CustomStep.tsx
@@ -0,0 +1,17 @@
+import { ReactElement } from "react";
+import { Slider } from "@salt-ds/lab";
+import { StackLayout } from "@salt-ds/core";
+
+export const CustomStep = (): ReactElement => (
+  <StackLayout gap={3}>
+    <Slider aria-label="single" min={0} max={100} style={{ width: "400px" }} />
+    <Slider
+      aria-label="single"
+      marks="all"
+      min={-1}
+      max={1}
+      step={0.2}
+      style={{ width: "400px" }}
+    />
+  </StackLayout>
+);
