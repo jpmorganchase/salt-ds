309ddc6b724f5b6721339974171acb1450f5b064
Liam Smith <liam.smith@jpmorgan.com>
2024-07-05 22:09:21 +0100

    Add meta description

--- site/src/pages/_document.tsx
+++ site/src/pages/_document.tsx
@@ -1,12 +1,16 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
+const description = 'Salt is the J.P. Morgan design system, an open-source solution for building exceptional products and digital experiences in financial services and other industries.'
+
 /*
  * This is the same as the default Next.js _document.js, but with lang="en".
  */
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name={"description"} content={description} />
+      </Head>
       <body>
         <Main />
         <NextScript />
