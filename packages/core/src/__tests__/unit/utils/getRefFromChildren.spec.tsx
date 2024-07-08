1b6f34c860dbed05e41b4df82c7ffac8c09ffadc
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2024-07-08 13:43:16 +0100

    Add test

---
+++ packages/core/src/__tests__/unit/utils/getRefFromChildren.spec.tsx
@@ -0,0 +1,25 @@
+import { getRefFromChildren } from "@salt-ds/core";
+import { describe, it, expect } from "vitest";
+import { ReactNode } from "react";
+
+describe("getRefFromChildren", () => {
+  it("should return null if child is not a valid element", () => {
+    expect(getRefFromChildren(null)).toBeNull();
+    expect(getRefFromChildren(undefined)).toBeNull();
+    expect(getRefFromChildren("string")).toBeNull();
+    expect(getRefFromChildren(123)).toBeNull();
+    expect(getRefFromChildren(<></>)).toBeNull();
+    expect(getRefFromChildren([])).toBeNull();
+  });
+
+  it("should return null if ref is not defined", () => {
+    const child = <button />;
+    expect(getRefFromChildren(child)).toBeNull();
+  });
+
+  it("should return ref from child", () => {
+    const ref = () => {};
+    const child = <button ref={ref} />;
+    expect(getRefFromChildren(child)).toBe(ref);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["**/*.spec.[jt]s"],
+    include: ["**/*.spec.[jt]s(x)"],
   },
 });
