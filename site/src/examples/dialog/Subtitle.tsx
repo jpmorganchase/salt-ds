054ed2cdb463c5399ef1af4e00af3dc7c291d835
Liam Smith <liam.smith@jpmorgan.com>
2024-07-20 22:07:59 +0100

    add dialog subtitle example

---
+++ site/src/examples/dialog/Subtitle.tsx
@@ -0,0 +1,56 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogCloseButton,
+  DialogContent,
+  DialogHeader,
+  FormField,
+  FormFieldLabel,
+  Input,
+  useId,
+} from "@salt-ds/core";
+import { type ReactElement, useState } from "react";
+
+export const Subtitle = (): ReactElement => {
+  const [open, setOpen] = useState(false);
+  const id = useId();
+
+  const handleRequestOpen = () => {
+    setOpen(true);
+  };
+
+  const onOpenChange = (value: boolean) => {
+    setOpen(value);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button onClick={handleRequestOpen}>Open dialog with subtitle</Button>
+      <Dialog open={open} onOpenChange={onOpenChange} size="small" id={id}>
+        <DialogHeader
+          header="Subscribe"
+          subtitle="Receive emails about the latest updates"
+        />
+        <DialogCloseButton onClick={handleClose} />
+
+        <DialogContent>
+          <FormField necessity="asterisk">
+            <FormFieldLabel> Email </FormFieldLabel>
+            <Input defaultValue="Email Address" />
+          </FormField>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button variant="cta" onClick={handleClose}>
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
