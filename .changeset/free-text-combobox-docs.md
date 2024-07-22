017c259f7ddae376e7a36f05df26821df35ac6c1
thushara joseph <thushara.joseph@chase.com>
2024-07-22 16:06:18 +0530

    added free text docs to site

---
+++ .changeset/free-text-combobox-docs.md
@@ -0,0 +1,5 @@
+---
+"@salt-ds/core": minor
+---
+
+Added site docs for Free text examples for combobox.
---
+++ site/src/examples/combo-box/FreeText.tsx
@@ -0,0 +1,56 @@
+import { ComboBox, Option } from "@salt-ds/core";
+import {
+  type ChangeEvent,
+  type ReactElement,
+  type SyntheticEvent,
+  useState,
+} from "react";
+import { shortColorData } from "./exampleData";
+
+export const FreeText = (): ReactElement => {
+  const [value, setValue] = useState("");
+  const [selectedValues, setSelectedValues] = useState<string[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    // React 16 backwards compatibility
+    event.persist();
+    const value = event.target.value;
+    setValue(value);
+  };
+
+  const handleSelectionChange = (
+    event: SyntheticEvent,
+    newSelected: string[],
+  ) => {
+    // React 16 backwards compatibility
+    event.persist();
+    setSelectedValues(newSelected);
+    setValue("");
+  };
+
+  return (
+    <ComboBox
+      multiselect
+      onChange={handleChange}
+      onSelectionChange={handleSelectionChange}
+      value={value}
+      style={{ width: "266px" }}
+      onBlur={() => {
+        setValue("");
+      }}
+    >
+      {Array.from(new Set(shortColorData.concat(selectedValues)))
+        .filter((state) =>
+          state.toLowerCase().includes(value.trim().toLowerCase()),
+        )
+        .map((state) => (
+          <Option value={state} key={state} />
+        ))}
+      {value && !shortColorData.includes(value) && (
+        <Option value={value} key={value}>
+          Add &quot;{value}&quot;
+        </Option>
+      )}
+    </ComboBox>
+  );
+};
