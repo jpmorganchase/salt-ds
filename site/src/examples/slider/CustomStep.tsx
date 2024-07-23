869818a2c819fa951b34521a21de516de393bf8d
Tom Hazledine <tom@tomhazledine.com>
2024-07-23 13:04:34 +0100

    clean up layout for slider examples

--- site/src/examples/slider/CustomStep.tsx
+++ site/src/examples/slider/CustomStep.tsx
@@ -1,17 +1,13 @@
-import { StackLayout } from "@salt-ds/core";
 import { Slider } from "@salt-ds/lab";
 import type { ReactElement } from "react";
 
 export const CustomStep = (): ReactElement => (
-  <StackLayout gap={3}>
-    <Slider aria-label="single" min={0} max={100} style={{ width: "400px" }} />
-    <Slider
-      aria-label="single"
-      marks="all"
-      min={-1}
-      max={1}
-      step={0.2}
-      style={{ width: "400px" }}
-    />
-  </StackLayout>
+  <Slider
+    aria-label="single"
+    marks="all"
+    min={-1}
+    max={1}
+    step={0.2}
+    style={{ width: "400px" }}
+  />
 );
