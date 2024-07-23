1b3d2ffdfb3314929ed4d7577506bc9a2d2b8690
Tom Hazledine <tom@tomhazledine.com>
2024-07-23 14:23:56 +0100

    smarter decimal rounding for step marks

--- packages/lab/src/slider/internal/utils.ts
+++ packages/lab/src/slider/internal/utils.ts
@@ -1,34 +1,32 @@
 import type { RefObject } from "react";
 import type { SliderChangeHandler, SliderValue } from "../types";
 
-export function getValue(
+export const getValue = (
   trackRef: RefObject<Element>,
   min: number,
   max: number,
   step: number,
   clientX: number,
-) {
+) => {
   const { width, x } = trackRef.current!.getBoundingClientRect();
   const localX = clientX - x;
   const normaliseBetweenValues = (localX / width) * (max - min) + min;
-  let value = roundToStep(normaliseBetweenValues, step);
-  value = roundToTwoDp(value);
-  value = clampValue(value, min, max);
+  const roundedToStep = roundToStep(normaliseBetweenValues, step);
+  const rounded = Number(roundedToStep.toFixed(1));
+  const value = clampValue(rounded, min, max);
   return value;
-}
+};
 
-export function setValue(
+export const setValue = (
   value: SliderValue,
   newValue: number,
   index: number,
   onChange: SliderChangeHandler,
-) {
+) => {
   const newValueArray = [...value];
   newValueArray.splice(index, 1, newValue);
   onChange(newValueArray);
-}
-
-export const roundToTwoDp = (value: number) => Math.round(value * 100) / 100;
+};
 
 export const roundToStep = (value: number, step: number) =>
   Math.round(value / step) * step;
@@ -43,37 +41,52 @@ export const clampValue = (value: number, min: number, max: number) => {
   return value;
 };
 
-export function getPercentage(min: number, max: number, value: number) {
+export const getPercentage = (min: number, max: number, value: number) => {
   const percentage = ((value - min) / (max - min)) * 100;
   return Math.min(Math.max(percentage, 0), 100);
-}
+};
 
-export function getPercentageDifference(
+export const getPercentageDifference = (
   min: number,
   max: number,
   value: number[],
-) {
+) => {
   const valueDiff = value[1] - value[0];
   const percentage = ((valueDiff - min) / (max - min)) * 100;
   return `${Math.min(Math.max(percentage, 0), 100)}%`;
-}
+};
 
-export function getPercentageOffset(min: number, max: number, value: number[]) {
+export const getPercentageOffset = (
+  min: number,
+  max: number,
+  value: number[],
+) => {
   const offsetLeft = ((value[0] - min) / (max - min)) * 100;
   return `${Math.min(Math.max(offsetLeft, 0), 100)}%`;
-}
+};
 
-export function getMarkStyles(min: number, max: number, step: number) {
+export const countDecimalPlaces = (num: number) => {
+  const parts = num.toString().split(".");
+  return parts.length > 1 ? parts[1].length : 0;
+};
+
+export const getMarkStyles = (min: number, max: number, step: number) => {
   const marks = [];
-  for (let i = min; i <= max; i = i + step) {
-    const MarkPosition = getPercentage(min, max, i);
-    const MarkLabel = roundToTwoDp(i);
-    marks.push({ index: MarkLabel, position: `${MarkPosition}%` });
+  for (let i = min; i <= max; i = Number((i + step).toPrecision(4))) {
+    const value = Number(i.toPrecision(4));
+    const position = `${getPercentage(min, max, value)}%`;
+    marks.push({ value, position });
   }
-  return marks;
-}
+  const decimals = Math.max(
+    ...marks.map((mark) => countDecimalPlaces(mark.value)),
+  );
+  return marks.map((mark) => ({
+    ...mark,
+    label: mark.value.toFixed(decimals),
+  }));
+};
 
-export function getNearestIndex(value: number[], newValue: number) {
+export const getNearestIndex = (value: number[], newValue: number) => {
   if (value.length === 1) return 0;
 
   const nearestIndex = value.reduce((acc, value) => {
@@ -83,4 +96,4 @@ export function getNearestIndex(value: number[], newValue: number) {
     return index;
   }, 0);
   return nearestIndex;
-}
+};
