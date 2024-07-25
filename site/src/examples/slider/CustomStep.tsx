409055b7e13b109aa8c20910df8274ea63eee78e
Tom Hazledine <tom@tomhazledine.com>
2024-07-25 11:26:12 +0100

    More detail for CustomStep example docs

--- site/src/examples/slider/CustomStep.tsx
+++ site/src/examples/slider/CustomStep.tsx
@@ -1,13 +1,12 @@
+import { StackLayout } from "@salt-ds/core";
 import { Slider } from "@salt-ds/lab";
 import type { ReactElement } from "react";
 
 export const CustomStep = (): ReactElement => (
-  <Slider
-    aria-label="single"
-    marks="all"
-    min={-1}
-    max={1}
-    step={0.2}
-    style={{ width: "400px" }}
-  />
+  <StackLayout gap={5} style={{ width: "400px" }}>
+    <Slider min={-1} max={1} marks="all" />
+    <Slider min={-1} max={1} step={0.5} marks="all" />
+    <Slider min={-1} max={1} step={0.2} marks="all" />
+    <Slider min={-1} max={1} step={0.25} marks="all" />
+  </StackLayout>
 );
