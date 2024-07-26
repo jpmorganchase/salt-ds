771c644ddbfda14cb540bbafd056e59b6c2151bb
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2024-07-26 11:54:30 +0100

    Update docs

--- site/src/examples/button/FullWidth.tsx
+++ site/src/examples/button/FullWidth.tsx
@@ -6,11 +6,20 @@ export const FullWidth = (): ReactElement => (
     <Button color="neutral" appearance="solid">
       Solid neutral full width Button
     </Button>
+    <Button color="neutral" appearance="outline">
+      Outline neutral full width Button
+    </Button>
     <Button color="neutral" appearance="transparent">
       Transparent neutral full width Button
     </Button>
     <Button color="accent" appearance="solid">
       Solid accent full width Button
+    </Button>
+    <Button color="accent" appearance="outline">
+      Outline accent full width Button
+    </Button>
+    <Button color="accent" appearance="transparent">
+      Transparent accent full width Button
     </Button>
   </StackLayout>
 );
