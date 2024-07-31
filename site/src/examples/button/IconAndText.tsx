8bb297cd4345e7c398a7e60c5e9854f4194714ec
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2024-07-31 08:50:33 +0100

    Updates

--- site/src/examples/button/IconAndText.tsx
+++ site/src/examples/button/IconAndText.tsx
@@ -8,27 +8,27 @@ import type { ReactElement } from "react";
 
 export const IconAndText = (): ReactElement => (
   <StackLayout gap={3}>
-      <FlowLayout>
-        <Button appearance="solid" color="accent">
-          <SearchIcon /> Search
-        </Button>
-        <Button appearance="outline" color="accent">
-          <DownloadIcon /> Download
-        </Button>
-        <Button appearance="transparent" color="accent">
-          Expand <DoubleChevronRightIcon />
-        </Button>
-      </FlowLayout>
-      <FlowLayout>
-        <Button appearance="solid" color="neutral">
-          <SearchIcon /> Search
-        </Button>
-        <Button appearance="outline" color="neutral">
-          <DownloadIcon /> Download
-        </Button>
-        <Button appearance="transparent" color="neutral">
-          Expand <DoubleChevronRightIcon />
-        </Button>
-      </FlowLayout>
+    <FlowLayout>
+      <Button appearance="solid" color="accent">
+        <SearchIcon /> Search
+      </Button>
+      <Button appearance="outline" color="accent">
+        <DownloadIcon /> Download
+      </Button>
+      <Button appearance="transparent" color="accent">
+        Expand <DoubleChevronRightIcon />
+      </Button>
+    </FlowLayout>
+    <FlowLayout>
+      <Button appearance="solid" color="neutral">
+        <SearchIcon /> Search
+      </Button>
+      <Button appearance="outline" color="neutral">
+        <DownloadIcon /> Download
+      </Button>
+      <Button appearance="transparent" color="neutral">
+        Expand <DoubleChevronRightIcon />
+      </Button>
+    </FlowLayout>
   </StackLayout>
 );
--- site/src/examples/button/IconOnly.tsx
+++ site/src/examples/button/IconOnly.tsx
@@ -1,38 +1,42 @@
-import { Button, GridLayout, Tooltip } from "@salt-ds/core";
+import { Button, FlowLayout, StackLayout, Tooltip } from "@salt-ds/core";
 import { SearchIcon, SendIcon, SettingsIcon } from "@salt-ds/icons";
 import type { ReactElement } from "react";
 
 export const IconOnly = (): ReactElement => (
-  <GridLayout columns={3}>
-    <Tooltip placement="top" content="Send comment">
-      <Button appearance="solid" color="accent">
-        <SendIcon />
-      </Button>
-    </Tooltip>
-    <Tooltip placement="top" content="Search document">
-      <Button appearance="outline" color="accent">
-        <SearchIcon />
-      </Button>
-    </Tooltip>
-    <Tooltip placement="top" content="Open settings">
-      <Button appearance="transparent" color="accent">
-        <SettingsIcon />
-      </Button>
-    </Tooltip>
-    <Tooltip placement="top" content="Send comment">
-      <Button appearance="solid" color="neutral">
-        <SendIcon />
-      </Button>
-    </Tooltip>
-    <Tooltip placement="top" content="Search document">
-      <Button appearance="outline" color="neutral">
-        <SearchIcon />
-      </Button>
-    </Tooltip>
-    <Tooltip placement="top" content="Open settings">
-      <Button appearance="transparent" color="neutral">
-        <SettingsIcon />
-      </Button>
-    </Tooltip>
-  </GridLayout>
+  <StackLayout gap={3}>
+    <FlowLayout>
+      <Tooltip placement="top" content="Send comment">
+        <Button appearance="solid" color="accent">
+          <SendIcon />
+        </Button>
+      </Tooltip>
+      <Tooltip placement="top" content="Search document">
+        <Button appearance="outline" color="accent">
+          <SearchIcon />
+        </Button>
+      </Tooltip>
+      <Tooltip placement="top" content="Open settings">
+        <Button appearance="transparent" color="accent">
+          <SettingsIcon />
+        </Button>
+      </Tooltip>
+    </FlowLayout>
+    <FlowLayout>
+      <Tooltip placement="top" content="Send comment">
+        <Button appearance="solid" color="neutral">
+          <SendIcon />
+        </Button>
+      </Tooltip>
+      <Tooltip placement="top" content="Search document">
+        <Button appearance="outline" color="neutral">
+          <SearchIcon />
+        </Button>
+      </Tooltip>
+      <Tooltip placement="top" content="Open settings">
+        <Button appearance="transparent" color="neutral">
+          <SettingsIcon />
+        </Button>
+      </Tooltip>
+    </FlowLayout>
+  </StackLayout>
 );
