5668b079203873a5889fac659007868b53e23196
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2024-08-07 08:49:55 +0100

    Update color example on text docs (#3956)

--- site/src/examples/text/Color.tsx
+++ site/src/examples/text/Color.tsx
@@ -5,7 +5,7 @@ export const Color = (): ReactElement => (
   <StackLayout>
     <Text color="primary">This is primary color of Text</Text>
     <Text color="secondary">This is secondary color of Text</Text>
-    <Text color="info">This is error color of Text</Text>
+    <Text color="info">This is info color of Text</Text>
     <Text color="error">This is error color of Text</Text>
     <Text color="warning">This is warning color of Text</Text>
     <Text color="success">This is success color of Text</Text>
