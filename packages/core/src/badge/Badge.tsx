3dddd8dfa84fd7c888f7373c1b81e9df0fe2ef1c
F745860 <vineet.dhiman@chase.com>
2024-08-09 14:53:04 +0530

    notification-dot-badge

--- packages/core/src/badge/Badge.tsx
+++ packages/core/src/badge/Badge.tsx
@@ -41,7 +41,7 @@ export const Badge = forwardRef<HTMLSpanElement, BadgeProps>(function Badge(
       {children}
       <span
         className={clsx(withBaseName("badge"), {
-          [withBaseName("topRight")]: children,
+          [withBaseName("topRight")]: children && value,
           [withBaseName("dotBadge")]: !value,
           [withBaseName("topRightCenter")]: children && !value,
         })}
