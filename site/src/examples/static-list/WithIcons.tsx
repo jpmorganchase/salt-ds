15f2f4a96a05f10d915180eaec169da2b74fff25
thushara joseph <thushara.joseph@chase.com>
2024-08-21 13:31:30 +0530

    design review alignemnet

--- site/src/examples/static-list/WithIcons.tsx
+++ site/src/examples/static-list/WithIcons.tsx
@@ -1,17 +1,21 @@
-import { FlexLayout, StackLayout, Text } from "@salt-ds/core";
-import { NotificationIcon } from "@salt-ds/icons";
+import { Button, StackLayout, Text } from "@salt-ds/core";
+import { NotificationIcon, NoteIcon } from "@salt-ds/icons";
 import { StaticList, StaticListItem } from "@salt-ds/lab";
 import type { ReactElement } from "react";
 
 const ListItem = () => (
   <StaticListItem>
-    <FlexLayout gap={1} style={{ width: "100%" }}>
-      <NotificationIcon />
-      <StackLayout gap={0.5}>
-        <Text color="inherit">Item label</Text>
-        <Text variant="secondary">Secondary label</Text>
-      </StackLayout>
-    </FlexLayout>
+    <NotificationIcon />
+    <StackLayout gap={0.5} style={{ padding: 'var(--salt-spacing-100) 0', width: '100%' }}>
+      <Text color="inherit">Item label</Text>
+      <Text variant="secondary">Secondary label</Text>
+    </StackLayout>
+    <Button variant="secondary" aria-label={"icon"}>
+      <NoteIcon aria-hidden />
+    </Button>
+    <Button variant="secondary" aria-label={"icon"}>
+      <NoteIcon aria-hidden />
+    </Button>
   </StaticListItem>
 );
 
