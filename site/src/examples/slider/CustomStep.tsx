ee14235fd7c69f32ccdaa4d247d9b5b56db8f2f9
Tom Hazledine <tom@tomhazledine.com>
2024-08-22 11:39:14 +0100

    clearer Slider examples

--- site/src/examples/slider/CustomStep.tsx
+++ site/src/examples/slider/CustomStep.tsx
@@ -1,12 +1,24 @@
-import { StackLayout } from "@salt-ds/core";
+import { FormField, FormFieldLabel, StackLayout } from "@salt-ds/core";
 import { Slider } from "@salt-ds/lab";
 import type { ReactElement } from "react";
 
 export const CustomStep = (): ReactElement => (
-  <StackLayout gap={5} style={{ width: "400px" }}>
-    <Slider min={-1} max={1} marks="all" />
-    <Slider min={-1} max={1} step={0.5} marks="all" />
-    <Slider min={-1} max={1} step={0.2} marks="all" />
-    <Slider min={-1} max={1} step={0.25} marks="all" />
+  <StackLayout gap={10} style={{ width: "400px" }}>
+    <FormField>
+      <FormFieldLabel>Step: 1 (default)</FormFieldLabel>
+      <Slider min={-1} max={1} marks="all" />
+    </FormField>
+    <FormField>
+      <FormFieldLabel>Step: 0.2</FormFieldLabel>
+      <Slider min={-1} max={1} step={0.2} marks="all" />
+    </FormField>
+    <FormField>
+      <FormFieldLabel>Step: 0.25 (two decimal places)</FormFieldLabel>
+      <Slider min={-1} max={1} step={0.25} marks="all" />
+    </FormField>
+    <FormField>
+      <FormFieldLabel>Step: 0.3 (not multiple of total range)</FormFieldLabel>
+      <Slider min={0} max={1} step={0.3} marks="all" defaultValue={[0.9]} />
+    </FormField>
   </StackLayout>
 );
--- site/src/examples/slider/index.ts
+++ site/src/examples/slider/index.ts
@@ -1,6 +1,5 @@
 export * from "./Single";
 export * from "./Range";
 export * from "./CustomStep";
-export * from "./SingleFormField";
-export * from "./RangeFormField";
+export * from "./WithFormField";
 export * from "./WithInput";
