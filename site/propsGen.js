0d38e1d0ed986090e5d72170d97ce93b6d737a7e
Zhihao Cui <5257855+origami-z@users.noreply.github.com>
2024-09-02 15:10:06 +0100

    Revert propsGen

--- site/propsGen.js
+++ site/propsGen.js
@@ -1,8 +1,6 @@
 const docgen = require("react-docgen-typescript");
 const path = require("node:path");
-// const { setTimeout } = require("node:timers/promises");
 const { writeFile } = require("node:fs");
-// const { writeFile } = require("node:fs/promises");
 
 const componentPackages = [
   "lab",
@@ -15,72 +13,29 @@ const componentPackages = [
 
 console.log("Generating props in packages:", componentPackages);
 
+const componentFiles = componentPackages.map((pkg) =>
+  path.join(__dirname, "..", "packages", pkg, "src", "index.ts"),
+);
+
 const options = {
   propFilter: (prop) =>
     !/@types[\\/]react[\\/]/.test(prop.parent?.fileName || ""),
 };
-console.log(new Date(), "start");
 
-const runPromise = () =>
-  Promise.all(
-    componentPackages.map((pkg) => {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          console.log(new Date(), pkg);
-          const componentFile = path.join(
-            __dirname,
-            "..",
-            "packages",
-            pkg,
-            "src",
-            "index.ts",
-          );
-          const parsedDoc = docgen.parse(componentFile, options);
-          const filePath = `./src/props/${pkg}-props.json`;
-          console.log(new Date(), "parsedDoc", filePath);
-          writeFile(filePath, JSON.stringify(parsedDoc, null, 2), (error) => {
-            if (error) {
-              console.log("An error has occurred ", error);
-              reject(error);
-            }
-            console.log(`${filePath} successfully generated`);
-            resolve();
-          });
-        }, 0);
-      });
-    }),
-  );
+const components = componentFiles.map((component) =>
+  docgen.parse(component, options),
+);
 
-runPromise()
-  .then((text) => {
-    console.log(new Date(), "end");
+const filePaths = componentPackages.map(
+  (pkg) => `./src/props/${pkg}-props.json`,
+);
 
-    console.log(text);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-// console.log(new Date(), "start");
-
-// const componentFiles = componentPackages.map((pkg) =>
-//   path.join(__dirname, "..", "packages", pkg, "src", "index.ts"),
-// );
-// const components = componentFiles.map((component) => {
-//   console.log(new Date(), "componentFiles.map", component);
-//   return docgen.parse(component, options);
-// });
-
-// const filePaths = componentPackages.map(
-//   (pkg) => `./src/props/${pkg}-props.json`,
-// );
-
-// components.forEach((component, index) =>
-//   writeFile(filePaths[index], JSON.stringify(component, null, 2), (error) => {
-//     if (error) {
-//       console.log("An error has occurred ", error);
-//       return;
-//     }
-//     console.log(`${filePaths[index]} successfully generated`);
-//   }),
-// );
+components.forEach((component, index) =>
+  writeFile(filePaths[index], JSON.stringify(component, null, 2), (error) => {
+    if (error) {
+      console.log("An error has occurred ", error);
+      return;
+    }
+    console.log(`${filePaths[index]} successfully generated`);
+  }),
+);
