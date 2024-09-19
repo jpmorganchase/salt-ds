a10b27cdc3f48df3785389ad6ddf1b203af80808
Tom Hazledine <tom@tomhazledine.com>
2024-09-19 11:35:33 +0100

    add scroll shadow to OverlayPanelContent

--- packages/core/src/overlay/OverlayPanelContent.tsx
+++ packages/core/src/overlay/OverlayPanelContent.tsx
@@ -4,15 +4,16 @@ import clsx from "clsx";
 import {
   type ComponentPropsWithoutRef,
   type ReactNode,
+  type UIEvent,
   forwardRef,
+  useState,
 } from "react";
 import { makePrefixer } from "../utils";
 import overlayPanelContentCss from "./OverlayPanelContent.css";
 
 const withBaseName = makePrefixer("saltOverlayPanelContent");
 
-export interface OverlayPanelContentProps
-  extends ComponentPropsWithoutRef<"div"> {
+export interface OverlayHeaderProps extends ComponentPropsWithoutRef<"div"> {
   /**
    * The content of Overlay Panel Content
    */
@@ -21,7 +22,7 @@ export interface OverlayPanelContentProps
 
 export const OverlayPanelContent = forwardRef<
   HTMLDivElement,
-  OverlayPanelContentProps
+  OverlayHeaderProps
 >(function OverlayPanelContent(props, ref) {
   const targetWindow = useWindow();
   useComponentCssInjection({
@@ -31,10 +32,23 @@ export const OverlayPanelContent = forwardRef<
   });
 
   const { children, className, ...rest } = props;
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const handleScroll = (event: UIEvent<HTMLElement>) => {
+    setScrollTop(event.currentTarget.scrollTop);
+  };
 
   return (
-    <div className={clsx(withBaseName(), className)} {...rest} ref={ref}>
-      {children}
-    </div>
+    <>
+      <div className={clsx({ [withBaseName("scroll")]: scrollTop > 0 })} />
+      <div
+        className={clsx(withBaseName(), className)}
+        onScroll={handleScroll}
+        {...rest}
+        ref={ref}
+      >
+        {children}
+      </div>
+    </>
   );
 });
