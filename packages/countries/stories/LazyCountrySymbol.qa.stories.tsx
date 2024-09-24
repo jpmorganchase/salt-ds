ab0c9d415500592a8725239c3895aae1d846ee05
origami-z <5257855+origami-z@users.noreply.github.com>
2024-09-24 16:02:24 +0100

    Update QA story

--- packages/countries/stories/LazyCountrySymbol.qa.stories.tsx
+++ packages/countries/stories/LazyCountrySymbol.qa.stories.tsx
@@ -1,6 +1,7 @@
 import { LazyCountrySymbol, countryMetaMap } from "@salt-ds/countries";
 import type { CountryCode } from "@salt-ds/countries";
 import type { Meta, StoryFn } from "@storybook/react";
+import { QAContainer } from "docs/components";
 import { Suspense } from "react";
 
 export default {
@@ -36,12 +37,24 @@ export const AllLazyCountrySymbols: StoryFn = () => {
             ))}
         </div>
       ))}
+    </Suspense>
+  );
+};
+
+AllLazyCountrySymbols.parameters = {
+  chromatic: { disableSnapshot: false },
+};
+
+export const AllLazyCountrySharpSymbols: StoryFn = (props) => {
+  return (
+    <Suspense fallback="Loading...">
       {sizes.map((size) => (
         <div
           key={size}
           style={{
             display: "grid",
-            gridTemplateColumns: "repeat(15, auto)",
+            // Different cols, to avoid overlap in TD in Chromatic
+            gridTemplateColumns: "repeat(10, auto)",
             gap: 8,
             padding: "12px 0",
           }}
