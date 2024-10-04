60a8d6a972d5391314e34c098e31eb6786bdc760
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2024-10-04 09:13:50 +0100

    Update pill to be active when a menu is open (#4225)

---
+++ .changeset/stale-jeans-sniff.md
@@ -0,0 +1,5 @@
+---
+"@salt-ds/core": minor
+---
+
+Pill will now go into an active state when it's a menu trigger and has the menu is open. This aligns Pill with Button.
--- packages/core/stories/pill/pill.qa.stories.tsx
+++ packages/core/stories/pill/pill.qa.stories.tsx
@@ -1,10 +1,10 @@
-import { Pill } from "@salt-ds/core";
+import { Menu, MenuItem, MenuPanel, MenuTrigger, Pill } from "@salt-ds/core";
 import { FavoriteIcon } from "@salt-ds/icons";
 import type { Meta, StoryFn } from "@storybook/react";
 import { QAContainer, type QAContainerProps } from "docs/components";
 
 export default {
-  title: "Core/Pill/QA",
+  title: "Core/Pill/Pill QA",
   component: Pill,
 } as Meta<typeof Pill>;
 
@@ -13,7 +13,14 @@ const noop = () => undefined;
 export const ExamplesGrid: StoryFn<QAContainerProps> = (props) => {
   const { className, ...rest } = props;
   return (
-    <QAContainer cols={3} height={300} itemPadding={3} width={1300} {...rest}>
+    <QAContainer
+      itemWidthAuto
+      cols={4}
+      height={300}
+      itemPadding={21}
+      width={1300}
+      {...rest}
+    >
       <Pill className={className} onClick={noop}>
         Default Pill
       </Pill>
@@ -23,6 +30,14 @@ export const ExamplesGrid: StoryFn<QAContainerProps> = (props) => {
       <Pill className={className} onClick={noop}>
         <FavoriteIcon /> With Icon Pill
       </Pill>
+      <Menu open>
+        <MenuTrigger>
+          <Pill>Menu Pill</Pill>
+        </MenuTrigger>
+        <MenuPanel>
+          <MenuItem>Copy</MenuItem>
+        </MenuPanel>
+      </Menu>
     </QAContainer>
   );
 };
