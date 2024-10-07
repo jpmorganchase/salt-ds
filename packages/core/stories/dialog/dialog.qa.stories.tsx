1145dc945f9be1db6a20c6650d335f099f3dcb14
Fernanda Castillo <fersitacastillo123@gmail.com>
2024-10-07 11:38:42 +0100

    dialog headers qas and iframe tests

--- packages/core/stories/dialog/dialog.qa.stories.tsx
+++ packages/core/stories/dialog/dialog.qa.stories.tsx
@@ -4,39 +4,53 @@ import {
   DialogActions,
   DialogCloseButton,
   DialogContent,
-  DialogContext,
   DialogHeader,
   type DialogProps,
-  FormField,
-  FormFieldLabel,
-  Input,
-  StackLayout,
+  FlowLayout,
+  SaltProvider,
+  ownerWindow,
 } from "@salt-ds/core";
 import type { Meta, StoryFn } from "@storybook/react";
 import { QAContainer, type QAContainerProps } from "docs/components";
 
 import "./dialog.stories.css";
+import font300iCss from "@fontsource/open-sans/300-italic.css";
+import font300Css from "@fontsource/open-sans/300.css";
+import font400iCss from "@fontsource/open-sans/400-italic.css";
+import font400Css from "@fontsource/open-sans/400.css";
+import font500iCss from "@fontsource/open-sans/500-italic.css";
+import font500Css from "@fontsource/open-sans/500.css";
+import font600iCss from "@fontsource/open-sans/600-italic.css";
+import font600Css from "@fontsource/open-sans/600.css";
+import font700iCss from "@fontsource/open-sans/700-italic.css";
+import font700Css from "@fontsource/open-sans/700.css";
+import font800iCss from "@fontsource/open-sans/800-italic.css";
+import font800Css from "@fontsource/open-sans/800.css";
+import { useComponentCssInjection } from "@salt-ds/styles";
+import themeCss from "@salt-ds/theme/index.css";
+import { WindowProvider, useWindow } from "@salt-ds/window";
+import { Fragment, type ReactNode, useCallback, useState } from "react";
+import { createPortal } from "react-dom";
 
-function FakeDialog({ children, status, id }: DialogProps) {
-  return (
-    <DialogContext.Provider value={{ status, id }}>
-      <div className="fakeDialogWindow">{children}</div>
-    </DialogContext.Provider>
-  );
-}
+const StyleInjection = () => {
+  const targetWindow = useWindow();
 
-function FakeLongDialog({ children, status, id }: DialogProps) {
-  return (
-    <DialogContext.Provider value={{ status, id }}>
-      <div
-        className="fakeDialogWindow longDialog"
-        style={{ display: "flex", flexDirection: "column" }}
-      >
-        {children}
-      </div>
-    </DialogContext.Provider>
-  );
-}
+  useComponentCssInjection({ css: themeCss, window: targetWindow });
+  useComponentCssInjection({ css: font300Css, window: targetWindow });
+  useComponentCssInjection({ css: font300iCss, window: targetWindow });
+  useComponentCssInjection({ css: font400Css, window: targetWindow });
+  useComponentCssInjection({ css: font400iCss, window: targetWindow });
+  useComponentCssInjection({ css: font500Css, window: targetWindow });
+  useComponentCssInjection({ css: font500iCss, window: targetWindow });
+  useComponentCssInjection({ css: font600Css, window: targetWindow });
+  useComponentCssInjection({ css: font600iCss, window: targetWindow });
+  useComponentCssInjection({ css: font700Css, window: targetWindow });
+  useComponentCssInjection({ css: font700iCss, window: targetWindow });
+  useComponentCssInjection({ css: font800Css, window: targetWindow });
+  useComponentCssInjection({ css: font800iCss, window: targetWindow });
+
+  return null;
+};
 
 export default {
   title: "Core/Dialog/QA",
@@ -44,237 +58,142 @@ export default {
 } as Meta<typeof Dialog>;
 
 const DialogTemplate: StoryFn<DialogProps & { header: string }> = ({
-  open: openProp = true,
   status,
   header,
 }) => {
   return (
-    <StackLayout>
-      <FakeDialog status={status}>
-        <DialogHeader header={header} />
-        <DialogContent>This is dialog content...</DialogContent>
-        <DialogActions>
-          <Button style={{ marginRight: "auto" }} variant="secondary">
-            Cancel
-          </Button>
-          <Button>Previous</Button>
-          <Button variant="cta">Next</Button>
-        </DialogActions>
-        <DialogCloseButton />
-      </FakeDialog>
-    </StackLayout>
+    <Dialog status={status} open>
+      <DialogHeader header={header} />
+      <DialogContent>This is dialog content...</DialogContent>
+      <DialogActions>
+        <Button style={{ marginRight: "auto" }} appearance="transparent">
+          Cancel
+        </Button>
+        <Button>Previous</Button>
+        <Button sentiment="accented" appearance="transparent">
+          Next
+        </Button>
+      </DialogActions>
+      <DialogCloseButton />
+    </Dialog>
   );
 };
 
-export const Default: StoryFn<QAContainerProps> = (props) => {
-  const { ...rest } = props;
-  return (
-    <QAContainer cols={3} height={300} itemPadding={3} {...rest}>
-      <DialogTemplate header={"Dialog Title"} />
-    </QAContainer>
+function FakeWindow({
+  children,
+  title,
+}: {
+  children?: ReactNode;
+  title: string;
+}) {
+  const [mountNode, setMountNode] = useState<HTMLElement | undefined>(
+    undefined,
   );
-};
 
-Default.parameters = {
-  chromatic: { disableSnapshot: false },
-};
+  const handleFrameRef = useCallback((node: HTMLIFrameElement) => {
+    setMountNode(node?.contentWindow?.document?.body);
+  }, []);
 
-export const LongContent: StoryFn<QAContainerProps> = () => {
   return (
-    <QAContainer width={1300} itemPadding={3}>
-      <FakeDialog>
-        <DialogHeader
-          header="Congratulations! You have created a Dialog."
-          style={{ width: "500px" }}
-        />
-
-        <DialogContent style={{ height: "500px" }}>
-          <StackLayout>
-            <div>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </div>
-            <div>
-              It has survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </div>
-            <div>
-              It is a long established fact that a reader will be distracted by
-              the readable content of a page when looking at its layout. The
-              point of using Lorem Ipsum is that it has a more-or-less normal
-              distribution of letters, as opposed to using 'Content here,
-              content here', making it look like readable English.
-            </div>
-            <div>
-              Many desktop publishing packages and web page editors now use
-              Lorem Ipsum as their default model text, and a search for 'lorem
-              ipsum' will uncover many web sites still in their infancy. Various
-              versions have evolved over the years, sometimes by accident,
-              sometimes on purpose (injected humour and the like).
-            </div>
-            <div>
-              Contrary to popular belief, Lorem Ipsum is not simply random text.
-              It has roots in a piece of classical Latin literature from 45 BC,
-              making it over 2000 years old. Richard McClintock, a Latin
-              professor at Hampden-Sydney College in Virginia, looked up one of
-              the more obscure Latin words, consectetur, from a Lorem Ipsum
-              passage, and going through the cites of the word in classical
-              literature, discovered the undoubtable source.
-            </div>
-            <div>
-              Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus
-              Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero,
-              written in 45 BC. This book is a treatise on the theory of ethics,
-              very popular during the Renaissance. The first line of Lorem
-              Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
-              section 1.10.32.
-            </div>
-          </StackLayout>
-        </DialogContent>
-        <DialogActions>
-          <Button>Cancel</Button>
-          <Button variant="cta">Subscribe</Button>
-        </DialogActions>
-      </FakeDialog>
-    </QAContainer>
+    <iframe
+      ref={handleFrameRef}
+      title={title}
+      style={{
+        border: "none",
+        width: 1200,
+        height: 380,
+      }}
+    >
+      <WindowProvider window={ownerWindow(mountNode)}>
+        <StyleInjection />
+        <SaltProvider applyClassesTo="root">
+          {mountNode && createPortal(children, mountNode)}
+        </SaltProvider>
+      </WindowProvider>
+    </iframe>
   );
-};
+}
 
-LongContent.parameters = {
-  chromatic: { disableSnapshot: false },
-};
-
-export const Preheader: StoryFn<QAContainerProps> = () => {
+export const StatusVariants: StoryFn<QAContainerProps> = () => {
+  const DensityValues = ["high", "medium", "low", "touch"] as const;
   return (
-    <QAContainer width={1300} itemPadding={3}>
-      <FakeDialog>
-        <DialogHeader
-          header="Subscribe"
-          preheader="Recieve emails about the latest updates"
-          style={{ width: "500px" }}
-        />
-        <DialogCloseButton />
-
-        <DialogContent>
-          <FormField necessity="asterisk">
-            <FormFieldLabel> Email </FormFieldLabel>
-            <Input defaultValue="Email Address" />
-          </FormField>
-        </DialogContent>
-        <DialogActions>
-          <Button>Cancel</Button>
-          <Button variant="cta">Subscribe</Button>
-        </DialogActions>
-      </FakeDialog>
-    </QAContainer>
-  );
-};
-
-Preheader.parameters = {
-  chromatic: { disableSnapshot: false },
-};
-
-type sizes = "small" | "medium" | "large";
-
-const sizes: sizes[] = ["small", "medium", "large"];
-
-export const SizeDialog: StoryFn<QAContainerProps> = (props) => {
-  const { ...rest } = props;
-  return (
-    <QAContainer cols={3} height={300} itemPadding={3} width={3000} {...rest}>
-      {sizes.map((size) => {
-        return <DialogTemplate key={size} header={"Info Dialog"} size={size} />;
+    <FlowLayout gap={0}>
+      {DensityValues.map((density) => {
+        return (
+          <Fragment key={density}>
+            <FakeWindow title={`dialog ${density} example`}>
+              <Button>Test button</Button>
+              <DialogTemplate header={"hello"} />
+            </FakeWindow>
+          </Fragment>
+        );
       })}
-    </QAContainer>
+    </FlowLayout>
   );
 };
 
-SizeDialog.parameters = {
-  chromatic: { disableSnapshot: false },
+StatusVariants.parameters = {
+  chromatic: {
+    disableSnapshot: false,
+    modes: {
+      theme: {
+        themeNext: "disable",
+      },
+      themeNext: {
+        themeNext: "enable",
+        corner: "rounded",
+        accent: "teal",
+        // Ignore headingFont given font is not loaded
+      },
+    },
+  },
 };
 
-export const InfoDialog: StoryFn<QAContainerProps> = (props) => {
-  const { ...rest } = props;
-  return (
-    <QAContainer cols={3} height={300} itemPadding={3} width={1300} {...rest}>
-      <DialogTemplate status={"info"} header={"Info Dialog"} />
-    </QAContainer>
-  );
-};
-
-InfoDialog.parameters = {
-  chromatic: { disableSnapshot: false },
-};
-
-export const SuccessDialog: StoryFn<QAContainerProps> = (props) => {
-  const { ...rest } = props;
-  return (
-    <QAContainer cols={3} height={300} itemPadding={3} width={1300} {...rest}>
-      <DialogTemplate status={"success"} header={"Success Dialog"} />
-    </QAContainer>
-  );
-};
-
-SuccessDialog.parameters = {
-  chromatic: { disableSnapshot: false },
-};
-
-export const WarningDialog: StoryFn<QAContainerProps> = (props) => {
-  const { ...rest } = props;
-  return (
-    <QAContainer cols={3} height={300} itemPadding={3} width={1300} {...rest}>
-      <DialogTemplate status={"warning"} header={"Warning Dialog"} />
-    </QAContainer>
-  );
-};
-
-WarningDialog.parameters = {
-  chromatic: { disableSnapshot: false },
-};
-
-export const ErrorDialog: StoryFn<QAContainerProps> = (props) => {
-  const { ...rest } = props;
-  return (
-    <QAContainer cols={3} height={300} itemPadding={3} width={1300} {...rest}>
-      <DialogTemplate status={"error"} header={"Error Dialog"} />
-    </QAContainer>
-  );
-};
-
-ErrorDialog.parameters = {
-  chromatic: { disableSnapshot: false },
-};
-
-export const StickyFooter: StoryFn<QAContainerProps> = () => {
-  return (
-    <QAContainer width={1300} itemPadding={3}>
-      <FakeLongDialog>
-        <DialogHeader
-          header="Congratulations! You have created a Dialog."
-          style={{ width: "500px" }}
-        />
-        <DialogCloseButton />
-        <DialogContent>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
-        </DialogContent>
-        <DialogActions>
-          <Button>Cancel</Button>
-          <Button variant="cta">Subscribe</Button>
-        </DialogActions>
-      </FakeLongDialog>
-    </QAContainer>
-  );
-};
-
-StickyFooter.parameters = {
-  chromatic: { disableSnapshot: false },
+export const DialogHeaders: StoryFn<QAContainerProps> = () => (
+  <QAContainer height={600} cols={1} itemPadding={5} width={1200}>
+    <DialogHeader
+      header="Terms and conditions"
+      style={{
+        width: 600,
+      }}
+    />
+    <DialogHeader
+      style={{
+        width: 600,
+      }}
+      header="Terms and conditions"
+      preheader="Ensure you read and agree to these Terms"
+    />
+    <DialogHeader
+      status="info"
+      header="Terms and conditions"
+      style={{
+        width: 600,
+      }}
+    />
+    <DialogHeader
+      status="info"
+      style={{
+        width: 600,
+      }}
+      header="Terms and conditions"
+      preheader="Ensure you read and agree to these Terms"
+    />
+  </QAContainer>
+);
+DialogHeaders.parameters = {
+  chromatic: {
+    disableSnapshot: false,
+    modes: {
+      theme: {
+        themeNext: "disable",
+      },
+      themeNext: {
+        themeNext: "enable",
+        corner: "rounded",
+        accent: "teal",
+        // Ignore headingFont given font is not loaded
+      },
+    },
+  },
 };
