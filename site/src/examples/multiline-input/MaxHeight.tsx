615d0b5c042763697340ff95a2fd1cfeaad526b9
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2024-10-08 10:00:59 +0100

    Updates

--- site/src/examples/multiline-input/MaxHeight.tsx
+++ site/src/examples/multiline-input/MaxHeight.tsx
@@ -4,7 +4,6 @@ import type { ReactElement } from "react";
 export const MaxHeight = (): ReactElement => (
   <MultilineInput
     defaultValue="Value"
-    style={{ maxWidth: "256px" }}
-    textAreaProps={{ style: { maxHeight: "256px" } }}
+    style={{ maxWidth: "256px", maxHeight: "256px" }}
   />
 );
