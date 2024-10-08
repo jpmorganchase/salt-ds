e36942ae40ae39c89a520c4fbb626eb9f9d12a6a
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2024-10-08 16:18:30 +0100

    Further improve CB performance (#4251)

--- packages/core/src/list-control/ListControlState.ts
+++ packages/core/src/list-control/ListControlState.ts
@@ -58,28 +58,51 @@ function findElementPosition(
   element: HTMLElement,
 ) {
   if (elements.length === 0) {
-    return 0;
+    return [0, false] as const;
   }
 
   if (
     element.compareDocumentPosition(elements[elements.length - 1].element) &
     Node.DOCUMENT_POSITION_PRECEDING
   ) {
-    return -1;
+    return [-1, false] as const;
   }
 
   if (
     element.compareDocumentPosition(elements[0].element) &
     Node.DOCUMENT_POSITION_FOLLOWING
   ) {
-    return 0;
+    return [0, false] as const;
   }
 
-  return elements.findIndex(
-    (option) =>
-      option.element.compareDocumentPosition(element) &
-      Node.DOCUMENT_POSITION_PRECEDING,
-  );
+  let left = 0;
+  let right = elements.length;
+  let leftLast = 0;
+  let rightLast = right;
+
+  let exists = false;
+
+  while (left < right) {
+    const inPos = Math.floor((right + left) / 2);
+    const compared = element.compareDocumentPosition(elements[inPos].element);
+    if (compared & Node.DOCUMENT_POSITION_PRECEDING) {
+      left = inPos;
+    } else if (compared & Node.DOCUMENT_POSITION_FOLLOWING) {
+      right = inPos;
+    } else {
+      right = inPos;
+      left = inPos;
+      exists = true;
+    }
+    // nothing has changed, must have found limits. insert between.
+    if (leftLast === left && rightLast === right) {
+      break;
+    }
+    leftLast = left;
+    rightLast = right;
+  }
+
+  return [right, exists] as const;
 }
 
 export function defaultValueToString<Item>(item: Item): string {
@@ -193,10 +216,9 @@ export function useListControl<Item>(props: ListControlProps<Item>) {
   const register = useCallback(
     (optionValue: OptionValue<Item>, element: HTMLElement) => {
       const { id } = optionValue;
-      const option = optionsRef.current.find((item) => item.data.id === id);
-      const index = findElementPosition(optionsRef.current, element);
+      const [index, exists] = findElementPosition(optionsRef.current, element);
 
-      if (!option) {
+      if (!exists) {
         if (index === -1) {
           optionsRef.current.push({ data: optionValue, element });
         } else {
