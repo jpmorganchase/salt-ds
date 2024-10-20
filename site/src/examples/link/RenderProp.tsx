7125f4d0cb932264e0f24601b1627bbe4b484fbc
Liam Smith <liam.smith@jpmorgan.com>
2024-10-20 17:37:41 +0100

    Add render prop to Link

---
+++ site/src/examples/link/RenderProp.tsx
@@ -0,0 +1,19 @@
+import { Link, Text } from "@salt-ds/core";
+import React, { type ReactElement } from "react";
+import styles from "./index.module.css";
+
+const CustomLinkImplementation = (props: any) => (
+  <a aria-label={"overridden-label"} {...props}>
+    <Text>Your own Link implementation</Text>
+  </a>
+);
+
+export const RenderProp = (): ReactElement => {
+  return (
+    <Link
+      href="#"
+      className={styles.linkExample}
+      render={(props) => <CustomLinkImplementation {...props} />}
+    />
+  );
+};
