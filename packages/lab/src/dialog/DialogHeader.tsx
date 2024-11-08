91973acd09fbca29d2708bb69212c6ad71c18dd7
Fernanda Castillo <fersitacastillo123@gmail.com>
2024-11-08 15:52:59 +0000

    Add DialogHeader to lab (#4298)

---
+++ packages/lab/src/dialog/DialogHeader.tsx
@@ -0,0 +1,103 @@
+import {
+  StatusIndicator,
+  Text,
+  type ValidationStatus,
+  makePrefixer,
+  useDialogContext,
+} from "@salt-ds/core";
+import { useComponentCssInjection } from "@salt-ds/styles";
+import { useWindow } from "@salt-ds/window";
+import { clsx } from "clsx";
+import {
+  type ComponentPropsWithoutRef,
+  type ReactNode,
+  forwardRef,
+} from "react";
+import dialogHeaderCss from "./DialogHeader.css";
+
+const withBaseName = makePrefixer("saltDialogHeader");
+
+export interface DialogHeaderProps extends ComponentPropsWithoutRef<"div"> {
+  /**
+   * The status of the Dialog
+   */
+  status?: ValidationStatus | undefined;
+  /**
+   * Displays the accent bar in the Dialog Title */
+  disableAccent?: boolean;
+  /**
+   * Displays the header at the top of the Dialog
+   */
+  header: ReactNode;
+  /**
+   * Displays the preheader just above the header
+   **/
+  preheader?: ReactNode;
+  /**
+   * Description text is displayed just below the header
+   **/
+  description?: ReactNode;
+  /**
+   * Actions to be displayed in header
+   */
+  actions?: ReactNode;
+}
+
+export const DialogHeader = forwardRef<HTMLDivElement, DialogHeaderProps>(
+  function DialogHeader(props, ref) {
+    const {
+      className,
+      description,
+      disableAccent,
+      actions,
+      header,
+      preheader,
+      status: statusProp,
+      ...rest
+    } = props;
+    const { status: statusContext, id } = useDialogContext();
+
+    const targetWindow = useWindow();
+    useComponentCssInjection({
+      testId: "salt-dialog-header",
+      css: dialogHeaderCss,
+      window: targetWindow,
+    });
+
+    const status = statusProp ?? statusContext;
+
+    return (
+      <div
+        id={id}
+        className={clsx(
+          withBaseName(),
+          {
+            [withBaseName("withAccent")]: !disableAccent && !status,
+            [withBaseName(status ?? "")]: !!status,
+          },
+          className,
+        )}
+        ref={ref}
+        {...rest}
+      >
+        {status && <StatusIndicator status={status} />}
+        <div className={withBaseName("container")}>
+          <div className={withBaseName("header")}>
+            {preheader && (
+              <Text className={withBaseName("preheader")}>{preheader}</Text>
+            )}
+            {header}
+          </div>
+          {description && (
+            <Text color="secondary" className={withBaseName("description")}>
+              {description}
+            </Text>
+          )}
+        </div>
+        {actions && (
+          <div className={withBaseName("actionsContainer")}>{actions}</div>
+        )}
+      </div>
+    );
+  },
+);
