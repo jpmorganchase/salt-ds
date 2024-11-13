5a2d06cc076d7a5795c48087dbe2aea398a7d64c
Fernanda Castillo <fersitacastillo123@gmail.com>
2024-11-13 09:33:32 +0000

    update from feedback

--- packages/core/stories/file-drop-zone/file-drop-zone.qa.stories.tsx
+++ packages/core/stories/file-drop-zone/file-drop-zone.qa.stories.tsx
@@ -8,14 +8,13 @@ import {
   QAContainer,
   QAContainerNoStyleInjection,
   type QAContainerNoStyleInjectionProps,
-  type QAContainerProps,
 } from "docs/components";
 export default {
   title: "Core/File Drop Zone/File Drop Zone QA",
   component: FileDropZone,
 } as Meta<typeof FileDropZone>;
 
-export const AllExamplesGrid: StoryFn<QAContainerProps> = () => {
+export const AllExamplesGrid: StoryFn = () => {
   return (
     <QAContainer cols={2} itemPadding={4}>
       <FileDropZone onDrop={() => console.log("files accepted")}>
