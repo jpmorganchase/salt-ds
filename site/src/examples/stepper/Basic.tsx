64867741d133a074b620a33f3b45d9f49d5c2edd
Kristiyan Serafimov <kristiyan.serafimov@gmail.com>
2024-11-27 17:25:18 +0000

    docs(stepper, site): add some documentation

---
+++ site/src/examples/stepper/Basic.tsx
@@ -0,0 +1,14 @@
+import { StackLayout } from "@salt-ds/core";
+import { Stepper, Step } from "@salt-ds/lab";
+
+export const Basic = () => {
+  return (
+    <StackLayout style={{ height: '40px', width: '100%' }}>
+      <Stepper>
+        <Step label="Step 1" stage="completed" />
+        <Step label="Step 2" stage="active" />
+        <Step label="Step 3" stage="pending" />
+      </Stepper>
+    </StackLayout>
+  )
+}
---
+++ site/src/examples/stepper/index.ts
@@ -0,0 +1,4 @@
+export * from './Basic';
+export * from './OrientationHorizontal';
+export * from './OrientationVertical';
+export * from './StageStatus';
