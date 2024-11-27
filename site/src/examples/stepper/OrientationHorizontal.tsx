7b53d8c4d48aa5c79b2839ce9611bf1be927482a
Kristiyan Serafimov <kristiyan.serafimov@gmail.com>
2024-11-27 17:25:18 +0000

    docs(stepper, site): add some documentation

---
+++ site/src/examples/stepper/OrientationHorizontal.tsx
@@ -0,0 +1,14 @@
+import { StackLayout } from "@salt-ds/core";
+import { Stepper, Step } from "@salt-ds/lab";
+
+export const OrientationHorizontal = () => {
+  return (
+    <StackLayout style={{ width: '100%' }}>
+      <Stepper orientation="horizontal">
+        <Step label="Step 1" stage="completed" />
+        <Step label="Step 2" stage="active" />
+        <Step label="Step 3" stage="pending" />
+      </Stepper>
+    </StackLayout>
+  )
+}
