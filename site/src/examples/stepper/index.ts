a07848777f07700bf30d58cd2a4ccdd76325876f
Kristiyan Serafimov <kristiyan.serafimov@gmail.com>
2024-11-28 15:09:11 +0000

    fix(stepper, site): linting issues

--- site/src/examples/stepper/index.ts
+++ site/src/examples/stepper/index.ts
@@ -2,6 +2,6 @@ export * from "./Basic";
 export * from "./OrientationHorizontal";
 export * from "./OrientationVertical";
 export * from "./StageStatus";
-export * from "./Nesting";
+export * from "./NestedSteps";
 export * from "./Hook";
 export * from "./HookAdvanced";
