eb07e4eee5c70a06cbbb44ee2eb1cd1e33f73f51
Kristiyan Serafimov <kristiyan.serafimov@gmail.com>
2024-11-28 12:37:57 +0000

    docs(stepper, site): add nesting and hook documentation

---
+++ site/src/examples/stepper/Nesting.tsx
@@ -0,0 +1,32 @@
+import { StackLayout } from "@salt-ds/core";
+import { Stepper, Step } from "@salt-ds/lab";
+
+
+export const Nesting = () => {
+  return (
+    <StackLayout style={{ minWidth: '240px' }}>
+      <Stepper orientation="vertical">
+        <Step label="Step 1">
+          <Step label="Step 1.1" />
+        </Step>
+        <Step label="Step 2">
+          <Step label="Step 2.1" />
+          <Step label="Step 2.2">
+            <Step label="Step 2.2.1" />
+            <Step label="Step 2.2.2" />
+            <Step label="Step 2.2.3" />
+          </Step>
+        </Step>
+        <Step label="Step 3">
+          <Step label="Step 3.1" />
+          <Step label="Step 3.2" />
+          <Step label="Step 3.3">
+            <Step label="Step 3.3.1" />
+            <Step label="Step 3.3.2" />
+            <Step label="Step 3.3.3" />
+          </Step>
+        </Step>
+      </Stepper>
+    </StackLayout>
+  )
+}
