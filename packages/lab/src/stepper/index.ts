36cc11c272932d96d04579c20defb23ae360a5b3
Kristiyan Serafimov <kristiyan.serafimov@gmail.com>
2024-11-29 16:23:36 +0000

    docs(stepper): add changeset

--- packages/lab/src/stepper/index.ts
+++ packages/lab/src/stepper/index.ts
@@ -1,13 +1,17 @@
 import type { Step } from "./Step";
 import type { Stepper } from "./Stepper";
+import type { SteppedReducer } from "./useSteppedReducer";
 
 export * from "./Stepper";
-export * from "./Step";
-export * from "./useSteppedReducer";
-
 export interface StepperProps extends Stepper.Props {}
-export interface StepProps extends Step.Props {}
-
 export type StepperOrientation = Stepper.Orientation;
+
+export * from "./Step";
+export interface StepProps extends Step.Props {}
 export type StepStatus = Step.Status;
 export type StepStage = Step.Stage;
+export type StepDepth = Step.Depth;
+
+export * from "./useSteppedReducer";
+export type SteppedReducerState = SteppedReducer.State;
+export type SteppedReducerAction = SteppedReducer.Action;
