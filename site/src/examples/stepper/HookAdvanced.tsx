b2e3c75ff5f1a41e17156eb45436ddac0ff08cd8
Kristiyan Serafimov <kristiyan.serafimov@gmail.com>
2024-11-29 16:23:36 +0000

    docs(stepper): add changeset

--- site/src/examples/stepper/HookAdvanced.tsx
+++ site/src/examples/stepper/HookAdvanced.tsx
@@ -1,5 +1,4 @@
-import { StackLayout } from "@salt-ds/core";
-import { Button, SegmentedButtonGroup } from "@salt-ds/core";
+import { StackLayout, SegmentedButtonGroup, Button } from "@salt-ds/core";
 import { Step, Stepper, useSteppedReducer } from "@salt-ds/lab";
 
 const initialSteps = [
