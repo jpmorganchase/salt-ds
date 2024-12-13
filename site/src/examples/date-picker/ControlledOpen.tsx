97e7b881c72300a232b8b26f377650e20c404e3e
mark-tate <143323+mark-tate@users.noreply.github.com>
2024-12-13 10:55:30 +0000

    enabled uncontrolled/un-controlled open behaviour for `DatePicker`
    - added `openOnClick`, `openOnKeyDown` and `openOnFocus` props to `DatePicker`.
    - revise the controlled behaviour of the `open` prop on `DatePickerOverlay`.
    - add examples for controlled and uncontrolled behaviour.

---
+++ site/src/examples/date-picker/ControlledOpen.tsx
@@ -0,0 +1,126 @@
+import type { OpenChangeReason } from "@floating-ui/react";
+import {
+  Divider,
+  FlexItem,
+  FlexLayout,
+  StackLayout,
+  ToggleButton,
+} from "@salt-ds/core";
+import type { DateFrameworkType } from "@salt-ds/date-adapters";
+import {
+  type DateInputSingleDetails,
+  DatePicker,
+  DatePickerActions,
+  DatePickerOverlay,
+  DatePickerSingleInput,
+  DatePickerSinglePanel,
+  DatePickerTrigger,
+  type SingleDateSelection,
+  useLocalization,
+} from "@salt-ds/lab";
+import {
+  type ReactElement,
+  type SyntheticEvent,
+  useCallback,
+  useRef,
+  useState,
+} from "react";
+
+export const ControlledOpen = (): ReactElement => {
+  const [open, setOpen] = useState(false);
+  const [selectedDate, setSelectedDate] = useState<
+    SingleDateSelection<DateFrameworkType> | null | undefined
+  >(null);
+  const { dateAdapter } = useLocalization();
+  const triggerRef = useRef<HTMLInputElement | null>(null);
+  const applyButtonRef = useRef<HTMLButtonElement>(null);
+  const datePickerRef = useRef<HTMLDivElement>(null);
+
+  const handleSelectionChange = useCallback(
+    (
+      _event: SyntheticEvent,
+      date: SingleDateSelection<DateFrameworkType> | null,
+      _details: DateInputSingleDetails | undefined,
+    ) => {
+      setSelectedDate(date ?? null);
+    },
+    [dateAdapter],
+  );
+
+  const handleApply = useCallback(
+    (
+      event: SyntheticEvent,
+      date: SingleDateSelection<DateFrameworkType> | null,
+    ) => {
+      console.log(
+        `Applied StartDate: ${date ? dateAdapter.format(date, "DD MMM YYYY") : date}`,
+      );
+      setSelectedDate(date);
+      setOpen(false);
+    },
+    [dateAdapter],
+  );
+
+  const handleOpen = useCallback(
+    (
+      newOpen: boolean,
+      _event?: Event | undefined,
+      reason?: OpenChangeReason | undefined,
+    ) => {
+      if (reason === undefined) {
+        triggerRef?.current?.focus();
+        setTimeout(() => {
+          triggerRef?.current?.setSelectionRange(
+            0,
+            triggerRef.current.value.length,
+          );
+        }, 1);
+      }
+      setOpen(newOpen);
+    },
+    [],
+  );
+
+  return (
+    <StackLayout>
+      <FlexLayout>
+        <ToggleButton
+          aria-label={"open picker"}
+          value={open ? "false" : "true"}
+          selected={open}
+          onChange={(event) => {
+            setOpen(event.currentTarget.value === "true");
+          }}
+        >
+          Open
+        </ToggleButton>
+      </FlexLayout>
+      <DatePicker
+        selectionVariant={"single"}
+        onSelectionChange={handleSelectionChange}
+        selectedDate={selectedDate}
+        onApply={handleApply}
+        onOpen={handleOpen}
+        open={open}
+      >
+        <DatePickerTrigger>
+          <DatePickerSingleInput inputRef={triggerRef} />
+        </DatePickerTrigger>
+        <DatePickerOverlay ref={datePickerRef}>
+          <FlexLayout gap={0} direction="column">
+            <FlexItem>
+              <DatePickerSinglePanel />
+              <Divider variant="tertiary" />
+            </FlexItem>
+            <FlexItem>
+              <DatePickerActions
+                selectionVariant="single"
+                applyButtonRef={applyButtonRef}
+              />
+            </FlexItem>
+          </FlexLayout>
+        </DatePickerOverlay>
+      </DatePicker>
+    </StackLayout>
+  );
+};
