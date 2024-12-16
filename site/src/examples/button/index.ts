fc097ab91fa3123f0fb1bc2473926dae101d253f
Kristiyan Serafimov <kristiyan.serafimov@gmail.com>
2024-12-16 13:06:23 +0000

    docs(button): add documentation for loading state

--- site/src/examples/button/index.ts
+++ site/src/examples/button/index.ts
@@ -2,6 +2,7 @@ export * from "./Accented";
 export * from "./Neutral";
 export * from "./Deprecated";
 export * from "./Disabled";
+export * from "./Loading";
 export * from "./FocusableWhenDisabled";
 export * from "./FullWidth";
 export * from "./IconAndText";
