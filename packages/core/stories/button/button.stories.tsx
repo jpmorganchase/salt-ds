776e4844b2da3d20ab4002e6ecc483f3c19651a5
Kristiyan Serafimov <kristiyan.serafimov@gmail.com>
2024-12-16 12:39:55 +0000

    feat(button): add loading functionality, prop

--- packages/core/stories/button/button.stories.tsx
+++ packages/core/stories/button/button.stories.tsx
@@ -143,6 +143,68 @@ export const Disabled: StoryFn = () => {
   );
 };
 
+export const Loading: StoryFn = () => {
+  return (
+    <StackLayout gap={3}>
+      <FlowLayout>
+        <Button appearance="solid" sentiment="accented" loading>
+          Solid
+        </Button>
+        <Button appearance="bordered" sentiment="accented" loading>
+          Bordered
+        </Button>
+        <Button appearance="transparent" sentiment="accented" loading>
+          Transparent
+        </Button>
+      </FlowLayout>
+      <FlowLayout>
+        <Button appearance="solid" sentiment="neutral" loading>
+          Solid
+        </Button>
+        <Button appearance="bordered" sentiment="neutral" loading>
+          Bordered
+        </Button>
+        <Button appearance="transparent" sentiment="neutral" loading>
+          Transparent
+        </Button>
+      </FlowLayout>
+      <FlowLayout>
+        <Button appearance="solid" sentiment="positive" loading>
+          Solid
+        </Button>
+        <Button appearance="bordered" sentiment="positive" loading>
+          Bordered
+        </Button>
+        <Button appearance="transparent" sentiment="positive" loading>
+          Transparent
+        </Button>
+      </FlowLayout>
+      <FlowLayout>
+        <Button appearance="solid" sentiment="negative" loading>
+          Solid
+        </Button>
+        <Button appearance="bordered" sentiment="negative" loading>
+          Bordered
+        </Button>
+        <Button appearance="transparent" sentiment="negative" loading>
+          Transparent
+        </Button>
+      </FlowLayout>
+      <FlowLayout>
+        <Button appearance="solid" sentiment="caution" loading>
+          Solid
+        </Button>
+        <Button appearance="bordered" sentiment="caution" loading>
+          Bordered
+        </Button>
+        <Button appearance="transparent" sentiment="caution" loading>
+          Transparent
+        </Button>
+      </FlowLayout>
+    </StackLayout>
+  );
+};
+
 export const FocusableWhenDisabled = SingleButtonTemplate.bind({});
 FocusableWhenDisabled.args = {
   focusableWhenDisabled: true,
