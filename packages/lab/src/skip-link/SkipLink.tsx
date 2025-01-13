030283020de4f2cf7e650ab90ee20e2aabc0a7fa
Fernanda Castillo <fersitacastillo123@gmail.com>
2025-01-13 17:11:49 +0000

    Updates to skip link (#4482)

--- packages/lab/src/skip-link/SkipLink.tsx
+++ packages/lab/src/skip-link/SkipLink.tsx
@@ -1,28 +1,34 @@
-import { Link, type LinkProps, makePrefixer } from "@salt-ds/core";
+import { makePrefixer } from "@salt-ds/core";
 import { useComponentCssInjection } from "@salt-ds/styles";
 import { useWindow } from "@salt-ds/window";
 import { clsx } from "clsx";
-import { type RefObject, forwardRef } from "react";
+import {
+  type ComponentPropsWithoutRef,
+  forwardRef,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { useManageFocusOnTarget } from "./internal/useManageFocusOnTarget";
 
 import skipLinkCss from "./SkipLink.css";
 
-interface SkipLinkProps extends LinkProps {
+interface SkipLinkProps extends ComponentPropsWithoutRef<"a"> {
   /**
-   * This is a ref that has access to the target element.
-   *
-   * This will be used to apply focus to that element
-   *
-   * Refs are referentially stable so if this changes it won't be picked up
-   * will need to find a better way of passing in the target element to apply the attributes
+   * The ID of the target element to apply focus when the link is clicked.
+   * If the element with this ID is not found, the SkipLink will not be rendered.
    */
-  targetRef?: RefObject<HTMLElement>;
+  targetId: string;
 }
 
 const withBaseName = makePrefixer("saltSkipLink");
 
 export const SkipLink = forwardRef<HTMLAnchorElement, SkipLinkProps>(
-  function SkipLink({ className, targetRef, ...rest }, ref) {
+  function SkipLink(
+    { className, targetId, children, onKeyUp, onBlur, onClick, ...rest },
+    ref,
+  ) {
+    const [isTargetAvailable, setIsTargetAvailable] = useState(false);
     const targetWindow = useWindow();
     useComponentCssInjection({
       testId: "salt-skip-link",
@@ -30,19 +36,35 @@ export const SkipLink = forwardRef<HTMLAnchorElement, SkipLinkProps>(
       window: targetWindow,
     });
 
-    const targetClass = clsx(withBaseName("target"), className);
+    const targetRef = useRef<HTMLElement | null>(null);
 
-    const eventHandlers = useManageFocusOnTarget({ targetRef, targetClass });
+    useEffect(() => {
+      if (targetWindow) {
+        targetRef.current = targetWindow.document.getElementById(targetId);
+      }
+      setIsTargetAvailable(!!targetRef.current);
+    }, [targetId, targetWindow]);
 
+    const eventHandlers = useManageFocusOnTarget({
+      onKeyUp,
+      onBlur,
+      onClick,
+      targetRef,
+      targetClass: withBaseName("target"),
+    });
+
+    if (!isTargetAvailable) return null;
     return (
-      <li>
-        <Link
-          {...eventHandlers}
-          {...rest}
-          className={clsx(withBaseName(), className)}
-          ref={ref}
-        />
-      </li>
+      <a
+        className={clsx(withBaseName(), className)}
+        href={`#${targetId}`}
+        ref={ref}
+        target="_self"
+        {...eventHandlers}
+        {...rest}
+      >
+        {children}
+      </a>
     );
   },
 );
--- packages/lab/stories/skip-link/skip-link.stories.tsx
+++ packages/lab/stories/skip-link/skip-link.stories.tsx
@@ -1,7 +1,21 @@
-import { Button } from "@salt-ds/core";
-import { SkipLink, SkipLinks } from "@salt-ds/lab";
+import {
+  BorderItem,
+  BorderLayout,
+  Button,
+  Card,
+  FlexItem,
+  FlexLayout,
+  GridLayout,
+  H1,
+  H2,
+  NavigationItem,
+  SplitLayout,
+  StackLayout,
+} from "@salt-ds/core";
+import { GithubIcon, StackoverflowIcon, SymphonyIcon } from "@salt-ds/icons";
+import { SkipLink } from "@salt-ds/lab";
 import type { Meta, StoryFn } from "@storybook/react";
-import { useRef } from "react";
+import { useState } from "react";
 import "./skip-link.stories.css";
 
 export default {
@@ -9,124 +23,120 @@ export default {
   component: SkipLink,
 } as Meta<typeof SkipLink>;
 
-export const Default: StoryFn<typeof SkipLink> = () => {
-  const articleRef = useRef<HTMLElement>(null);
+const DefaultStory: StoryFn<typeof SkipLink> = (args) => {
+  const headerItems = ["Home", "Transactions", "FX", "Credit Manager"];
+
+  const headerUtilities = [
+    {
+      icon: <SymphonyIcon />,
+      key: "Symphony",
+    },
+    {
+      icon: <StackoverflowIcon />,
+      key: "Stack Overflow",
+    },
+    {
+      icon: <GithubIcon />,
+      key: "GitHub",
+    },
+  ];
+  const cardHeaders = [
+    "Economic trends",
+    "Machine learning",
+    "Financial conditions",
+    "Balance sheets",
+    "Outlook hub",
+    "Trading strategies",
+    "Emerging markets",
+    "Support tools",
+    "Index",
+  ];
+
+  const [activeHeaderNav, setActiveHeaderNav] = useState(headerItems[0]);
 
   return (
-    <>
-      <span style={{ height: 50, lineHeight: "50px" }} tabIndex={-1}>
-        Click here and press the Tab key to see the Skip Link
-      </span>
-      <div style={{ position: "relative", maxWidth: 500 }}>
-        <SkipLinks>
-          <SkipLink data-testid="skipLink" href="#main" targetRef={articleRef}>
-            Skip to main content
-          </SkipLink>
-        </SkipLinks>
-
-        <div
-          style={{
-            borderTop: "2px solid grey",
-            fontSize: 24,
-            lineHeight: 3.5,
-          }}
-        >
-          What we do
-        </div>
-
-        <article id="main" ref={articleRef}>
-          <section>
-            <h1>Salt</h1>
-            <p>
-              Salt provides you with a suite of UI components and a flexible
-              theming system. With no customisation, the default theme offers an
-              attractive and modern look-and-feel, with both light and dark
-              variants and support for a range of UI densities. We have included
-              a theming system which allows you to easily create theme
-              variations, or in fact substitute alternate themes.
-            </p>
-          </section>
-          <section>
-            <h1>Goals</h1>
-            <p>Salt has been developed with the following design goals:</p>
-            <ul className="goalsList">
-              <li>
-                Providing a comprehensive set of commonly-used UI controls
-              </li>
-              <li>Complying with WCAG 2.1 accessibility guidelines</li>
-              <li> To be lightweight and performant</li>
-              <li> Offering flexible styling and theming support</li>
-              <li> Minimizing dependencies on third-party libraries</li>
+    <BorderLayout>
+      <BorderItem position="north" sticky className="app-header" as="header">
+        <SkipLink {...args}>Skip to main content</SkipLink>
+        <FlexLayout className="header" justify="space-between" gap={3}>
+          <FlexItem align="center" />
+          <nav>
+            <ul className="navigation">
+              {headerItems?.map((item) => (
+                <li key={item}>
+                  <NavigationItem
+                    active={activeHeaderNav === item}
+                    href="#"
+                    onClick={(event) => {
+                      // prevent default to avoid navigation in storybook example
+                      event.preventDefault();
+                      setActiveHeaderNav(item);
+                    }}
+                  >
+                    {item}
+                  </NavigationItem>
+                </li>
+              ))}
             </ul>
+          </nav>
+          <FlexItem align="center">
+            <StackLayout direction="row" gap={1}>
+              {headerUtilities?.map((utility) => (
+                <Button key={utility.key} appearance="transparent">
+                  {utility.icon}
+                </Button>
+              ))}
+            </StackLayout>
+          </FlexItem>
+        </FlexLayout>
+      </BorderItem>
+      <BorderItem position="center" className="center">
+        <StackLayout as="article">
+          <section>
+            <H1 id="main" className="content-header">
+              Explore our offering
+            </H1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam,
+              consequuntur culpa dolor excepturi fugit in ipsa iusto laudantium
+              magnam minima necessitatibus odio qui quia repellendus sit tempore
+              veniam. At, veritatis.
+            </p>
+            <GridLayout columns={3}>
+              {cardHeaders.map((title) => {
+                return (
+                  <Card key={title}>
+                    <H2 styleAs="h4">{title}</H2>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Aliquam, consequuntur culpa dolor excepturi.
+                    </p>
+                    <p>
+                      Fugit in ipsa iusto laudantium magnam minima
+                      necessitatibus odio qui quia repellendus sit tempore
+                      veniam. At, veritatis.
+                    </p>
+                  </Card>
+                );
+              })}
+            </GridLayout>
           </section>
-        </article>
-        <div style={{ overflow: "auto" }}>
-          <Button style={{ marginTop: 30, float: "right" }}>Next</Button>
+          <SplitLayout
+            endItem={<Button appearance="bordered">See all themes</Button>}
+          />
+        </StackLayout>
+        <div className="help-text">
+          Click here and press the Tab key to see the Skip Link
         </div>
-      </div>
-    </>
+      </BorderItem>
+    </BorderLayout>
   );
 };
 
-export const MultipleLinks: StoryFn<typeof SkipLink> = () => {
-  const sectionRef1 = useRef<HTMLElement>(null);
-  const sectionRef2 = useRef<HTMLElement>(null);
-
-  return (
-    <>
-      <span style={{ height: 50, lineHeight: "50px" }} tabIndex={-1}>
-        Click here and press the Tab key to see the Skip Link
-      </span>
-      <div style={{ position: "relative", maxWidth: 500 }}>
-        <SkipLinks>
-          <SkipLink href="#introduction" targetRef={sectionRef1}>
-            Skip to Introduction
-          </SkipLink>
-          <SkipLink href="#goals" targetRef={sectionRef2}>
-            Skip to Goals
-          </SkipLink>
-        </SkipLinks>
-
-        <div
-          style={{
-            borderTop: "2px solid grey",
-            fontSize: 24,
-            lineHeight: 3.5,
-          }}
-        >
-          What we do
-        </div>
-
-        <article>
-          <section id="introduction" ref={sectionRef1}>
-            <h1>Salt</h1>
-            <p>
-              Salt provides you with a suite of UI components and a flexible
-              theming system. With no customisation, the default theme offers an
-              attractive and modern look-and-feel, with both light and dark
-              variants and support for a range of UI densities. We have included
-              a theming system which allows you to easily create theme
-              variations, or in fact substitute alternate themes.
-            </p>
-          </section>
-          <section id="goals" ref={sectionRef2}>
-            <h1>Goals</h1>
-            <p>Salt has been developed with the following design goals:</p>
-            <ul className="goalsList">
-              <li>
-                Providing a comprehensive set of commonly-used UI controls
-              </li>
-              <li>Complying with WCAG 2.1 accessibility guidelines</li>
-              <li> To be lightweight and performant</li>
-              <li> Offering flexible styling and theming support</li>
-              <li> Minimizing dependencies on third-party libraries</li>
-            </ul>
-          </section>
-        </article>
-        <div style={{ overflow: "auto" }}>
-          <Button style={{ marginTop: 30, float: "right" }}>Next</Button>
-        </div>
-      </div>
-    </>
-  );
+export const Default = DefaultStory.bind({});
+Default.args = {
+  targetId: "main",
+};
+Default.parameters = {
+  layout: "fullscreen",
 };
