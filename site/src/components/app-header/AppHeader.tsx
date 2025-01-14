055a7ef85fd49bd416560df01f31b54c5988a1ca
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2025-01-14 09:05:29 +0000

    Update site logo

--- site/src/components/app-header/AppHeader.tsx
+++ site/src/components/app-header/AppHeader.tsx
@@ -77,7 +77,11 @@ export const AppHeader: FC<AppHeaderProps> = ({
         {!isMobileOrTablet && (
           <>
             {homeLink && (
-              <Link href={homeLink} variant="component">
+              <Link
+                className={styles.logoLink}
+                href={homeLink}
+                variant="component"
+              >
                 {logo && (
                   <Logo>
                     <LogoImage
