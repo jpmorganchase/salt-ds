0be5b203fc8b5c44f6573a1f6990edaf3a5efd39
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2025-01-27 11:28:41 +0000

    Improve deprecated token tooling

--- tooling/stylelint/no-deprecated-token-usage/index.js
+++ tooling/stylelint/no-deprecated-token-usage/index.js
@@ -1,5 +1,8 @@
 const valueParser = require("postcss-value-parser");
 const stylelint = require("stylelint");
+const glob = require("fast-glob");
+const csstree = require("css-tree");
+const fs = require("node:fs");
 
 const { report, ruleMessages } = stylelint.utils;
 
@@ -32,13 +35,30 @@ const declarationValueIndex = function declarationValueIndex(decl) {
   }, 0);
 };
 
+const deprecatedTokensSet = new Set(
+  glob
+    // Matches all files in src folders that start with a capital letter which should be all components.
+    .sync("./packages/theme/css/deprecated/*.css")
+    .flatMap((file) => {
+      const ast = csstree.parse(fs.readFileSync(file, { encoding: "utf-8" }));
+      return csstree
+        .findAll(
+          ast,
+          (node, item, list) =>
+            node.type === "Declaration" && node.property.startsWith("--salt"),
+        )
+        .map((decl) => decl.property);
+    })
+    .filter(Boolean),
+);
+
 // ---- Start of plugin ----
 
-const ruleName = "salt/correct-theme-token-usage";
+const ruleName = "salt/no-deprecated-token-usage";
 
 const messages = ruleMessages(ruleName, {
-  noExpectedFoundationPalette: (propertyChecked) =>
-    `No foundation or palette variables (${propertyChecked}) should be used in component`, // Can encode option in error message if needed
+  noDeprecated: (propertyChecked) =>
+    `No deprecated tokens should be used. (${propertyChecked})`,
 });
 
 const meta = {
@@ -46,64 +66,9 @@ const meta = {
   url: "https://saltdesignsystem-storybook.pages.dev/?path=/story/theme-characteristics-about-characteristics--docs",
 };
 
-/**
- * Test whether a property value is from theme.
- *
- * We have 2 type of `--salt` prefixes
- * - `--salt-xyz` from theme
- * - `--saltAbc` from a component
- */
-const isSaltThemeCustomProperty = (property) => property.startsWith("--salt-");
-
-const allAllowedKeys = [
-  // characteristics
-  "accent",
-  "actionable",
-  "category",
-  "container",
-  "draggable",
-  "differential", // TODO: **deprecated:** delete here
-  "editable",
-  "focused",
-  "measured", // TODO: **deprecated:** delete here
-  "navigable",
-  "overlayable",
-  "selectable",
-  "separable",
-  "status",
-  "taggable", // TODO: **deprecated:** delete here
-  "target",
-  "text",
-  "track",
-  "content",
-  // additional to decide
-  "animation",
-  "delay", // TODO: **deprecated:** delete here
-  "duration", // TODO: to be merged with animation
-  "size",
-  "spacing",
-  // icon size
-  "icon-size",
-  "zIndex",
-  // palette
-  "palette-opacity",
-  "palette-neutral",
-  "palette-interact",
-  // Corner is approved use case for palette layer (not curve from foundation)
-  "palette-corner",
-];
-
-const varEndDetector = "(?![\\w-])";
-
-const allowedKeyRegexpPattern = new RegExp(
-  `--salt(w+)?-(${allAllowedKeys.join("|")}).*${varEndDetector}`,
-);
-
-function isAllowedKeys(property, verboseLog) {
-  const checkResult =
-    !isSaltThemeCustomProperty(property) ||
-    allowedKeyRegexpPattern.test(property);
-  verboseLog && console.log("Checking", property, "isAllowedKeys", checkResult);
+function isDeprecatedToken(property, verboseLog) {
+  const checkResult = deprecatedTokensSet.has(property);
+  verboseLog && console.log("Checking", property, "is deprecated", checkResult);
   return checkResult;
 }
 
@@ -131,8 +96,8 @@ module.exports = stylelint.createPlugin(
 
           if (!firstNode) return;
 
-          if (!isAllowedKeys(firstNode.value, verboseLog)) {
-            complainNoExpecteFoundationOrPalette(
+          if (isDeprecatedToken(firstNode.value, verboseLog)) {
+            complainDeprecatedTokenUsage(
               declarationValueIndex(decl) + firstNode.sourceIndex,
               firstNode.value.length,
               decl,
@@ -145,8 +110,8 @@ module.exports = stylelint.createPlugin(
 
         verboseLog && console.log({ prop });
 
-        if (!isAllowedKeys(prop, verboseLog)) {
-          complainNoExpecteFoundationOrPalette(0, prop.length, decl, prop);
+        if (isDeprecatedToken(prop, verboseLog)) {
+          complainDeprecatedTokenUsage(0, prop.length, decl, prop);
         }
 
         return;
@@ -165,6 +130,23 @@ module.exports = stylelint.createPlugin(
           node: decl,
           index,
           endIndex: index + length,
+        });
+      }
+
+      function complainDeprecatedTokenUsage(
+        index,
+        length,
+        decl,
+        propertyChecked,
+      ) {
+        report({
+          result,
+          ruleName,
+          message: messages.noDeprecated(propertyChecked),
+          node: decl,
+          index,
+          endIndex: index + length,
+          severity: "warning", // Keep deprecated token in warning for now
         });
       }
     };
