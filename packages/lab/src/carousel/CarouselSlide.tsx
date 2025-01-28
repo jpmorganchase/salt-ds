21708a1ec0ef22e2c484ca9c950ede7e6708ab37
Fernanda Castillo <fersitacastillo123@gmail.com>
2025-01-28 16:45:41 +0000

    updates to carousel

--- packages/lab/src/carousel/CarouselSlide.tsx
+++ packages/lab/src/carousel/CarouselSlide.tsx
@@ -1,76 +1,41 @@
-import { makePrefixer } from "@salt-ds/core";
-import {
-  type ElementType,
-  type HTMLAttributes,
-  type ReactElement,
-  forwardRef,
-  useRef,
-} from "react";
-
+import { makePrefixer, useId } from "@salt-ds/core";
+import { useComponentCssInjection } from "@salt-ds/styles";
+import { useWindow } from "@salt-ds/window";
 import { clsx } from "clsx";
+import { type HTMLAttributes, forwardRef, useEffect, useRef } from "react";
+import { useCarousel } from "./CarouselContext";
+import carouselSlideCss from "./CarouselSlide.css";
 
-export interface CarouselSlideProps extends HTMLAttributes<HTMLDivElement> {
-  ButtonBar?: ElementType;
-  Media: ReactElement;
-  description?: string;
-  title?: string;
-  contentAlignment?: "center" | "left" | "right";
-}
+export interface CarouselSlideProps extends HTMLAttributes<HTMLDivElement> {}
 
 const withBaseName = makePrefixer("saltCarouselSlide");
 
 export const CarouselSlide = forwardRef<HTMLDivElement, CarouselSlideProps>(
-  function CarouselSlide(
-    { ButtonBar, Media, description, title, contentAlignment },
-    ref,
-  ) {
-    const buttonBarRef = useRef(null);
+  function CarouselSlide({ children }, ref) {
+    const targetWindow = useWindow();
+    useComponentCssInjection({
+      testId: "salt-carousel-slide",
+      css: carouselSlideCss,
+      window: targetWindow,
+    });
 
+    const slideRef = useRef(useId());
+    const { activeSlide, registerSlide, slides } = useCarousel();
+
+    useEffect(() => {
+      slideRef.current && registerSlide(slideRef.current);
+    }, []);
+
+    const isActive = slides[activeSlide] === slideRef.current;
     return (
-      <div ref={ref}>
-        {Media && <div className={withBaseName("mediaContainer")}>{Media}</div>}
-        <div className={withBaseName("fixedContainer")} ref={buttonBarRef}>
-          <div
-            className={clsx({
-              [withBaseName("textContainer")]: contentAlignment === "center",
-              [withBaseName("textContainerLeft")]: contentAlignment === "left",
-            })}
-          >
-            {title && (
-              <div
-                aria-level={1}
-                className={withBaseName("titleContainer")}
-                role="heading"
-              >
-                {title}
-              </div>
-            )}
-            {description && (
-              <div className={withBaseName("descriptionContainer")}>
-                {description}
-              </div>
-            )}
-          </div>
-          {ButtonBar && (
-            <div
-              className={clsx({
-                [withBaseName("buttonBarOverride")]:
-                  contentAlignment === "center",
-                [withBaseName("buttonBarOverrideLeft")]:
-                  contentAlignment === "left",
-              })}
-            >
-              <ButtonBar
-                className={clsx({
-                  [withBaseName("buttonBarContainer")]:
-                    contentAlignment === "center",
-                  [withBaseName("buttonBarContainerLeft")]:
-                    contentAlignment === "left",
-                })}
-              />
-            </div>
-          )}
-        </div>
+      <div
+        role="group"
+        ref={ref}
+        className={clsx(withBaseName(), {
+          [withBaseName("active")]: isActive,
+        })}
+      >
+        {children}
       </div>
     );
   },
