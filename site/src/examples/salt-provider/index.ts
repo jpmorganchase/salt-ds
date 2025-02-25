7aa49182793bb946079bd07e071381f2bdb6f3d6
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2025-02-25 21:38:40 +0000

    Add responsive density example

--- site/src/examples/salt-provider/index.ts
+++ site/src/examples/salt-provider/index.ts
@@ -1,5 +1,6 @@
 export * from "./Breakpoints";
 export * from "./Density";
+export * from "./DensityResponsive";
 export * from "./Modes";
 export * from "./NestedProviders";
 export * from "./Theme";
