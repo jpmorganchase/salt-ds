223eb4d24f1aa85560f65f0bce8b1b6c9d5129ec
origami-z <5257855+origami-z@users.noreply.github.com>
2025-02-25 15:21:52 +0000

    Add card background example

--- site/src/examples/card/CustomAccent.tsx
+++ site/src/examples/card/CustomAccent.tsx
@@ -6,13 +6,13 @@ export const CustomAccent = (): ReactElement => {
   return (
     <StackLayout direction="row">
       {/* in your css:
-            .category1 { 
-              --saltCard-accent-color: rgb(70, 118, 191);
-            }
-            .category1:hover {
-              --saltCard-accent-color: rgb(35, 77, 140);
-            }
-              */}
+        .category1 { 
+          --saltCard-accent-color: rgb(70, 118, 191);
+        }
+        .category1:hover {
+          --saltCard-accent-color: rgb(35, 77, 140);
+        }
+      */}
       <Card
         className={styles.category1}
         style={{
@@ -32,14 +32,14 @@ export const CustomAccent = (): ReactElement => {
         </StackLayout>
       </Card>
       {/* in your css:
-            .category2 {
-              --saltCard-accent-color: rgb(171, 101, 40);
-            }
+        .category2 {
+          --saltCard-accent-color: rgb(171, 101, 40);
+        }
 
-            .category2:hover {
-              --saltCard-accent-color: rgb(133, 72, 20);
-            }
-              */}
+        .category2:hover {
+          --saltCard-accent-color: rgb(133, 72, 20);
+        }
+      */}
       <Card
         className={styles.category2}
         style={{
