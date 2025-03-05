8be5fb722ec493ede644d36791b4a85dd5b93e8e
origami-z <5257855+origami-z@users.noreply.github.com>
2025-03-05 12:59:14 +0000

    Add more Banner and Card

--- packages/core/figma-src/Banner.figma.tsx
+++ packages/core/figma-src/Banner.figma.tsx
@@ -9,7 +9,275 @@ import {
   Text,
 } from "../src/index";
 
+// Info - https://www.figma.com/design/7Fh5JbUUM84pYwLt9tpJh2/Salt-DS-Components-and-Patterns?m=auto&node-id=7997-77923
+// Need to split to 2 blocks - see issue https://github.com/figma/code-connect/issues/200#issuecomment-2457330375
+figma.connect(
+  Banner,
+  "https://www.figma.com/design/7Fh5JbUUM84pYwLt9tpJh2/Salt-DS-Components-and-Patterns?node-id=7997-77923",
+  {
+    variant: { "Show title": true },
+    props: {
+      // inlineContent: figma.instance("🔁 Inline content"), // Icon next to the close button
+      title: figma.string("Title text"),
+      // inlineSwap: figma.boolean("👁️ Inline swap"),
+      contentArea: figma.boolean("Content area", {
+        true: figma.instance("Banner content"),
+        false: undefined,
+      }),
+      description: figma.string("Description text"),
+      closeButton: figma.boolean("Close button", {
+        true: (
+          <BannerActions>
+            <Button aria-label="close" appearance="transparent">
+              <CloseIcon />
+            </Button>
+          </BannerActions>
+        ),
+        false: undefined,
+      }),
+      variant: figma.enum("Variant", {
+        Primary: "primary",
+        Secondary: "secondary",
+      }),
+    },
+    example: (props) => (
+      <Banner status="info" variant={props.variant}>
+        <BannerContent>
+          <StackLayout gap={1}>
+            <Text>
+              <strong>{props.title}</strong>
+            </Text>
+            {props.description}
+            {props.contentArea}
+          </StackLayout>
+        </BannerContent>
+        {props.closeButton}
+      </Banner>
+    ),
+  },
+);
+figma.connect(
+  Banner,
+  "https://www.figma.com/design/7Fh5JbUUM84pYwLt9tpJh2/Salt-DS-Components-and-Patterns?node-id=7997-77923",
+  {
+    variant: { "Show title": false },
+    props: {
+      // inlineContent: figma.instance("🔁 Inline content"), // Icon next to the close button
+      // title: figma.string("Title text"),
+      // inlineSwap: figma.boolean("👁️ Inline swap"),
+      contentArea: figma.boolean("Content area", {
+        true: figma.instance("Banner content"),
+        false: undefined,
+      }),
+      description: figma.string("Description text"),
+      closeButton: figma.boolean("Close button", {
+        true: (
+          <BannerActions>
+            <Button aria-label="close" appearance="transparent">
+              <CloseIcon />
+            </Button>
+          </BannerActions>
+        ),
+        false: undefined,
+      }),
+      variant: figma.enum("Variant", {
+        Primary: "primary",
+        Secondary: "secondary",
+      }),
+    },
+    example: (props) => (
+      <Banner status="info" variant={props.variant}>
+        <BannerContent>
+          <StackLayout gap={1}>
+            {props.description}
+            {props.contentArea}
+          </StackLayout>
+        </BannerContent>
+        {props.closeButton}
+      </Banner>
+    ),
+  },
+);
+
+// Error - https://www.figma.com/design/7Fh5JbUUM84pYwLt9tpJh2/Salt-DS-Components-and-Patterns?m=auto&node-id=7997-77972
+// Need to split to 2 blocks - see issue https://github.com/figma/code-connect/issues/200#issuecomment-2457330375
+figma.connect(
+  Banner,
+  "https://www.figma.com/design/7Fh5JbUUM84pYwLt9tpJh2/Salt-DS-Components-and-Patterns?node-id=7997-77972",
+  {
+    variant: { "Show title": true },
+    props: {
+      // inlineContent: figma.instance("🔁 Inline content"), // Icon next to the close button
+      title: figma.string("Title text"),
+      // inlineSwap: figma.boolean("👁️ Inline swap"),
+      contentArea: figma.boolean("Content area", {
+        true: figma.instance("Banner content"),
+        false: undefined,
+      }),
+      description: figma.string("Description text"),
+      closeButton: figma.boolean("Close button", {
+        true: (
+          <BannerActions>
+            <Button aria-label="close" appearance="transparent">
+              <CloseIcon />
+            </Button>
+          </BannerActions>
+        ),
+        false: undefined,
+      }),
+      variant: figma.enum("Variant", {
+        Primary: "primary",
+        Secondary: "secondary",
+      }),
+    },
+    example: (props) => (
+      <Banner status="error" variant={props.variant}>
+        <BannerContent>
+          <StackLayout gap={1}>
+            <Text>
+              <strong>{props.title}</strong>
+            </Text>
+            {props.description}
+            {props.contentArea}
+          </StackLayout>
+        </BannerContent>
+        {props.closeButton}
+      </Banner>
+    ),
+  },
+);
+figma.connect(
+  Banner,
+  "https://www.figma.com/design/7Fh5JbUUM84pYwLt9tpJh2/Salt-DS-Components-and-Patterns?node-id=7997-77972",
+  {
+    variant: { "Show title": false },
+    props: {
+      // inlineContent: figma.instance("🔁 Inline content"), // Icon next to the close button
+      // title: figma.string("Title text"),
+      // inlineSwap: figma.boolean("👁️ Inline swap"),
+      contentArea: figma.boolean("Content area", {
+        true: figma.instance("Banner content"),
+        false: undefined,
+      }),
+      description: figma.string("Description text"),
+      closeButton: figma.boolean("Close button", {
+        true: (
+          <BannerActions>
+            <Button aria-label="close" appearance="transparent">
+              <CloseIcon />
+            </Button>
+          </BannerActions>
+        ),
+        false: undefined,
+      }),
+      variant: figma.enum("Variant", {
+        Primary: "primary",
+        Secondary: "secondary",
+      }),
+    },
+    example: (props) => (
+      <Banner status="error" variant={props.variant}>
+        <BannerContent>
+          <StackLayout gap={1}>
+            {props.description}
+            {props.contentArea}
+          </StackLayout>
+        </BannerContent>
+        {props.closeButton}
+      </Banner>
+    ),
+  },
+);
+
 // Warning https://www.figma.com/design/7Fh5JbUUM84pYwLt9tpJh2/Salt-DS-Components-and-Patterns?m=auto&node-id=7997-78021
+// Need to split to 2 blocks - see issue https://github.com/figma/code-connect/issues/200#issuecomment-2457330375
+figma.connect(
+  Banner,
+  "https://www.figma.com/design/7Fh5JbUUM84pYwLt9tpJh2/Salt-DS-Components-and-Patterns?node-id=7997-78021",
+  {
+    variant: { "Show title": true },
+    props: {
+      // inlineContent: figma.instance("🔁 Inline content"), // Icon next to the close button
+      title: figma.string("Title text"),
+      // inlineSwap: figma.boolean("👁️ Inline swap"),
+      contentArea: figma.boolean("Content area", {
+        true: figma.instance("Banner content"),
+        false: undefined,
+      }),
+      description: figma.string("Description text"),
+      closeButton: figma.boolean("Close button", {
+        true: (
+          <BannerActions>
+            <Button aria-label="close" appearance="transparent">
+              <CloseIcon />
+            </Button>
+          </BannerActions>
+        ),
+        false: undefined,
+      }),
+      variant: figma.enum("Variant", {
+        Primary: "primary",
+        Secondary: "secondary",
+      }),
+    },
+    example: (props) => (
+      <Banner status="warning" variant={props.variant}>
+        <BannerContent>
+          <StackLayout gap={1}>
+            <Text>
+              <strong>{props.title}</strong>
+            </Text>
+            {props.description}
+            {props.contentArea}
+          </StackLayout>
+        </BannerContent>
+        {props.closeButton}
+      </Banner>
+    ),
+  },
+);
+figma.connect(
+  Banner,
+  "https://www.figma.com/design/7Fh5JbUUM84pYwLt9tpJh2/Salt-DS-Components-and-Patterns?node-id=7997-78021",
+  {
+    variant: { "Show title": false },
+    props: {
+      // inlineContent: figma.instance("🔁 Inline content"), // Icon next to the close button
+      // title: figma.string("Title text"),
+      // inlineSwap: figma.boolean("👁️ Inline swap"),
+      contentArea: figma.boolean("Content area", {
+        true: figma.instance("Banner content"),
+        false: undefined,
+      }),
+      description: figma.string("Description text"),
+      closeButton: figma.boolean("Close button", {
+        true: (
+          <BannerActions>
+            <Button aria-label="close" appearance="transparent">
+              <CloseIcon />
+            </Button>
+          </BannerActions>
+        ),
+        false: undefined,
+      }),
+      variant: figma.enum("Variant", {
+        Primary: "primary",
+        Secondary: "secondary",
+      }),
+    },
+    example: (props) => (
+      <Banner status="warning" variant={props.variant}>
+        <BannerContent>
+          <StackLayout gap={1}>
+            {props.description}
+            {props.contentArea}
+          </StackLayout>
+        </BannerContent>
+        {props.closeButton}
+      </Banner>
+    ),
+  },
+);
 
 // Success Banner
 // Need to split to 2 blocks - see issue https://github.com/figma/code-connect/issues/200#issuecomment-2457330375
