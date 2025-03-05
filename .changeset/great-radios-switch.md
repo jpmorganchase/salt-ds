b3e0f1d6bcee300d9d7b13974296da326ef373c1
origami-z <5257855+origami-z@users.noreply.github.com>
2025-03-05 13:54:55 +0000

    Update ghost site example

--- .changeset/great-radios-switch.md
+++ .changeset/great-radios-switch.md
@@ -2,5 +2,5 @@
 "@salt-ds/core": minor
 ---
 
-- Added `variant="ghost"` to Card.
+- Added `variant="ghost"` to Card with translucent background.
 - Added default shadow to all Card variants.
--- site/src/examples/card/Ghost.tsx
+++ site/src/examples/card/Ghost.tsx
@@ -1,13 +1,13 @@
-import { Card, Text } from "@salt-ds/core";
+import { Card, Panel, Text } from "@salt-ds/core";
 import type { ReactElement } from "react";
 import styles from "./index.module.css";
 
 export const Ghost = (): ReactElement => {
   return (
-    <Card variant="tertiary" className={styles.variantCard}>
-      <Card variant="ghost" className={styles.ghostCard}>
+    <Panel className={styles.exampleGradient}>
+      <Card variant="ghost" className={styles.variantCard}>
         <Text>Ghost Card</Text>
       </Card>
-    </Card>
+    </Panel>
   );
 };
