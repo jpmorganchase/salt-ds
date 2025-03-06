590114a65eddd44485f3d60e4cccb5db21d6282b
mark-tate <143323+mark-tate@users.noreply.github.com>
2025-03-06 14:29:03 +0000

    refactored `Landing layout used by home page

--- site/src/layouts/Landing/Landing.tsx
+++ site/src/layouts/Landing/Landing.tsx
@@ -1,15 +1,28 @@
+import { Text } from "@salt-ds/core";
 import type { FC } from "react";
-import { AppHeader, Footer } from "../../components";
 
-import { LayoutBase } from "@jpmorganchase/mosaic-layouts";
-import { LayoutFullWidth } from "../LayoutFullWidth";
-import layoutStyles from "../index.module.css";
+import { Footer, Hero } from "../../components/index";
+import { Search } from "../../components/search";
 import type { LayoutProps } from "../types/index";
+import styles from "./Landing.module.css";
 
-export const Landing: FC<LayoutProps> = ({ FooterProps, children }) => (
-  <LayoutBase Header={<AppHeader />} className={layoutStyles.base}>
-    <LayoutFullWidth Footer={<Footer {...FooterProps} />}>
-      {children}
-    </LayoutFullWidth>
-  </LayoutBase>
-);
+export const Landing: FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className={styles.root}>
+      <main className={styles.main}>
+        <Hero />
+        <div className={styles.middle}>
+          <div className={styles.search}>
+            <Text styleAs="h1">Discover the power of Salt</Text>
+            <Search
+              aria-label="Search for components, guides and more."
+              placeholder="Search for components, guides and more."
+            />
+          </div>
+          <div className={styles.content}>{children}</div>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
