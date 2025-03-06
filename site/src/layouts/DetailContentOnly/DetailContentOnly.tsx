b42456bc8eda8691da6dd29ed417665383690630
mark-tate <143323+mark-tate@users.noreply.github.com>
2025-03-06 14:28:30 +0000

    refactored DetailContentOnly layout

--- site/src/layouts/DetailContentOnly/DetailContentOnly.tsx
+++ site/src/layouts/DetailContentOnly/DetailContentOnly.tsx
@@ -1,42 +1,15 @@
-import { LayoutBase } from "@jpmorganchase/mosaic-layouts";
-import { Breadcrumbs } from "@jpmorganchase/mosaic-site-components";
-import { useMeta } from "@jpmorganchase/mosaic-store";
-import { SaltProvider } from "@salt-ds/core";
-import clsx from "clsx";
 import type { FC } from "react";
-import { AppHeader, Footer } from "../../components";
-import { LayoutFullWidth } from "../LayoutFullWidth";
-import layoutStyles from "../index.module.css";
+import { TopLevelNavigation } from "../../components/navigation/TopLevelNavigation";
+import { Base } from "../Base/index";
 import type { LayoutProps } from "../types/index";
 import styles from "./DetailContentOnly.module.css";
 
-export const DetailContentOnly: FC<LayoutProps> = ({
-  FooterProps,
-  children,
-}) => {
-  const {
-    meta: { title },
-  } = useMeta();
-
-  return (
-    <LayoutBase
-      Header={<AppHeader />}
-      className={clsx(layoutStyles.base, styles.base)}
-    >
-      <div className={layoutStyles.docsWrapper}>
-        <LayoutFullWidth
-          Footer={<Footer {...FooterProps} />}
-          className={styles.layoutFullWidth}
-        >
-          <Breadcrumbs />
-          <h1 className={layoutStyles.title}>{title}</h1>
-          <SaltProvider mode="light">
-            <div className={layoutStyles.docsPageContainer}>
-              <div className={layoutStyles.docsPageContent}>{children}</div>
-            </div>
-          </SaltProvider>
-        </LayoutFullWidth>
-      </div>
-    </LayoutBase>
+export const DetailContentOnly: FC<LayoutProps> = ({ children }) => {
+  const PrimarySidebar = (
+    <div className={styles.primarySidebar}>
+      <TopLevelNavigation />
+    </div>
   );
+
+  return <Base LeftSidebar={PrimarySidebar}>{children}</Base>;
 };
