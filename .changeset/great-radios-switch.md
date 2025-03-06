aac150028f1be99bb791f4d5eba22b2bd5fca089
Zhihao Cui <5257855+origami-z@users.noreply.github.com>
2025-03-06 13:43:32 +0000

    Add ghost Card and update default shadow (#4771)
    Co-authored-by: mark-tate <143323+mark-tate@users.noreply.github.com>

---
+++ .changeset/great-radios-switch.md
@@ -0,0 +1,6 @@
+---
+"@salt-ds/core": minor
+---
+
+- Added `variant="ghost"` to Card with translucent background.
+- Added default shadow to all Card, Link Card and Interactable Card variants.
--- site/src/examples/card/Variants.tsx
+++ site/src/examples/card/Variants.tsx
@@ -1,22 +1,24 @@
-import { Card, GridLayout } from "@salt-ds/core";
+import { Card, GridLayout, StackLayout } from "@salt-ds/core";
 import type { ReactElement } from "react";
 import styles from "./index.module.css";
 
 export const Variants = (): ReactElement => {
   return (
-    <GridLayout
-      columns="repeat(auto-fill, 260px)"
-      className={styles.variantContainer}
-    >
-      <Card variant="primary" className={styles.variantCard}>
-        Primary
-      </Card>
-      <Card variant="secondary" className={styles.variantCard}>
-        Secondary
-      </Card>
-      <Card variant="tertiary" className={styles.variantCard}>
-        Tertiary
-      </Card>
-    </GridLayout>
+    <StackLayout className={styles.variantContainer}>
+      <GridLayout
+        columns="repeat(auto-fill, 260px)"
+        className={styles.variantContainer}
+      >
+        <Card variant="primary" className={styles.variantCard}>
+          Primary
+        </Card>
+        <Card variant="secondary" className={styles.variantCard}>
+          Secondary
+        </Card>
+        <Card variant="tertiary" className={styles.variantCard}>
+          Tertiary
+        </Card>
+      </GridLayout>
+    </StackLayout>
   );
 };
