7a7ad539e554cc1be08299e4acc918c219f443ba
mark-tate <143323+mark-tate@users.noreply.github.com>
2025-03-06 15:02:58 +0000

    refactor the `DetailTechnical` layout

--- site/src/layouts/DetailTechnical/DetailTechnical.tsx
+++ site/src/layouts/DetailTechnical/DetailTechnical.tsx
@@ -1,15 +1,34 @@
-import { Sidebar } from "@jpmorganchase/mosaic-site-components";
+import { PageNavigation } from "@jpmorganchase/mosaic-site-components";
 import type { FC } from "react";
-import { TableOfContents } from "../../components/toc";
-import { DetailBase } from "../DetailBase";
+import { TableOfContents } from "../../components/toc/index";
+import { Base } from "../Base/index";
 import type { LayoutProps } from "../types/index";
+import styles from "./DetailTechnical.module.css";
+import { TopLevelNavigation } from "../../components/navigation/TopLevelNavigation";
+import { Divider } from "@salt-ds/core";
 
 export const DetailTechnical: FC<LayoutProps> = ({ children }) => {
-  const SecondarySidebar = <TableOfContents />;
+  const PrimarySidebar = (
+    <div className={styles.primarySidebar}>
+      <TopLevelNavigation />
+      <Divider variant="tertiary" />
+      <PageNavigation />
+    </div>
+  );
+
+  const RightSidebar = (
+    <div className={styles.secondarySidebar}>
+      <TableOfContents />
+    </div>
+  );
 
   return (
-    <DetailBase sidebar={<Sidebar sticky>{SecondarySidebar}</Sidebar>}>
+    <Base
+      className={styles.root}
+      LeftSidebar={PrimarySidebar}
+      RightSidebar={RightSidebar}
+    >
       {children}
-    </DetailBase>
+    </Base>
   );
 };
