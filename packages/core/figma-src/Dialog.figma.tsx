65bf319a924f9b62a5f0846153e62875c949fb09
origami-z <5257855+origami-z@users.noreply.github.com>
2025-03-07 16:08:45 +0000

    Add a few more components

---
+++ packages/core/figma-src/Dialog.figma.tsx
@@ -0,0 +1,7 @@
+// import figma from "@figma/code-connect";
+// import { Dialog } from "../src/dialog/";
+
+// .Header https://www.figma.com/design/7Fh5JbUUM84pYwLt9tpJh2/Salt-DS-Components-and-Patterns?node-id=10513-4326&m=dev
+// pending header lab to core work
+
+// https://www.figma.com/design/7Fh5JbUUM84pYwLt9tpJh2/Salt-DS-Components-and-Patterns?node-id=9899-2649&m=dev
