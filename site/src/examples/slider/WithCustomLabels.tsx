9d7981b146615d56fb8a693025e9df429ac58b8c
Tayyaba Taimur <19307765+tayyabataimur@users.noreply.github.com>
2025-03-07 14:14:38 +0000

    fixup! Examples and stories cleanup

--- site/src/examples/slider/WithCustomLabels.tsx
+++ site/src/examples/slider/WithCustomLabels.tsx
@@ -1,15 +1,27 @@
+import { StackLayout } from "@salt-ds/core";
 import { Slider } from "@salt-ds/lab";
 import type { ReactElement } from "react";
 
 export const WithCustomLabels = (): ReactElement => (
-  <Slider
-    aria-label="single"
-    min={0}
-    max={50}
-    defaultValue={30}
-    style={{ width: "400px" }}
-    minLabel="Very low"
-    maxLabel="Very high"
-    labelPosition="bottom"
-  />
+  <StackLayout gap={3}>
+    <Slider
+      aria-label="single"
+      min={0}
+      max={50}
+      defaultValue={30}
+      style={{ width: "400px" }}
+      minLabel="Very low"
+      maxLabel="Very high"
+    />
+    <Slider
+      aria-label="single"
+      min={0}
+      max={50}
+      defaultValue={30}
+      style={{ width: "400px" }}
+      minLabel="Very low"
+      maxLabel="Very high"
+      labelPosition="bottom"
+    />
+  </StackLayout>
 );
