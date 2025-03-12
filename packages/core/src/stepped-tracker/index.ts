50f3b05fdd0763a352ab3d3335cac59311505d4d
mark-tate <143323+mark-tate@users.noreply.github.com>
2025-03-12 16:48:07 +0000

    move `StepTracker` types back into code, to align with existing conventions

--- packages/core/src/stepped-tracker/index.ts
+++ packages/core/src/stepped-tracker/index.ts
@@ -1,8 +1,3 @@
 export * from "./SteppedTracker";
-export * from "./SteppedTracker.types";
-
 export * from "./Step";
-export * from "./Step.types";
-
 export * from "./useStepReducer";
-export * from "./stepReducer.types";
