efa98c47693933f22b15048e24e878fd7e1ac14d
mark-tate <143323+mark-tate@users.noreply.github.com>
2025-03-12 00:08:18 +0000

    `DatePicker`now supports `isDayDisabled`, `isDayUnselectable` and `isDayHighlighted`, rather than providing them directly to the `Calendar`

---
+++ packages/lab/src/date-picker/DatePickerSingleGridPanel.tsx
@@ -0,0 +1,223 @@
+import {
+  FlexItem,
+  FlexLayout,
+  FormFieldContext,
+  type FormFieldContextValue,
+  FormFieldHelperText,
+  type ResponsiveProp,
+  StackLayout,
+  makePrefixer,
+  resolveResponsiveValue,
+  useBreakpoint,
+  useControlled,
+} from "@salt-ds/core";
+import type { DateFrameworkType } from "@salt-ds/date-adapters";
+import { useComponentCssInjection } from "@salt-ds/styles";
+import { useWindow } from "@salt-ds/window";
+import clsx from "clsx";
+import {
+  type ComponentPropsWithoutRef,
+  type SyntheticEvent,
+  forwardRef,
+  useCallback,
+  useState,
+} from "react";
+import {
+  CalendarGrid,
+  type CalendarGridProps,
+  CalendarNavigation,
+  type CalendarNavigationProps,
+  type CalendarSingleProps,
+} from "../calendar";
+import {
+  Calendar,
+  type DateRangeSelection,
+  type SingleDateSelection,
+} from "../calendar";
+import { useLocalization } from "../localization-provider";
+import {
+  type SingleDatePickerState,
+  useDatePickerContext,
+} from "./DatePickerContext";
+import datePickerPanelCss from "./DatePickerPanel.css";
+
+export interface DatePickerPanelBaseProps<TDate extends DateFrameworkType>
+  extends ComponentPropsWithoutRef<"div"> {
+  helperText?: string;
+  visibleMonth?: TDate;
+  columns?: ResponsiveProp<number | string>;
+  numberOfVisibleMonths?: ResponsiveProp<
+    1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
+  >;
+  defaultVisibleMonth?: TDate;
+  onVisibleMonthChange?: (event: SyntheticEvent, visibleMonth: TDate) => void;
+  CalendarNavigationProps?: Partial<CalendarNavigationProps<TDate>>;
+  CalendarGridProps?: Partial<CalendarGridProps<TDate>>;
+}
+
+const withBaseName = makePrefixer("saltDatePickerPanel");
+
+export type DatePickerSingleGridPanelProps<TDate extends DateFrameworkType> =
+  DatePickerPanelBaseProps<TDate> &
+    SingleDateSelection<TDate> & {
+      onSelectionChange?: (
+        event: SyntheticEvent,
+        selectedDate?: TDate | null,
+      ) => void;
+      CalendarProps?: Partial<
+        Omit<
+          CalendarSingleProps<TDate>,
+          | "selectionVariant"
+          | "selectedDate"
+          | "defaultSelectedDate"
+          | "onSelectionChange"
+          | "onVisibleMonthChange"
+        >
+      >;
+    };
+
+export const DatePickerSingleGridPanel = forwardRef(
+  function DatePickerSingleGridPanel<TDate extends DateFrameworkType>(
+    props: DatePickerSingleGridPanelProps<TDate>,
+    ref: React.Ref<HTMLDivElement>,
+  ) {
+    const { dateAdapter } = useLocalization<TDate>();
+
+    const {
+      CalendarProps,
+      CalendarNavigationProps,
+      CalendarGridProps,
+      className,
+      defaultVisibleMonth,
+      visibleMonth: visibleMonthProp,
+      onVisibleMonthChange,
+      helperText,
+      onSelectionChange,
+      numberOfVisibleMonths = 1,
+      columns = numberOfVisibleMonths,
+      ...rest
+    } = props;
+
+    const targetWindow = useWindow();
+    useComponentCssInjection({
+      testId: "salt-date-picker-single-grid-panel",
+      css: datePickerPanelCss,
+      window: targetWindow,
+    });
+
+    const stateAndHelpers: SingleDatePickerState<TDate> = useDatePickerContext({
+      selectionVariant: "single",
+    });
+
+    const {
+      state: {
+        selectedDate = null,
+        minDate = dateAdapter.startOf(dateAdapter.today(), "month"),
+        maxDate = dateAdapter.add(minDate, { months: 1 }),
+      },
+      helpers: { select, isDayDisabled, isDayHighlighted, isDayUnselectable },
+    } = stateAndHelpers;
+
+    const { matchedBreakpoints } = useBreakpoint();
+
+    const responsiveColumns =
+      resolveResponsiveValue(columns, matchedBreakpoints) ?? 1;
+    const responsiveNumberOfVisibleMonths =
+      resolveResponsiveValue(numberOfVisibleMonths, matchedBreakpoints) ?? 1;
+    const [hoveredDate, setHoveredDate] = useState<TDate | null>(null);
+
+    const [uncontrolledDefaultVisibleMonth] = useState(() => {
+      const validDate = dateAdapter.isValid(selectedDate)
+        ? selectedDate
+        : dateAdapter.today();
+      return defaultVisibleMonth || dateAdapter.startOf(validDate, "month");
+    });
+    const [visibleMonth, setVisibleMonth] = useControlled({
+      controlled: visibleMonthProp,
+      default: uncontrolledDefaultVisibleMonth,
+      name: "DatePickerGridPanel",
+      state: "visibleMonth",
+    });
+
+    const handleSelectionChange = useCallback(
+      (
+        event: SyntheticEvent,
+        newDate: TDate | DateRangeSelection<TDate> | null,
+      ) => {
+        const singleDate = newDate as TDate | null;
+        select(event, singleDate);
+        onSelectionChange?.(event, singleDate);
+      },
+      [onSelectionChange, select],
+    );
+
+    const handleHoveredDateChange = useCallback(
+      (event: SyntheticEvent, newHoveredDate: TDate | null) => {
+        setHoveredDate(newHoveredDate);
+        if (newHoveredDate && CalendarProps?.onHoveredDateChange) {
+          CalendarProps.onHoveredDateChange(event, newHoveredDate);
+        }
+      },
+      [CalendarProps?.onHoveredDateChange],
+    );
+
+    const handleVisibleMonthChange = useCallback(
+      (event: SyntheticEvent, newVisibleMonth: TDate) => {
+        setVisibleMonth(newVisibleMonth);
+        if (onVisibleMonthChange) {
+          onVisibleMonthChange(event, newVisibleMonth);
+        }
+      },
+      [onVisibleMonthChange],
+    );
+
+    console.log('visibleMonth', visibleMonth);
+
+    const calendarProps = {
+      visibleMonth,
+      hoveredDate,
+      isDayDisabled,
+      isDayHighlighted,
+      isDayUnselectable,
+      onHoveredDateChange: handleHoveredDateChange,
+      onVisibleMonthChange: handleVisibleMonthChange,
+      onSelectionChange: handleSelectionChange,
+      hideOutOfRangeDates: true,
+      selectedDate,
+      minDate,
+      maxDate,
+      ...CalendarProps,
+    };
+
+    return (
+      <StackLayout
+        separators
+        gap={0}
+        className={clsx(className, withBaseName("container"))}
+        ref={ref}
+        {...rest}
+      >
+        {helperText && (
+          <FlexItem className={withBaseName("header")}>
+            <FormFieldHelperText>{helperText}</FormFieldHelperText>
+          </FlexItem>
+        )}
+        <FlexLayout gap={0}>
+          <FormFieldContext.Provider value={{} as FormFieldContextValue}>
+            <Calendar
+              selectionVariant={"single"}
+              numberOfVisibleMonths={responsiveNumberOfVisibleMonths}
+              {...(calendarProps as Partial<CalendarSingleProps<TDate>>)}
+            >
+              <CalendarNavigation {...CalendarNavigationProps} />
+              <CalendarGrid
+                columns={responsiveColumns}
+                {...CalendarGridProps}
+              />
+            </Calendar>
+          </FormFieldContext.Provider>
+        </FlexLayout>
+      </StackLayout>
+    );
+  },
+);
