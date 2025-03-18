5b61d80f6174fd9a960d28b77e3979589932d827
mark-tate <143323+mark-tate@users.noreply.github.com>
2025-03-18 00:36:38 +0000

    standardize exports

--- packages/core/src/splitter/index.ts
+++ packages/core/src/splitter/index.ts
@@ -1,5 +1,5 @@
-export { Splitter, type SplitterProps } from "./Splitter";
-export { SplitPanel, type SplitPanelProps } from "./SplitPanel";
-export { SplitHandle, type SplitHandleProps } from "./SplitHandle";
+export * from './Splitter';
+export * from './SplitHandle';
+export * from './SplitPanel';
 
 export type { ImperativePanelHandle } from "react-resizable-panels";
