c76d496123d77cc1edfd395b4352d152cec9a7db
mark-tate <143323+mark-tate@users.noreply.github.com>
2025-03-26 00:16:20 +0000

    add responsive grid view examples to site

---
+++ site/src/examples/date-picker/SingleResponsiveGridPanel.tsx
@@ -0,0 +1,93 @@
+import { FormField, FormFieldLabel as FormLabel } from "@salt-ds/core";
+import type { DateFrameworkType } from "@salt-ds/date-adapters";
+import {
+  type DateInputSingleDetails,
+  DatePicker,
+  DatePickerHelperText,
+  DatePickerOverlay,
+  DatePickerSingleGridPanel,
+  DatePickerSingleInput,
+  DatePickerTrigger,
+  type SingleDateSelection,
+  useLocalization,
+} from "@salt-ds/lab";
+import {
+  type ReactElement,
+  type SyntheticEvent,
+  useCallback,
+  useState,
+} from "react";
+
+export const SingleResponsiveGridPanel = (): ReactElement => {
+  const { dateAdapter } = useLocalization();
+  const defaultHelperText = "Date format DD MMM YYYY (e.g. 09 Jun 2024)";
+  const errorHelperText = "Please enter a valid date in DD MMM YYYY format";
+  const [helperText, setHelperText] = useState(defaultHelperText);
+  const [validationStatus, setValidationStatus] = useState<"error" | undefined>(
+    undefined,
+  );
+  const handleSelectionChange = useCallback(
+    (
+      _event: SyntheticEvent,
+      date: SingleDateSelection<DateFrameworkType> | null,
+      details: DateInputSingleDetails | undefined,
+    ) => {
+      const { value, errors } = details || {};
+      console.log(
+        `Selected date: ${dateAdapter.isValid(date) ? dateAdapter.format(date, "DD MMM YYYY") : date}`,
+      );
+      if (errors?.length) {
+        console.log(
+          `Error(s): ${errors
+            .map(({ type, message }) => `type=${type} message=${message}`)
+            .join(",")}`,
+        );
+        if (value) {
+          console.log(`Original Value: ${value}`);
+        }
+      }
+      if (errors?.length && details?.value?.length) {
+        setHelperText(`${errorHelperText} - ${errors[0].message}`);
+        setValidationStatus("error");
+      } else {
+        setHelperText(defaultHelperText);
+        setValidationStatus(undefined);
+      }
+    },
+    [dateAdapter],
+  );
+
+  return (
+    <FormField style={{ width: "256px" }} validationStatus={validationStatus}>
+      <FormLabel>Select a date</FormLabel>
+      <DatePicker
+        selectionVariant="single"
+        onSelectionChange={handleSelectionChange}
+      >
+        <DatePickerTrigger>
+          <DatePickerSingleInput />
+        </DatePickerTrigger>
+        <DatePickerOverlay>
+          <DatePickerSingleGridPanel
+            helperText={helperText}
+            columns={{
+              xs: 1,
+              sm: 2,
+              md: 3,
+              lg: 3,
+              xl: 3,
+            }}
+            numberOfVisibleMonths={{
+              xs: 1,
+              sm: 2,
+              md: 3,
+              lg: 6,
+              xl: 12,
+            }}
+          />
+        </DatePickerOverlay>
+        <DatePickerHelperText>{helperText}</DatePickerHelperText>
+      </DatePicker>
+    </FormField>
+  );
+};
