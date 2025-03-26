333fb7d9f362bbc3cfcd2a955731060e3fc25a71
mark-tate <143323+mark-tate@users.noreply.github.com>
2025-03-26 00:16:20 +0000

    add responsive grid view examples to site

---
+++ site/src/examples/date-picker/RangeResponsiveGridPanel.tsx
@@ -0,0 +1,116 @@
+import { FormField, FormFieldLabel as FormLabel } from "@salt-ds/core";
+import type { DateFrameworkType } from "@salt-ds/date-adapters";
+import {
+  type DateInputRangeDetails,
+  DatePicker,
+  DatePickerHelperText,
+  DatePickerOverlay,
+  DatePickerRangeInput,
+  DatePickerRangeGridPanel,
+  DatePickerTrigger,
+  type DateRangeSelection,
+  useLocalization,
+} from "@salt-ds/lab";
+import {
+  type ReactElement,
+  type SyntheticEvent,
+  useCallback,
+  useState,
+} from "react";
+
+export const RangeResponsiveGridPanel = (): ReactElement => {
+  const { dateAdapter } = useLocalization();
+  const defaultHelperText =
+    "Select range DD MMM YYYY - DD MMM YYYY (e.g. 09 Jun 2024)";
+  const errorHelperText = "Please enter a valid date in DD MMM YYYY format";
+  const [helperText, setHelperText] = useState(defaultHelperText);
+  const [validationStatus, setValidationStatus] = useState<"error" | undefined>(
+    undefined,
+  );
+  const handleSelectionChange = useCallback(
+    (
+      _event: SyntheticEvent,
+      date: DateRangeSelection<DateFrameworkType> | null,
+      details: DateInputRangeDetails | undefined,
+    ) => {
+      const { startDate, endDate } = date ?? {};
+      const {
+        startDate: {
+          value: startDateOriginalValue = undefined,
+          errors: startDateErrors = undefined,
+        } = {},
+        endDate: {
+          value: endDateOriginalValue = undefined,
+          errors: endDateErrors = undefined,
+        } = {},
+      } = details || {};
+      console.log(
+        `StartDate: ${dateAdapter.isValid(startDate) ? dateAdapter.format(startDate, "DD MMM YYYY") : startDate}, EndDate: ${dateAdapter.isValid(endDate) ? dateAdapter.format(endDate, "DD MMM YYYY") : endDate}`,
+      );
+      if (startDateErrors?.length) {
+        console.log(
+          `StartDate Error(s): ${startDateErrors.map(({ type, message }) => `type: ${type} message: ${message}`).join(",")}`,
+        );
+        if (startDateOriginalValue) {
+          console.log(`StartDate Original Value: ${startDateOriginalValue}`);
+        }
+      }
+      if (endDateErrors?.length) {
+        console.log(
+          `EndDate Error(s): ${endDateErrors.map(({ type, message }) => `type: ${type} message: ${message}`).join(",")}`,
+        );
+        if (endDateOriginalValue) {
+          console.log(`EndDate Original Value: ${endDateOriginalValue}`);
+        }
+      }
+      if (startDateErrors?.length && startDateOriginalValue) {
+        setValidationStatus("error");
+        setHelperText(
+          `${errorHelperText} - start date ${startDateErrors[0].message}`,
+        );
+      } else if (endDateErrors?.length && endDateOriginalValue) {
+        setValidationStatus("error");
+        setHelperText(
+          `${errorHelperText} - end date ${endDateErrors[0].message}`,
+        );
+      } else {
+        setValidationStatus(undefined);
+        setHelperText(defaultHelperText);
+      }
+    },
+    [dateAdapter],
+  );
+
+  return (
+    <FormField style={{ width: "256px" }} validationStatus={validationStatus}>
+      <FormLabel>Select a date range</FormLabel>
+      <DatePicker
+        selectionVariant="range"
+        onSelectionChange={handleSelectionChange}
+      >
+        <DatePickerTrigger>
+          <DatePickerRangeInput />
+        </DatePickerTrigger>
+        <DatePickerOverlay>
+          <DatePickerRangeGridPanel
+            columns={{
+              xs: 1,
+              sm: 2,
+              md: 3,
+              lg: 3,
+              xl: 3,
+            }}
+            numberOfVisibleMonths={{
+              xs: 1,
+              sm: 2,
+              md: 3,
+              lg: 6,
+              xl: 12,
+            }}
+          />
+        </DatePickerOverlay>
+        <DatePickerHelperText>{helperText}</DatePickerHelperText>
+      </DatePicker>
+    </FormField>
+  );
+};
