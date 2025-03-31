d975e53191e4d512359906d4e9b4926a6ce3cff3
origami-z <5257855+origami-z@users.noreply.github.com>
2025-03-31 11:16:44 +0100

    Add dark mode source support to image, fix themes page dark mode

--- site/src/components/mdx/image/Image.tsx
+++ site/src/components/mdx/image/Image.tsx
@@ -1,3 +1,4 @@
+import { useColorMode } from "@jpmorganchase/mosaic-store";
 import { clsx } from "clsx";
 import NextImage, { type ImageProps as NextImageProps } from "next/image";
 import { forwardRef } from "react";
@@ -9,14 +10,27 @@ export interface ImageProps extends Omit<NextImageProps, "src"> {
    * Image source url
    */
   src: string;
+  /**
+   * Image source url for dark mode.
+   */
+  srcDark?: string;
 }
 
 export const Image = forwardRef<HTMLDivElement, ImageProps>(function Image(
-  { className, src, fill, width, height, alt, unoptimized, ...rest },
+  { className, src, srcDark, fill, width, height, alt, unoptimized, ...rest },
   ref,
 ) {
-  const resolvedSrc = useResolveRelativeUrl(src);
+  const siteMode = useColorMode();
+
+  const srcToUse = siteMode === "dark" && srcDark ? srcDark : src;
+
+  const resolvedSrc = useResolveRelativeUrl(srcToUse);
+
+  const resolvedSrcToUse =
+    srcToUse.match(/^(http[s]?:)?\/{1,2}/) === null ? resolvedSrc : srcToUse;
+
   return (
+    // Can't use `picture` with `src`, it only works with system preference
     <div className={clsx(styles.root, className)} ref={ref}>
       {fill || (width && height) ? (
         <NextImage
@@ -25,16 +39,12 @@ export const Image = forwardRef<HTMLDivElement, ImageProps>(function Image(
           {...rest}
           height={height}
           fill={fill}
-          src={src.match(/^(http[s]?:)?\/{1,2}/) === null ? resolvedSrc : src}
+          src={resolvedSrcToUse}
           width={width}
           unoptimized={unoptimized}
         />
       ) : (
-        <img
-          alt={alt}
-          className={styles.img}
-          src={src.match(/^(http[s]?:)?\/{1,2}/) === null ? resolvedSrc : src}
-        />
+        <img alt={alt} className={styles.img} src={resolvedSrcToUse} />
       )}
     </div>
   );
