2ba3d9c63be7c7851c2d98b113aada379b9caa94
tayyabataimur <19307765+tayyabataimur@users.noreply.github.com>
2025-04-09 21:12:47 +0100

    Group slider docs and add change events example

---
+++ site/src/examples/range-slider/WithChangeEvents.tsx
@@ -0,0 +1,34 @@
+import { StackLayout, Text } from "@salt-ds/core";
+import { RangeSlider } from "@salt-ds/lab";
+import { useState } from "react";
+
+export const WithChangeEvents = () => {
+  const [valueOnChange, setValueOnChange] = useState<[number, number]>([0, 50]);
+  const [valueOnChangeEnd, setValueOnChangeEnd] = useState<[number, number]>([
+    0, 50,
+  ]);
+
+  return (
+    <StackLayout style={{ width: "80%" }}>
+      <RangeSlider
+        minLabel="0"
+        maxLabel="100"
+        defaultValue={[0, 50]}
+        onChange={(_, value) => setValueOnChange(value)}
+        onChangeEnd={(_, value) => setValueOnChangeEnd(value)}
+      />
+      <Text>
+        Value onChange:{" "}
+        <span style={{ color: "var(--salt-color-teal-500)" }}>
+          [{valueOnChange[0]}, {valueOnChange[1]}]
+        </span>
+      </Text>
+      <Text>
+        Value onChangeEnd:{" "}
+        <span style={{ color: "var(--salt-color-teal-500)" }}>
+          [{valueOnChangeEnd[0]}, {valueOnChangeEnd[1]}]
+        </span>
+      </Text>
+    </StackLayout>
+  );
+};
---
+++ site/src/examples/slider/WithChangeEvents.tsx
@@ -0,0 +1,32 @@
+import { StackLayout, Text } from "@salt-ds/core";
+import { Slider } from "@salt-ds/lab";
+import { useState } from "react";
+
+export const WithChangeEvents = () => {
+  const [valueOnChange, setValueOnChange] = useState<number>(50);
+  const [valueOnChangeEnd, setValueOnChangeEnd] = useState<number>(50);
+
+  return (
+    <StackLayout style={{ width: "80%" }}>
+      <Slider
+        minLabel="0"
+        maxLabel="100"
+        defaultValue={50}
+        onChange={(_, value) => setValueOnChange(value)}
+        onChangeEnd={(_, value) => setValueOnChangeEnd(value)}
+      />
+      <Text>
+        Value onChange:{" "}
+        <span style={{ color: "var(--salt-color-teal-500)" }}>
+          {valueOnChange}
+        </span>
+      </Text>
+      <Text>
+        Value onChangeEnd:{" "}
+        <span style={{ color: "var(--salt-color-teal-500)" }}>
+          {valueOnChangeEnd}
+        </span>
+      </Text>
+    </StackLayout>
+  );
+};
