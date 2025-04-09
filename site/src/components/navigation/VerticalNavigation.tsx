6ef2bbc4e0bae92c0c13ccdd63f0194ece1d7935
origami-z <5257855+origami-z@users.noreply.github.com>
2025-04-09 17:27:55 +0100

    Site font size 16px

--- site/src/components/navigation/VerticalNavigation.tsx
+++ site/src/components/navigation/VerticalNavigation.tsx
@@ -3,7 +3,7 @@ import {
   NavigationItem,
   type NavigationItemProps,
   StackLayout,
-  Tag,
+  Badge,
 } from "@salt-ds/core";
 import type React from "react";
 import { type MouseEventHandler, useState } from "react";
@@ -84,7 +84,7 @@ const renderNavigationItem = (
         expanded={isExpanded}
         level={level}
       >
-        {name} {status && <Tag bordered>{status}</Tag>}
+        {name} {status && <Badge value={status} />}
       </NavigationItem>
       {shouldRenderAsParent && isExpanded ? (
         <StackLayout
