98642f12ca411c3a5dea6e4e47905f8a83ef9f79
Fernanda Castillo <fersitacastillo123@gmail.com>
2025-05-01 15:15:12 +0100

    add dots to controlled

--- site/src/examples/carousel/ControlledCarousel.tsx
+++ site/src/examples/carousel/ControlledCarousel.tsx
@@ -1,28 +1,40 @@
-import { Button, H3, StackLayout, Text, useId } from "@salt-ds/core";
+import {
+  Button,
+  H3,
+  RadioButton,
+  RadioButtonGroup,
+  StackLayout,
+  Text,
+  useId,
+} from "@salt-ds/core";
 import { Carousel, CarouselSlide, CarouselSlider } from "@salt-ds/lab";
 import clsx from "clsx";
-import { type ReactElement, useState } from "react";
+import { type ChangeEvent, type ReactElement, useState } from "react";
 import { sliderData } from "./exampleData";
 import styles from "./index.module.css";
 
 export const ControlledCarousel = (): ReactElement => {
   const [slide, setSlide] = useState<number>(0);
+
+  const handleRadioChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    const newIndex = sliderData.findIndex((i) => i.title === value);
+    setSlide(newIndex);
+  };
+
   return (
     <StackLayout>
-      <StackLayout gap={1} direction="row" align="center">
-        <Text>Current slide: {slide + 1}</Text>
-      </StackLayout>
       <StackLayout direction="row" gap={1}>
         <Button
           appearance="transparent"
-          onClick={() => setSlide(slide - 1)}
+          onClick={() => setSlide(Math.max(0, slide - 1))}
           disabled={slide === 0}
           className={styles.carouselButton}
         >
           Left
         </Button>
         <Carousel aria-label="Account overview" activeSlideIndex={slide}>
-          <CarouselSlider onSelectionChange={(_, index) => setSlide(index)}>
+          <CarouselSlider>
             {sliderData.map((slide, index) => {
               const slideId = useId();
               return (
@@ -46,12 +58,25 @@ export const ControlledCarousel = (): ReactElement => {
         </Carousel>
         <Button
           appearance="transparent"
-          onClick={() => setSlide(slide + 1)}
+          onClick={() => setSlide(Math.min(sliderData.length - 1, slide + 1))}
           disabled={slide >= sliderData.length - 1}
           className={styles.carouselButton}
         >
           Right
         </Button>
+      </StackLayout>
+      <StackLayout gap={1} direction="column" align="center">
+        <RadioButtonGroup
+          direction="horizontal"
+          onChange={(event) => handleRadioChange(event)}
+          value={sliderData[slide].title}
+        >
+          {sliderData.map((slideData) => {
+            return (
+              <RadioButton key={slideData.title} value={slideData.title} />
+            );
+          })}
+        </RadioButtonGroup>
       </StackLayout>
     </StackLayout>
   );
