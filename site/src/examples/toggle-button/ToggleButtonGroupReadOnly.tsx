39b57f4ef06f5d83751bd177a980d08b4589eba8
tayyabataimur <tayyaba.taimur@jpmorgan.com>
2025-05-01 11:47:10 +0100

    fixup! Action PR and documentation feedback

--- site/src/examples/toggle-button/ToggleButtonGroupReadOnly.tsx
+++ site/src/examples/toggle-button/ToggleButtonGroupReadOnly.tsx
@@ -3,7 +3,7 @@ import type { ReactElement } from "react";
 
 export const ToggleButtonGroupReadOnly = (): ReactElement => (
   <ToggleButtonGroup defaultValue="0" readOnly>
-    <ToggleButton value="0">Selectd readonly</ToggleButton>
+    <ToggleButton value="0">Selected readonly</ToggleButton>
     <ToggleButton value="1">Unselected readonly</ToggleButton>
     <ToggleButton value="2">Unselected readonly</ToggleButton>
   </ToggleButtonGroup>
