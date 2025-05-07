178f24e4f350ed8d72566ee441b4793525bf1ace
ImmanuelBaskaran <immanuel.baskaran.16@ucl.ac.uk>
2025-05-07 10:28:58 +0100

    feat(splitter): scope theme to classname react-resizable-panels-theme-salt

--- site/src/examples/react-resizable-panels-theme/Variant.tsx
+++ site/src/examples/react-resizable-panels-theme/Variant.tsx
@@ -6,44 +6,46 @@ import styles from "./splitter.module.css";
 
 export function Variant() {
   return (
-    <FlexLayout className={styles.box}>
-      <PanelGroup direction="horizontal">
-        <Panel
-          minSize={0}
-          defaultSize={25}
-          className={clsx(
-            styles.center,
-            "resizable-panel-salt-variant-secondary",
-          )}
-        >
-          <Text>Left</Text>
-        </Panel>
-        <PanelResizeHandle
-          className={clsx(
-            "resize-handle-border-right",
-            "resize-handle-salt-variant-secondary",
-          )}
-        />
-        <Panel minSize={50} className={styles.center}>
-          <Text>Center</Text>
-        </Panel>
-        <PanelResizeHandle
-          className={clsx(
-            "resize-handle-border-left",
-            "resize-handle-salt-variant-tertiary",
-          )}
-        />
-        <Panel
-          minSize={0}
-          defaultSize={25}
-          className={clsx(
-            styles.center,
-            "resizable-panel-salt-variant-tertiary",
-          )}
-        >
-          <Text>Right</Text>
-        </Panel>
-      </PanelGroup>
-    </FlexLayout>
+    <div className="react-resizable-panels-theme-salt">
+      <FlexLayout className={styles.box}>
+        <PanelGroup direction="horizontal">
+          <Panel
+            minSize={0}
+            defaultSize={25}
+            className={clsx(
+              styles.center,
+              "resizable-panel-salt-variant-secondary",
+            )}
+          >
+            <Text>Left</Text>
+          </Panel>
+          <PanelResizeHandle
+            className={clsx(
+              "resize-handle-border-right",
+              "resize-handle-salt-variant-secondary",
+            )}
+          />
+          <Panel minSize={50} className={styles.center}>
+            <Text>Center</Text>
+          </Panel>
+          <PanelResizeHandle
+            className={clsx(
+              "resize-handle-border-left",
+              "resize-handle-salt-variant-tertiary",
+            )}
+          />
+          <Panel
+            minSize={0}
+            defaultSize={25}
+            className={clsx(
+              styles.center,
+              "resizable-panel-salt-variant-tertiary",
+            )}
+          >
+            <Text>Right</Text>
+          </Panel>
+        </PanelGroup>
+      </FlexLayout>
+    </div>
   );
 }
--- site/src/examples/react-resizable-panels-theme/Vertical.tsx
+++ site/src/examples/react-resizable-panels-theme/Vertical.tsx
@@ -5,20 +5,22 @@ import styles from "./splitter.module.css";
 
 export function Vertical() {
   return (
-    <FlexLayout className={styles.box}>
-      <PanelGroup direction="vertical">
-        <Panel id="top" className={styles.center}>
-          <Text>Top</Text>
-        </Panel>
-        <PanelResizeHandle className="resize-handle-border-top" />
-        <Panel id="center" className={styles.center}>
-          <Text>Center</Text>
-        </Panel>
-        <PanelResizeHandle className="resize-handle-border-bottom" />
-        <Panel id="bottom" className={styles.center}>
-          <Text>Bottom</Text>
-        </Panel>
-      </PanelGroup>
-    </FlexLayout>
+    <div className="react-resizable-panels-theme-salt">
+      <FlexLayout className={styles.box}>
+        <PanelGroup direction="vertical">
+          <Panel id="top" className={styles.center}>
+            <Text>Top</Text>
+          </Panel>
+          <PanelResizeHandle className="resize-handle-border-top" />
+          <Panel id="center" className={styles.center}>
+            <Text>Center</Text>
+          </Panel>
+          <PanelResizeHandle className="resize-handle-border-bottom" />
+          <Panel id="bottom" className={styles.center}>
+            <Text>Bottom</Text>
+          </Panel>
+        </PanelGroup>
+      </FlexLayout>
+    </div>
   );
 }
