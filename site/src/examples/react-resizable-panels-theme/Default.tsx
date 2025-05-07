41c58ccdc99173b0f3b6fc1f388ebbecdbbd1c5f
ImmanuelBaskaran <immanuel.baskaran.16@ucl.ac.uk>
2025-05-07 10:28:58 +0100

    feat(splitter): scope theme to classname react-resizable-panels-theme-salt

--- site/src/examples/react-resizable-panels-theme/Default.tsx
+++ site/src/examples/react-resizable-panels-theme/Default.tsx
@@ -6,61 +6,63 @@ import styles from "./splitter.module.css";
 
 export function Default() {
   return (
-    <FlexLayout className={clsx(styles.box, styles.boxGrey)}>
-      <PanelGroup direction="horizontal">
-        <Panel>
-          <PanelGroup direction="vertical">
-            <Panel
-              className={clsx(
-                styles.center,
-                "resizable-panel-salt-variant-primary",
-              )}
-            >
-              <Text>Top Left</Text>
-            </Panel>
-            <PanelResizeHandle className="" />
-            <Panel
-              className={clsx(
-                styles.center,
-                "resizable-panel-salt-variant-primary",
-              )}
-            >
-              <Text>Middle Left</Text>
-            </Panel>
-            <PanelResizeHandle className="" />
-            <Panel
-              className={clsx(
-                styles.center,
-                "resizable-panel-salt-variant-primary",
-              )}
-            >
-              <Text>Bottom Left</Text>
-            </Panel>
-          </PanelGroup>
-        </Panel>
-        <PanelResizeHandle />
-        <Panel>
-          <PanelGroup direction="vertical">
-            <Panel
-              className={clsx(
-                styles.center,
-                "resizable-panel-salt-variant-primary",
-              )}
-            >
-              <Text>Top Right</Text>
-            </Panel>
-            <PanelResizeHandle className="" />
-            <Panel
-              className={clsx(
-                styles.center,
-                "resizable-panel-salt-variant-primary",
-              )}
-            >
-              <Text>Bottom Right</Text>
-            </Panel>
-          </PanelGroup>
-        </Panel>
-      </PanelGroup>
-    </FlexLayout>
+    <div className="react-resizable-panels-theme-salt">
+      <FlexLayout className={clsx(styles.box, styles.boxGrey)}>
+        <PanelGroup direction="horizontal">
+          <Panel>
+            <PanelGroup direction="vertical">
+              <Panel
+                className={clsx(
+                  styles.center,
+                  "resizable-panel-salt-variant-primary",
+                )}
+              >
+                <Text>Top Left</Text>
+              </Panel>
+              <PanelResizeHandle className="" />
+              <Panel
+                className={clsx(
+                  styles.center,
+                  "resizable-panel-salt-variant-primary",
+                )}
+              >
+                <Text>Middle Left</Text>
+              </Panel>
+              <PanelResizeHandle className="" />
+              <Panel
+                className={clsx(
+                  styles.center,
+                  "resizable-panel-salt-variant-primary",
+                )}
+              >
+                <Text>Bottom Left</Text>
+              </Panel>
+            </PanelGroup>
+          </Panel>
+          <PanelResizeHandle />
+          <Panel>
+            <PanelGroup direction="vertical">
+              <Panel
+                className={clsx(
+                  styles.center,
+                  "resizable-panel-salt-variant-primary",
+                )}
+              >
+                <Text>Top Right</Text>
+              </Panel>
+              <PanelResizeHandle className="" />
+              <Panel
+                className={clsx(
+                  styles.center,
+                  "resizable-panel-salt-variant-primary",
+                )}
+              >
+                <Text>Bottom Right</Text>
+              </Panel>
+            </PanelGroup>
+          </Panel>
+        </PanelGroup>
+      </FlexLayout>
+    </div>
   );
 }
--- site/src/examples/react-resizable-panels-theme/Horizontal.tsx
+++ site/src/examples/react-resizable-panels-theme/Horizontal.tsx
@@ -5,20 +5,22 @@ import styles from "./splitter.module.css";
 
 export function Horizontal() {
   return (
-    <FlexLayout className={styles.box}>
-      <PanelGroup direction="horizontal">
-        <Panel id="left" className={styles.center}>
-          <Text>Left</Text>
-        </Panel>
-        <PanelResizeHandle className="resize-handle-border-right" />
-        <Panel id="center" className={styles.center}>
-          <Text>Center</Text>
-        </Panel>
-        <PanelResizeHandle className="resize-handle-border-leftt" />
-        <Panel id="right" className={styles.center}>
-          <Text>Right</Text>
-        </Panel>
-      </PanelGroup>
-    </FlexLayout>
+    <div className="react-resizable-panels-theme-salt">
+      <FlexLayout className={styles.box}>
+        <PanelGroup direction="horizontal">
+          <Panel id="left" className={styles.center}>
+            <Text>Left</Text>
+          </Panel>
+          <PanelResizeHandle className="resize-handle-border-right" />
+          <Panel id="center" className={styles.center}>
+            <Text>Center</Text>
+          </Panel>
+          <PanelResizeHandle className="resize-handle-border-leftt" />
+          <Panel id="right" className={styles.center}>
+            <Text>Right</Text>
+          </Panel>
+        </PanelGroup>
+      </FlexLayout>
+    </div>
   );
 }
