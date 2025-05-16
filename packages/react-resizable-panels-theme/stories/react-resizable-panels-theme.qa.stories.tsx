7a6a6e63dc1278369a318760347b7b7315617caf
origami-z <5257855+origami-z@users.noreply.github.com>
2025-05-16 09:57:31 +0100

    - Update site title - Fix QA not captured, combine to 1 story - Fix pgk version to be 3.0.0

--- packages/react-resizable-panels-theme/stories/react-resizable-panels-theme.qa.stories.tsx
+++ packages/react-resizable-panels-theme/stories/react-resizable-panels-theme.qa.stories.tsx
@@ -1,4 +1,4 @@
-import { FlexLayout, StackLayout, Text } from "@salt-ds/core";
+import { Text } from "@salt-ds/core";
 import { QAContainer } from "docs/components";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
@@ -10,96 +10,96 @@ export default {
   subcomponents: { Panel, PanelResizeHandle },
 };
 
-export function Horizontal() {
+export function Variants() {
   return (
-    <QAContainer>
-      <StackLayout className={"react-resizable-panels-theme-salt"}>
-        <PanelGroup
-          direction="horizontal"
-          style={{
-            width: 240,
-            height: 120,
-            boxSizing: "border-box",
-            border: "1px solid lightgrey",
-          }}
-        >
-          <Panel>
-            <Text>Panel 1</Text>
-          </Panel>
-          <PanelResizeHandle className="resize-handle-salt-border-left resize-handle-salt-border-right" />
-          <Panel>
-            <Text>Panel 2</Text>
-          </Panel>
-          <PanelResizeHandle className="resize-handle-salt-border-left resize-handle-salt-border-right" />
-          <Panel>
-            <Text>Panel 3</Text>
-          </Panel>
-        </PanelGroup>
-      </StackLayout>
-    </QAContainer>
-  );
-}
-
-export function Vertical() {
-  return (
-    <QAContainer>
-      <FlexLayout
+    <QAContainer className="react-resizable-panels-theme-salt">
+      <PanelGroup
+        direction="horizontal"
         style={{
           width: 240,
           height: 120,
           boxSizing: "border-box",
           border: "1px solid lightgrey",
         }}
-        className="react-resizable-panels-theme-salt"
       >
-        <PanelGroup direction="vertical">
-          <Panel>
-            <Text>Panel 1</Text>
-          </Panel>
-          <PanelResizeHandle className="resize-handle-salt-border-top resize-handle-salt-border-bottom" />
-          <Panel>
-            <Text>Panel 2</Text>
-          </Panel>
-          <PanelResizeHandle className="resize-handle-salt-border-top resize-handle-salt-border-bottom" />
-          <Panel>
-            <Text>Panel 3</Text>
-          </Panel>
-        </PanelGroup>
-      </FlexLayout>
+        <Panel>
+          <Text>Panel 1</Text>
+        </Panel>
+        <PanelResizeHandle className="resize-handle-salt-border-left resize-handle-salt-border-right" />
+        <Panel>
+          <Text>Panel 2</Text>
+        </Panel>
+        <PanelResizeHandle className="resize-handle-salt-border-left resize-handle-salt-border-right" />
+        <Panel>
+          <Text>Panel 3</Text>
+        </Panel>
+      </PanelGroup>
+      <PanelGroup
+        direction="vertical"
+        style={{
+          width: 240,
+          height: 120,
+          boxSizing: "border-box",
+          border: "1px solid lightgrey",
+        }}
+      >
+        <Panel>
+          <Text>Panel 1</Text>
+        </Panel>
+        <PanelResizeHandle className="resize-handle-salt-border-top resize-handle-salt-border-bottom" />
+        <Panel>
+          <Text>Panel 2</Text>
+        </Panel>
+        <PanelResizeHandle className="resize-handle-salt-border-top resize-handle-salt-border-bottom" />
+        <Panel>
+          <Text>Panel 3</Text>
+        </Panel>
+      </PanelGroup>
+
+      <PanelGroup
+        direction="horizontal"
+        style={{
+          width: 480,
+          height: 120,
+          boxSizing: "border-box",
+          border: "1px solid lightgrey",
+          // Background color to test transparent panels
+          backgroundColor: "var(--salt-accent-background)",
+        }}
+      >
+        <Panel className="resizable-panel-salt-variant-primary">
+          <Text>Primary</Text>
+        </Panel>
+        <PanelResizeHandle className="resize-handle-salt-border-right resize-handle-salt-variant-primary" />
+        <Panel className="resizable-panel-salt-variant-secondary">
+          <Text>Secondary</Text>
+        </Panel>
+        <PanelResizeHandle className="resize-handle-salt-border-right resize-handle-salt-variant-secondary" />
+        <Panel className="resizable-panel-salt-variant-tertiary">
+          <Text>Tertiary</Text>
+        </Panel>
+        <PanelResizeHandle className="resize-handle-salt-border-right resize-handle-salt-variant-tertiary" />
+        <Panel>
+          <Text>Transparent</Text>
+        </Panel>
+      </PanelGroup>
     </QAContainer>
   );
 }
 
-export function Variant() {
-  return (
-    <QAContainer>
-      <FlexLayout className="react-resizable-panels-theme-salt">
-        <PanelGroup
-          direction="horizontal"
-          style={{
-            width: 480,
-            height: 120,
-            boxSizing: "border-box",
-            border: "1px solid lightgrey",
-          }}
-        >
-          <Panel className="resizable-panel-salt-variant-primary">
-            <Text>Primary</Text>
-          </Panel>
-          <PanelResizeHandle className="resize-handle-salt-border-right resize-handle-salt-variant-primary" />
-          <Panel className="resizable-panel-salt-variant-secondary">
-            <Text>Secondary</Text>
-          </Panel>
-          <PanelResizeHandle className="resize-handle-salt-border-right resize-handle-salt-variant-secondary" />
-          <Panel className="resizable-panel-salt-variant-tertiary">
-            <Text>Tertiary</Text>
-          </Panel>
-          <PanelResizeHandle className="resize-handle-salt-border-right resize-handle-salt-variant-tertiary" />
-          <Panel>
-            <Text>Transparent</Text>
-          </Panel>
-        </PanelGroup>
-      </FlexLayout>
-    </QAContainer>
-  );
-}
+Variants.parameters = {
+  chromatic: {
+    disableSnapshot: false,
+    modes: {
+      theme: {
+        themeNext: "disable",
+      },
+      themeNext: {
+        themeNext: "enable",
+        corner: "rounded",
+        accent: "teal",
+        // Ignore headingFont given font is not loaded
+      },
+    },
+  },
+};
