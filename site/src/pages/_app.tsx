2afd8caa2f8e7f19a96472ebcdcca696bddc1043
libertymayc <liberty.curtis@live.co.uk>
2025-05-19 16:21:34 -0400

    add brand prop to salt provider

--- site/src/pages/_app.tsx
+++ site/src/pages/_app.tsx
@@ -8,13 +8,13 @@ import * as saltComponents from "../components";
 import { amplitude, openSans, ptMono } from "../fonts";
 import * as saltLayouts from "../layouts";
 import { LayoutProvider } from "../layouts/LayoutProvider";
+import { Metadata } from "../Metadata/index";
 import type { MyAppProps } from "../types/mosaic";
 
 import "@salt-ds/theme/css/theme-next.css";
 import "@salt-ds/theme/index.css";
 
 import "../css/index.css";
-import { Metadata } from "../Metadata/index";
 
 const components = {
   ...saltComponents,
