c6dd3853508bd892a7d9a47f24fea06095ada5e2
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2025-06-05 10:46:26 +0100

    Initial Vertical Navigation work

---
+++ packages/lab/src/vertical-navigation/index.ts
@@ -0,0 +1,6 @@
+export * from "./VerticalNavigation";
+export * from "./VerticalNavigationItem";
+export * from "./VerticalNavigationGroup";
+export * from "./VerticalNavigationGroupContent";
+export * from "./VerticalNavigationGroupTrigger";
+export * from "./VerticalNavigationItemExpansionIcon";
