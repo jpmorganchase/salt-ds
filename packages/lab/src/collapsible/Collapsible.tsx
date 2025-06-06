33ee9061657d32e8f59f5a56b77ccf7c9e809c41
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2025-06-06 09:42:51 +0100

    Initial Collapsible work

---
+++ packages/lab/src/collapsible/Collapsible.tsx
@@ -0,0 +1,72 @@
+import {
+  type ComponentPropsWithoutRef,
+  forwardRef,
+  type SyntheticEvent,
+  useCallback,
+  useMemo,
+} from "react";
+import { makePrefixer, useControlled } from "@salt-ds/core";
+import { CollapsibleContext } from "./CollapsibleContext";
+import { clsx } from "clsx";
+
+export interface CollapsibleProps
+  extends Omit<ComponentPropsWithoutRef<"div">, "onToggle"> {
+  /**
+   * Whether the accordion is expanded.
+   */
+  expanded?: boolean;
+  /**
+   * Whether the accordion is expanded by default.
+   */
+  defaultExpanded?: boolean;
+  /**
+   * Callback fired when the accordion is toggled.
+   */
+  onToggle?: (
+    event: SyntheticEvent<HTMLButtonElement>,
+    expanded: boolean,
+  ) => void;
+}
+
+const withBaseName = makePrefixer("saltCollapsible");
+
+export const Collapsible = forwardRef<HTMLDivElement, CollapsibleProps>(
+  function Collapsible(props, ref) {
+    const {
+      className,
+      expanded: expandedProp,
+      defaultExpanded,
+      onToggle,
+      ...rest
+    } = props;
+
+    const [expanded, setExpandedState] = useControlled({
+      default: Boolean(defaultExpanded),
+      controlled: expandedProp,
+      name: "Collapsible",
+      state: "expanded",
+    });
+
+    const setExpanded = useCallback(
+      (event: SyntheticEvent<HTMLButtonElement>, newExpanded: boolean) => {
+        setExpandedState(newExpanded);
+        onToggle?.(event, newExpanded);
+      },
+      [onToggle],
+    );
+
+    const contextValue = useMemo(
+      () => ({
+        expanded,
+        setExpanded,
+      }),
+      [expanded, setExpanded],
+    );
+
+    return (
+      <CollapsibleContext.Provider value={contextValue}>
+        <div className={clsx(withBaseName(), className)} ref={ref} {...rest} />
+      </CollapsibleContext.Provider>
+    );
+  },
+);
