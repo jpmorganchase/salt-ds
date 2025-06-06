85ec094ca5b3e8ab6e9c8af4ab71d65536eb34dd
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2025-06-06 09:42:51 +0100

    Initial Collapsible work

---
+++ packages/lab/src/collapsible/CollapsibleContext.ts
@@ -0,0 +1,28 @@
+import { type SyntheticEvent, useContext } from "react";
+import { createContext } from "@salt-ds/core";
+
+export type CollapsibleContextValue = {
+  expanded: boolean;
+  setExpanded: (
+    event: SyntheticEvent<HTMLButtonElement>,
+    expanded: boolean,
+  ) => void;
+  disabled?: boolean;
+  panelId?: string;
+  setPanelId?: (panelId: string) => void;
+};
+
+export const CollapsibleContext = createContext<CollapsibleContextValue>(
+  "CollapsibleContext",
+  {
+    expanded: false,
+    setExpanded: () => {},
+    disabled: false,
+    panelId: undefined,
+    setPanelId: () => {},
+  },
+);
+
+export function useCollapsibleContext() {
+  return useContext(CollapsibleContext);
+}
