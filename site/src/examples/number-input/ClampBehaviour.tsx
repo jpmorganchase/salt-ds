438a4881c3b9297cf33c04e3fd51557bd082cfaf
tayyabataimur <19307765+tayyabataimur@users.noreply.github.com>
2025-06-18 13:24:49 +0100

    fixup! Update number input

--- site/src/examples/number-input/ClampBehaviour.tsx
+++ site/src/examples/number-input/ClampBehaviour.tsx
@@ -5,22 +5,8 @@ export const ClampBehaviour = () => {
   return (
     <StackLayout style={{ width: "256px" }}>
       <FormField>
-        <FormFieldLabel>Number input with default clamping</FormFieldLabel>
-        <NumberInput
-          defaultValue={5}
-          max={100}
-          min={0}
-          clampBehaviour="default"
-        />
-      </FormField>
-      <FormField>
-        <FormFieldLabel>Number input with strict clamping</FormFieldLabel>
-        <NumberInput
-          defaultValue={5}
-          max={100}
-          min={0}
-          clampBehaviour="strict"
-        />
+        <FormFieldLabel>Number input with clamping, max = 100</FormFieldLabel>
+        <NumberInput defaultValue={5} max={100} clampValue />
       </FormField>
     </StackLayout>
   );
