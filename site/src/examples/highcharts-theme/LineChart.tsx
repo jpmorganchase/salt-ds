9b09bd23277489413185bef70c67e9f956a00f14
Tymon Rybak <tymon.rybak@jpmorgan.com>
2025-06-20 09:30:24 +0100

    type the example

--- site/src/examples/highcharts-theme/LineChart.tsx
+++ site/src/examples/highcharts-theme/LineChart.tsx
@@ -1,11 +1,11 @@
-import Highcharts from "highcharts";
+import Highcharts, { type Options } from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
 import accessibility from "highcharts/modules/accessibility";
 
 accessibility(Highcharts);
 
-const lineDataOptions = {
+const lineDataOptions: Options = {
   chart: {
     type: "line",
   },
@@ -50,78 +50,97 @@ const lineDataOptions = {
     {
       name: "USD",
       data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      type: "line",
     },
     {
       name: "EUR",
       data: [5, 6, 7, 6, 5, 4, 3, 4, 5, 6],
+      type: "line",
     },
     {
       name: "CAD",
       data: [9, 8, 7, 6, 5, 4, 3, 2, 1, 0],
+      type: "line",
     },
     {
       name: "AUD",
       data: [2, 3, 4, 5, 6, 7, 8, 7, 6, 5],
+      type: "line",
     },
     {
       name: "GBP",
       data: [8, 7, 6, 5, 4, 3, 2, 3, 4, 5],
+      type: "line",
     },
     {
       name: "JPY",
       data: [3, 4, 5, 6, 7, 8, 7, 6, 5, 4],
+      type: "line"
     },
     {
       name: "CHF",
       data: [7, 6, 5, 4, 3, 2, 3, 4, 5, 6],
+      type: "line"
     },
     {
       name: "NZD",
       data: [2, 4, 6, 8, 6, 4, 2, 4, 6, 8],
+      type: "line"
     },
     {
       name: "SEK",
       data: [5, 5.5, 6, 5.5, 5, 4.5, 4, 4.5, 5, 5.5],
+      type: "line"
     },
     {
       name: "NOK",
       data: [1, 3, 5, 7, 9, 7, 5, 3, 1, 3],
+      type: "line"
     },
     {
       name: "SGD",
       data: [6, 5, 4, 3, 2, 3, 4, 5, 6, 7],
+      type: "line"
     },
     {
       name: "MXN",
       data: [4, 5, 6, 7, 8, 7, 6, 5, 4, 3],
+      type: "line"
     },
     {
       name: "ZAR",
       data: [3, 4, 5, 6, 7, 6, 5, 4, 3, 2],
+      type: "line"
     },
     {
       name: "INR",
       data: [7, 8, 9, 8, 7, 6, 5, 6, 7, 8],
+      type: "line"
     },
     {
       name: "BRL",
       data: [2, 3, 2, 3, 2, 3, 2, 3, 2, 3],
+      type: "line"
     },
     {
       name: "CNY",
       data: [8, 7, 6, 5, 4, 5, 6, 7, 8, 9],
+      type: "line"
     },
     {
       name: "KRW",
       data: [5, 6, 5, 4, 5, 6, 5, 4, 5, 6],
+      type: "line"
     },
     {
       name: "TRY",
       data: [3, 5, 7, 5, 3, 5, 7, 5, 3, 5],
+      type: "line"
     },
     {
       name: "PLN",
       data: [4, 4.5, 5, 5.5, 6, 5.5, 5, 4.5, 4, 4.5],
+      type: "line"
     },
   ],
 };
