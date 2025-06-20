0c6133322a586986b0c1ad31763bf7498e7856f2
tayyabataimur <19307765+tayyabataimur@users.noreply.github.com>
2025-06-20 17:37:52 +0100

    Caret position

--- site/src/examples/number-input/Decimals.tsx
+++ site/src/examples/number-input/Decimals.tsx
@@ -7,14 +7,5 @@ export const Decimals = () => (
       <FormFieldLabel>Number input</FormFieldLabel>
       <NumberInput defaultValue={100} step={0.01} endAdornment="USD" />
     </FormField>
-    <FormField style={{ width: "256px" }}>
-      <FormFieldLabel>Number input with strict clamping</FormFieldLabel>
-      <NumberInput
-        defaultValue={100}
-        step={0.01}
-        startAdornment="£"
-        clampBehaviour="strict"
-      />
-    </FormField>
   </StackLayout>
 );
