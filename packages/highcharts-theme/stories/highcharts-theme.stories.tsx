3425496e11b8d831c8917d59ea0b59da41b83fd5
Tymon Rybak <tymon.rybak@jpmorgan.com>
2025-06-25 12:40:32 +0100

    organise charts storybook to match ag-grid

---
+++ packages/highcharts-theme/stories/highcharts-theme.stories.tsx
@@ -0,0 +1,28 @@
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import accessibility from "highcharts/modules/accessibility";
+import type { Decorator } from "@storybook/react-vite";
+
+import "../index.css";
+
+import { saltHCThemeOptions } from "../src";
+
+accessibility(Highcharts);
+
+const withHighchartsTheme: Decorator = (Story) => {
+  Highcharts.setOptions(saltHCThemeOptions);
+  return <Story />;
+};
+
+export default {
+  title: "Highcharts/Highcharts Theme",
+  component: HighchartsReact,
+  decorators: [withHighchartsTheme],
+  parameters: {
+    // chromatic: {
+    //   disableSnapshot: false,
+    // },
+  },
+};
+
+export { LineChart } from "../src/examples";
