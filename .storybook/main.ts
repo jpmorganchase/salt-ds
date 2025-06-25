58e3409a11fb77ba34fcd37f5f772b0483b592b8
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2025-06-25 16:30:05 +0100

    Update ESM

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,4 @@
+import { createRequire } from "node:module";
 import { dirname, join } from "node:path";
 import type { StorybookConfig } from "@storybook/react-vite";
 import { cssInline } from "css-inline-plugin";
@@ -5,6 +6,7 @@ import remarkGfm from "remark-gfm";
 import type { UserConfig } from "vite";
 import { typescriptTurbosnap } from "vite-plugin-typescript-turbosnap";
 
+const require = createRequire(import.meta.url);
 const config: StorybookConfig = {
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
@@ -58,7 +60,7 @@ const config: StorybookConfig = {
   },
 };
 
-module.exports = config;
+export default config;
 
 function getAbsolutePath<Value>(value: string): Value {
   return dirname(
