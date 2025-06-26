131b856e25a792a651859fadc4db130cd24cdb37
tayyabataimur <tayyaba.taimur@jpmorgan.com>
2025-06-26 14:02:22 +0100

    Improve decimal places docs

--- site/src/examples/number-input/Decimals.tsx
+++ site/src/examples/number-input/Decimals.tsx
@@ -4,8 +4,14 @@ import { NumberInput } from "@salt-ds/lab";
 export const Decimals = () => (
   <StackLayout>
     <FormField style={{ width: "256px" }}>
-      <FormFieldLabel>Number input</FormFieldLabel>
+      <FormFieldLabel>
+        Number input with auto-calculated decimal scale
+      </FormFieldLabel>
       <NumberInput defaultValue={100} step={0.01} endAdornment="USD" />
+    </FormField>
+    <FormField style={{ width: "256px" }}>
+      <FormFieldLabel>Number input with set decimal scale</FormFieldLabel>
+      <NumberInput defaultValue={100} decimalScale={2} endAdornment="USD" />
     </FormField>
   </StackLayout>
 );
