f7b372be93eca5964069382f72506d394f6af29e
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2025-07-08 08:23:16 +0100

    Update

--- packages/lab/src/__tests__/__e2e__/system-status/SystemStatus.cy.tsx
+++ packages/lab/src/__tests__/__e2e__/system-status/SystemStatus.cy.tsx
@@ -18,7 +18,7 @@ describe("GIVEN a System status", () => {
   it('should render a success status when `status="success"`', () => {
     cy.mount(<Success />);
 
-    cy.findByTestId("SuccessTickIcon").should("exist");
+    cy.findByTestId("CheckmarkIcon").should("exist");
   });
   it('should render a warning status when `status="warning"`', () => {
     cy.mount(<Warning />);
