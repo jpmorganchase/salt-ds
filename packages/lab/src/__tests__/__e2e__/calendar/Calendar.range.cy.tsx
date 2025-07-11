9dc84eb545eaa0e67ca16a1f45a9a8aaabe12770
mark-tate <143323+mark-tate@users.noreply.github.com>
2025-07-11 13:51:34 +0100

    Update packages/lab/src/__tests__/__e2e__/calendar/Calendar.range.cy.tsx
    Co-authored-by: Josh Wooding <12938082+joshwooding@users.noreply.github.com>

--- packages/lab/src/__tests__/__e2e__/calendar/Calendar.range.cy.tsx
+++ packages/lab/src/__tests__/__e2e__/calendar/Calendar.range.cy.tsx
@@ -30,7 +30,7 @@ const adapterMoment = new AdapterMoment();
 
 const adapters = [adapterDateFns, adapterDayjs, adapterLuxon, adapterMoment];
 
-describe('GIVEN a Calendar with `selectionVariant="range"', () => {
+describe('GIVEN a Calendar with `selectionVariant="range"`', () => {
   adapters.forEach((adapter: SaltDateAdapter<DateFrameworkType>) => {
     describe(`Tests with ${adapter.lib}`, () => {
       beforeEach(() => {
