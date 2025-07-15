f000166c578be9ceff1b6860eb757cdd269242b3
tayyabataimur <tayyaba.taimur@jpmorgan.com>
2025-07-15 13:28:46 +0100

    Clean up

--- packages/lab/stories/number-input/number-input.stories.tsx
+++ packages/lab/stories/number-input/number-input.stories.tsx
@@ -501,7 +501,7 @@ export const UncontrolledFormatting: StoryFn<NumberInputProps> = (args) => {
   );
 };
 
-export const Fractions: StoryFn<NumberInputProps> = () => {
+export const Fractions: StoryFn<NumberInputProps> = (args) => {
   const [val, setVal] = useState(100.03125);
 
   return (
@@ -517,6 +517,7 @@ export const Fractions: StoryFn<NumberInputProps> = () => {
         }}
         onChange={(e, v) => setVal(v)}
         value={val}
+        {...args}
       />
       <Text>Returned value: {val}</Text>
     </StackLayout>
