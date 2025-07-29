6ff7cd8099cd18e83be1c86aeef25b62d47403f8
Tymon <78102189+tycup@users.noreply.github.com>
2025-07-29 10:11:16 +0100

    Highcharts Theme | LineChart (#5246)
    Co-authored-by: origami-z <5257855+origami-z@users.noreply.github.com>
    Co-authored-by: Josh Wooding <12938082+joshwooding@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Liberty Curtis <91206958+libertymayc@users.noreply.github.com>
    Co-authored-by: mark-tate <143323+mark-tate@users.noreply.github.com>

---
+++ packages/highcharts-theme/stories/highcharts-theme.stories.tsx
@@ -0,0 +1,29 @@
+import type { Decorator } from "@storybook/react-vite";
+import Highcharts from "highcharts";
+import accessibility from "highcharts/modules/accessibility";
+import HighchartsReact from "highcharts-react-official";
+
+import "@salt-ds/highcharts-theme/stories/highcharts-default.css";
+import "@salt-ds/highcharts-theme/index.css";
+
+import { highchartsOptionsSalt } from "@salt-ds/highcharts-theme";
+
+accessibility(Highcharts);
+
+const withHighchartsTheme: Decorator = (Story) => {
+  Highcharts.setOptions(highchartsOptionsSalt);
+  return <Story />;
+};
+
+export default {
+  title: "Highcharts/Highcharts Theme",
+  component: HighchartsReact,
+  decorators: [withHighchartsTheme],
+  parameters: {
+    chromatic: {
+      disableSnapshot: false,
+    },
+  },
+};
+
+export { LineChart } from "../src/examples";
