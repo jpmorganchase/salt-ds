29bd3b4a88d9f02d64ba0c1a07f7b3653136f0d0
Jake <93048400+jake-costa@users.noreply.github.com>
2025-08-06 12:55:48 -0500

    spelling fix

--- site/src/components/app-header/AppHeader.tsx
+++ site/src/components/app-header/AppHeader.tsx
@@ -46,7 +46,7 @@ export const AppHeader = () => {
         <div className={styles.actions}>
           <Search className={styles.search} />
           {!isMobileOrTablet && (
-            <Tooltip aria-hiden="true" content="GitHub repository" placement="bottom">
+            <Tooltip aria-hidden="true" content="GitHub repository" placement="bottom">
               <CTALink
                 href="https://github.com/jpmorganchase/salt-ds"
                 aria-label="GitHub repository"
