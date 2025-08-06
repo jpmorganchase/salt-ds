bb7ac829ffe28e7c5d8ca0347ef2ac9a81022b35
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2025-08-06 20:51:42 +0100

    Update site/docs/components/table/examples.mdx

--- site/src/examples/table/TextAlignment.tsx
+++ site/src/examples/table/TextAlignment.tsx
@@ -1,4 +1,4 @@
-import { Table, TBody, TD, TH, THead, TR } from "@salt-ds/lab";
+import { Table, TBody, TD, TFoot, TH, THead, TR } from "@salt-ds/lab";
 import type { ReactElement } from "react";
 
 export const TextAlignment = (): ReactElement => {
@@ -6,20 +6,47 @@ export const TextAlignment = (): ReactElement => {
     <Table>
       <THead>
         <TR>
-          <TH>City</TH>
-          <TH textAlign="right">Population</TH>
+          {Array.from({ length: 3 }, (arrItem, i) => {
+            return (
+              <TH textAlign={i === 0 ? "left" : "right"} key={`col-${arrItem}`}>
+                Column {i + 1}
+              </TH>
+            );
+          })}
         </TR>
       </THead>
       <TBody>
-        <TR>
-          <TD>London</TD>
-          <TD textAlign="right">9.8 million</TD>
-        </TR>
-        <TR>
-          <TD>New York</TD>
-          <TD textAlign="right">8.8 million</TD>
-        </TR>
+        {Array.from({ length: 5 }, (arrItem, x) => {
+          return (
+            <TR key={`tr-${arrItem}`}>
+              {Array.from({ length: 3 }, (nestedArrItem, i) => {
+                return (
+                  <TD
+                    textAlign={i === 0 ? "left" : "right"}
+                    key={`td-${nestedArrItem}`}
+                  >
+                    Row {x + 1}
+                  </TD>
+                );
+              })}
+            </TR>
+          );
+        })}
       </TBody>
+      <TFoot>
+        <TR>
+          {Array.from({ length: 3 }, (arrItem, i) => {
+            return (
+              <TD
+                textAlign={i === 0 ? "left" : "right"}
+                key={`footer-${arrItem}`}
+              >
+                Footer {i + 1}
+              </TD>
+            );
+          })}
+        </TR>
+      </TFoot>
     </Table>
   );
 };
