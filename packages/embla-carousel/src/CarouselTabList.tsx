f9d061c4a525f473eff62e9f9b1bdd897a1635a4
Jake <93048400+jake-costa@users.noreply.github.com>
2025-08-26 13:52:55 -0500

    spelling

--- packages/embla-carousel/src/CarouselTabList.tsx
+++ packages/embla-carousel/src/CarouselTabList.tsx
@@ -86,7 +86,7 @@ export const CarouselTabList = forwardRef<HTMLDivElement, CarouselTabListProps>(
     return (
       <div
         role="tablist"
-        aria-label="Choose slide to display."
+        aria-label="Choose slide to display"
         className={clsx(withBaseName(), className)}
         onKeyDown={handleKeyDown}
         ref={ref}
