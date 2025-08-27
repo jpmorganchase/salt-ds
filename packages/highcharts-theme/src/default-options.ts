79a0e7aa39145ae2e3d7594f674f539d2254dd48
Tymon Rybak <tymon.rybak@jpmorgan.com>
2025-08-27 10:41:55 +0100

    Refactor Highcharts useChart hook
    - Ensure chart properties are density sensitive
    - Take token values from DOM where possible
    - Remove prior implementation which requires setting
    global options
    - Improve documentation and examples

---
+++ packages/highcharts-theme/src/default-options.ts
@@ -0,0 +1,61 @@
+import type { Density } from "@salt-ds/core";
+import type { Options } from "highcharts";
+import { getDensityTokenMap } from "./density-token-map";
+import { saltPatternDef } from "./patterns";
+import type { HighchartsOptionsCompat } from "./types";
+
+export const getDefaultOptions = (
+  density: Density,
+  hostElement?: Element | null,
+): Options => {
+  const tokens = getDensityTokenMap(density, hostElement ?? undefined);
+
+  const defaultOptions: HighchartsOptionsCompat = {
+    chart: {
+      styledMode: true,
+      colorCount: 20,
+    },
+    defs: saltPatternDef,
+    title: {
+      margin: tokens["--salt-spacing-200"],
+    },
+    yAxis: {
+      title: {
+        margin: tokens["--salt-spacing-200"],
+      },
+    },
+    xAxis: {
+      title: {
+        margin: tokens["--salt-spacing-200"],
+      },
+    },
+    legend: {
+      layout: "vertical",
+      align: "right",
+      verticalAlign: "top",
+      symbolWidth: tokens["--salt-size-icon"],
+      symbolHeight: tokens["--salt-size-icon"],
+      symbolRadius: tokens["--salt-palette-corner-weaker"],
+      itemMarginBottom: tokens["--salt-spacing-150"],
+      margin: tokens["--salt-spacing-150"],
+    },
+    plotOptions: {
+      line: {
+        marker: {
+          enabled: false,
+        },
+      },
+      pie: {
+        borderRadius: 0,
+        dataLabels: {
+          distance: 6,
+          format:
+            '{point.name} <span class="value">{point.percentage:.1f}%</span>',
+          enabled: true,
+        },
+      },
+    },
+  };
+
+  return defaultOptions;
+};
