64a61c78361f14d48955cb5017fb4f24567dca55
Tymon Rybak <tymon.rybak@jpmorgan.com>
2025-08-27 10:41:55 +0100

    Add Stacked Bar Chart
    - Implement stacked bar chart component
    - Add configurable chart options and controls
    - Include storybook examples and documentation

--- packages/highcharts-theme/src/examples/index.ts
+++ packages/highcharts-theme/src/examples/index.ts
@@ -1,3 +1,4 @@
 export { default as DonutChart } from "./DonutChart";
 export { default as LineChart } from "./LineChart";
 export { default as PieChart } from "./PieChart";
+export { default as StackedBarChart } from "./StackedBarChart";
--- site/src/examples/highcharts-theme/index.ts
+++ site/src/examples/highcharts-theme/index.ts
@@ -1,3 +1,4 @@
 export * from "./DonutChart";
 export * from "./LineChart";
 export * from "./PieChart";
+export * from "./StackedBarChart";
