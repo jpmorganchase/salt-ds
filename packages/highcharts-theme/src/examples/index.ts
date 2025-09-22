0d3fa7be417bd07e25b6311c95a2c5259e3f143a
Tymon <78102189+tycup@users.noreply.github.com>
2025-09-22 14:59:07 +0100

    Add Column Chart
    prevent opacity on hover and v11 fixes

--- packages/highcharts-theme/src/examples/index.ts
+++ packages/highcharts-theme/src/examples/index.ts
@@ -1,5 +1,6 @@
 export { default as BarChart } from "./BarChart";
 export { default as BulletChart } from "./BulletChart";
+export { default as ColumnChart } from "./ColumnChart";
 export { default as DonutChart } from "./DonutChart";
 export { default as LineChart } from "./LineChart";
 export { default as PieChart } from "./PieChart";
