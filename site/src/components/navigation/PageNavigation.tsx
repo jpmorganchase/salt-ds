4d109cf3888b476460c6167c9df48172415665d4
Wemmy <wemmy.oyedeji@jpmorgan.com>
2025-10-01 09:45:46 +0100

    Action PR comments

--- site/src/components/navigation/PageNavigation.tsx
+++ site/src/components/navigation/PageNavigation.tsx
@@ -1,15 +1,12 @@
 import { useSidebar, useStore } from "@jpmorganchase/mosaic-store";
 import { VerticalNavigation } from "./vertical-navigation";
+import type { SidebarNodeWithStatus } from "./vertical-navigation/utils";
 
 export function PageNavigation() {
-  const menu = useStore((data) => data?.sharedConfig?.sidebar);
-  const { selectedNodeId, selectedGroupIds } = useSidebar();
-  return (
-    <VerticalNavigation
-      // @ts-expect-error
-      menu={menu}
-      selectedNodeId={selectedNodeId}
-      selectedGroupIds={selectedGroupIds}
-    />
-  );
+  const menu = useStore(
+    (data) => data?.sharedConfig?.sidebar,
+  ) as unknown as SidebarNodeWithStatus[];
+
+  const { selectedNodeId } = useSidebar();
+  return <VerticalNavigation menu={menu} selectedNodeId={selectedNodeId} />;
 }
