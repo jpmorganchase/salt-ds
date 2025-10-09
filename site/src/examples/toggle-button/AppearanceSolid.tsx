9e79e82f3937af8b47eaba151298f340fa5a2c5d
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2025-10-09 12:11:47 +0100

    Improve docs

---
+++ site/src/examples/toggle-button/AppearanceSolid.tsx
@@ -0,0 +1,6 @@
+import { FlowLayout, ToggleButton } from "@salt-ds/core";
+import type { ReactElement } from "react";
+
+export const AppearanceSolid = (): ReactElement => (
+  <ToggleButton value="solid">Toggle</ToggleButton>
+);
