cc45034e0db00b20115e9284362a85d68c435b75
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2025-10-10 08:32:43 +0100

    Update toggle button docs

--- site/src/examples/toggle-button/ToggleButtonGroupVertical.tsx
+++ site/src/examples/toggle-button/ToggleButtonGroupVertical.tsx
@@ -1,4 +1,9 @@
-import { FlowLayout, ToggleButton, ToggleButtonGroup } from "@salt-ds/core";
+import {
+  FlowLayout,
+  ToggleButton,
+  ToggleButtonGroup,
+  Tooltip,
+} from "@salt-ds/core";
 import {
   SemanticDissatisfiedIcon,
   SemanticNeutralIcon,
@@ -33,21 +38,31 @@ export const ToggleButtonGroupVertical = (): ReactElement => (
       </ToggleButton>
     </ToggleButtonGroup>
     <ToggleButtonGroup defaultValue="4" orientation="vertical">
-      <ToggleButton value="5">
-        <SemanticVerySatisfiedIcon aria-hidden />
-      </ToggleButton>
-      <ToggleButton value="4">
-        <SemanticSatisfiedIcon aria-hidden />
-      </ToggleButton>
-      <ToggleButton value="3">
-        <SemanticNeutralIcon aria-hidden />
-      </ToggleButton>
-      <ToggleButton value="2">
-        <SemanticDissatisfiedIcon aria-hidden />
-      </ToggleButton>
-      <ToggleButton value="1">
-        <SemanticVeryDissatisfiedIcon aria-hidden />
-      </ToggleButton>
+      <Tooltip content="Very satisfied">
+        <ToggleButton value="5">
+          <SemanticVerySatisfiedIcon aria-hidden />
+        </ToggleButton>
+      </Tooltip>
+      <Tooltip content="Satisfied">
+        <ToggleButton value="4">
+          <SemanticSatisfiedIcon aria-hidden />
+        </ToggleButton>
+      </Tooltip>
+      <Tooltip content="Neutral">
+        <ToggleButton value="3">
+          <SemanticNeutralIcon aria-hidden />
+        </ToggleButton>
+      </Tooltip>
+      <Tooltip content="Dissatisfied">
+        <ToggleButton value="2">
+          <SemanticDissatisfiedIcon aria-hidden />
+        </ToggleButton>
+      </Tooltip>
+      <Tooltip content="Very dissatisfied">
+        <ToggleButton value="1">
+          <SemanticVeryDissatisfiedIcon aria-hidden />
+        </ToggleButton>
+      </Tooltip>
     </ToggleButtonGroup>
   </FlowLayout>
 );
