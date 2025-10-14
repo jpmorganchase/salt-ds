51d6f33584a8772ed3ad967301f04212ee4abba7
Wemmy <wemmy.oyedeji@jpmorgan.com>
2025-10-14 15:14:17 +0100

    Add cancel and success dialogs

--- packages/core/stories/patterns/wizard/wizard.stories.tsx
+++ packages/core/stories/patterns/wizard/wizard.stories.tsx
@@ -25,6 +25,7 @@ import {
   useResponsiveProp,
 } from "@salt-ds/core";
 import type { Meta } from "@storybook/react-vite";
+import { SuccessCircleSolidIcon } from "packages/icons/src";
 import {
   DatePicker,
   DatePickerOverlay,
@@ -36,6 +37,92 @@ import { type ElementType, useState } from "react";
 export default {
   title: "Patterns/Wizard",
 } as Meta;
+
+const CancelWarningDialog = ({
+  open,
+  onOpenChange,
+}: {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}) => {
+  const direction: StackLayoutProps<ElementType>["direction"] =
+    useResponsiveProp(
+      {
+        xs: "column",
+        sm: "row",
+      },
+      "row",
+    );
+
+  const handleClose = () => {
+    onOpenChange(false);
+  };
+
+  const no = (
+    <Button appearance="bordered" sentiment="accented" onClick={handleClose}>
+      No
+    </Button>
+  );
+
+  const yes = (
+    <Button sentiment="accented" onClick={handleClose}>
+      Yes
+    </Button>
+  );
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={onOpenChange}
+      status="warning"
+      size="small"
+    >
+      <DialogHeader header="Are you sure you want to cancel" />
+      <DialogContent>
+        Any updates you've made so far will be lost after you confirm
+        cancellation.
+      </DialogContent>
+      <DialogActions>
+        {direction === "column" ? (
+          <StackLayout gap={1} style={{ width: "100%" }}>
+            {yes}
+            {no}
+          </StackLayout>
+        ) : (
+          <FlexLayout gap={1}>
+            {no}
+            {yes}
+          </FlexLayout>
+        )}
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+const AccountCreatedSuccessDialog = ({
+  open,
+  onOpenChange,
+}: {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}) => (
+  <Dialog open={open} onOpenChange={onOpenChange} size="small" status="success">
+    <DialogHeader header="Account created" />
+    <DialogContent>You can now start using this new account.</DialogContent>
+    <DialogActions>
+      <Button sentiment="accented" onClick={() => onOpenChange(false)}>
+        Done
+      </Button>
+    </DialogActions>
+  </Dialog>
+);
+
+const AccountCreatedContent = () => (
+  <StackLayout align="center">
+    <SuccessCircleSolidIcon size={2} />
+    <Text styleAs="h2">Account created</Text>
+    <Text>You can now start using this new account.</Text>
+  </StackLayout>
+);
 
 const AdditionalInfoContent = ({ style }: { style?: React.CSSProperties }) => (
   <FlowLayout style={style}>
@@ -282,6 +369,8 @@ const ReviewAccountContent = () => (
 
 export const Horizontal = () => {
   const [activeStep, setActiveStep] = useState(0);
+  const [cancelOpen, setCancelOpen] = useState(false);
+  const [successOpen, setSuccessOpen] = useState(false);
   const allSteps = [
     {
       id: "account-details",
@@ -334,16 +423,28 @@ export const Horizontal = () => {
   const content = allSteps[activeStep].content;
 
   const nextStep = () =>
-    activeStep < allSteps.length - 1 && setActiveStep(activeStep + 1);
-  const previousStep = () => activeStep > 0 && setActiveStep(activeStep - 1);
-  const resetSteps = () => setActiveStep(0);
+    activeStep < allSteps.length - 1 &&
+    setActiveStep((prevStep) => prevStep + 1);
+  const previousStep = () =>
+    activeStep > 0 && setActiveStep((prevStep) => prevStep - 1);
+  // const resetSteps = () => setActiveStep(0);
+
+  const handleCancel = () => {
+    setCancelOpen(true);
+  };
+
+  const handleSuccess = () => {
+    setSuccessOpen(true);
+  };
+
+  const isLastStep = activeStep === allSteps.length - 1;
 
   const actions = (
     <FlexLayout gap={1} justify="end">
       <Button
         sentiment="accented"
         appearance="transparent"
-        onClick={resetSteps}
+        onClick={handleCancel}
       >
         Cancel
       </Button>
@@ -356,18 +457,28 @@ export const Horizontal = () => {
           Previous
         </Button>
       )}
-      <Button sentiment="accented" onClick={nextStep}>
+      <Button
+        sentiment="accented"
+        onClick={isLastStep ? handleSuccess : nextStep}
+      >
         Next
       </Button>
     </FlexLayout>
   );
 
   return (
-    <StackLayout className="container" gap={0}>
-      <div className="header">{header}</div>
-      <div className="content">{content}</div>
-      <div className="actions">{actions}</div>
-    </StackLayout>
+    <>
+      <StackLayout className="container" gap={0}>
+        <div className="header">{header}</div>
+        <div className="content">{content}</div>
+        <div className="actions">{actions}</div>
+      </StackLayout>
+      <CancelWarningDialog open={cancelOpen} onOpenChange={setCancelOpen} />
+      <AccountCreatedSuccessDialog
+        open={successOpen}
+        onOpenChange={setSuccessOpen}
+      />
+    </>
   );
 };
 
@@ -395,6 +506,12 @@ export const Modal = () => {
       label: "Review and create",
       content: <ReviewAccountContent />,
     },
+    {
+      id: "account-created",
+      label: "Account created",
+      content: <AccountCreatedContent />,
+      hidden: true,
+    },
   ];
 
   const handleRequestOpen = () => {
@@ -407,6 +524,9 @@ export const Modal = () => {
 
   const handleCancel = () => {
     setOpen(false);
+    setTimeout(() => {
+      setActiveStep(0);
+    }, 300);
   };
 
   const content = allSteps[activeStep].content;
@@ -447,49 +567,73 @@ export const Modal = () => {
 
   const stepperComponent = (
     <Stepper orientation="horizontal" style={{ width: 300 }}>
-      {allSteps.map((step, index) => (
-        <Step
-          key={step.id}
-          label={step.label}
-          stage={
-            index === activeStep
-              ? "active"
-              : index < activeStep
-                ? "completed"
-                : "pending"
-          }
-        />
-      ))}
+      {allSteps
+        .filter((step) => !step.hidden)
+        .map((step, index) => (
+          <Step
+            key={step.id}
+            label={step.label}
+            stage={
+              index === activeStep
+                ? "active"
+                : index < activeStep
+                  ? "completed"
+                  : "pending"
+            }
+          />
+        ))}
     </Stepper>
   );
+
+  const isCompleted = activeStep === allSteps.length - 1;
 
   return (
     <>
       <Button data-testid="dialog-button" onClick={handleRequestOpen}>
         Open wizard
       </Button>
-      <Dialog open={open} onOpenChange={onOpenChange}>
-        <DialogHeader
-          header={allSteps[activeStep].label}
-          preheader="Create a new account"
-          actions={stepperComponent}
-        />
-        <DialogContent style={{ height: 400 }}>{content}</DialogContent>
-        <DialogActions>
-          {direction === "column" ? (
-            <StackLayout gap={1} style={{ width: "100%" }}>
-              {next}
-              {previous}
-              {cancel}
-            </StackLayout>
-          ) : (
-            <FlexLayout gap={1}>
-              {cancel}
-              {previous}
-              {next}
-            </FlexLayout>
-          )}
-        </DialogActions>
+      <Dialog
+        open={open}
+        onOpenChange={onOpenChange}
+        status={isCompleted ? "success" : undefined}
+        style={{ height: 588 }}
+      >
+        {isCompleted ? (
+          <>
+            <DialogContent>
+              <AccountCreatedContent />
+            </DialogContent>
+            <DialogActions>
+              <Button sentiment="accented" onClick={handleCancel}>
+                Done
+              </Button>
+            </DialogActions>
+          </>
+        ) : (
+          <>
+            <DialogHeader
+              header={allSteps[activeStep].label}
+              preheader="Create a new account"
+              actions={stepperComponent}
+            />
+            <DialogContent>{content}</DialogContent>
+            <DialogActions>
+              {direction === "column" ? (
+                <StackLayout gap={1} style={{ width: "100%" }}>
+                  {next}
+                  {previous}
+                  {cancel}
+                </StackLayout>
+              ) : (
+                <FlexLayout gap={1}>
+                  {cancel}
+                  {previous}
+                  {next}
+                </FlexLayout>
+              )}
+            </DialogActions>
+          </>
+        )}
       </Dialog>
     </>
   );
