a674ef81b6e1c80d893e95ea59a60dc09ad65bb8
David Oyedeji <40142093+wemmyo@users.noreply.github.com>
2025-11-12 11:16:48 +0000

    Remove ToastGroup example from site (#5787)
    Co-authored-by: Josh Wooding <12938082+joshwooding@users.noreply.github.com>

--- site/src/examples/toast/index.ts
+++ site/src/examples/toast/index.ts
@@ -3,6 +3,4 @@ export * from "./Default";
 export * from "./Error";
 export * from "./Info";
 export * from "./Success";
-export * from "./ToastGroupBottom";
-export * from "./ToastGroupTop";
 export * from "./Warning";
