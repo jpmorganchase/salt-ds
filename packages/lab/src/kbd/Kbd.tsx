1915e18f6e93e9eae96deb81d7e99dbf5a8e894c
thushara joseph <thushara.joseph@chase.com>
2025-12-15 21:31:55 +0530

    design review and PR review WIP

---
+++ packages/lab/src/kbd/Kbd.tsx
@@ -0,0 +1,30 @@
+import { makePrefixer, Text } from "@salt-ds/core";
+import { useComponentCssInjection } from "@salt-ds/styles";
+import { useWindow } from "@salt-ds/window";
+import { clsx } from "clsx";
+import { forwardRef, type HTMLAttributes } from "react";
+
+import kbdCss from "./Kbd.css";
+
+export interface KbdProps extends HTMLAttributes<HTMLElement> {}
+
+const withBaseName = makePrefixer("saltKbd");
+
+export const Kbd = forwardRef<HTMLDivElement, KbdProps>(
+  function Kbd(props, ref) {
+    const { children, className, ...rest } = props;
+
+    const targetWindow = useWindow();
+    useComponentCssInjection({
+      testId: "salt-kbd",
+      css: kbdCss,
+      window: targetWindow,
+    });
+
+    return (
+      <kbd ref={ref} className={clsx(withBaseName(), className)} {...rest}>
+        <Text>{children}</Text>
+      </kbd>
+    );
+  },
+);
