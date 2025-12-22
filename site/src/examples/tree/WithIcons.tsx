fe694c0f5f6fa42b1b57150c39186b405a3bbc18
Tymon Rybak <tymon.rybak@jpmorgan.com>
2025-12-22 15:55:47 +0000

    fix example

--- site/src/examples/tree/WithIcons.tsx
+++ site/src/examples/tree/WithIcons.tsx
@@ -8,38 +8,30 @@ export const WithIcons = (): ReactElement => (
     defaultExpanded={["documents"]}
     style={{ width: "30ch" }}
   >
-    <TreeNode value="documents" label="Documents" icon={<FolderOpenIcon />}>
-      <TreeNode value="reports" label="Reports" icon={<FolderClosedIcon />}>
+    <TreeNode value="documents" label="Documents" icon={FolderOpenIcon}>
+      <TreeNode value="reports" label="Reports" icon={FolderClosedIcon}>
         <TreeNode
           value="annual-report"
           label="Annual Report"
-          icon={<DocumentIcon />}
+          icon={DocumentIcon}
         />
         <TreeNode
           value="quarterly-report"
           label="Quarterly Report"
-          icon={<DocumentIcon />}
+          icon={DocumentIcon}
         />
       </TreeNode>
-      <TreeNode value="invoices" label="Invoices" icon={<FolderClosedIcon />}>
-        <TreeNode
-          value="invoice-001"
-          label="Invoice 001"
-          icon={<DocumentIcon />}
-        />
-        <TreeNode
-          value="invoice-002"
-          label="Invoice 002"
-          icon={<DocumentIcon />}
-        />
+      <TreeNode value="invoices" label="Invoices" icon={FolderClosedIcon}>
+        <TreeNode value="invoice-001" label="Invoice 001" icon={DocumentIcon} />
+        <TreeNode value="invoice-002" label="Invoice 002" icon={DocumentIcon} />
       </TreeNode>
     </TreeNode>
-    <TreeNode value="pictures" label="Pictures" icon={<FolderClosedIcon />}>
-      <TreeNode value="vacation" label="Vacation" icon={<FolderClosedIcon />}>
-        <TreeNode value="beach" label="Beach" icon={<DocumentIcon />} />
-        <TreeNode value="mountains" label="Mountains" icon={<DocumentIcon />} />
+    <TreeNode value="pictures" label="Pictures" icon={FolderClosedIcon}>
+      <TreeNode value="vacation" label="Vacation" icon={FolderClosedIcon}>
+        <TreeNode value="beach" label="Beach" icon={DocumentIcon} />
+        <TreeNode value="mountains" label="Mountains" icon={DocumentIcon} />
       </TreeNode>
     </TreeNode>
-    <TreeNode value="downloads" label="Downloads" icon={<FolderClosedIcon />} />
+    <TreeNode value="downloads" label="Downloads" icon={FolderClosedIcon} />
   </Tree>
 );
