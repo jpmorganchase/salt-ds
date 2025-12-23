ca7e5d67d4c29b535aaae18651c0023823405be6
mark-tate <143323+mark-tate@users.noreply.github.com>
2025-12-23 10:54:13 +0100

    improve docs - show errors in Formfield based examples - remove dependency on getDayWeekName from adapters - document toJSDate in adapters - re-enforce the need for LocalizationProvider through examples - show how to set the date

--- site/src/examples/date-input/CustomParser.tsx
+++ site/src/examples/date-input/CustomParser.tsx
@@ -3,24 +3,27 @@ import {
   FormFieldHelperText as FormHelperText,
   FormFieldLabel as FormLabel,
 } from "@salt-ds/core";
-import { AdapterDateFns } from "@salt-ds/date-adapters/date-fns";
+import {
+  DateDetailError,
+  type DateFrameworkType,
+  type ParserResult,
+} from "@salt-ds/date-adapters";
 import {
   DateInputSingle,
   type DateInputSingleProps,
-  LocalizationProvider,
   type SingleDateSelection,
   useLocalization,
 } from "@salt-ds/lab";
-import { es as dateFnsEs } from "date-fns/locale";
-import { type ReactElement, useState } from "react";
+import { type ReactElement, useCallback, useState } from "react";
 
-const Single = (): ReactElement => {
+export const CustomParser = (): ReactElement => {
   const { dateAdapter } = useLocalization();
   const [selectedDate, setSelectedDate] = useState<SingleDateSelection | null>(
-    dateAdapter.parse("23 ago 2025", "DD MMM YYYY").date,
+    null,
   );
-  const defaultHelperText = "Aug will be invalid and ago will be valid";
-  const errorHelperText = "Please enter a valid ES date in DD MMM YYYY format";
+  const defaultHelperText =
+    "Date format DD MMM YYYY (e.g. 09 Jun 2024) or +/-D (e.g. +7)";
+  const errorHelperText = "Please enter a valid date in DD MMM YYYY format";
   const [helperText, setHelperText] = useState(defaultHelperText);
   const [validationStatus, setValidationStatus] = useState<"error" | undefined>(
     undefined,
@@ -53,23 +56,46 @@ const Single = (): ReactElement => {
     }
     setSelectedDate(date ?? null);
   };
+
+  const customParser = useCallback(
+    (inputDate: string, format: string): ParserResult<DateFrameworkType> => {
+      if (!inputDate?.length) {
+        const parsedDate = dateAdapter.parse("invalid date", "DD/MMM/YYYY");
+        return {
+          date: parsedDate.date,
+          value: inputDate,
+          errors: [
+            { type: DateDetailError.UNSET, message: "no date provided" },
+          ],
+        };
+      }
+      const parsedDate = inputDate;
+      const offsetMatch = parsedDate?.match(/^([+-]?\d+)$/);
+      if (offsetMatch) {
+        const offsetDays = Number.parseInt(offsetMatch[1], 10);
+        let offsetDate = dateAdapter.isValid(selectedDate)
+          ? selectedDate
+          : dateAdapter.today();
+        offsetDate = dateAdapter.add(offsetDate, { days: offsetDays });
+        return {
+          date: offsetDate,
+          value: inputDate,
+        };
+      }
+      return dateAdapter.parse(parsedDate || "", format);
+    },
+    [dateAdapter, selectedDate],
+  );
+
   return (
     <FormField style={{ width: "250px" }} validationStatus={validationStatus}>
-      <FormLabel>Enter ES locale date</FormLabel>
+      <FormLabel>Enter a date</FormLabel>
       <DateInputSingle
         date={selectedDate}
-        format={"DD MMM YYYY"}
         onDateChange={handleDateChange}
+        parse={customParser}
       />
       <FormHelperText>{helperText}</FormHelperText>
     </FormField>
-  );
-};
-
-export const SingleWithLocale = (): ReactElement => {
-  return (
-    <LocalizationProvider DateAdapter={AdapterDateFns} locale={dateFnsEs}>
-      <Single />
-    </LocalizationProvider>
   );
 };
