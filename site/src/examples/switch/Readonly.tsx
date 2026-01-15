23c2909a221a4536a5aacaa49664a7d43f392d96
Wemmy <wemmy.oyedeji@jpmorgan.com>
2026-01-15 11:04:15 +0000

    Add non-visual label examples

--- site/src/examples/switch/Readonly.tsx
+++ site/src/examples/switch/Readonly.tsx
@@ -1,12 +1,24 @@
-import { FormField, FormFieldLabel, StackLayout, Switch } from "@salt-ds/core";
+import {
+  FlexLayout,
+  FormField,
+  FormFieldLabel,
+  StackLayout,
+  Switch,
+} from "@salt-ds/core";
 import type { ReactElement } from "react";
 
 export const Readonly = (): ReactElement => (
-  <StackLayout>
-    <FormField>
-      <FormFieldLabel>Reminders (Read-only)</FormFieldLabel>
-      <Switch readOnly label="Off" />
-    </FormField>
-    <Switch readOnly checked label="Reminders" />
-  </StackLayout>
+  <FlexLayout align="end">
+    <StackLayout>
+      <Switch readOnly aria-label="Reminders" />
+      <Switch readOnly checked aria-label="Reminders" />
+    </StackLayout>
+    <StackLayout>
+      <FormField>
+        <FormFieldLabel>Reminders (Read-only)</FormFieldLabel>
+        <Switch readOnly label="Off" />
+      </FormField>
+      <Switch readOnly checked label="Reminders" />
+    </StackLayout>
+  </FlexLayout>
 );
