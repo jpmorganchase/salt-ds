791d4d8e02cea694607c2bf651b851a160bd1956
Wemmy <wemmy.oyedeji@jpmorgan.com>
2026-01-16 15:42:28 +0000

    Remove unlabelled examples

--- site/src/examples/switch/Readonly.tsx
+++ site/src/examples/switch/Readonly.tsx
@@ -1,24 +1,12 @@
-import {
-  FlexLayout,
-  FormField,
-  FormFieldLabel,
-  StackLayout,
-  Switch,
-} from "@salt-ds/core";
+import { FormField, FormFieldLabel, StackLayout, Switch } from "@salt-ds/core";
 import type { ReactElement } from "react";
 
 export const Readonly = (): ReactElement => (
-  <FlexLayout align="end">
-    <StackLayout>
-      <Switch readOnly aria-label="Reminders" />
-      <Switch readOnly checked aria-label="Reminders" />
-    </StackLayout>
-    <StackLayout>
-      <FormField>
-        <FormFieldLabel>Reminders (Read-only)</FormFieldLabel>
-        <Switch readOnly label="Off" />
-      </FormField>
-      <Switch readOnly checked label="Reminders" />
-    </StackLayout>
-  </FlexLayout>
+  <StackLayout>
+    <FormField>
+      <FormFieldLabel>Reminders (Read-only)</FormFieldLabel>
+      <Switch readOnly label="Off" />
+    </FormField>
+    <Switch readOnly checked label="Reminders" />
+  </StackLayout>
 );
