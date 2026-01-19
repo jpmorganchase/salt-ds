4e29732cfb96069e450bfc6b489165f91541622c
thushara joseph <thushara.joseph@chase.com>
2026-01-19 15:51:57 +0530

    spellcheck

--- site/src/examples/kbd/NestedInMenu.tsx
+++ site/src/examples/kbd/NestedInMenu.tsx
@@ -129,7 +129,7 @@ export const NestedInMenu = (): ReactElement => (
           align="center"
           justify="space-between"
         >
-          Dupicate
+          Duplicate
           <FlexLayout gap={0.5} wrap align="center">
             <Kbd>Cmd</Kbd>
             <Kbd>Shift</Kbd>
