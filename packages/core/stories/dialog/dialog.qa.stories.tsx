5b52b2749e001f9acbf4c195f97974f452e5320e
Wemmy <wemmy.oyedeji@jpmorgan.com>
2026-02-09 15:51:37 +0000

    Add id setter to qa story

--- packages/core/stories/dialog/dialog.qa.stories.tsx
+++ packages/core/stories/dialog/dialog.qa.stories.tsx
@@ -27,7 +27,7 @@ export default {
 
 function FakeDialog({ children, status, id }: DialogProps) {
   return (
-    <DialogContext.Provider value={{ status, id }}>
+    <DialogContext.Provider value={{ status, id, setId: () => {} }}>
       <div className="fakeDialogWindow">{children}</div>
     </DialogContext.Provider>
   );
