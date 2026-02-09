327a0f2fff7b3e3d743de90db9c84802069f56e3
Wemmy <wemmy.oyedeji@jpmorgan.com>
2026-02-09 10:46:16 +0000

    Fix Vertical Nav not active

--- site/src/components/navigation/vertical-navigation/utils/index.spec.ts
+++ site/src/components/navigation/vertical-navigation/utils/index.spec.ts
@@ -54,19 +54,25 @@ describe("normalizeSelectedNodeId", () => {
     { title: "Button", href: "/salt/components/button" },
   ];
 
-  it("replaces /examples with /index if /index exists", () => {
-    expect(
-      normalizeSelectedNodeId("/salt/components/slider/examples", navData),
-    ).toBe("/salt/components/slider/index");
-  });
+  it.each(["/examples", "/usage", "/accessibility"])(
+    "replaces %s with /index if /index exists",
+    (route) => {
+      expect(
+        normalizeSelectedNodeId(`/salt/components/slider${route}`, navData),
+      ).toBe("/salt/components/slider/index");
+    },
+  );
 
-  it("removes /examples if /index does not exist", () => {
-    expect(
-      normalizeSelectedNodeId("/salt/components/button/examples", navData),
-    ).toBe("/salt/components/button");
-  });
+  it.each(["/examples", "/usage", "/accessibility"])(
+    "removes %s if /index does not exist",
+    (route) => {
+      expect(
+        normalizeSelectedNodeId(`/salt/components/button${route}`, navData),
+      ).toBe("/salt/components/button");
+    },
+  );
 
-  it("returns link unchanged if not ending with /examples", () => {
+  it("returns link unchanged if not ending with a tab route", () => {
     expect(normalizeSelectedNodeId("/salt/components/slider", navData)).toBe(
       "/salt/components/slider",
     );
