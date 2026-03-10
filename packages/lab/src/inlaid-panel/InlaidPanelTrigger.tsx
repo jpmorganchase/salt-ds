93482c17512425bc5bedb7b9e6feb4ebd43c4146
David Oyedeji <wemmyoyedeji@gmail.com>
2026-03-10 21:48:36 +0000

    Add Inlaid Panel to Lab

---
+++ packages/lab/src/inlaid-panel/InlaidPanelTrigger.tsx
@@ -0,0 +1,34 @@
+import { useForkRef, useIsomorphicLayoutEffect } from "@salt-ds/core";
+import { type ComponentPropsWithoutRef, forwardRef, useRef } from "react";
+import { useInlaidPanel } from "./InlaidPanelContext";
+
+export type InlaidPanelTriggerProps = ComponentPropsWithoutRef<"button">;
+
+export const InlaidPanelTrigger = forwardRef<
+  HTMLButtonElement,
+  InlaidPanelTriggerProps
+>(function InlaidPanelTrigger({ children, ...props }, forwardedRef) {
+  const { open, onOpenChange, panelId, triggerRef } = useInlaidPanel();
+  const ref = useRef<HTMLButtonElement>(null);
+  const handleRef = useForkRef(ref, forwardedRef);
+
+  useIsomorphicLayoutEffect(() => {
+    triggerRef.current = ref.current;
+    return () => {
+      triggerRef.current = null;
+    };
+  }, [triggerRef]);
+
+  return (
+    <button
+      ref={handleRef}
+      type="button"
+      aria-expanded={open}
+      aria-controls={panelId}
+      onClick={() => onOpenChange(!open)}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+});
