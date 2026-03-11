e4de9117f98a3c8e6f9a0bb250e3d9153c89e173
Wemmy <wemmy.oyedeji@jpmorgan.com>
2026-03-11 16:39:48 +0000

    improve inlaid panel props and ref forwarding

--- packages/lab/src/inlaid-panel/InlaidPanelClose.tsx
+++ packages/lab/src/inlaid-panel/InlaidPanelClose.tsx
@@ -1,25 +1,30 @@
 import { Button } from "@salt-ds/core";
 import { CloseIcon } from "@salt-ds/icons";
+import type { MouseEvent } from "react";
 import { type ComponentPropsWithoutRef, forwardRef } from "react";
 import { useInlaidPanel } from "./InlaidPanelContext";
 
 export type InlaidPanelCloseProps = ComponentPropsWithoutRef<"button">;
 
-// const withBaseName = makePrefixer("saltInlaidPanelClose");
-
 export const InlaidPanelClose = forwardRef<
   HTMLButtonElement,
   InlaidPanelCloseProps
->(function InlaidPanelClose({ children, ...props }, ref) {
+>(function InlaidPanelClose(props, ref) {
   const { onOpenChange } = useInlaidPanel();
+
+  const { children, onClick } = props;
+
+  const handleOnClick = (event: MouseEvent<HTMLButtonElement>) => {
+    onClick?.(event);
+    onOpenChange(false);
+  };
+
   return (
-    // <div className={withBaseName("container")}>
     <Button
       ref={ref}
       aria-label="Close Drawer"
       appearance="transparent"
-      // className={clsx(withBaseName(), className)}
-      onClick={() => onOpenChange(false)}
+      onClick={handleOnClick}
       {...props}
     >
       {children || <CloseIcon aria-hidden />}
