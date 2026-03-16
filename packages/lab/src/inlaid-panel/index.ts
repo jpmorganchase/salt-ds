cc813003062f890c19f81424b83321fa208d0e9a
Wemmy <wemmy.oyedeji@jpmorgan.com>
2026-03-16 12:09:01 +0000

    Rename components

--- packages/lab/src/inlaid-panel/index.ts
+++ packages/lab/src/inlaid-panel/index.ts
@@ -1,4 +1,4 @@
 export * from "./InlaidPanel";
+export * from "./InlaidPanelContent";
 export * from "./InlaidPanelContext";
-export * from "./InlaidPanelGroup";
 export * from "./InlaidPanelTrigger";
