3ecde95199e0feffee8a200c6c54c32495f5919f
David Oyedeji <wemmyoyedeji@gmail.com>
2026-04-29 17:56:55 +0100

    Remove conditional aria label

--- site/src/examples/side-panel/WithAppHeader.tsx
+++ site/src/examples/side-panel/WithAppHeader.tsx
@@ -29,7 +29,6 @@ import {
 import { ContentExample } from "src/examples/side-panel/ContentExample";
 
 const DesktopAppHeader = () => {
-  const { openState } = useSidePanelContext();
   return (
     <header>
       <FlexLayout
@@ -58,12 +57,7 @@ const DesktopAppHeader = () => {
           <StackLayout direction="row" gap={1}>
             <Tooltip content="Toggle help panel" hideArrow>
               <SidePanelTrigger>
-                <Button
-                  appearance="transparent"
-                  aria-label={
-                    openState ? "Close help panel" : "Open help panel"
-                  }
-                >
+                <Button appearance="transparent" aria-label="Open help panel">
                   <HelpCircleIcon aria-hidden />
                 </Button>
               </SidePanelTrigger>
