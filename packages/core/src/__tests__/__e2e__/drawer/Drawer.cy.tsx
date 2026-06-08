52daa64679affb087f1cd92a342e7547e47105c9
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2026-06-08 16:40:58 +0100

    Add `initialFocus` prop to Drawer (#6637)

--- packages/core/src/__tests__/__e2e__/drawer/Drawer.cy.tsx
+++ packages/core/src/__tests__/__e2e__/drawer/Drawer.cy.tsx
@@ -3,7 +3,8 @@ import { composeStories } from "@storybook/react-vite";
 
 const composedStories = composeStories(drawerStories);
 
-const { Default, OptionalCloseAction } = composedStories;
+const { Default, OptionalCloseAction, InitialFocusIndex, InitialFocusRef } =
+  composedStories;
 
 describe("GIVEN a Drawer", () => {
   describe("WHEN a drawer with close button is open", () => {
@@ -100,6 +101,24 @@ describe("GIVEN a Drawer", () => {
       cy.findByRole("dialog").should("be.visible");
       cy.findByRole("button", { name: "Submit" }).click();
       cy.findByRole("dialog").should("not.be.visible");
+    });
+  });
+
+  describe("WHEN initialFocus prop is provided", () => {
+    it("THEN it should focus the element at the given tabbable index", () => {
+      cy.mount(<InitialFocusIndex />);
+
+      cy.findByRole("button", { name: "Open Drawer" }).realClick();
+      cy.findByRole("dialog").should("be.visible");
+      cy.findByRole("textbox", { name: "Third" }).should("be.focused");
+    });
+
+    it("THEN it should focus the element referenced by the provided ref", () => {
+      cy.mount(<InitialFocusRef />);
+
+      cy.findByRole("button", { name: "Open Drawer" }).realClick();
+      cy.findByRole("dialog").should("be.visible");
+      cy.findByRole("textbox", { name: "Third" }).should("be.focused");
     });
   });
 });
