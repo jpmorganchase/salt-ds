278ac5dcd2e4a8fd7a01042c633e37f781913e14
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2026-07-12 14:41:23 +0100

    Restore Spinner completion announcement tests

--- packages/core/src/__tests__/__e2e__/spinner/Spinner.accessibility.cy.tsx
+++ packages/core/src/__tests__/__e2e__/spinner/Spinner.accessibility.cy.tsx
@@ -1,8 +1,18 @@
 import { Spinner } from "@salt-ds/core";
-import { getContainerEl } from "cypress/react";
-import ReactDOM from "react-dom";
+import { type ComponentProps, useState } from "react";
 
 const ariaLabel = "Loading component";
+
+function SpinnerHarness(props: ComponentProps<typeof Spinner>) {
+  const [showSpinner, setShowSpinner] = useState(true);
+
+  return (
+    <>
+      <button onClick={() => setShowSpinner(false)}>Unmount spinner</button>
+      {showSpinner ? <Spinner {...props} /> : null}
+    </>
+  );
+}
 
 describe("GIVEN a Spinner", () => {
   it("THEN it should render with the correct default ARIA attributes", () => {
@@ -26,31 +36,72 @@ describe("GIVEN a Spinner", () => {
 
 describe("GIVEN an available announcer", () => {
   it("THEN the announcer should be called with aria-label every 5 seconds", () => {
+    cy.clock();
     cy.mount(<Spinner aria-label={ariaLabel} />);
 
-    cy.wait(5000);
+    cy.findByRole("img").should("announce", ariaLabel);
+    cy.tick(5000);
 
     cy.findByRole("img").should("announce", ariaLabel);
   });
 
-  // TODO fix unmount announcement
-  it.skip("THEN the announcer should be called when the component unmounts", () => {
-    cy.mount(<Spinner aria-label={ariaLabel} />);
-    cy.then(() => ReactDOM.unmountComponentAtNode(getContainerEl()));
+  it("THEN the announcer should be called once when the component unmounts", () => {
+    cy.clock();
+    cy.mount(
+      <SpinnerHarness
+        aria-label={ariaLabel}
+        announcerInterval={1000}
+        announcerTimeout={2000}
+      />,
+    );
+
+    cy.findByRole("img").should("announce", ariaLabel);
+    cy.tick(1000);
+    cy.get("body").should("announce", ariaLabel);
+    cy.tick(1000);
+    cy.get("body").should("announce", ariaLabel);
+    cy.tick(1000);
+    cy.get("body").should(
+      "announce",
+      `${ariaLabel} is still in progress, but will no longer announce.`,
+    );
+    cy.tick(300);
+
+    cy.findByRole("button", { name: "Unmount spinner" }).click();
     cy.get("body").should("announce", `finished ${ariaLabel}`);
+    cy.tick(300);
+    cy.get("body").should("not.announce", `finished ${ariaLabel}`);
+
+    cy.tick(25000);
+    cy.get("body").should("not.announce", ariaLabel);
   });
 
-  it.skip("THEN nothing should be announced when announcer is disabled", () => {
-    cy.mount(<Spinner aria-label={ariaLabel} disableAnnouncer />);
-    cy.findByRole("img").should("not.announce", ariaLabel);
-    cy.then(() => ReactDOM.unmountComponentAtNode(getContainerEl()));
-    cy.findByRole("img").should("not.announce", `finished ${ariaLabel}`);
+  it("THEN nothing should be announced when announcer is disabled", () => {
+    cy.clock();
+    cy.mount(<SpinnerHarness aria-label={ariaLabel} disableAnnouncer />);
+
+    cy.get("body").should("not.announce", ariaLabel);
+    cy.findByRole("button", { name: "Unmount spinner" }).click();
+    cy.get("body").should("not.announce", `finished ${ariaLabel}`);
+
+    cy.tick(25000);
+    cy.get("body").should("not.announce", ariaLabel);
   });
 
-  it.skip("THEN it should not announce completion message when set to null", () => {
-    cy.mount(<Spinner aria-label={ariaLabel} completionAnnouncement={null} />);
+  it("THEN it should not announce completion message when set to null", () => {
+    cy.clock();
+    cy.mount(
+      <SpinnerHarness
+        aria-label={ariaLabel}
+        completionAnnouncement={null}
+      />,
+    );
 
-    cy.then(() => ReactDOM.unmountComponentAtNode(getContainerEl()));
-    cy.findByRole("img").should("not.announce", `finished ${ariaLabel}`);
+    cy.findByRole("img").should("announce", ariaLabel);
+    cy.findByRole("button", { name: "Unmount spinner" }).click();
+    cy.get("body").should("not.announce", `finished ${ariaLabel}`);
+
+    cy.tick(25000);
+    cy.get("body").should("not.announce", ariaLabel);
   });
 });
