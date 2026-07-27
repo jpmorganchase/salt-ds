0e8f045eff825f545acc7b832132da94a4ad16bd
Wemmy <wemmyoyedeji@gmail.com>
2026-07-27 14:43:34 +0100

    revert changes to avatar interactive example

--- site/src/examples/avatar/Interactive.tsx
+++ site/src/examples/avatar/Interactive.tsx
@@ -1,7 +1,6 @@
 import {
   Avatar,
   Divider,
-  FlexLayout,
   Menu,
   MenuItem,
   MenuPanel,
@@ -12,32 +11,28 @@ import type { ReactElement } from "react";
 
 export const Interactive = (): ReactElement => {
   return (
-    <FlexLayout align="center">
-      <Menu placement="bottom-end">
-        <MenuTrigger>
-          <Avatar
-            name="Alex Brailescu"
-            render={
-              <button type="button" aria-label="Alex Brailescu profile" />
-            }
-          />
-        </MenuTrigger>
-        <MenuPanel>
-          <MenuItem>
-            <NotificationIcon aria-hidden />
-            Notifications
-          </MenuItem>
-          <MenuItem>
-            <SettingsIcon aria-hidden />
-            Settings
-          </MenuItem>
-          <Divider />
-          <MenuItem>
-            <ExportIcon aria-hidden />
-            Log out
-          </MenuItem>
-        </MenuPanel>
-      </Menu>
-    </FlexLayout>
+    <Menu placement="bottom-end">
+      <MenuTrigger>
+        <Avatar
+          name="Alex Brailescu"
+          render={<button type="button" aria-label="Alex Brailescu profile" />}
+        />
+      </MenuTrigger>
+      <MenuPanel>
+        <MenuItem>
+          <NotificationIcon aria-hidden />
+          Notifications
+        </MenuItem>
+        <MenuItem>
+          <SettingsIcon aria-hidden />
+          Settings
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ExportIcon aria-hidden />
+          Log out
+        </MenuItem>
+      </MenuPanel>
+    </Menu>
   );
 };
