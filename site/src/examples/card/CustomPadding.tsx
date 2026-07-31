e3c0230a67afc1ee722a87f8bfabe8033a15625f
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2026-07-31 13:42:41 +0100

    Add Card structure components (#4101)
    Co-authored-by: Tymon Rybak <tymon.rybak@jpmorgan.com>
    Co-authored-by: aimee <152992155+aimee-liou@users.noreply.github.com>

--- site/src/examples/card/CustomPadding.tsx
+++ site/src/examples/card/CustomPadding.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   Card,
+  CardContent,
   Display3,
   FlowLayout,
   H3,
@@ -23,70 +24,76 @@ export const CustomPadding = (): ReactElement => {
           href="#"
           style={
             {
-              "--saltLinkCard-padding": "var(--salt-spacing-100)",
+              "--saltCardContent-padding": "var(--salt-spacing-100)",
               width: "160px",
             } as CSSProperties
           }
         >
-          <StackLayout gap={1} direction="row" align="center">
-            <MessageIcon size={1} />
-            <Text>Contact us</Text>
-          </StackLayout>
+          <CardContent>
+            <StackLayout gap={1} direction="row" align="center">
+              <MessageIcon size={1} />
+              <Text>Contact us</Text>
+            </StackLayout>
+          </CardContent>
         </LinkCard>
       )}
       {padding === "spacing-200" && (
         <Card
           style={
             {
-              "--saltCard-padding": "var(--salt-spacing-200)",
+              "--saltCardContent-padding": "var(--salt-spacing-200)",
               width: "260px",
             } as CSSProperties
           }
         >
-          <StackLayout gap={1}>
-            <H3>Sustainable investing products</H3>
-            <Text>
-              We have a commitment to provide a wide range of investment
-              solutions to enable you to align your financial goals to your
-              values.
-            </Text>
-          </StackLayout>
+          <CardContent>
+            <StackLayout gap={1}>
+              <H3 style={{ margin: 0 }}>Sustainable investing products</H3>
+              <Text>
+                We have a commitment to provide a wide range of investment
+                solutions to enable you to align your financial goals to your
+                values.
+              </Text>
+            </StackLayout>
+          </CardContent>
         </Card>
       )}
       {padding === "spacing-300" && (
         <Card
           style={
             {
-              "--saltCard-padding": "var(--salt-spacing-300)",
+              "--saltCardContent-padding": "var(--salt-spacing-300)",
               width: "500px",
             } as CSSProperties
           }
         >
-          <StackLayout>
-            <StackLayout direction="row" align="end" gap={1}>
-              <H3>Threshold Summary</H3>
-              <Text variant="secondary">(Projected Revenue)</Text>
-            </StackLayout>
-            <FlowLayout gap={3}>
-              <StackLayout gap={0}>
-                <Text>Below Threshold 1</Text>
-                <Display3>$1,000,000</Display3>
+          <CardContent>
+            <StackLayout>
+              <StackLayout direction="row" align="end" gap={1}>
+                <H3 style={{ margin: 0 }}>Threshold Summary</H3>
+                <Text variant="secondary">(Projected Revenue)</Text>
               </StackLayout>
-              <StackLayout gap={0}>
-                <Text>Below Threshold 1 & 2</Text>
-                <Display3>$450,000</Display3>
-              </StackLayout>
-              <StackLayout gap={0}>
-                <Text>Below Threshold 2 & 3</Text>
-                <Display3>$0</Display3>
-              </StackLayout>
-            </FlowLayout>
+              <FlowLayout gap={3}>
+                <StackLayout gap={0}>
+                  <Text>Below Threshold 1</Text>
+                  <Display3>$1,000,000</Display3>
+                </StackLayout>
+                <StackLayout gap={0}>
+                  <Text>Below Threshold 1 & 2</Text>
+                  <Display3>$450,000</Display3>
+                </StackLayout>
+                <StackLayout gap={0}>
+                  <Text>Below Threshold 2 & 3</Text>
+                  <Display3>$0</Display3>
+                </StackLayout>
+              </FlowLayout>
 
-            <StackLayout direction="row" gap={1}>
-              <Button>Cancel</Button>
-              <Button sentiment="accented">Update tier</Button>
+              <StackLayout direction="row" gap={1}>
+                <Button>Cancel</Button>
+                <Button sentiment="accented">Update tier</Button>
+              </StackLayout>
             </StackLayout>
-          </StackLayout>
+          </CardContent>
         </Card>
       )}
       <RadioButtonGroup
