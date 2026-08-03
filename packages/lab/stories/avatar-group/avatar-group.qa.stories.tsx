a57166132d9862d3e51d723ad27c0ab768064280
Chris Sapa <47253291+kriskoder@users.noreply.github.com>
2026-08-03 16:32:35 +0100

    [AvatarGroup] - add support for entity avatars in AvatarGroup component

--- packages/lab/stories/avatar-group/avatar-group.qa.stories.tsx
+++ packages/lab/stories/avatar-group/avatar-group.qa.stories.tsx
@@ -11,6 +11,7 @@ export default {
 
 export const AvatarGroupGrid: StoryFn<QAContainerProps> = (props) => (
   <QAContainer height={600} width={1000} cols={1} itemPadding={12} {...props}>
+    {/* person avatars */}
     <AvatarGroup>
       <Avatar name="Alex Brailescu" src={persona1} />
       <Avatar name="Peter Piper" color="category-2" />
@@ -22,6 +23,22 @@ export const AvatarGroupGrid: StoryFn<QAContainerProps> = (props) => (
       <Avatar name="Peter Piper" color="category-2" />
       <Avatar name="John Doe" color="category-3" />
       <AvatarGroupSurplus name="2 more">+2</AvatarGroupSurplus>
+    </AvatarGroup>
+
+    {/* entity avatars */}
+    <AvatarGroup>
+      <Avatar kind="entity" name="Alpha" color="category-2" />
+      <Avatar kind="entity" name="Beta" color="category-3" />
+      <Avatar kind="entity" name="Gamma" color="category-4" />
+    </AvatarGroup>
+
+    <AvatarGroup>
+      <Avatar kind="entity" name="Alpha" color="category-2" />
+      <Avatar kind="entity" name="Beta" color="category-3" />
+      <Avatar kind="entity" name="Gamma" color="category-4" />
+      <AvatarGroupSurplus kind="entity" name="2 more">
+        +2
+      </AvatarGroupSurplus>
     </AvatarGroup>
   </QAContainer>
 );
