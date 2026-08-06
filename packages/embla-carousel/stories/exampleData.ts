4db5da6f913ea718fe5ff8b04976dad2f66ed97e
Josh <12938082+joshwooding@users.noreply.github.com>
2026-08-06 10:27:06 +0100

    Fix import alias usage

--- packages/embla-carousel/stories/exampleData.ts
+++ packages/embla-carousel/stories/exampleData.ts
@@ -1,7 +1,7 @@
-import carouselSlide1 from "@stories/assets/carouselSlide1.png";
-import carouselSlide2 from "@stories/assets/carouselSlide2.png";
-import carouselSlide3 from "@stories/assets/carouselSlide3.png";
-import carouselSlide4 from "@stories/assets/carouselSlide4.png";
+import carouselSlide1 from "~stories/assets/carouselSlide1.png";
+import carouselSlide2 from "~stories/assets/carouselSlide2.png";
+import carouselSlide3 from "~stories/assets/carouselSlide3.png";
+import carouselSlide4 from "~stories/assets/carouselSlide4.png";
 
 export const sliderData = [
   {
