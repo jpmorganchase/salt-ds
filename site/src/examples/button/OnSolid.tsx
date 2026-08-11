8ae9f65ee55c0b2742fed66c71a4dc61261f121a
Chris Sapa <47253291+kriskoder@users.noreply.github.com>
2026-08-11 11:49:44 +0100

    formatting(lab/OnSolidButton) - fix formatting

--- site/src/examples/button/OnSolid.tsx
+++ site/src/examples/button/OnSolid.tsx
@@ -19,4 +19,3 @@ export const OnSolid = (): ReactElement => (
     </OnSolidButton>
   </div>
 );
-
--- site/src/examples/on-solid/Default.tsx
+++ site/src/examples/on-solid/Default.tsx
@@ -15,4 +15,3 @@ export const Default = (): ReactElement => (
     <OnSolid>Dismiss</OnSolid>
   </div>
 );
-
