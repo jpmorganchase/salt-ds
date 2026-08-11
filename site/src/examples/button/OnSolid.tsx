3d2a0de90e50fc0c8f4fd6861743cf6af5e46f19
Chris Sapa <47253291+kriskoder@users.noreply.github.com>
2026-08-11 12:30:38 +0100

    site(lab/OnSolidButton) - add examples rendered on 4 different  backgrounds

--- site/src/examples/button/OnSolid.tsx
+++ site/src/examples/button/OnSolid.tsx
@@ -1,21 +1,34 @@
+import { FlowLayout } from "@salt-ds/core";
 import { SaltShakerIcon } from "@salt-ds/icons";
 import { OnSolid as OnSolidButton } from "@salt-ds/lab";
 import type { ReactElement } from "react";
 
+const backgrounds = [
+  "var(--salt-status-error-bold-background)",
+  "var(--salt-status-info-bold-background)",
+  "var(--salt-status-warning-bold-background)",
+  "var(--salt-status-success-bold-background)",
+];
+
 export const OnSolid = (): ReactElement => (
-  <div
-    style={{
-      background: "var(--salt-status-info-bold-background)",
-      padding: "var(--salt-spacing-300)",
-      borderRadius: "var(--salt-palette-corner-weak)",
-      display: "inline-flex",
-      alignItems: "center",
-      justifyContent: "center",
-    }}
-  >
-    <OnSolidButton>
-      <SaltShakerIcon aria-hidden />
-      Dismiss
-    </OnSolidButton>
-  </div>
+  <FlowLayout gap={3}>
+    {backgrounds.map((background) => (
+      <div
+        key={background}
+        style={{
+          background,
+          padding: "var(--salt-spacing-300)",
+          borderRadius: "var(--salt-palette-corner-weak)",
+          display: "inline-flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <OnSolidButton>
+          <SaltShakerIcon aria-hidden />
+          Dismiss
+        </OnSolidButton>
+      </div>
+    ))}
+  </FlowLayout>
 );
