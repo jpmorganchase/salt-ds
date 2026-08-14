ba9edebeaec9d15ddd0d9b1c6c4db973e5a8d7ac
Chris Sapa <47253291+kriskoder@users.noreply.github.com>
2026-08-14 14:49:27 +0100

    feat(lab/avatarGroup): Refine Avatar Group docs and add entity example

---
+++ site/src/examples/avatar-group/Entity.tsx
@@ -0,0 +1,31 @@
+import { Avatar } from "@salt-ds/core";
+import { AvatarGroup, AvatarGroupCount } from "@salt-ds/lab";
+import type { ReactElement } from "react";
+
+export const Entity = (): ReactElement => {
+  return (
+    <AvatarGroup>
+      <Avatar
+        kind="entity"
+        name="Operations"
+        nameToInitials={() => "OPS"}
+        color="category-2"
+      />
+      <Avatar
+        kind="entity"
+        name="Technology"
+        nameToInitials={() => "TEC"}
+        color="category-3"
+      />
+      <Avatar
+        kind="entity"
+        name="Risk"
+        nameToInitials={() => "RSK"}
+        color="category-4"
+      />
+      <AvatarGroupCount kind="entity" name="2 more">
+        +2
+      </AvatarGroupCount>
+    </AvatarGroup>
+  );
+};
