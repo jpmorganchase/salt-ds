e1e8efb914b6d4735335599e3defbda7412d3398
Josh <12938082+joshwooding@users.noreply.github.com>
2026-08-16 12:57:49 +0100

    Address feedback

--- site/src/examples/link-button/WithIcon.tsx
+++ site/src/examples/link-button/WithIcon.tsx
@@ -1,9 +1,15 @@
-import { ArrowRightIcon } from "@salt-ds/icons";
+import { StackLayout } from "@salt-ds/core";
+import { ArrowLeftIcon, ArrowRightIcon } from "@salt-ds/icons";
 import { LinkButton } from "@salt-ds/lab";
 import type { ReactElement } from "react";
 
 export const WithIcon = (): ReactElement => (
-  <LinkButton href="/salt/components/card">
-    View card documentation <ArrowRightIcon aria-hidden />
-  </LinkButton>
+  <StackLayout align="start">
+    <LinkButton href="/salt/components/card">
+      View card documentation <ArrowRightIcon aria-hidden />
+    </LinkButton>
+    <LinkButton href="/salt/getting-started">
+      <ArrowLeftIcon aria-hidden /> Back to getting started
+    </LinkButton>
+  </StackLayout>
 );
