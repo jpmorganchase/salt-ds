0b17b862b5e73c96bdc16ad6bc58d05e06fda39e
Josh <12938082+joshwooding@users.noreply.github.com>
2026-08-17 22:48:58 +0100

    tweaks:

--- site/src/examples/link-button/IconOnly.tsx
+++ site/src/examples/link-button/IconOnly.tsx
@@ -1,9 +1,27 @@
-import { ArrowLeftIcon } from "@salt-ds/icons";
+import { FlowLayout, Tooltip } from "@salt-ds/core";
+import { ArrowLeftIcon, CartIcon, GithubIcon } from "@salt-ds/icons";
 import { LinkButton } from "@salt-ds/lab";
 import type { ReactElement } from "react";
 
 export const IconOnly = (): ReactElement => (
-  <LinkButton aria-label="Back to components" href="/salt/components">
-    <ArrowLeftIcon aria-hidden />
-  </LinkButton>
+  <FlowLayout>
+    <Tooltip content="Back to components" placement="top">
+      <LinkButton aria-label="Back to components" href="/salt/components">
+        <ArrowLeftIcon aria-hidden />
+      </LinkButton>
+    </Tooltip>
+    <Tooltip content="Salt GitHub repository" placement="top">
+      <LinkButton
+        aria-label="Salt GitHub repository"
+        href="https://github.com/jpmorganchase/salt-ds"
+      >
+        <GithubIcon aria-hidden />
+      </LinkButton>
+    </Tooltip>
+    <Tooltip content="View basket" placement="top">
+      <LinkButton aria-label="View basket" href="/basket">
+        <CartIcon aria-hidden />
+      </LinkButton>
+    </Tooltip>
+  </FlowLayout>
 );
