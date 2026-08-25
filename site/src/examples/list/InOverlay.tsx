84f4ec52c1b180c043917b3e7ccee1f6f33224c0
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2026-08-25 09:02:15 +0100

    tweaks

--- site/src/examples/list/InOverlay.tsx
+++ site/src/examples/list/InOverlay.tsx
@@ -6,23 +6,38 @@ import {
   ListItemActions,
   ListItemContent,
   Overlay,
+  OverlayHeader,
   OverlayPanel,
-  OverlayPanelCloseButton,
   OverlayPanelContent,
   OverlayTrigger,
+  useId,
 } from "@salt-ds/core";
+import { CloseIcon } from "@salt-ds/icons";
 import { type ReactElement, useState } from "react";
 
 export const InOverlay = (): ReactElement => {
   const [open, setOpen] = useState(false);
+  const headerId = useId();
+
+  const handleClose = () => setOpen(false);
+
+  const closeButton = (
+    <Button
+      aria-label="Close overlay"
+      appearance="transparent"
+      onClick={handleClose}
+    >
+      <CloseIcon aria-hidden />
+    </Button>
+  );
 
   return (
     <Overlay open={open} onOpenChange={setOpen} placement="bottom">
       <OverlayTrigger>
         <Button>Show reports</Button>
       </OverlayTrigger>
-      <OverlayPanel>
-        <OverlayPanelCloseButton />
+      <OverlayPanel aria-labelledby={headerId}>
+        <OverlayHeader header="Reports" actions={closeButton} id={headerId} />
         <OverlayPanelContent>
           <List aria-label="Reports" style={{ width: 320 }}>
             <ListItem>
