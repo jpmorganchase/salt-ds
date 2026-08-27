5f20c5a620563fdfda289089d30dfd3c900571bc
Chris Sapa <47253291+kriskoder@users.noreply.github.com>
2026-08-27 23:35:03 +0100

    feat(core/DrawerHeader) - implement basic Header block in Drawer

---
+++ packages/core/src/drawer/DrawerHeader.tsx
@@ -0,0 +1,103 @@
+import { useComponentCssInjection } from "@salt-ds/styles";
+import { useWindow } from "@salt-ds/window";
+import { clsx } from "clsx";
+import {
+  type ComponentPropsWithoutRef,
+  forwardRef,
+  type ReactNode,
+} from "react";
+import { H2, Text } from "../text";
+import { makePrefixer, useId, useIsomorphicLayoutEffect } from "../utils";
+import { useDrawerContext } from "./DrawerContext";
+import drawerHeaderCss from "./DrawerHeader.css";
+
+const withBaseName = makePrefixer("saltDrawerHeader");
+
+export interface DrawerHeaderProps extends ComponentPropsWithoutRef<"div"> {
+  // TODO: confirm if DrawerHeader supports rendering status icon instead of accent bar, based on the status prop -> look Dialog in core
+  /**
+   * Displays the accent bar in the Drawer Header */
+  disableAccent?: boolean;
+  /**
+   * Displays the header at the top of the Drawer
+   */
+  // TODO: technically this should be named heading -> check if can rename, css classes use `header` too
+  header: ReactNode;
+  /**
+   * Displays the preheader just above the header
+   **/
+  // TODO: technically this should be named preheading -> check if can rename
+  preheader?: ReactNode;
+  /**
+   * Description text is displayed just below the header
+   **/
+  description?: ReactNode;
+  /**
+   * Actions to be displayed in header
+   */
+  actions?: ReactNode;
+}
+
+export const DrawerHeader = forwardRef<HTMLDivElement, DrawerHeaderProps>(
+  function DrawerHeader(props, ref) {
+    const {
+      actions,
+      className,
+      description,
+      disableAccent,
+      header,
+      preheader,
+      ...rest
+    } = props;
+
+    const targetWindow = useWindow();
+    useComponentCssInjection({
+      testId: "salt-drawer-header",
+      css: drawerHeaderCss,
+      window: targetWindow,
+    });
+
+    const { setHeaderId } = useDrawerContext();
+    const headingId = useId();
+    const hasHeading = Boolean(header || preheader);
+
+    useIsomorphicLayoutEffect(() => {
+      setHeaderId?.(hasHeading ? headingId : undefined);
+
+      return () => {
+        setHeaderId?.(undefined);
+      };
+    }, [hasHeading, headingId, setHeaderId]);
+
+    return (
+      <div
+        className={clsx(
+          withBaseName(),
+          {
+            [withBaseName("withAccent")]: !disableAccent,
+          },
+          className,
+        )}
+        ref={ref}
+        {...rest}
+      >
+        <div className={withBaseName("container")}>
+          {hasHeading && (
+            <H2 id={headingId} className={withBaseName("header")}>
+              {preheader && <Text color="primary">{preheader}</Text>}
+              {header}
+            </H2>
+          )}
+          {description && (
+            <Text color="secondary" className={withBaseName("description")}>
+              {description}
+            </Text>
+          )}
+        </div>
+        {actions && (
+          <div className={withBaseName("actionsContainer")}>{actions}</div>
+        )}
+      </div>
+    );
+  },
+);
--- packages/core/src/drawer/index.ts
+++ packages/core/src/drawer/index.ts
@@ -1,2 +1,5 @@
 export * from "./Drawer";
 export * from "./DrawerCloseButton";
+export * from "./DrawerContent";
+export * from "./DrawerContext";
+export * from "./DrawerHeader";
