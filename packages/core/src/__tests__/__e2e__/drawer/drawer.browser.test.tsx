30c7724fd676555385607ead6cf1b0861e7467c1
Chris Sapa <47253291+kriskoder@users.noreply.github.com>
2026-09-01 12:02:22 +0100

    feat(core/Drawer) - merge  header and actions stories into 1

--- packages/core/src/__tests__/__e2e__/drawer/drawer.browser.test.tsx
+++ packages/core/src/__tests__/__e2e__/drawer/drawer.browser.test.tsx
@@ -15,12 +15,11 @@ import { renderWithSalt } from "~browser-test-utils/render";
 import * as drawerStories from "~stories/drawer/drawer.stories";
 
 const {
-  Actions,
   Default,
   OptionalCloseAction,
   InitialFocusIndex,
   InitialFocusRef,
-  Header,
+  HeaderAndActions,
 } = composeStories(drawerStories);
 
 const headingName = "Payments Check deposit #1278";
@@ -252,7 +251,7 @@ describe("GIVEN a Drawer with sections nested in a fragment", () => {
 
 describe("GIVEN a Drawer with a DrawerHeader", () => {
   it("names and describes the drawer from the header", async () => {
-    await renderWithSalt(<Header />);
+    await renderWithSalt(<HeaderAndActions />);
     await page.getByRole("button", { name: "Open Drawer" }).click();
 
     const drawer = page.getByRole("dialog");
@@ -266,7 +265,7 @@ describe("GIVEN a Drawer with a DrawerHeader", () => {
   });
 
   it("closes from a close button placed in the header actions", async () => {
-    await renderWithSalt(<Header />);
+    await renderWithSalt(<HeaderAndActions />);
     await page.getByRole("button", { name: "Open Drawer" }).click();
 
     const closeButton = page.getByRole("button", { name: "Close Drawer" });
@@ -400,7 +399,7 @@ describe("GIVEN a Drawer with DrawerActions", () => {
   });
 
   it("supports actions configured to close the drawer", async () => {
-    await renderWithSalt(<Actions />);
+    await renderWithSalt(<HeaderAndActions />);
     const openButton = page.getByRole("button", { name: "Open Drawer" });
 
     await openButton.click();
