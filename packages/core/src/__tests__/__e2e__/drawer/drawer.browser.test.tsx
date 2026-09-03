4b73be100cc5d6ede96b05a5363fa41418de6b3c
Chris Sapa <47253291+kriskoder@users.noreply.github.com>
2026-09-03 13:40:17 +0100

    test(core/DrawerHeader) - add wait until close button has focus

--- packages/core/src/__tests__/__e2e__/drawer/drawer.browser.test.tsx
+++ packages/core/src/__tests__/__e2e__/drawer/drawer.browser.test.tsx
@@ -199,6 +199,9 @@ describe("GIVEN a Drawer", () => {
 
     const content = page.getByRole("region", { name: headingName });
     await expect.element(content).toBeVisible();
+    await expect
+      .element(page.getByRole("button", { name: "Close Drawer" }))
+      .toHaveFocus();
     await userEvent.tab();
     await expect.element(content).toHaveFocus();
   });
