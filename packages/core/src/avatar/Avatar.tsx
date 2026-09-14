89310124fa987d346f43e611e6d159c9e63aa2de
navneet kaur <navneet.x1.kaur@jpmorgan.com>
2026-09-14 14:52:41 +0100

    test

--- packages/core/src/avatar/Avatar.tsx
+++ packages/core/src/avatar/Avatar.tsx
@@ -75,6 +75,7 @@ export interface AvatarProps extends HTMLAttributes<HTMLDivElement> {
    * Render prop to enable customization of the avatar root element.
    */
   render?: RenderPropsType["render"];
+  test? : string;
 }
 
 const withBaseName = makePrefixer("saltAvatar");
